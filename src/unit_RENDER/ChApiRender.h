51d1b3aa1d769c2ce96f4f32ea5721a1e568b9ac
Hammad Mazhar <hammad.maz@gmail.com>
2011-11-24 17:05:28 +0000

    Fixed bug preventing matlab demos from compiling updated demo_gpubasic.cpp

---
+++ src/unit_RENDER/ChApiRender.h
@@ -0,0 +1,40 @@
+#ifndef CHAPIRENDER_H
+#define CHAPIRENDER_H
+
+//////////////////////////////////////////////////
+//
+//   ChApiCE.h
+//
+//   Base header for all headers that have symbols
+//   that can be exported.
+//
+//   HEADER file for CHRONO,
+//	 Multibody dynamics engine
+//
+// ------------------------------------------------
+// 	 Copyright:Alessandro Tasora / DeltaKnowledge
+//             www.deltaknowledge.com
+// ------------------------------------------------
+///////////////////////////////////////////////////
+
+#include "core/ChPlatform.h"
+
+// Chrono::Engine version
+//
+// This is an integer, as 0xaabbccdd where
+// for example version 1.2.0 is 0x00010200
+
+#define CH_VERSION_UNIT_RENDER 0x00000100
+
+// When compiling this library, remember to define CH_API_COMPILE
+// (so that the symbols with 'ChApi' in front of them will be
+// marked as exported). Otherwise, just do not define it if you
+// link the library to your code, and the symbols will be imported.
+
+#if defined(CH_API_COMPILE_UNIT_RENDER)
+	#define ChApi ChApiEXPORT
+#else
+	#define ChApi ChApiINPORT
+#endif
+
+#endif  // END of header
