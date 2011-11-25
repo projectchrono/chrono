c7504daa68e8281c1d64fb86a24ecb033d99c417
Hammad Mazhar <hammad.maz@gmail.com>
2011-11-25 21:30:32 +0000

    Changed ChSystem to add extra parameter to prevent Collision system , container and solver from being initialized

--- src/testing/gpu/test_gpu_CD.cpp
+++ src/testing/gpu/test_gpu_CD.cpp
@@ -1,7 +1,7 @@
 #include "common.h"
 void System::DoTimeStep() {
 	if (mNumCurrentObjects < mNumObjects && mFrameNumber % 5000 == 0) {
-		float x = 30, y = 30, z = 300;
+		float x = 30, y = 30, z = 30;
 		float posX = 0, posY = -14, posZ = 0;
 		float radius = .1, mass = 4, mu = .5, rest = 0;
 		ShapeType type = SPHERE;
