958a1299c53a40a3aa24c83299cbdc02d7183e67
armanpazouki <apazouki@gmail.com>
2012-05-29 11:25:55 -0500

    bringing the length and dist out of the kernel

--- src/chrono_fsi/SDKCollisionSystem.cu
+++ src/chrono_fsi/SDKCollisionSystem.cu
@@ -29,34 +29,33 @@ __device__ uint calcGridHash(int3 gridPos) {
 //--------------------------------------------------------------------------------------------------------------------------------
 //computes dV/dt and dRho/dt, i.e. force terms. First
 __device__ inline float4 DifVelocityRho(
-		const float4 & posRadA,
-		const float4 & posRadB,
+		const float3 & dist3,
+		const float & d,
+		const float & rSPH,
 		const float4 & velMasA,
 		const float3 & vel_XSPH_A,
 		const float4 & velMasB,
 		const float3 & vel_XSPH_B,
 		const float4 & rhoPresMuA,
 		const float4 & rhoPresMuB) {
-	float3 dist3 = Distance(posRadA, posRadB);
 	//
-	float d2 =dot(dist3,dist3);//length(dist3); //sqrt( dot(dist3, dist3) );
 	//if (d > posRadA.w + posRadB.w) { return F4(0); } //else {printf("here is contact\n posRadA posRadB dist\n (%f, %f, %f, %f) (%f, %f, %f, %f) %f\n\n",posRadA.x, posRadA.y, posRadA.z, posRadA.w, posRadB.x, posRadB.y, posRadB.z, posRadB.w, d);}		//?! added in the SDK version
-	float3 gradW = GradW(dist3, posRadA.w);
+	float3 gradW = GradW(dist3, rSPH);
 	float vAB_Dot_rAB = dot(F3(velMasA - velMasB), dist3);
 	float epsilonMutualDistance = .01f;
 
 //	//*** Artificial viscosity type 1.1
 //	float alpha = .001;
 //	float c_ab = 10 * v_Max; //Ma = .1;//sqrt(7.0f * 10000 / ((rhoPresMuA.x + rhoPresMuB.x) / 2.0f));
-//	float h = .5f * (posRadA.w + posRadB.w);
+//	//float h = .5f * (posRadA.w + posRadB.w);
 //	float rho = .5f * (rhoPresMuA.x + rhoPresMuB.x);
-//	float nu = alpha * h * c_ab / rho;
+//	float nu = alpha * rSPH * c_ab / rho;
 
 //	//*** Artificial viscosity type 1.2
 //	float nu = 22.8f * mu0 / 2.0f / (rhoPresMuA.x * rhoPresMuB.x);
 //	float3 derivV = -velMasB.w * (
 //		rhoPresMuA.y / (rhoPresMuA.x * rhoPresMuA.x) + rhoPresMuB.y / (rhoPresMuB.x * rhoPresMuB.x)
-//		- nu * vAB_Dot_rAB / ( d * d + epsilonMutualDistance * posRadA.w * posRadA.w )
+//		- nu * vAB_Dot_rAB / ( d * d + epsilonMutualDistance * rSPH * rSPH )
 //		) * gradW;
 //	return F4(derivV,
 //		rhoPresMuA.x * velMasB.w / rhoPresMuB.x * dot(vel_XSPH_A - vel_XSPH_B, gradW));
@@ -65,48 +64,47 @@ __device__ inline float4 DifVelocityRho(
 	float rAB_Dot_GradW = dot(dist3, gradW);
 	float invrhoPresMuBx=1/ rhoPresMuB.x;
 	float3 derivV = -velMasB.w * (rhoPresMuA.y / (rhoPresMuA.x * rhoPresMuA.x) + rhoPresMuB.y *(invrhoPresMuBx * invrhoPresMuBx)) * gradW
-			+ velMasB.w * 8.0f * mu0 * rAB_Dot_GradW * pow(rhoPresMuA.x + rhoPresMuB.x, -2) / (d2 + epsilonMutualDistance * posRadA.w * posRadA.w)
+			+ velMasB.w * 8.0f * mu0 * rAB_Dot_GradW * pow(rhoPresMuA.x + rhoPresMuB.x, -2) / (d * d + epsilonMutualDistance * rSPH * rSPH)
 					* F3(velMasA - velMasB);
 	return F4(derivV, rhoPresMuA.x * velMasB.w * invrhoPresMuBx * dot(vel_XSPH_A - vel_XSPH_B, gradW));
 
 //	//*** Artificial viscosity type 1.3
 //	float rAB_Dot_GradW = dot(dist3, gradW);
 //	float3 derivV = -velMasB.w * (rhoPresMuA.y / (rhoPresMuA.x * rhoPresMuA.x) + rhoPresMuB.y / (rhoPresMuB.x * rhoPresMuB.x)) * gradW
-//		+ velMasB.w / (rhoPresMuA.x * rhoPresMuB.x) * 2.0f * mu0 * rAB_Dot_GradW / ( d * d + epsilonMutualDistance * posRadA.w * posRadA.w ) * F3(velMasA - velMasB);
+//		+ velMasB.w / (rhoPresMuA.x * rhoPresMuB.x) * 2.0f * mu0 * rAB_Dot_GradW / ( d * d + epsilonMutualDistance * rSPH * rSPH ) * F3(velMasA - velMasB);
 //	return F4(derivV,
 //		rhoPresMuA.x * velMasB.w / rhoPresMuB.x * dot(vel_XSPH_A - vel_XSPH_B, gradW));
 }
 //--------------------------------------------------------------------------------------------------------------------------------
 //computes dV/dt and dRho/dt, i.e. force terms. First
 __device__ inline float4 DifVelocityRho2(
-		const float4 & posRadA,
-		const float4 & posRadB,
+		const float3 & dist3,
+		const float & d,
+		const float & rSPH,
 		const float4 & velMasA,
 		const float3 & vel_XSPH_A,
 		const float4 & velMasB,
 		const float3 & vel_XSPH_B,
 		const float4 & rhoPresMuA,
 		const float4 & rhoPresMuB) {
-	float3 dist3 = Distance(posRadA, posRadB);
 	//
-	float d2 =dot(dist3,dist3);// length(dist3); //sqrt( dot(dist3, dist3) );
 	//if (d > posRadA.w + posRadB.w) { return F4(0); } //else {printf("here is contact\n posRadA posRadB dist\n (%f, %f, %f, %f) (%f, %f, %f, %f) %f\n\n",posRadA.x, posRadA.y, posRadA.z, posRadA.w, posRadB.x, posRadB.y, posRadB.z, posRadB.w, d);}		//?! added in the SDK version
-	float3 gradW = GradW(dist3, posRadA.w);
+	float3 gradW = GradW(dist3, rSPH);
 	float vAB_Dot_rAB = dot(F3(velMasA - velMasB), dist3);
 	float epsilonMutualDistance = .01f;
 
 //	//*** Artificial viscosity type 1.1
 //	float alpha = .001;
 //	float c_ab = 10 * v_Max; //Ma = .1;//sqrt(7.0f * 10000 / ((rhoPresMuA.x + rhoPresMuB.x) / 2.0f));
-//	float h = .5f * (posRadA.w + posRadB.w);
+//	//float h = .5f * (posRadA.w + posRadB.w);
 //	float rho = .5f * (rhoPresMuA.x + rhoPresMuB.x);
-//	float nu = alpha * h * c_ab / rho;
+//	float nu = alpha * rSPH * c_ab / rho;
 
 //	//*** Artificial viscosity type 1.2
 //	float nu = 100.0f * mu0 / 2.0f / (rhoPresMuA.x * rhoPresMuB.x);
 //	float3 derivV = -velMasB.w * (
 //		rhoPresMuA.y / (rhoPresMuA.x * rhoPresMuA.x) + rhoPresMuB.y / (rhoPresMuB.x * rhoPresMuB.x)
-//		- nu * vAB_Dot_rAB / ( d * d + epsilonMutualDistance * posRadA.w * posRadA.w )
+//		- nu * vAB_Dot_rAB / ( d * d + epsilonMutualDistance * rSPH * rSPH )
 //		) * gradW;
 //	return F4(derivV,
 //		rhoPresMuA.x * velMasB.w / rhoPresMuB.x * dot(vel_XSPH_A - vel_XSPH_B, gradW));
@@ -116,30 +114,33 @@ __device__ inline float4 DifVelocityRho2(
 	float invrhoPresMuBx=1/ rhoPresMuB.x;
 	//printf("1e20 * rAB_Dot_GradW %f, 1e6 * vAB_Dot_rAB %f, length(F3(velMasA - velMasB)) %f, vA %f, vB %f\n", 1e20 * rAB_Dot_GradW, 1e6 * vAB_Dot_rAB, length(F3(velMasA - velMasB)),  length(F3(velMasA )),  length(F3(velMasB)));
 	float3 derivV = -velMasB.w * (rhoPresMuA.y / (rhoPresMuA.x * rhoPresMuA.x) + rhoPresMuB.y * (invrhoPresMuBx * invrhoPresMuBx)) * gradW
-			+ velMasB.w * 40.0f * mu0 * rAB_Dot_GradW *pow(rhoPresMuA.x + rhoPresMuB.x, -2) / (d2 + epsilonMutualDistance * posRadA.w * posRadA.w)
+			+ velMasB.w * 40.0f * mu0 * rAB_Dot_GradW *pow(rhoPresMuA.x + rhoPresMuB.x, -2) / (d * d + epsilonMutualDistance * rSPH * rSPH)
 					* F3(velMasA - velMasB);
 	return F4(derivV, rhoPresMuA.x * velMasB.w *invrhoPresMuBx * dot(vel_XSPH_A - vel_XSPH_B, gradW));
 
 //	//*** Artificial viscosity type 1.3
 //	float rAB_Dot_GradW = dot(dist3, gradW);
 //	float3 derivV = -velMasB.w * (rhoPresMuA.y / (rhoPresMuA.x * rhoPresMuA.x) + rhoPresMuB.y / (rhoPresMuB.x * rhoPresMuB.x)) * gradW
-//		+ velMasB.w / (rhoPresMuA.x * rhoPresMuB.x) * 2.0f * mu0 * rAB_Dot_GradW / ( d * d + epsilonMutualDistance * posRadA.w * posRadA.w ) * F3(velMasA - velMasB);
+//		+ velMasB.w / (rhoPresMuA.x * rhoPresMuB.x) * 2.0f * mu0 * rAB_Dot_GradW / ( d * d + epsilonMutualDistance * rSPH * rSPH ) * F3(velMasA - velMasB);
 //	return F4(derivV,
 //		rhoPresMuA.x * velMasB.w / rhoPresMuB.x * dot(vel_XSPH_A - vel_XSPH_B, gradW));
 }
 //--------------------------------------------------------------------------------------------------------------------------------
-__device__ inline float3 DifVelocity_SSI_DEM(const float4 & posRadA, const float4 & posRadB, const float4 & velMasA, const float4 & velMasB) {
+__device__ inline float3 DifVelocity_SSI_DEM(
+				const float3 & dist3,
+				const float & d,
+				const float & rSPH,
+				const float4 & velMasA,
+				const float4 & velMasB) {
 //printf("** DifVelocity_SSI_DEM\n");
 	float kS = 6;//3; //50; //1000.0; //392400.0;	//spring. 50 worked almost fine. I am using 30 to be sure!
 	float kD = 40;//20.0; //420.0;				//damper
-	float3 dist3 = Distance(posRadA, posRadB);
-	float ldist3=length(dist3);
-	float l = posRadA.w + posRadB.w - ldist3;
+	float l = 2 * rSPH - d;
 
 	if (l < 0) {
 		return F3(0);
 	}
-	float3 n = dist3 / ldist3; //unit vector B to A
+	float3 n = dist3 / d; //unit vector B to A
 	float m_eff = (velMasA.w * velMasB.w) / (velMasA.w + velMasB.w);
 	float3 force = (/*pow(sizeScale, 3) * */kS * l - kD * m_eff * dot(F3(velMasA - velMasB), n)) * n; //relative velocity at contact is simply assumed as the relative vel of the centers. If you are updating the rotation, this should be modified.
 	return force / velMasA.w; //return dV/dT same as SPH
@@ -175,7 +176,7 @@ float3 deltaVShare(
 				float4 rhoPresMuB = FETCH(sortedRhoPreMu, j);
 				float3 dist3 = Distance(posRadA, posRadB);
 				float d = length(dist3);
-				if (d > 2 * HSML) continue;
+				if (d > RESOLUTION_LENGTH * HSML) continue;
 				if (rhoPresMuA.w <0) { //# A_fluid				** -1:			i.e. negative, i.e. fluid particle
 					if (rhoPresMuB.w < 0) { //## A_fluid : B_fluid, accoring to colagrossi (2003), the other phase (i.e. rigid) should not be considered)
 						deltaV += velMasB.w * F3(velMasB - velMasA) * W3(d, posRadA.w) / (.5 * (rhoPresMuA.x + rhoPresMuB.x));
@@ -221,23 +222,25 @@ float4 collideCell(
 				float4 velMasB = FETCH(sortedVelMas, j);
 				float4 rhoPresMuB = FETCH(sortedRhoPreMu, j);
 				float3 vel_XSPH_B = FETCH(vel_XSPH_D, gridParticleIndex[j]);
-				float d = length(F3(posRadA - posRadB));
-				if (d > 2 * HSML) continue;
+				float3 dist3 = Distance(posRadA, posRadB);
+				float d = length(dist3);
+				if (d > RESOLUTION_LENGTH * HSML) continue;
+				float rSPH = posRadA.w;
 
 				if (rhoPresMuA.w < 0) { //# A_fluid				** -1:			i.e. negative, i.e. fluid particle
 					if (rhoPresMuB.w < 0) { //## A_fluid : B_fluid
 						//printf("good morning\n");
-						derivVelRho = DifVelocityRho(posRadA, posRadB, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
+						derivVelRho = DifVelocityRho(dist3, d, rSPH, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
 						derivV += F3(derivVelRho);
 						derivRho += derivVelRho.w;
 					} else if (rhoPresMuB.w ==0) { //## A_fluid : B_boundary
-						derivVelRho = DifVelocityRho2(posRadA, posRadB, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
+						derivVelRho = DifVelocityRho2(dist3, d, rSPH, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
 						derivV += F3(derivVelRho);
 						derivRho += derivVelRho.w;
 					} else { //## A_fluid : B_solid, sub rhoPresMuB with rhoPresMuA (except pressure, i.e. rhoPresMuB.y)
 						//derivVelRho = DifVelocityRho(posRadA, posRadB, velMasA, velMasB, rhoPresMuA, F4(rhoPresMuA.x, rhoPresMuB.y, rhoPresMuA.z, rhoPresMuA.w));
 
-						derivVelRho = DifVelocityRho(posRadA, posRadB, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
+						derivVelRho = DifVelocityRho(dist3, d, rSPH, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
 						derivV += F3(derivVelRho);
 						derivRho += derivVelRho.w;
 
@@ -263,7 +266,7 @@ float4 collideCell(
 					//}
 				} else if (rhoPresMuA.w ==0) { //# A_boundary								** 0:				i.e. 0, i.e. boundary
 					if (rhoPresMuB.w < 0) { //## A_boundary : B_fluid
-						derivVelRho = DifVelocityRho2(posRadA, posRadB, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
+						derivVelRho = DifVelocityRho2(dist3, d, rSPH, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
 						//derivV += F3(derivVelRho);
 						derivRho += derivVelRho.w;
 					}
@@ -272,7 +275,7 @@ float4 collideCell(
 					if (rhoPresMuB.w < 0) { //## A_Solid : B_fluid, sub rhoPresMuA with rhoPresMuB (except pressure, i.e. rhoPresMuA.y)
 						//derivVelRho = DifVelocityRho(posRadA, posRadB, velMasA, velMasB, F4(rhoPresMuB.x, rhoPresMuA.y, rhoPresMuB.z, rhoPresMuB.w), rhoPresMuB);
 
-						derivVelRho = DifVelocityRho(posRadA, posRadB, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
+						derivVelRho = DifVelocityRho(dist3, d, rSPH, velMasA, vel_XSPH_A, velMasB, vel_XSPH_B, rhoPresMuA, rhoPresMuB);
 						derivV += F3(derivVelRho);
 						derivRho += derivVelRho.w;
 
@@ -288,7 +291,7 @@ float4 collideCell(
 						//// TODO: rigid body contact will be included here
 						////			also note that by far, derivV is not derivV (i,e it is -derivV*rhoA, and derivRho is derivRho/rhoA. Hey, I guess i have modified it
 						if (fabs(rhoPresMuA.w - rhoPresMuB.w) > 0) { //i.e collision with the other object, or boundary
-							derivV += DifVelocity_SSI_DEM(posRadA, posRadB, velMasA, velMasB);
+							derivV += DifVelocity_SSI_DEM(dist3, d, rSPH, velMasA, velMasB);
 							//derivRho += 0;
 						}
 					}
