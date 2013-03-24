1c18d7532956f2424a80f4a3243bdb465339390d
Hammad Mazhar <hammad.maz@gmail.com>
2013-03-24 03:03:09 +0000

    Cleaning up code, code is not compiling but should be soon

--- src/unit_PARALLEL/solver/ChSolverCG.h
+++ src/unit_PARALLEL/solver/ChSolverCG.h
@@ -15,27 +15,8 @@ namespace chrono {
 			uint SolveCG(custom_vector<real> &x, const custom_vector<real> &b, const uint max_iter);
 			uint SolveGD(custom_vector<real> &x, const custom_vector<real> &b, const uint max_iter);
 			uint SolveSD(custom_vector<real> &x, const custom_vector<real> &b, const uint max_iter);
-			void host_shurA(real3 *JXYZA, real3 *JXYZB, real3 *JUVWA, real3 *JUVWB, real *gamma, int2 *ids, real *inv_mass, real3 *inv_inertia, bool *active, real3 *QXYZ, real3 *QUVW);
-			void host_shurB(real3 *JXYZA, real3 *JXYZB, real3 *JUVWA, real3 *JUVWB, real *gamma, int2 *ids, real *inv_mass, real3 *inv_inertia, bool *active, real3 *QXYZ, real3 *QUVW, real *AX);
-			void host_RHS(
-					int2 *ids,
-					real3 *JXYZA,
-					real3 *JXYZB,
-					real3 *JUVWA,
-					real3 *JUVWB,
-					real3 *vel,
-					real3 *omega,
-					real *correction,
-					real step_size,
-					real *rhs);
-			void host_Project(real *gam, real *fric, int2 *ids);
-			custom_vector<real> ShurProduct( custom_vector<real> &x_t);
-			uint number_of_constraints, number_of_contacts;
 
-			custom_vector<int2> temp_bids;
-			custom_vector<real> AX, rhs, correction;
-			real step_size;
-			gpu_container *gpu_data;
+
 		};}
 
 #endif
--- src/unit_PARALLEL/solver/ChSolverGPU.h
+++ src/unit_PARALLEL/solver/ChSolverGPU.h
@@ -6,21 +6,41 @@
 #include "ChOptimization.h"
 
 namespace chrono {
-    class ChApiGPU ChSolverGPU {
-        public:
-            ChSolverGPU(){}
+	class ChApiGPU ChSolverGPU {
+		public:
+			ChSolverGPU() {
+			}
+			void host_shurA(real3 *JXYZA, real3 *JXYZB, real3 *JUVWA, real3 *JUVWB, real *gamma, int2 *ids, real *inv_mass, real3 *inv_inertia, bool *active, real3 *QXYZ, real3 *QUVW);
+			void host_shurB(real3 *JXYZA, real3 *JXYZB, real3 *JUVWA, real3 *JUVWB, real *gamma, int2 *ids, real *inv_mass, real3 *inv_inertia, bool *active, real3 *QXYZ, real3 *QUVW, real *AX);
+			void host_RHS(int2 *ids, real3 *JXYZA, real3 *JXYZB, real3 *JUVWA, real3 *JUVWB, real3 *vel, real3 *omega, real *correction, real step_size, real *rhs);
+			void host_Project(real *gam, real *fric, int2 *ids);custom_vector<real> ShurProduct( custom_vector<real> &x_t);
+			void Solve() {
+			}
+			protected:
 
-            void Solve(){}
-        protected:
+			real step_size;
+			real lcp_omega_bilateral;
+			real lcp_omega_contact;
+			real lcp_contact_factor;
+			real compliance;
+			real complianceT;
+			real alpha;
+			uint number_of_bilaterals;
+			uint number_of_contacts;
+			uint number_of_objects;
+			uint number_of_updates;
+			uint number_of_constraints;
 
-            int current_iteration;
-            int max_iteration;
-            real residual;
-            real tolerance;
-            real epsilon;
+			int current_iteration;
+			int max_iteration;
+			real residual, epsilon, tolerance;
 
-    };
+			custom_vector<int2> temp_bids;
+			custom_vector<real> AX, rhs, correction;
+			real step_size;
+			gpu_container *gpu_data;
+		};
 
-}
+	}
 
 #endif
