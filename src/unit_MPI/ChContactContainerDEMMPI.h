3442ef95340e9edd944424c9daf5aeb55fd8843b
Andrew Seidl <git@aas.io>
2013-10-31 16:01:03 -0500

    Adding license and copyright header

--- src/unit_MPI/ChContactContainerDEMMPI.h
+++ src/unit_MPI/ChContactContainerDEMMPI.h
@@ -1,3 +1,14 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2012 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
 #ifndef CHCONTACTCONTAINERDEMMPI_H
 #define CHCONTACTCONTAINERDEMMPI_H
 
@@ -12,7 +23,6 @@
 //	 Multibody dynamics engine
 //
 // ------------------------------------------------
-// 	 Copyright:Alessandro Tasora / DeltaKnowledge
 //             www.deltaknowledge.com
 // ------------------------------------------------
 ///////////////////////////////////////////////////
