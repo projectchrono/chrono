90b2118ae13e7f43d299c12e8871b1e83cb0c889
Radu Serban <serban@engr.wisc.edu>
2013-11-16 10:48:08 -0600

    Update ChCollisionModelParallel to reflect changes in its base class.
    Modified signature of all virtual Add***() methods used to specify contact
    geometry so that they match the declarations in the ChCollisionModel base
    class (use const refs to specify position and orientation).

--- src/unit_PARALLEL/src/collision/ChCCollisionModelParallel.cpp
+++ src/unit_PARALLEL/src/collision/ChCCollisionModelParallel.cpp
@@ -46,26 +46,18 @@ int ChCollisionModelParallel::BuildModel() {
 
 	return 1;
 }
-bool ChCollisionModelParallel::AddSphere(double radius, ChVector<> *posv) {
-	ChVector<> *pos;
-
-	if (posv != 0) {
-		pos = posv;
-	} else {
-		pos = new ChVector<>(0, 0, 0);
-	}
-
+bool ChCollisionModelParallel::AddSphere(double radius, const ChVector<> &pos) {
 	double mass = this->GetBody()->GetMass();
 
 	real3 local_inertia = R3(2 / 5.0 * mass * radius * radius, 2 / 5.0 * mass * radius * radius, 2 / 5.0 * mass * radius * radius);
-	inertia.x += local_inertia.x+mass*(pos->Length2()-pos->x*pos->x);
-	inertia.y += local_inertia.y+mass*(pos->Length2()-pos->y*pos->y);
-	inertia.z += local_inertia.z+mass*(pos->Length2()-pos->z*pos->z);
+	inertia.x += local_inertia.x+mass*(pos.Length2()-pos.x*pos.x);
+	inertia.y += local_inertia.y+mass*(pos.Length2()-pos.y*pos.y);
+	inertia.z += local_inertia.z+mass*(pos.Length2()-pos.z*pos.z);
 
 	model_type = SPHERE;
 	nObjects++;
 	bData tData;
-	tData.A = R3(pos->x, pos->y, pos->z);
+	tData.A = R3(pos.x, pos.y, pos.z);
 	tData.B = R3(radius, 0, 0);
 	tData.C = R3(0, 0, 0);
 	tData.R = R4(1, 0, 0, 0);
@@ -73,160 +65,78 @@ bool ChCollisionModelParallel::AddSphere(double radius, ChVector<> *posv) {
 	mData.push_back(tData);
 	return true;
 }
-bool ChCollisionModelParallel::AddEllipsoid(double rx, double ry, double rz, ChVector<> *posv, ChMatrix33<> *rotv) {
-	ChVector<> *pos;
-
-	if (posv != 0) {
-		pos = posv;
-	} else {
-		pos = new ChVector<>(0, 0, 0);
-	}
-
-	ChMatrix33<> *rot;
-
-	if (rotv != 0) {
-		rot = rotv;
-	} else {
-		rot = new ChMatrix33<>();
-	}
-
+bool ChCollisionModelParallel::AddEllipsoid(double rx, double ry, double rz, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	double mass = this->GetBody()->GetMass();
 
-
 	real3 local_inertia =R3(1 / 5.0 * mass * (ry * ry + rz * rz), 1 / 5.0 * mass * (rx * rx + rz * rz), 1 / 5.0 * mass * (rx * rx + ry * ry));
-	inertia.x += local_inertia.x+mass*(pos->Length2()-pos->x*pos->x);
-	inertia.y += local_inertia.y+mass*(pos->Length2()-pos->y*pos->y);
-	inertia.z += local_inertia.z+mass*(pos->Length2()-pos->z*pos->z);
+	inertia.x += local_inertia.x+mass*(pos.Length2()-pos.x*pos.x);
+	inertia.y += local_inertia.y+mass*(pos.Length2()-pos.y*pos.y);
+	inertia.z += local_inertia.z+mass*(pos.Length2()-pos.z*pos.z);
 
 
 	model_type = ELLIPSOID;
 	nObjects++;
 	bData tData;
-	tData.A = R3(pos->x, pos->y, pos->z);
+	tData.A = R3(pos.x, pos.y, pos.z);
 	tData.B = R3(rx, ry, rz);
 	tData.C = R3(0, 0, 0);
-	tData.R = R4(rot->Get_A_quaternion().e0, rot->Get_A_quaternion().e1, rot->Get_A_quaternion().e2, rot->Get_A_quaternion().e3);
+	tData.R = R4(rot.Get_A_quaternion().e0, rot.Get_A_quaternion().e1, rot.Get_A_quaternion().e2, rot.Get_A_quaternion().e3);
 	tData.type = ELLIPSOID;
 	mData.push_back(tData);
 	return true;
 }
-bool ChCollisionModelParallel::AddBox(double rx, double ry, double rz, ChVector<> *posv, ChMatrix33<> *rotv) {
-	ChVector<> *pos;
-
-	if (posv != 0) {
-		pos = posv;
-	} else {
-		pos = new ChVector<>(0, 0, 0);
-	}
-
-	ChMatrix33<> *rot;
-
-	if (rotv != 0) {
-		rot = rotv;
-	} else {
-		rot = new ChMatrix33<>();
-	}
-
+bool ChCollisionModelParallel::AddBox(double rx, double ry, double rz, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	double mass = this->GetBody()->GetMass();
 
 	real3 local_inertia =R3(1 / 12.0 * mass * (ry * ry + rz * rz), 1 / 12.0 * mass * (rx * rx + rz * rz), 1 / 12.0 * mass * (rx * rx + ry * ry));
-	inertia.x += local_inertia.x+mass*(pos->Length2()-pos->x*pos->x);
-	inertia.y += local_inertia.y+mass*(pos->Length2()-pos->y*pos->y);
-	inertia.z += local_inertia.z+mass*(pos->Length2()-pos->z*pos->z);
+	inertia.x += local_inertia.x+mass*(pos.Length2()-pos.x*pos.x);
+	inertia.y += local_inertia.y+mass*(pos.Length2()-pos.y*pos.y);
+	inertia.z += local_inertia.z+mass*(pos.Length2()-pos.z*pos.z);
 
 	model_type = BOX;
 	nObjects++;
 	bData tData;
-	tData.A = R3(pos->x, pos->y, pos->z);
+	tData.A = R3(pos.x, pos.y, pos.z);
 	tData.B = R3(rx, ry, rz);
 	tData.C = R3(0, 0, 0);
-	tData.R = R4(rot->Get_A_quaternion().e0, rot->Get_A_quaternion().e1, rot->Get_A_quaternion().e2, rot->Get_A_quaternion().e3);
+	tData.R = R4(rot.Get_A_quaternion().e0, rot.Get_A_quaternion().e1, rot.Get_A_quaternion().e2, rot.Get_A_quaternion().e3);
 	tData.type = BOX;
 	mData.push_back(tData);
 	return true;
 }
-bool ChCollisionModelParallel::AddTriangle(ChVector<> A, ChVector<> B, ChVector<> C, ChVector<> *posv, ChMatrix33<> *rotv) {
-	ChVector<> *pos;
-
-	if (posv != 0) {
-		pos = posv;
-	} else {
-		pos = new ChVector<>(0, 0, 0);
-	}
-
-	ChMatrix33<> *rot;
-
-	if (rotv != 0) {
-		rot = rotv;
-	} else {
-		rot = new ChMatrix33<>();
-	}
-
+bool ChCollisionModelParallel::AddTriangle(ChVector<> A, ChVector<> B, ChVector<> C, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	double mass = this->GetBody()->GetMass();
 	model_type = TRIANGLEMESH;
 	nObjects++;
 	bData tData;
-	tData.A = R3(A.x + pos->x, A.y + pos->y, A.z + pos->z);
-	tData.B = R3(B.x + pos->x, B.y + pos->y, B.z + pos->z);
-	tData.C = R3(C.x + pos->x, C.y + pos->y, C.z + pos->z);
-	tData.R = R4(rot->Get_A_quaternion().e0, rot->Get_A_quaternion().e1, rot->Get_A_quaternion().e2, rot->Get_A_quaternion().e3);
+	tData.A = R3(A.x + pos.x, A.y + pos.y, A.z + pos.z);
+	tData.B = R3(B.x + pos.x, B.y + pos.y, B.z + pos.z);
+	tData.C = R3(C.x + pos.x, C.y + pos.y, C.z + pos.z);
+	tData.R = R4(rot.Get_A_quaternion().e0, rot.Get_A_quaternion().e1, rot.Get_A_quaternion().e2, rot.Get_A_quaternion().e3);
 	tData.type = TRIANGLEMESH;
 	mData.push_back(tData);
 	return true;
 }
-
-bool ChCollisionModelParallel::AddCylinder(double rx, double ry, double rz, ChVector<> *posv, ChMatrix33<> *rotv) {
-	ChVector<> *pos;
-
-	if (posv != 0) {
-		pos = posv;
-	} else {
-		pos = new ChVector<>(0, 0, 0);
-	}
-
-	ChMatrix33<> *rot;
-
-	if (rotv != 0) {
-		rot = rotv;
-	} else {
-		rot = new ChMatrix33<>();
-	}
-
+bool ChCollisionModelParallel::AddCylinder(double rx, double ry, double rz, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	double mass = this->GetBody()->GetMass();
 
 	real3 local_inertia =R3(1 / 12.0 * mass * (3 * rx * rx + ry * ry), 1 / 2.0 * mass * (rx * rx), 1 / 12.0 * mass * (3 * rx * rx + ry * ry));
-	inertia.x += local_inertia.x+mass*(pos->Length2()-pos->x*pos->x);
-	inertia.y += local_inertia.y+mass*(pos->Length2()-pos->y*pos->y);
-	inertia.z += local_inertia.z+mass*(pos->Length2()-pos->z*pos->z);
+	inertia.x += local_inertia.x+mass*(pos.Length2()-pos.x*pos.x);
+	inertia.y += local_inertia.y+mass*(pos.Length2()-pos.y*pos.y);
+	inertia.z += local_inertia.z+mass*(pos.Length2()-pos.z*pos.z);
 	model_type = CYLINDER;
 	nObjects++;
 	bData tData;
-	tData.A = R3(pos->x, pos->y, pos->z);
+	tData.A = R3(pos.x, pos.y, pos.z);
 	tData.B = R3(rx, ry, rz);
 	tData.C = R3(0, 0, 0);
-	tData.R = R4(rot->Get_A_quaternion().e0, rot->Get_A_quaternion().e1, rot->Get_A_quaternion().e2, rot->Get_A_quaternion().e3);
+	tData.R = R4(rot.Get_A_quaternion().e0, rot.Get_A_quaternion().e1, rot.Get_A_quaternion().e2, rot.Get_A_quaternion().e3);
 	tData.type = CYLINDER;
 	mData.push_back(tData);
 	return true;
 }
 
-bool ChCollisionModelParallel::AddCone(double rx, double ry, double rz, ChVector<> *posv, ChMatrix33<> *rotv) {
-	ChVector<> *pos;
-
-	if (posv != 0) {
-		pos = posv;
-	} else {
-		pos = new ChVector<>(0, 0, 0);
-	}
-
-	ChMatrix33<> *rot;
-
-	if (rotv != 0) {
-		rot = rotv;
-	} else {
-		rot = new ChMatrix33<>();
-	}
-
+bool ChCollisionModelParallel::AddCone(double rx, double ry, double rz, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	double mass = this->GetBody()->GetMass();
 	real radius = rx;
 	real height = ry;
@@ -235,27 +145,27 @@ bool ChCollisionModelParallel::AddCone(double rx, double ry, double rz, ChVector
 			(3.0f / 80.0f) * mass * (radius * radius + 4 * height * height),
 			(3.0f / 10.0f) * mass * radius * radius,
 			(3.0f / 80.0f) * mass * (radius * radius + 4 * height * height));
-	inertia.x += local_inertia.x+mass*(pos->Length2()-pos->x*pos->x);
-	inertia.y += local_inertia.y+mass*(pos->Length2()-pos->y*pos->y);
-	inertia.z += local_inertia.z+mass*(pos->Length2()-pos->z*pos->z);
+	inertia.x += local_inertia.x+mass*(pos.Length2()-pos.x*pos.x);
+	inertia.y += local_inertia.y+mass*(pos.Length2()-pos.y*pos.y);
+	inertia.z += local_inertia.z+mass*(pos.Length2()-pos.z*pos.z);
 
 	model_type = CONE;
 	nObjects++;
 	bData tData;
-	tData.A = R3(pos->x, pos->y, pos->z);
+	tData.A = R3(pos.x, pos.y, pos.z);
 	tData.B = R3(radius, height, radius);
 	tData.C = R3(0, 0, 0);
-	tData.R = R4(rot->Get_A_quaternion().e0, rot->Get_A_quaternion().e1, rot->Get_A_quaternion().e2, rot->Get_A_quaternion().e3);
+	tData.R = R4(rot.Get_A_quaternion().e0, rot.Get_A_quaternion().e1, rot.Get_A_quaternion().e2, rot.Get_A_quaternion().e3);
 	tData.type = CONE;
 	mData.push_back(tData);
 	return true;
 }
 
-bool ChCollisionModelParallel::AddConvexHull(std::vector<ChVector<double> > &pointlist, ChVector<> *pos, ChMatrix33<> *rot) {
+bool ChCollisionModelParallel::AddConvexHull(std::vector<ChVector<double> > &pointlist, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	//NOT SUPPORTED
 	return false;
 }
-bool ChCollisionModelParallel::AddBarrel(double Y_low, double Y_high, double R_vert, double R_hor, double R_offset, ChVector<> *pos, ChMatrix33<> *rot) {
+bool ChCollisionModelParallel::AddBarrel(double Y_low, double Y_high, double R_vert, double R_hor, double R_offset, const ChVector<> &pos, const ChMatrix33<> &rot) {
 	//NOT SUPPORTED
 	return false;
 }
@@ -276,36 +186,21 @@ bool ChCollisionModelParallel::AddCone(double rad, double h) {
 }
 /// Add a triangle mesh to this model
 bool ChCollisionModelParallel::AddTriangleMesh(
-		const geometry::ChTriangleMesh &trimesh,
-		bool is_static,
-		bool is_convex,
-		ChVector<> *posv,
-		ChMatrix33<> *rotv) {
-	ChVector<> *pos;
-
-	// if (posv != 0) {
-	//     pos = posv;
-	//  } else {
-	pos = new ChVector<>(0, 0, 0);
-	// }
-
-	ChMatrix33<> *rot;
-
-	if (rotv != 0) {
-		rot = rotv;
-	} else {
-		rot = new ChMatrix33<>();
-	}
-
+                 const geometry::ChTriangleMesh &trimesh,
+                 bool                            is_static,
+                 bool                            is_convex,
+                 const ChVector<>               &pos,
+                 const ChMatrix33<>             &rot)
+{
 	model_type = TRIANGLEMESH;
 	nObjects += trimesh.getNumTriangles();
 	bData tData;
 	for (int i = 0; i < trimesh.getNumTriangles(); i++) {
 		ChTriangle temptri = trimesh.getTriangle(i);
-		tData.A = R3(temptri.p1.x + pos->x, temptri.p1.y + pos->y, temptri.p1.z + pos->z);
-		tData.B = R3(temptri.p2.x + pos->x, temptri.p2.y + pos->y, temptri.p2.z + pos->z);
-		tData.C = R3(temptri.p3.x + pos->x, temptri.p3.y + pos->y, temptri.p3.z + pos->z);
-		tData.R = R4(rot->Get_A_quaternion().e0, rot->Get_A_quaternion().e1, rot->Get_A_quaternion().e2, rot->Get_A_quaternion().e3);
+		tData.A = R3(temptri.p1.x + pos.x, temptri.p1.y + pos.y, temptri.p1.z + pos.z);
+		tData.B = R3(temptri.p2.x + pos.x, temptri.p2.y + pos.y, temptri.p2.z + pos.z);
+		tData.C = R3(temptri.p3.x + pos.x, temptri.p3.y + pos.y, temptri.p3.z + pos.z);
+		tData.R = R4(rot.Get_A_quaternion().e0, rot.Get_A_quaternion().e1, rot.Get_A_quaternion().e2, rot.Get_A_quaternion().e3);
 		tData.type = TRIANGLEMESH;
 		mData.push_back(tData);
 	}
