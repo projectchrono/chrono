6dbe0296d7058f3e408f478e9c00bae327958879
armanpazouki <apazouki@gmail.com>
2013-11-21 13:10:54 -0600

    Flexible added

---
+++ src/chrono_fsi/FlexibleBodies.cuh
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//	collideSphereSphere.cu
+//	Does the required calculations for SPH, both fluid and rigid bodies
+//
+//	Related Files: collideSphereSphere.cuh, SDKCollisionSystem.cu, SDKCollisionSystem.cu
+//	Input File:		-
+/////////////////////////////////////////////////////////////////////////////////
+//	Created by Arman Pazouki, apazouki@gmail.com
+//	Copyright 2013 Arman Pazouki
+//
+//	This file is part of SPH_FSI.
+//
+//	SPH_FSI is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	any later version.
+//
+//	SPH_FSI is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with SPH_FSI.  If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////
+
