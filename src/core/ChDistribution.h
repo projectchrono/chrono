ff8d3e064a8a88b97e5a2d44a2d8c0d64474a3dd
Alessandro Tasora <tasora@ied.unipr.it>
2013-12-18 00:20:03 +0100

    New ChDiscreteDistribution class , for discrete probability distribution  (atomic measures instead of the probability 'curve'). To be tested.

--- src/core/ChDistribution.h
+++ src/core/ChDistribution.h
@@ -27,8 +27,9 @@ class ChDistribution
 public:
 		/// Compute a random value whose probability is defined by the distribution.
 		/// MUST be implemented by children classes.
-	double GetRandom() = 0;
-}
+	virtual double GetRandom() = 0;
+};
+
 
 
 /// Class that can be used to generate sample numbers according to a 
@@ -51,7 +52,7 @@ public:
 		if (mx.GetRows() != my.GetRows())
 			throw ChException("Probability curve must have same number of rows in abscysse and ordinates");
 		if ((mx.GetColumns() != 1) || (my.GetColumns() != 1))
-			throw ChException("Probability curve must have column-vectors as input");
+			throw ChException("Probability curve must be column-vectors as input");
 
 		x = new ChMatrixDynamic<>;
 		y = new ChMatrixDynamic<>;
@@ -96,7 +97,7 @@ public:
 
 		/// Compute a random value whose probability is the probability curve that has 
 		/// been entered with x,y points during the creation of this object.
-	double GetRandom()
+	virtual double GetRandom()
 	{
 		double mx1 =  (*x)(0);
 		double mx2 =  (*cdf_x)(0);
@@ -138,6 +139,91 @@ private:
 
 
 
+/// Class that can be used to generate sample numbers according to a discrete
+/// probability distribution. Probability distribution is defined with discrete
+/// values, each with a percentual of probability.
+
+class ChDiscreteDistribution : public ChDistribution
+{
+public:
+
+		/// Create an object that can be used to generate sample numbers according to a discrete
+		/// probability distribution. Probability distribution is defined with N discrete
+		/// values, each with a percentual of probability. The sum of the N probability values
+		/// must be unit, i.e normalized (but if not, a normalization will be enforced)
+		/// For example, to get '12.3' for 30% of the times you call GetRandom(), and '150' for
+		/// the remaining 70% of the times, create  ChDiscreteDistribution with 
+		/// mx = [12.3; 150] and my = [0.3; 0.7]
+	ChDiscreteDistribution (ChMatrix<>& mx, ChMatrix<>& my)
+	{
+	
+		if (mx.GetRows() != my.GetRows())
+			throw ChException("Probability values and percentages must have the same size");
+		if ((mx.GetColumns() != 1) || (my.GetColumns() != 1))
+			throw ChException("Probability values and percentages must be column-vectors as input");
+
+		x = new ChMatrixDynamic<>;
+		y = new ChMatrixDynamic<>;
+		cdf_y = new ChMatrixDynamic<>;
+
+		*x = mx;
+		*y = my;
+		*cdf_y = my;
+
+			// compute CDF
+		double integral = 0;
+		for (int i = 0; i< x->GetRows(); i++)
+		{
+			integral += (*y)(i);
+			(*cdf_y)(i) = integral;
+		}
+			// normalize if P(x) had not unit integral
+		double totintegral = (*cdf_y)(x->GetRows()-1);
+		if (totintegral != 1.0)
+		{
+			for (int i = 0; i< x->GetRows(); i++)
+			{
+				(*cdf_y)(i) *= 1./totintegral;
+			}
+		}
+		(*cdf_y)(x->GetRows()-1) = 1.0;
+		
+	}
+
+	~ChDiscreteDistribution()
+	{
+		delete x;
+		delete y;
+		delete cdf_y;
+	}
+
+		/// Compute a random value, according to the discrete probabilty values entered
+		/// when you created this object
+	virtual double GetRandom()
+	{
+		double rand = ChRandom();
+		double lastval = 0;
+		for (int i = 0; i< x->GetRows(); i++)
+		{
+			if (( rand <= (*cdf_y)(i) ) &&
+				( rand >  lastval   ) )
+			{
+				return (*x)(i);
+			}
+			lastval = (*cdf_y)(i);
+		}
+		return 0;
+	}
+
+	ChMatrix<>* GetProbabilityXpoints() {return x;}
+	ChMatrix<>* GetProbabilityYpoints() {return y;}
+	ChMatrix<>* GetProbabilityCDFcumulativeY() {return cdf_y;}
+
+private:
+	ChMatrix<>* x;
+	ChMatrix<>* y;
+	ChMatrix<>* cdf_y;
+};
 
 
 
