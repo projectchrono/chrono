213cc9b2b208809ca5965a7cb7274008a1fe71a9
Alessandro Tasora <tasora@ied.unipr.it>
2014-01-08 15:34:43 +0100

    Experimental support of Euler-Bernoulli beams. Work in progress.

---
+++ src/unit_FEM/ChElementBeamEuler.h
@@ -0,0 +1,396 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2013 Project Chrono
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+// File author: Alessandro Tasora
+
+#ifndef CHELEMENTBEAMEULER_H
+#define CHELEMENTBEAMEULER_H
+
+
+#include "ChElementGeneric.h"
+#include "ChNodeFEMxyzrot.h"
+
+
+namespace chrono
+{
+namespace fem
+{
+
+
+
+/// Simple beam element with two nodes and Euler-Bernoulli 
+/// formulation.
+/// For this 'basic' implementation, constant section and 
+/// constant material are assumed.
+
+class ChApiFem ChElementBeamEuler : public ChElementGeneric
+{
+protected:
+	std::vector< ChSharedPtr<ChNodeFEMxyzrot> > nodes;
+	double A;
+	double Iyy;
+	double Izz;
+	double G;
+	double E;
+	double density;
+	double rdamping;
+	double mass;
+	double length;
+	ChMatrixDynamic<> StiffnessMatrix; // undeformed local stiffness matrix
+
+public:
+
+	ChElementBeamEuler()
+				{
+					nodes.resize(2);
+
+					A = 0.01*0.01; // default area: 1cmx1cm
+					Izz = (1.0/12.0)*0.1*pow(0.1,3);
+					Iyy = (1.0/12.0)*0.1*pow(0.1,3);
+				
+					E = 0.01e9;		  // default stiffness: rubber
+					G = 0.3 * E;
+
+					density = 1000;   // default density: water
+					rdamping = 0.01;  // default raleygh damping.
+
+					length = 0;		// will be computed by Setup(), later
+					mass = 0;		// will be computed by Setup(), later
+
+					this->StiffnessMatrix.Resize(this->GetNdofs(), this->GetNdofs());
+				}
+
+	virtual ~ChElementBeamEuler() {}
+
+	virtual int GetNnodes()  {return 2;}
+	virtual int GetNcoords() {return 2*6;}
+	virtual int GetNdofs()   {return 2*6;}
+
+	virtual ChSharedPtr<ChNodeFEMbase> GetNodeN(int n) {return nodes[n];}
+
+	virtual void SetNodes( ChSharedPtr<ChNodeFEMxyzrot> nodeA, ChSharedPtr<ChNodeFEMxyzrot> nodeB) 
+				{
+					nodes[0]=nodeA; 
+					nodes[1]=nodeB;
+					std::vector<ChLcpVariables*> mvars;
+					mvars.push_back(&nodes[0]->Variables());
+					mvars.push_back(&nodes[1]->Variables());
+					Kmatr.SetVariables(mvars);
+				}
+
+
+			//
+			// FEM functions
+			//
+
+				/// Fills the D vector (column matrix) with the current 
+				/// field values at the nodes of the element, with proper ordering.
+				/// If the D vector has not the size of this->GetNdofs(), it will be resized.
+				/// For corotational elements, field is assumed in local reference!
+				/// Give that this element includes rotations at nodes, this gives: 
+				///  {x_a y_a z_a Rx_a Ry_a Rz_a x_b y_b z_b Rx_b Ry_b Rz_b}
+	virtual void GetField(ChMatrixDynamic<>& mD)
+				{
+					mD.Reset(12,1);
+
+					ChVector<> delta_rot_dir;
+					double     delta_rot_angle;
+
+					// Compute corotational frame (x is distance between nodes, y is average of the y of the two nodes)
+					ChMatrix33<> A; 
+					ChVector<> mXele = nodes[1]->Frame().GetPos() - nodes[0]->Frame().GetPos();
+					ChVector<> myele = ( nodes[0]->Frame().GetA()->Get_A_Yaxis() + nodes[1]->Frame().GetA()->Get_A_Yaxis() );
+					A.Set_A_Xdir(mXele, myele);
+					ChQuaternion<> quatA = A.Get_A_quaternion();
+
+					// Node 0, displacement (in local element frame, corotated back by A' )
+					mD.PasteVector(A.MatrT_x_Vect(nodes[0]->Frame().GetPos()) - nodes[0]->GetX0().GetPos(), 0, 0);
+
+					// Node 0, x,y,z small rotations (in local element frame, corotated back by A' )
+					//  with quaternion algebra: q_delta = q_reference' * q_corotation' * q_node  
+					//  with linear algebra in SO(3): [A_delta] = [A_ref]'[A_coro.]'[A_node]
+					ChQuaternion<> q_delta0 = nodes[0]->GetX0().GetRot().GetConjugate() % (quatA.GetConjugate() % nodes[0]->Frame().GetRot());
+						// note, for small incremental rotations this is opposite of ChNodeFEMxyzrot::VariablesQbIncrementPosition 
+					q_delta0.Q_to_AngAxis(delta_rot_angle, delta_rot_dir); 
+					mD.PasteVector(delta_rot_dir*delta_rot_angle, 3, 0);
+
+					// Node 1, displacement (in local element frame, corotated back by A' )
+					mD.PasteVector(A.MatrT_x_Vect(nodes[1]->Frame().GetPos()) - nodes[1]->GetX0().GetPos(), 6, 0);
+
+					// Node 1, x,y,z small rotations (in local element frame, corotated back by A' )
+					//  with quaternion algebra: q_delta = q_reference' * q_corotation' * q_node  
+					//  with linear algebra in SO(3): [A_delta] = [A_ref]'[A_coro.]'[A_node]
+					ChQuaternion<> q_delta1 = nodes[1]->GetX0().GetRot().GetConjugate() % (quatA.GetConjugate() % nodes[1]->Frame().GetRot());
+						// note, for small incremental rotations this is opposite of ChNodeFEMxyzrot::VariablesQbIncrementPosition 
+					q_delta1.Q_to_AngAxis(delta_rot_angle, delta_rot_dir); 
+					mD.PasteVector(delta_rot_dir*delta_rot_angle, 9, 0);
+
+				}
+
+				/// Fills the Ddt vector (column matrix) with the current 
+				/// tme derivatives of field values at the nodes of the element, with proper ordering.
+				/// If the D vector has not the size of this->GetNdofs(), it will be resized.
+				/// For corotational elements, field is assumed in local reference!
+				/// Give that this element includes rotations at nodes, this gives: 
+				///  {v_a v_a v_a wx_a wy_a wz_a v_b v_b v_b wx_b wy_b wz_b}
+	virtual void GetField_dt(ChMatrixDynamic<>& mD_dt)
+				{
+					mD_dt.Reset(12,1);
+
+					// Compute corotational frame (x is distance between nodes, y is average of the y of the two nodes)
+					ChMatrix33<> A; 
+					ChVector<> mXele = nodes[1]->Frame().GetPos() - nodes[0]->Frame().GetPos();
+					ChVector<> myele = ( nodes[0]->Frame().GetA()->Get_A_Yaxis() + nodes[1]->Frame().GetA()->Get_A_Yaxis() );
+					A.Set_A_Xdir(mXele, myele);
+					ChQuaternion<> quatA = A.Get_A_quaternion();
+
+					// Node 0, velocity (in local element frame, corotated back by A' )
+					mD_dt.PasteVector(A.MatrT_x_Vect(nodes[0]->Frame().GetPos_dt()),    0, 0);
+
+					// Node 0, x,y,z ang.velocity (in local element frame, corotated back by A' )
+					mD_dt.PasteVector(A.MatrT_x_Vect(nodes[0]->Frame().GetWvel_par() ), 3, 0);
+
+					// Node 1, velocity (in local element frame, corotated back by A' )
+					mD_dt.PasteVector(A.MatrT_x_Vect(nodes[1]->Frame().GetPos_dt()),    6, 0);
+
+					// Node 1, x,y,z ang.velocity (in local element frame, corotated back by A' )
+					mD_dt.PasteVector(A.MatrT_x_Vect(nodes[1]->Frame().GetWvel_par() ), 9, 0);
+
+				}
+				
+				/// Computes the local STIFFNESS MATRIX of the element:    
+				/// K = integral( [B]' * [D] * [B] ),
+				/// Note: in this 'basic' implementation, constant section and 
+				/// constant material are assumed, so the explicit result of quadrature is used. 
+	virtual void ComputeStiffnessMatrix()
+				{	
+					double om_xz = 0; // For Euler-Bernoulli
+					double om_xy = 0; // For Euler-Bernoulli
+					// double om_xz = ...; // For Reddy's RBT 
+					// double om_xy = ...; // For Reddy's RBT 
+					double u_xz = 1+12*om_xz;
+					double l_xz = 1+3*om_xz;
+					double e_xz = 1-6*om_xz;
+					double u_xy = 1+12*om_xy;
+					double l_xy = 1+3*om_xy;
+					double e_xy = 1-6*om_xy;
+
+					double h = this->length;
+					double Jpolar = Izz+Iyy;
+					double k_u = E*A/h;
+					double k_f = G*Jpolar/h;
+					
+					double k_w = 12*E*Iyy / (u_xz*h*h*h);
+					double k_v = 12*E*Izz / (u_xy*h*h*h);
+
+					double k_t = 4*E*Iyy*l_xz / (u_xz*h);
+					double k_p = 4*E*Izz*l_xy / (u_xy*h);
+
+					double k_wt = 6*E*Iyy / (u_xz*h*h);
+					double k_vp = 6*E*Izz / (u_xy*h*h);
+
+					double k_tt = 2*E*Iyy*e_xz / (u_xz*h);
+					double k_pp = 2*E*Izz*e_xy / (u_xy*h);
+
+					StiffnessMatrix(0,0)  = k_u;
+					StiffnessMatrix(1,1)  = k_v;
+					StiffnessMatrix(2,2)  = k_w;
+					StiffnessMatrix(3,3)  = k_f;
+					StiffnessMatrix(4,4)  = k_t;
+					StiffnessMatrix(5,5)  = k_p;
+					StiffnessMatrix(6,6)  = k_u;
+					StiffnessMatrix(7,7)  = k_v;
+					StiffnessMatrix(8,8)  = k_w;
+					StiffnessMatrix(9,9)  = k_f;
+					StiffnessMatrix(10,10)= k_t;
+					StiffnessMatrix(11,11)= k_p;
+	
+					StiffnessMatrix(0,6)  = -k_u;
+					StiffnessMatrix(1,7)  = -k_v;
+					StiffnessMatrix(2,8)  = -k_w;
+					StiffnessMatrix(3,9)  = -k_f;
+					StiffnessMatrix(4,10) =  k_tt;
+					StiffnessMatrix(5,11) =  k_pp;
+
+					StiffnessMatrix(4,8)  =  k_wt;
+					StiffnessMatrix(5,7)  = -k_vp;
+					StiffnessMatrix(1,11) =  k_vp;
+					StiffnessMatrix(2,10) = -k_wt;
+
+					StiffnessMatrix(1,5)  =  k_vp;
+					StiffnessMatrix(2,4)  = -k_wt;
+					StiffnessMatrix(7,11) = -k_vp;
+					StiffnessMatrix(8,10) =  k_wt;
+
+					// symm part;
+					for (int r = 0; r < 12; r++)
+						for (int c = r+1; c < 12; c++)
+							StiffnessMatrix(c,r) = StiffnessMatrix(r,c);
+				}
+
+
+				/// Setup. Precompute mass and matrices that do not change during the 
+				/// simulation, such as the local tangent stiffness Kl of each element, if needed, etc.
+
+	virtual void SetupInitial() 
+				{
+					// Compute rest length, mass:
+					this->length = (nodes[1]->GetX0().GetPos() - nodes[0]->GetX0().GetPos()).Length();
+					this->mass   = this->length * this->A * this->density;
+
+					// Compute local stiffness matrix:
+					ComputeStiffnessMatrix();
+				}
+
+
+				/// Sets H as the global stiffness matrix K, scaled  by Kfactor. Optionally, also
+				/// superimposes global damping matrix R, scaled by Rfactor, and global mass matrix M multiplied by Mfactor.
+	virtual void ComputeKRMmatricesGlobal	(ChMatrix<>& H, double Kfactor, double Rfactor=0, double Mfactor=0) 
+				{
+					assert((H.GetRows() == 12) && (H.GetColumns()==12));
+
+					// Compute corotational frame (x is distance between nodes, y is average of the y of the two nodes)
+					ChMatrix33<> A; 
+					ChVector<> mXele = nodes[1]->Frame().GetPos() - nodes[0]->Frame().GetPos();
+					ChVector<> myele = ( nodes[0]->Frame().GetA()->Get_A_Yaxis() + nodes[1]->Frame().GetA()->Get_A_Yaxis() );
+					A.Set_A_Xdir(mXele, myele);
+
+					ChMatrixDynamic<> CK(12,12);
+					ChMatrixDynamic<> CKCt(12,12); // the global, corotated, K matrix
+					ChMatrixCorotation<>::ComputeCK(StiffnessMatrix, this->A, 4, CK);
+					ChMatrixCorotation<>::ComputeKCt(CK, this->A, 4, CKCt);
+
+
+					// For K stiffness matrix and R matrix:
+
+					double mkrfactor = Kfactor + Rfactor * this->rdamping;  
+
+					CKCt.MatrScale( mkrfactor );
+
+					H.PasteMatrix(&CKCt,0,0); // because [R] = r*[K] , so kf*[K]+rf*[R] = (kf+rf*r)*[K] 
+
+
+						
+					// For M mass matrix, do mass lumping:
+
+					double lmass = mass*0.5;
+					double liner = mass*0.5* pow((this->length* 0.25),2); //***APPROX just for test 
+					
+					H(0,0) += Mfactor* lmass; //node A x,y,z
+					H(1,1) += Mfactor* lmass;
+					H(2,2) += Mfactor* lmass;
+
+					H(3,3) += Mfactor* liner; //node A Ixx,Iyy,Izz
+					H(4,4) += Mfactor* liner;
+					H(5,5) += Mfactor* liner;
+
+					H(6,6) += Mfactor* lmass; //node B x,y,z
+					H(7,7) += Mfactor* lmass;
+					H(8,8) += Mfactor* lmass;
+
+					H(9,9) +=  Mfactor* liner; //node B Ixx,Iyy,Izz
+					H(10,10)+= Mfactor* liner;
+					H(11,11)+= Mfactor* liner;
+					
+					//***TO DO*** better per-node lumping, or 4x4 consistent mass matrices, maybe with integration if not uniform materials.
+				}
+
+
+				/// Computes the internal forces (ex. the actual position of
+				/// nodes is not in relaxed reference position) and set values
+				/// in the Fi vector.
+	virtual void ComputeInternalForces	(ChMatrixDynamic<>& Fi)
+				{
+					assert((Fi.GetRows() == 12) && (Fi.GetColumns()==12));
+
+						// set up vector of nodal displacements and small rotations (in local element system) 
+					ChMatrixDynamic<> displ(12,1);
+					this->GetField(displ);
+					
+						// [local Internal Forces] = [Klocal] * displ + [Rlocal] * displ_dt
+					ChMatrixDynamic<> FiK_local(12,1);
+					FiK_local.MatrMultiply(StiffnessMatrix, displ);
+
+						// set up vector of nodal velocities (in local element system) 
+					ChMatrixDynamic<> displ_dt(12,1);
+					this->GetField_dt(displ_dt);
+
+					ChMatrixDynamic<> FiR_local(12,1);
+					FiR_local.MatrMultiply(StiffnessMatrix, displ_dt);
+					FiR_local.MatrScale(this->GetBeamRaleyghDamping());
+
+					FiK_local.MatrInc(FiR_local);
+
+
+					FiK_local.MatrScale(-1.0);
+
+						// Fi = C * Fi_local  with C block-diagonal rotations A  , for nodal forces in abs. frame 
+					ChMatrixCorotation<>::ComputeCK(FiK_local, this->A, 4, Fi);
+				}
+
+			//
+			// Custom properties functions
+			//
+
+				/// Set the cross sectional area A of the beam (m^2) 
+	void   SetArea(double ma) { this->A = ma;  }
+	double GetArea() {return this->A;}
+
+				/// Set the Iyy moment of inertia of the beam 
+	void   SetIyy(double ma) { this->Iyy = ma;  }
+	double GetIyy() {return this->Iyy;}
+
+				/// Set the Iyy moment of inertia of the beam 
+	void   SetIzz(double ma) { this->Izz = ma;  }
+	double GetIzz() {return this->Izz;}
+
+				/// Set the density of the beam (kg/m^3)
+	void   SetDensity(double md) { this->density = md;  }
+	double GetDensity() {return this->density;}
+
+				/// Set E, the Young elastic modulus (N/m^2) 
+	void   SetYoungModulus(double mE) { this->E = mE; }
+	double GetYoungModulus() {return this->E;}
+				/// Set G, the shear modulus 
+	void   SetGshearModulus(double mG) { this->G = mG; }
+	double GetGshearModulus() {return this->G;}
+
+	/// Set the Raleygh damping ratio r (as in: R = r * K ), to do: also mass-proportional term
+	void   SetBeamRaleyghDamping(double mr) { this->rdamping = mr; }
+	double GetBeamRaleyghDamping() {return this->rdamping;}
+
+				/// The full mass of the beam, (with const. section, density, etc.)
+	double  GetMass() {return this->mass;}
+
+				/// The rest length of the bar
+	double  GetRestLength() {return this->length;}
+
+
+			//
+			// Functions for interfacing to the LCP solver 
+			//            (***not needed, thank to bookkeeping in parent class ChElementGeneric)
+
+};
+
+
+
+
+
+} // END_OF_NAMESPACE____
+} // END_OF_NAMESPACE____
+
+
+#endif
+
+
+
+
+
+
---
+++ src/unit_FEM/ChNodeFEMxyzrot.h
@@ -0,0 +1,227 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2013 Project Chrono
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+// File authors: Alessandro Tasora
+
+#ifndef CHNODEFEMXYZROT_H
+#define CHNODEFEMXYZROT_H
+
+
+#include "ChNodeFEMbase.h"
+#include "core/ChFrameMoving.h"
+#include "lcp/ChLcpVariablesBodyOwnMass.h"
+
+
+namespace chrono
+{
+namespace fem
+{
+
+
+/// Class for a generic finite element node 
+/// in 3D space, with x,y,z displacement and 3D rotation.
+/// This is the typical node that can be used for beams, etc.
+
+class ChApiFem ChNodeFEMxyzrot : public ChNodeFEMbase
+
+{
+public:
+
+	ChNodeFEMxyzrot(ChVector<> initial_pos = VNULL)
+					{
+						X0    = ChFrame<>(initial_pos);
+						frame = ChFrameMoving<>(initial_pos);
+						
+						Force = VNULL;
+						Torque = VNULL;
+
+						variables.SetBodyMass(0.0);
+						variables.GetBodyInertia().FillElem(0.0);
+					}
+
+	~ChNodeFEMxyzrot() {};
+
+	ChNodeFEMxyzrot (const ChNodeFEMxyzrot& other) :
+						ChNodeFEMbase(other) 
+	{
+		this->X0 = other.X0;
+		this->frame = other.frame;
+
+		this->Force = other.Force;
+		this->Force = other.Torque;
+
+		this->variables = other.variables;
+	}
+
+	ChNodeFEMxyzrot& operator= (const ChNodeFEMxyzrot& other)
+	{
+		if (&other == this) 
+			return *this;
+
+		ChNodeFEMbase::operator=(other);
+
+		this->X0 = other.X0;
+		this->frame = other.frame;
+
+		this->Force = other.Force;
+		this->Force = other.Torque;
+
+		this->variables = other.variables;
+
+		return *this;
+	}
+
+	virtual ChLcpVariables& Variables()
+					{
+						return this->variables; 
+					} 
+
+				/// Set the rest position as the actual position.
+	virtual void Relax () 
+					{
+						this->X0 = this->frame; 
+						this->frame.GetPos_dt() = VNULL; 
+						this->frame.GetRot_dtdt() = QNULL;
+						this->frame.GetPos_dtdt() = VNULL; 
+						this->frame.GetRot_dtdt() = QNULL;
+					}
+
+				/// Get atomic mass of the node.
+	virtual double GetMass() {return this->variables.GetBodyMass();}
+				/// Set atomic mass of the node.
+	virtual void SetMass(double mm) {this->variables.SetBodyMass(mm);}
+
+				/// Access atomic inertia of the node.
+	virtual ChMatrix33<>& GetInertia() {return this->variables.GetBodyInertia();}
+
+				/// Set the initial (reference) frame
+	virtual void SetX0(ChFrame<> mx) { X0 = mx;}
+				/// Get the initial (reference) frame
+	virtual ChFrame<> GetX0 () {return X0;}
+
+				/// Set the 3d applied force, in absolute reference
+	virtual void SetForce(ChVector<> mf) { Force = mf;}
+				/// Get the 3d applied force, in absolute reference
+	virtual ChVector<> GetForce () {return Force;}
+
+				/// Set the 3d applied torque, in absolute reference
+	virtual void SetTorque(ChVector<> mf) { Torque = mf;}
+				/// Get the 3d applied torque, in absolute reference
+	virtual ChVector<> GetTorque () {return Torque;}
+
+				/// Access the frame of the node - in absolute csys,
+				/// with infos on actual position, speed, acceleration, etc.
+	ChFrameMoving<>& Frame() {return frame;}
+
+
+				/// Sets the 'fixed' state of the node. If true, it does not move
+                /// respect to the absolute world, despite constraints, forces, etc.
+	void SetFixed  (bool mev) { variables.SetDisabled(mev); }
+				/// Gets the 'fixed' state of the node.
+    bool GetFixed()  {return variables.IsDisabled(); }
+
+
+			//
+			// Functions for interfacing to the LCP solver
+			//
+
+	virtual void VariablesFbLoadForces(double factor=1.) 
+					{ 
+						ChVector<> gyro = Vcross ( frame.GetWvel_loc(), (variables.GetBodyInertia().Matr_x_Vect (frame.GetWvel_loc())));
+
+						this->variables.Get_fb().PasteSumVector( this->Force * factor ,0,0);
+						this->variables.Get_fb().PasteSumVector((this->Torque - gyro)* factor ,3,0);
+					};
+
+	virtual void VariablesQbLoadSpeed() 
+					{ 
+						// set current speed in 'qb', it can be used by the LCP solver when working in incremental mode
+						this->variables.Get_qb().PasteVector(frame.GetCoord_dt().pos,0,0);
+						this->variables.Get_qb().PasteVector(frame.GetWvel_loc()    ,3,0);
+					};
+
+	virtual void VariablesQbSetSpeed(double step=0.) 
+					{
+						ChCoordsys<> old_coord_dt = frame.GetCoord_dt();
+
+						// from 'qb' vector, sets body speed, and updates auxiliary data
+						frame.SetPos_dt(   this->variables.Get_qb().ClipVector(0,0) );
+						frame.SetWvel_loc( this->variables.Get_qb().ClipVector(3,0) );
+
+						// apply limits (if in speed clamping mode) to speeds.
+						//ClampSpeed(); 
+
+						// Compute accel. by BDF (approximate by differentiation);
+						if (step)
+						{
+							frame.SetPos_dtdt( (frame.GetCoord_dt().pos - old_coord_dt.pos)  / step);
+							frame.SetRot_dtdt( (frame.GetCoord_dt().rot - old_coord_dt.rot)  / step);
+						}
+					};
+
+	virtual void VariablesFbIncrementMq() 
+					{
+						this->variables.Compute_inc_Mb_v(this->variables.Get_fb(), this->variables.Get_qb());
+					};
+
+	virtual void VariablesQbIncrementPosition(double step) 
+					{
+						//if (!this->IsActive()) 
+						//	return;
+
+						// Updates position with incremental action of speed contained in the
+						// 'qb' vector:  pos' = pos + dt * speed   , like in an Eulero step.
+
+						ChVector<> newspeed = variables.Get_qb().ClipVector(0,0);
+						ChVector<> newwel   = variables.Get_qb().ClipVector(3,0);
+
+						// ADVANCE POSITION: pos' = pos + dt * vel
+						frame.SetPos( frame.GetPos() + newspeed * step);
+
+						// ADVANCE ROTATION: rot' = [dt*wwel]%rot  (use quaternion for delta rotation)
+						ChQuaternion<> mdeltarot;
+						ChQuaternion<> moldrot = frame.GetRot();
+						ChVector<> newwel_abs = frame.Amatrix * newwel;
+						double mangle = newwel_abs.Length() * step;
+						newwel_abs.Normalize();
+						mdeltarot.Q_from_AngAxis(mangle, newwel_abs);
+						ChQuaternion<> mnewrot = mdeltarot % moldrot;
+						frame.SetRot( mnewrot );
+					};
+
+private:
+	ChLcpVariablesBodyOwnMass	variables; /// 3D node variables, with x,y,z displ. and 3D rot.
+
+	ChFrameMoving<> frame;	///< frame
+
+	ChFrame<> X0;		///< reference frame
+
+	ChVector<> Force;	///< applied force
+	ChVector<> Torque;	///< applied torque
+	
+
+
+};
+
+
+
+
+
+} // END_OF_NAMESPACE____
+} // END_OF_NAMESPACE____
+
+
+#endif
+
+
+
+
+
+
