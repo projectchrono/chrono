2dafb760302f560f488fa86b51e4c75df3df4013
armanpazouki <apazouki@gmail.com>
2014-01-13 16:43:57 -0600

    SDK Slightly modified

--- src/chrono_fsi/SDKCollisionSystem.cuh
+++ src/chrono_fsi/SDKCollisionSystem.cuh
@@ -67,6 +67,7 @@ struct fluidData {
 		real3 velocity;
 };
 __constant__ SimParams paramsD;
+__constant__ ANCF_Params flexParamsD;
 __constant__ int3 cartesianGridDimsD;
 __constant__ real_ resolutionD;
 
