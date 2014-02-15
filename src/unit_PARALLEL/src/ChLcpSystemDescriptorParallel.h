75a8199b89c4f3694a0e15b9247b69ae3a2a44d5
Radu Serban <serban@engr.wisc.edu>
2014-02-14 18:30:25 -0600

    First set of changes to accommodate Penalty Method (DEM) for contact & friction.
    - make ChSystemParallel a base class and derive ChSystemParallelDVI and ChSystemParallelDEM which implement two (pure) virtual methods, LoadMaterialSurfaceData() and UpdateBodies()
    - make ChLcpSolverParallel a base class and derive ChLcpSolverParallelDVI and ChSystemLcpSolverParallelDEM which implement the (pure) virtual method RunTimeStep()
    - create two different system descriptor classes, ChLcpSystemDescriptorParallelDVI and ChLcpSystemDescriptorParallelDEM
    
    A few other fixes (e.g. using references where appropriate).

--- src/unit_PARALLEL/src/ChLcpSystemDescriptorParallel.h
+++ src/unit_PARALLEL/src/ChLcpSystemDescriptorParallel.h
@@ -18,53 +18,83 @@
 #include "ChParallelDefines.h"
 #include "ChLcpSolverParallel.h"
 #include "lcp/ChLcpSystemDescriptor.h"
+
 namespace chrono {
-class ChApiGPU ChLcpSystemDescriptorParallel: public ChLcpSystemDescriptor {
+
+class ChApiGPU ChLcpSystemDescriptorParallelDVI : public ChLcpSystemDescriptor {
 public:
-	ChLcpSystemDescriptorParallel() {
-	}
-	~ChLcpSystemDescriptorParallel() {
-	}
+	ChLcpSystemDescriptorParallelDVI() {}
+	~ChLcpSystemDescriptorParallelDVI() {}
 
-	void ConvertToMatrixForm(ChSparseMatrix* Cq, ChSparseMatrix* M, ChSparseMatrix* E, ChMatrix<>* Fvector, ChMatrix<>* Bvector, ChMatrix<>* Frict) {
-
+	void ConvertToMatrixForm(ChSparseMatrix* Cq, ChSparseMatrix* M, ChSparseMatrix* E, ChMatrix<>* Fvector, ChMatrix<>* Bvector, ChMatrix<>* Frict)
+	{
 		std::vector<ChLcpConstraint*>& mconstraints = this->GetConstraintsList();
-			std::vector<ChLcpVariables*>&  mvariables	= this->GetVariablesList();
-
+		std::vector<ChLcpVariables*>&  mvariables	= this->GetVariablesList();
 
 		int n_c = data_container->number_of_rigid_rigid * 3 + data_container->number_of_bilaterals;
-
 		int n_q = Thrust_Count(data_container->host_data.active_data,1)*6;
+
 		//cout << " " << n_q << " " << data_container->number_of_objects << " " << endl;
-		if (Cq)
-			Cq->Reset(n_c, n_q);
-		if (M)
-			M->Reset(n_q, n_q);
-		if (E)
-			E->Reset(n_c, n_c);
-		if (Fvector)
-			Fvector->Reset(n_q, 1);
-		if (Bvector)
-			Bvector->Reset(n_c, 1);
-		if (Frict)
-			Frict->Reset(n_c, 1);
+
+		if (Cq)      Cq->Reset(n_c, n_q);
+		if (M)       M->Reset(n_q, n_q);
+		if (E)       E->Reset(n_c, n_c);
+		if (Fvector) Fvector->Reset(n_q, 1);
+		if (Bvector) Bvector->Reset(n_c, 1);
+		if (Frict)   Frict->Reset(n_c, 1);
 
 		int s_q = 0;
-		for (unsigned int iv = 0; iv< mvariables.size(); iv++)
-			{
-				if (mvariables[iv]->IsActive())
-				{
-					if (M)
-						mvariables[iv]->Build_M(*M, s_q, s_q);				// .. fills  M
-					if (Fvector)
-						Fvector->PasteMatrix(&vvariables[iv]->Get_fb(), s_q, 0);// .. fills  'f'
-					s_q += mvariables[iv]->Get_ndof();
-				}
+		for (unsigned int iv = 0; iv< mvariables.size(); iv++) {
+			if (mvariables[iv]->IsActive()) {
+				if (M)       mvariables[iv]->Build_M(*M, s_q, s_q);
+				if (Fvector) Fvector->PasteMatrix(&vvariables[iv]->Get_fb(), s_q, 0);
+
+				s_q += mvariables[iv]->Get_ndof();
 			}
+		}
 	}
 
 	ChParallelDataManager* data_container;
 };
+
+
+class ChApiGPU ChLcpSystemDescriptorParallelDEM : public ChLcpSystemDescriptor {
+public:
+	ChLcpSystemDescriptorParallelDEM() {}
+	~ChLcpSystemDescriptorParallelDEM() {}
+
+	void ConvertToMatrixForm(ChSparseMatrix* Cq, ChSparseMatrix* M, ChSparseMatrix* E, ChMatrix<>* Fvector, ChMatrix<>* Bvector, ChMatrix<>* Frict)
+	{
+		std::vector<ChLcpConstraint*>& mconstraints = this->GetConstraintsList();
+		std::vector<ChLcpVariables*>&  mvariables   = this->GetVariablesList();
+
+		int n_c = data_container->number_of_bilaterals;
+		int n_q = Thrust_Count(data_container->host_data.active_data,1)*6;
+
+		//cout << " " << n_q << " " << data_container->number_of_objects << " " << endl;
+
+		if (Cq)      Cq->Reset(n_c, n_q);
+		if (M)       M->Reset(n_q, n_q);
+		if (E)       E->Reset(n_c, n_c);
+		if (Fvector) Fvector->Reset(n_q, 1);
+		if (Bvector) Bvector->Reset(n_c, 1);
+		if (Frict)   Frict->Reset(n_c, 1);
+
+		int s_q = 0;
+		for (unsigned int iv = 0; iv< mvariables.size(); iv++) {
+			if (mvariables[iv]->IsActive()) {
+				if (M)       mvariables[iv]->Build_M(*M, s_q, s_q);
+				if (Fvector) Fvector->PasteMatrix(&vvariables[iv]->Get_fb(), s_q, 0);
+
+				s_q += mvariables[iv]->Get_ndof();
+			}
+		}
+	}
+
+	ChParallelDataManager* data_container;
+};
+
+
 } // END_OF_NAMESPACE____
 
 #endif
