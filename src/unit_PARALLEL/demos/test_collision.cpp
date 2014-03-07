a66c8a43e57e27a727964a99e672feac87bbb4fd
Radu Serban <serban@engr.wisc.edu>
2014-03-06 19:33:45 -0600

    Cleanup collision test for more flexibility.

--- src/unit_PARALLEL/demos/test_collision.cpp
+++ src/unit_PARALLEL/demos/test_collision.cpp
@@ -8,112 +8,143 @@
 using namespace chrono;
 using namespace geometry;
 
+using std::cout;
+using std::endl;
+
 
 #define DEM
+#define NARROW_NEW
+
+
+int counter = 0;
+
+// ------------------------------------------------------------------------------
 
 void writeContactInfo(ChParallelDataManager* data_container)
 {
 	int    ncontacts = data_container->number_of_rigid_rigid;
+
 	real3* pt1 = data_container->host_data.cpta_rigid_rigid.data();
 	real3* pt2 = data_container->host_data.cptb_rigid_rigid.data();
 	real3* nrm = data_container->host_data.norm_rigid_rigid.data();
 	real*  depth = data_container->host_data.dpth_rigid_rigid.data();
+	real*  erad = data_container->host_data.erad_rigid_rigid.data();
 
-	std::cout << "Number contacts: " << ncontacts << std::endl;
+	long long* pairs = data_container->host_data.pair_rigid_rigid.data();
+	uint*  bid = data_container->host_data.id_rigid.data();
+
+	cout << endl << "Number contacts: " << ncontacts << endl << endl;
 	for (int i = 0; i < ncontacts; i++) {
-		std::cout << "Depth: " << depth[i] << std::endl;
-		std::cout << "Point1: " << pt1[i].x << " " << pt1[i].y << " " << pt1[i].z << std::endl;
-		std::cout << "Point2: " << pt2[i].x << " " << pt2[i].y << " " << pt2[i].z << std::endl;
-		std::cout << "Normal: " << nrm[i].x << " " << nrm[i].y << " " << nrm[i].z << std::endl << std::endl;
+		int2 pair = I2(int(pairs[i] >> 32), int(pairs[i] & 0xffffffff));
+		int body1 = bid[pair.x];
+		int body2 = bid[pair.y];
+
+		cout << body1 << " - " << body2 << endl;
+#ifdef NARROW_NEW
+		cout << "Radius: " << erad[i] << endl;
+#endif
+		cout << "Depth:  " << depth[i] << endl;
+		cout << "Point1: " << pt1[i].x << " " << pt1[i].y << " " << pt1[i].z << endl;
+		cout << "Point2: " << pt2[i].x << " " << pt2[i].y << " " << pt2[i].z << endl;
+		cout << "Normal: " << nrm[i].x << " " << nrm[i].y << " " << nrm[i].z << endl << endl;
 	}
 }
 
+// ------------------------------------------------------------------------------
+
+void
+createBall(ChSystemParallel* sys,
+           const ChVector<>& pos,
+           const ChQuaternion<>& rot,
+           double radius)
+{
+#ifdef DEM
+	ChSharedBodyDEMPtr ball(new ChBodyDEM(new ChCollisionModelParallel));
+#else
+	ChSharedBodyPtr ball(new ChBody(new ChCollisionModelParallel));
+#endif
+	ball->SetPos(pos);
+	ball->SetRot(rot);
+	ball->SetBodyFixed(false);
+	ball->SetCollide(true);
+
+	ball->GetCollisionModel()->ClearModel();
+	ball->GetCollisionModel()->AddSphere(radius);
+	ball->GetCollisionModel()->BuildModel();
+
+	sys->AddBody(ball);
+
+	cout << counter++ << " [ball]" << endl;
+	cout << "   radius: " << radius << endl;
+	cout << "   pos:    " << pos.x << "  " << pos.y << "  " << pos.z << endl;
+	cout << "   rot:    " << rot.e0 << "  " << rot.e1 << "  " << rot.e2 << "  " << rot.e3 << endl;
+}
+
+// ------------------------------------------------------------------------------
+
+void
+createBox(ChSystemParallel* sys,
+          const ChVector<>& pos,
+          const ChQuaternion<>& rot,
+          const ChVector<>& hdims)
+{
+#ifdef DEM
+	ChSharedBodyDEMPtr box(new ChBodyDEM(new ChCollisionModelParallel));
+#else
+	ChSharedBodyPtr box(new ChBody(new ChCollisionModelParallel));
+#endif
+	box->SetPos(pos);
+	box->SetRot(rot);
+	box->SetCollide(true);
+	box->SetBodyFixed(false);
+
+	box->GetCollisionModel()->ClearModel();
+	box->GetCollisionModel()->AddBox(hdims.x, hdims.y, hdims.z);
+	box->GetCollisionModel()->BuildModel();
+
+	sys->AddBody(box);
+
+	cout << counter++ << " [box]" << endl;
+	cout << "   hdims:  " << hdims.x << "  " << hdims.y << "  " << hdims.z << endl;
+	cout << "   pos:    " << pos.x << "  " << pos.y << "  " << pos.z << endl;
+	cout << "   rot:    " << rot.e0 << "  " << rot.e1 << "  " << rot.e2 << "  " << rot.e3 << endl;
+}
+
+// ------------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
 	int threads = 8;
 	omp_set_num_threads(threads);
 
-	ChVector<>     ball_pos(0.629447, -1.45809, 0.045702643641292666);
-	ChQuaternion<> ball_rot(1, 0, 0, 0);
-	real           ball_radius = 0.1;
-
-	ChVector<>     box_pos(0, 0, -0.1);
-	ChQuaternion<> box_rot(1, 0, 0, 0);
-	ChVector<>     box_hdims(5, 2, 0.1);
-
-	std::cout << "Box position: " << box_pos.x << "  " << box_pos.y << "  " << box_pos.z << std::endl;
-	std::cout << "Box half-dims: " << box_hdims.x << "  " << box_hdims.y << "  " << box_hdims.z << std::endl << std::endl;
-
+	// Create system
 #ifdef DEM
-	std::cout << "DEM" << std::endl << std::endl;
+	cout << "DEM";
 	ChSystemParallelDEM msystem;
+#ifdef NARROW_NEW
+	cout << "  R" << endl << endl;
+	((ChCollisionSystemParallel*) msystem.GetCollisionSystem())->ChangeNarrowphase(new ChCNarrowphaseR);
 #else
-	std::cout << "DVI" << std::endl << std::endl;
+	cout << "  MPR" << endl << endl;
+#endif
+#else
+	cout << "DVI" << endl << endl;
 	ChSystemParallelDVI msystem;
 #endif
 
 
-	((ChCollisionSystemParallel*) msystem.GetCollisionSystem())->ChangeNarrowphase(new ChCNarrowphaseR);
+	createBox(&msystem, ChVector<>(0, 0, -0.1), ChQuaternion<>(1, 0, 0, 0), ChVector<>(5, 2, 0.1));
+	createBall(&msystem, ChVector<>(0.629447, -1.45809, 0.045702643641292666), ChQuaternion<>(1, 0, 0, 0), 0.1);
+	createBall(&msystem, ChVector<>(1, -1, 0.045702643641292666), ChQuaternion<>(1, 0, 0, 0), 0.1);
 
-	// Create the box body
-#ifdef DEM
-	ChSharedBodyDEMPtr bin(new ChBodyDEM(new ChCollisionModelParallel));
-#else
-	ChSharedBodyPtr bin(new ChBody(new ChCollisionModelParallel));
-#endif
-	bin->SetPos(box_pos);
-	bin->SetRot(ChQuaternion<>(1, 0, 0, 0));
-	bin->SetCollide(true);
-	bin->SetBodyFixed(true);
-
-	bin->GetCollisionModel()->ClearModel();
-	bin->GetCollisionModel()->AddBox(box_hdims.x, box_hdims.y, box_hdims.z);
-	bin->GetCollisionModel()->BuildModel();
-
-	msystem.AddBody(bin);
-
-
-	// Create the ball body
-#ifdef DEM
-	ChSharedBodyDEMPtr ball(new ChBodyDEM(new ChCollisionModelParallel));
-#else
-	ChSharedBodyPtr ball(new ChBody(new ChCollisionModelParallel));
-#endif
-	ball->SetPos(ball_pos);
-	ball->SetRot(ball_rot);
-	ball->SetBodyFixed(false);
-	ball->SetCollide(true);
-
-	ball->GetCollisionModel()->ClearModel();
-	ball->GetCollisionModel()->AddSphere(ball_radius);
-	ball->GetCollisionModel()->BuildModel();
-
-	msystem.AddBody(ball);
-
+	createBall(&msystem, ChVector<>(0, 0, 7), ChQuaternion<>(1, 0, 0, 0), 1.1);
+	createBall(&msystem, ChVector<>(0, 0, 4), ChQuaternion<>(1, 0, 0, 0), 2);
 
 	// Perform the collision detection.
 	msystem.Update();
 	msystem.GetCollisionSystem()->Run();
 
-	// Print contact information
-	std::cout << "Ball radius: " << ball_radius << std::endl;
-	std::cout << "Ball position: " << ball_pos.x << "  " << ball_pos.y << "  " << ball_pos.z << std::endl;
 	writeContactInfo(msystem.gpu_data_manager);
-
-	std::cout << " --------------------" << std::endl;
-
-	// Move ball in horizontal plane, perform collision detection, print info
-	ball_pos.x = 1;
-	ball_pos.y = -1;
-	ball->SetPos(ball_pos);
-
-	msystem.Update();
-	msystem.GetCollisionSystem()->Run();
-	std::cout << "Ball radius: " << ball_radius << std::endl;
-	std::cout << "Ball position: " << ball_pos.x << "  " << ball_pos.y << "  " << ball_pos.z << std::endl;
-	writeContactInfo(msystem.gpu_data_manager);
-
 
 	return 0;
 }
