9226bd147b097c5aed06848694c975e40195e03c
Alessandro Tasora <tasora@ied.unipr.it>
2014-03-19 21:56:11 +0100

    New ChBodyEasy classes! Useful for creating simple spherical / boxy / cylindrical bodies, it automatically does: 1) compute mass & inertia tensor 2) add visualization shape 3) add collision shape if needed.

---
+++ src/physics/ChBodyEasy.h
@@ -0,0 +1,172 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2011 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+#ifndef CHBODYEASY_H
+#define CHBODYEASY_H
+
+
+#include "physics/ChBody.h"
+#include "assets/ChCylinderShape.h"
+#include "assets/ChBoxShape.h"
+#include "assets/ChSphereShape.h"
+
+
+namespace chrono
+{
+
+// This header includes some easy-to-use bodies, that can be used
+// to create ChBody objects that already include a visualization shape
+// without the need of adding it afterward with AddAsset(). 
+// Also, a collision shape is created and added automatically too, if 
+// collision is needed.
+
+
+
+/// Easy-to-use class for quick creation of rigid bodies with a 
+/// spherical shape. 
+/// Compared to the base ChBody class, this class also does
+/// automatically, at object creation, the following tasks that
+/// you would normally do by hand if using ChBody:
+/// - a visualization shape is created and added,
+/// - a collision shape is created and added, if collision is desired,
+/// - mass and moment of inertia is automatically set, according to the geometry.
+
+class ChBodyEasySphere : public ChBody {
+
+	
+public:
+
+		
+		/// Creates a ChBody plus adds a visualization shape and, optionally,
+		/// a collision shape. Mass and inertia are set automatically depending
+		/// on density.
+		/// Sphere is assumed with center at body reference coordsystem.
+	ChBodyEasySphere ( double radius, double mdensity, bool collide = false)
+	{
+		ChSharedPtr<ChSphereShape> vshape (new ChSphereShape() );
+		vshape->GetSphereGeometry().rad = radius;
+		this->AddAsset( vshape );
+		
+		double mmass =  mdensity * ( (4.0/3.0) * CH_C_PI* pow (radius,3) );
+		double inertia = (2.0/5.0)* mmass * pow (radius,2);
+
+		this->SetMass( mmass );
+		this->SetInertiaXX( ChVector<> ( inertia,inertia,inertia) );
+
+		if (collide)
+		{
+			GetCollisionModel()->ClearModel();
+			GetCollisionModel()->AddSphere(radius);  // radius, radius, height on y
+			GetCollisionModel()->BuildModel();
+			SetCollide(true);
+		}
+	}
+
+};
+
+
+/// Easy-to-use class for quick creation of rigid bodies with a 
+/// cylindrical shape. 
+/// Compared to the base ChBody class, this class also does
+/// automatically, at object creation, the following tasks that
+/// you would normally do by hand if using ChBody:
+/// - a visualization shape is created and added,
+/// - a collision shape is created and added, if collision is desired,
+/// - mass and moment of inertia is automatically set, according to the geometry.
+
+class ChBodyEasyCylinder : public ChBody {
+
+	
+public:
+
+		
+		/// Creates a ChBody plus adds a visualization shape and, optionally,
+		/// a collision shape. Mass and inertia are set automatically depending
+		/// on density.
+		/// Cylinder is assumed with body Y axis as vertical, and reference is at half height.
+	ChBodyEasyCylinder ( double radius, double height, double mdensity, bool collide = false)
+	{
+		ChSharedPtr<ChCylinderShape> vshape (new ChCylinderShape() );
+		vshape->GetCylinderGeometry().p1 = ChVector<> (0,-height*0.5, 0);
+		vshape->GetCylinderGeometry().p2 = ChVector<> (0, height*0.5, 0);
+		vshape->GetCylinderGeometry().rad = radius;
+		this->AddAsset( vshape );
+		
+		double mmass =  mdensity * (CH_C_PI* pow (radius,2) * height);
+		this->SetMass( mmass );
+		this->SetInertiaXX( ChVector<> ( (1.0/12.0)*mmass * (3*pow (radius,2) + pow (height,2)) ,
+										  0.5*mmass * pow (radius,2) ,
+										 (1.0/12.0)*mmass * (3*pow (radius,2) + pow (height,2)) ));
+		if (collide)
+		{
+			GetCollisionModel()->ClearModel();
+			GetCollisionModel()->AddCylinder(radius,  radius,  height*0.5);  // radius x, radiusz, height on y
+			GetCollisionModel()->BuildModel();
+			SetCollide(true);
+		}
+	}
+
+};
+
+
+
+
+/// Easy-to-use class for quick creation of rigid bodies with a 
+/// box shape. 
+/// Compared to the base ChBody class, this class also does
+/// automatically, at object creation, the following tasks that
+/// you would normally do by hand if using ChBody:
+/// - a visualization shape is created and added,
+/// - a collision shape is created and added, if collision is desired,
+/// - mass and moment of inertia is automatically set, according to the geometry.
+
+class ChBodyEasyBox : public ChBody {
+
+	
+public:
+
+		
+		/// Creates a ChBody plus adds a visualization shape and, optionally,
+		/// a collision shape. Mass and inertia are set automatically depending
+		/// on density.
+		/// Box is assumed centered, ie. and reference of body in the middle.
+	ChBodyEasyBox ( double Xsize, double Ysize, double Zsize, double mdensity, bool collide = false)
+	{
+		ChSharedPtr<ChBoxShape> vshape (new ChBoxShape() );
+		vshape->GetBoxGeometry().SetLenghts( ChVector<> (Xsize,Ysize,Zsize) );
+		this->AddAsset( vshape );
+		
+		double mmass =  mdensity * (Xsize * Ysize * Zsize);
+		GetLog() << " computed mass " << mmass*100000 << "\n";
+		this->SetMass( mmass );
+		this->SetInertiaXX( ChVector<> (  (1.0/12.0) * mmass * ( pow(Ysize,2) + pow (Zsize,2) ) ,
+										  (1.0/12.0) * mmass * ( pow(Xsize,2) + pow (Zsize,2) ),
+										  (1.0/12.0) * mmass * ( pow(Xsize,2) + pow (Ysize,2) ) ));
+		if (collide)
+		{
+			GetCollisionModel()->ClearModel();
+			GetCollisionModel()->AddBox(Xsize*0.5,  Ysize*0.5,  Zsize*0.5);  // radius x, radius z, height on y
+			GetCollisionModel()->BuildModel();
+			SetCollide(true);
+		}
+		GetLog() << "  set mass " << this->GetMass()*1000000 << "\n";
+	}
+
+};
+
+
+
+
+
+} // END_OF_NAMESPACE____
+
+
+#endif
