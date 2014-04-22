c7af42521e6abefc272cc9b753be9ed0253ca27e
Alessandro Tasora <tasora@ied.unipr.it>
2014-04-22 20:00:58 +0200

    The ChLcpMatlabSolver is a solver that can be plugged into the ChSystem, so that the Matlab solver will be used to solve the linear system (for the moment this is limited to linear systems, not to LCP or CCP, but it could be adapted in future). The Matlab "A\b " style linear solver is used. This can be used to test finite elements problems that require very high precision (the default iterative solvers would need preconditioning, that is not yet available in C::E, natively)

--- src/unit_MATLAB/ChMatlabEngine.h
+++ src/unit_MATLAB/ChMatlabEngine.h
@@ -187,6 +187,69 @@ public:
 
 
 
+class ChLcpMatlabSolver : public ChLcpSolver
+{
+protected:
+			//
+			// DATA
+			//
+	ChMatlabEngine* mengine;
+public:
+			//
+			// CONSTRUCTORS
+			//
+
+	ChLcpMatlabSolver(ChMatlabEngine& me) { mengine = &me;};
+				
+	virtual ~ChLcpMatlabSolver(){};
+
+
+				/// Solve using the Matlab default direct solver (as in x=A\b)
+
+	virtual double Solve(
+				ChLcpSystemDescriptor& sysd		///< system description with constraints and variables	
+				)
+	{
+		chrono::ChSparseMatrix mdM;
+		chrono::ChSparseMatrix mdCq;
+		chrono::ChSparseMatrix mdE;
+		chrono::ChMatrixDynamic<double> mdf;
+		chrono::ChMatrixDynamic<double> mdb;
+		chrono::ChMatrixDynamic<double> mdfric;
+		sysd.ConvertToMatrixForm(&mdCq, &mdM, &mdE, &mdf, &mdb, &mdfric);
+
+		mengine->PutSparseMatrix(mdM, "mdM");
+		mengine->PutSparseMatrix(mdCq,"mdCq");
+		mengine->PutSparseMatrix(mdE, "mdE");
+		mengine->PutVariable(mdf, "mdf");
+		mengine->PutVariable(mdb, "mdb");
+		mengine->PutVariable(mdfric, "mdfric");
+
+		mengine->Eval("mdZ = [mdM, mdCq'; mdCq, -mdE]; mdd=[mdf;-mdb];");
+
+		mengine->Eval("mdx = mldivide(mdZ , mdd);");
+		
+		chrono::ChMatrixDynamic<> mx;
+		if (!mengine->GetVariable(mx, "mdx"))
+			GetLog() << "ERROR!! cannot fetch mdx";
+
+		sysd.FromVectorToUnknowns(mx);
+
+		
+		mengine->Eval("resid = norm(mdZ*mdx - mdd);");
+		chrono::ChMatrixDynamic<> mres;
+		mengine->GetVariable(mres, "resid");
+		GetLog() << " Matlab computed residual:" << mres(0,0) << "\n";
+		
+
+		return 0;
+
+	}
+};
+
+
+
+
 } // END_OF_NAMESPACE____
 
 
