7670ae9b046134f41fbde738dffeef528b2ccad9
Alessandro Tasora <tasora@ied.unipr.it>
2014-06-10 00:44:45 +0200

    Many new types of distributions

--- src/core/ChDistribution.h
+++ src/core/ChDistribution.h
@@ -32,15 +32,182 @@ public:
 
 
 
+/// Class for a distribution with a single 'value' that 
+/// has probability 1.0 (that is, the distribution has
+/// a spike corresponding to 'value' and zero elsewhere).
+
+class ChConstantDistribution : public ChDistribution
+{
+public:
+	ChConstantDistribution (double mvalue)
+	{
+		value  = mvalue;
+	}
+	
+		/// Compute a random value whose probability is defined by the distribution.
+		/// In this very simple case, however, returns always the single value.
+	virtual double GetRandom() {return value;}
+
+private:
+	double value;
+};
+
+
+/// Class for a distribution with uniform probability between 
+/// a lower 'min' value and upper 'max' value (that is, the 
+/// distribution looks like a rectangle)
+
+class ChMinMaxDistribution : public ChDistribution
+{
+public:
+	ChMinMaxDistribution (double mmin, double mmax)
+	{
+		min = mmin;
+		max = mmax;
+	}
+	
+		/// Compute a random value whose probability is defined by the distribution,
+		/// that is a value between min and max.
+	virtual double GetRandom() {return min + (::chrono::ChRandom()*(max-min));}
+
+private:
+	double min;
+	double max;
+};
+
+
+/// Class that generates the Gauss normal distribution (the 'bell' distribution)
+/// using the Box–Muller transform.
+
+class ChNormalDistribution : public ChDistribution
+{
+public:
+		/// Create the normal distribution with assigned variance.
+		/// The mean is 0 by default, but you can offset the curve if
+		/// you want by providing a mean.
+	ChNormalDistribution (double mvariance, double mmean=0)
+	{
+		variance = mvariance;
+		mean = mmean;
+		hasSpare = false;
+	}
+
+		/// Compute a random value whose probability density is the normal distribution.
+		/// It uses the Box–Muller transform.
+	virtual double GetRandom()
+	{
+		if(hasSpare)
+		{
+			hasSpare = false;
+			return (mean + sqrt(variance * rand1) * sin(rand2));
+		}
+	 
+		hasSpare = true;
+	 
+		rand1 = ::chrono::ChRandom();
+		if(rand1 < 1e-100) rand1 = 1e-100;
+		rand1 = -2.0 * log(rand1);
+		rand2 = ::chrono::ChRandom() * CH_C_2PI;
+	 
+		return (mean + sqrt(variance * rand1) * cos(rand2));
+	}
+
+	double GetMean() {return mean;}
+	double GetVariance() {return variance;}
+
+private:
+	double mean;
+	double variance;
+
+	bool hasSpare;
+	double rand1;
+	double rand2;
+};
+
+
+
+/// Class that generates the Weibull distribution
+/// It can be used for example to describe particle size
+/// distribution, as in the subcase of Rosin & Rammler distribution.
+
+class ChWeibullDistribution : public ChDistribution
+{
+public:
+		/// Create the Weibull distribution with assigned scale
+		/// factor 'lambda' and with shape 'k'.
+		/// The larger 'labda' is, the more horizontally stretched is the distribution.
+		/// For k<1, there is a vertical peak at 0.
+		/// For k=1, you get the exponential distribution
+		/// For k>1, you get an asymmetric bell shape 
+	ChWeibullDistribution (double mlambda, double mk)
+	{
+		lambda = mlambda;
+		k = mk;
+	}
+
+		/// Compute a random value whose probability density is the Weibull distribution.
+		/// It uses the "Smirnov transform" (inverse probability integral transform)
+	virtual double GetRandom()
+	{
+		double rand = ::chrono::ChRandom();
+		if (rand < 1e-100) 
+			rand = 1e-100; 
+		return lambda * pow( - log(rand), (1.0/k));
+	}
+
+	double GetK() {return k;}
+	double GetLambda() {return lambda;}
+
+private:
+	double k;
+	double lambda;
+};
+
+
+/// Class that generates the Zhang distribution, a modified exponential distribution.
+/// It is used to describe particle size distribution, with minimum part.size.
+
+class ChZhangDistribution : public ChDistribution
+{
+public:
+		/// Create the Zhang distribution with average part. size
+		/// and minimum particle size. Usually average:minimum = 3.25:1
+	ChZhangDistribution (double average_size, double minimum_size)
+	{
+		minsize = minimum_size;
+		lambdar = 1.0/(average_size-minimum_size);
+	}
+
+		/// Compute a random value whose probability density is the Weibull distribution.
+		/// It uses the "Smirnov transform" (inverse probability integral transform)
+	virtual double GetRandom()
+	{
+		double rand = ::chrono::ChRandom();
+		if (rand < 1e-100) 
+			rand = 1e-100; 
+		return ( minsize + (1.0/lambdar) * ( - log(rand)  ) );
+	}
+
+	double GetMinSize() {return minsize;}
+	double GetAverageSize() {return (minsize + (1.0/lambdar));}
+
+private:
+	double minsize;
+	double lambdar;
+};
+
+
+
 /// Class that can be used to generate sample numbers according to a 
 /// probability distribution. Probability distribution is defined with x,y points,
 /// at least a dozen of pairs to be more precise in the reconstruction of probability.
+/// It uses the "Smirnov transform" (inverse probability integral transform)
 
 class ChContinuumDistribution : public ChDistribution
 {
 public:
 
-		/// Create an object that can be used to generate sample numbers according to a 
+		/// Create an object that can be used to generate sample numbers according to a generic
 		/// probability distribution. The probability distribution is a curve represented
 		/// by simplified x,y pairs of points. The integral of the probability curve
 		/// must be unit, i.e normalized (but if not, a normalization will be enforced)
