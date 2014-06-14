af8623de58bdf746561acec719c0b52c46af08c9
armanpazouki <apazouki@gmail.com>
2014-06-14 09:45:58 -0500

    fair set of parameters for snake channel

--- src/chrono_fsi/main.cpp
+++ src/chrono_fsi/main.cpp
@@ -1954,12 +1954,12 @@ int main() {
 		paramsH.solidSurfaceAdjust = .6 * (paramsH.HSML * paramsH.MULT_INITSPACE);
 		paramsH.BASEPRES = 0;
 		paramsH.LARGE_PRES = 10000;
-		paramsH.nPeriod = 3;
+		paramsH.nPeriod = 1;
 		paramsH.gravity = R3(0, 0, 0);//R3(0);//R3(0, -9.81, 0);
-		paramsH.bodyForce4 = R4(5e-5,0,0,0);//R4(3.2e-3,0,0,0);// R4(0);;// /*Re = 100 */ //R4(3.2e-4, 0, 0, 0);/*Re = 100 */
+		paramsH.bodyForce4 = R4(5e-2,0,0,0);//R4(3.2e-3,0,0,0);// R4(0);;// /*Re = 100 */ //R4(3.2e-4, 0, 0, 0);/*Re = 100 */
 		paramsH.rho0 = 1000;
 		paramsH.mu0 = .001;
-		paramsH.v_Max = 18e-5;//18e-3;//1.5;//2e-1; /*0.2 for Re = 100 */ //2e-3;
+		paramsH.v_Max = 30e-3;//18e-3;//1.5;//2e-1; /*0.2 for Re = 100 */ //2e-3;
 		paramsH.EPS_XSPH = .5f;
 		paramsH.dT = 4e-5;//.001; //sph alone: .01 for Re 10;
 		paramsH.tFinal = 20;//20 * paramsH.dT; //400
--- src/chrono_fsi/printToFile.cu
+++ src/chrono_fsi/printToFile.cu
@@ -494,7 +494,7 @@ void PrintToFile(
 			fileNameFluidBoundaries.open(nameFluidBoundaries);
 			stringstream ssFluidBoundaryParticles;
 	//		ssFluidBoundaryParticles.precision(20);
-			for (int i = referenceArray[1].x; i < referenceArray[1].y; i++) {
+			for (int i = referenceArray[0].x; i < referenceArray[1].y; i++) {
 				real3 pos = posRadH[i];
 				real3 vel = R3(velMasH[i]);
 				real4 rP = rhoPresMuH[i];
