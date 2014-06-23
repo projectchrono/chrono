77d18dd56501532a2bc38ad6ffe20eea6e70b85c
Alessandro Tasora <tasora@ied.unipr.it>
2014-06-23 23:57:24 +0200

    I am slowly changing the old demos so that they will use the new 'asset based' approach for visualization instead than the old approach of the functions "addChBodySceneNode_easyBox(...) etc. Those functions will be made deprecated in future. The new approach offers advantages: - it is not dependent on Irrlicht - it can be used also for POVray postprocessing - it is bound to Irrlicht scene manager in a more reliable way - it is easier to understand.

--- src/demos/irrlicht/demo_bricks.cpp
+++ src/demos/irrlicht/demo_bricks.cpp
@@ -33,12 +33,11 @@
  
 #include "physics/ChApidll.h" 
 #include "physics/ChSystem.h"
-#include "irrlicht_interface/ChBodySceneNode.h"
-#include "irrlicht_interface/ChBodySceneNodeTools.h" 
-#include "irrlicht_interface/ChIrrAppInterface.h"
-#include "core/ChRealtimeStep.h"
-#include "lcp/ChLcpIterativeMINRES.h" // test
+#include "physics/ChBodyEasy.h"
 #include "physics/ChMaterialSurface.h"
+#include "assets/ChTexture.h"
+#include "irrlicht_interface/ChIrrApp.h"
+
 
 
  
@@ -57,135 +56,180 @@ using namespace gui;
 
 
 
-void create_some_falling_items(ChSystem& mphysicalSystem, ISceneManager* msceneManager, IVideoDriver* driver)
+	// Create a bunch of ChronoENGINE rigid bodies that
+	// represent bricks in a large wall.
+
+void create_wall_bodies(ChSystem& mphysicalSystem)
 {
-
-	ChBodySceneNode* mrigidBody; 
-
+	// Create a material that will be shared between bricks
 	ChSharedPtr<ChMaterialSurface> mmaterial(new ChMaterialSurface);
 	mmaterial->SetFriction(0.4f);
-	mmaterial->SetCompliance (0.00005f);
-	mmaterial->SetComplianceT(0.00005f);
-	mmaterial->SetDampingF(0.2);
-
-	// Create a bunch of ChronoENGINE rigid bodies (spheres and
-	// boxes) which will fall..
-	// Bodies are Irrlicht nodes of the special class ChBodySceneNode, 
-	// which encapsulates ChBody items).  
-	
-	video::ITexture* cubeMap   = driver->getTexture("../data/cubetexture_borders.png");
-	video::ITexture* sphereMap = driver->getTexture("../data/bluwhite.png");
+	mmaterial->SetCompliance (0.0000005f);
+	mmaterial->SetComplianceT(0.0000005f);
+	mmaterial->SetDampingF(0.2f);
 		
-
+	// Create bricks
 	for (int ai = 0; ai < 1; ai++)  // N. of walls
 	{ 
 		for (int bi = 0; bi < 10; bi++)  // N. of vert. bricks
 		{ 
 			for (int ui = 0; ui < 15; ui++)  // N. of hor. bricks
 			{ 
-				mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-													&mphysicalSystem, msceneManager,
-													0.8,
-													ChVector<>(-8+ui*4.0+2*(bi%2),  1.0+bi*2.0, ai*9),
-													ChQuaternion<>(1,0,0,0), 
-													ChVector<>(3.96,2,4) );
-				mrigidBody->GetBody()->SetMaterialSurface(mmaterial);
-				mrigidBody->setMaterialTexture(0,	cubeMap);
-				mrigidBody->addShadowVolumeSceneNode();
+				ChSharedPtr<ChBodyEasyBox> mrigidBody(new ChBodyEasyBox(
+											3.96, 2, 4, // x,y,z size
+											 100,		// density
+											true,		// collide enable?
+											true));		// visualization?
+				mrigidBody->SetPos(ChVector<>(-8+ui*4.0+2*(bi%2),  1.0+bi*2.0, ai*9));
+				mrigidBody->SetMaterialSurface(mmaterial); // use shared surface properties
+				
+				mphysicalSystem.Add(mrigidBody);
+				
+				// optional, attach a texture for better visualization
+				ChSharedPtr<ChTexture> mtexture(new ChTexture());
+				mtexture->SetTextureFilename("../data/cubetexture_borders.png");
+				mrigidBody->AddAsset(mtexture);
 			}
 		}
 	}
 
-
-	// Jenga tower
-	/*
-	for (int bi = 0; bi < 12; bi+=2) 
-	{ 
-		ChBodySceneNode* mrigidBody1;
-		mrigidBody1 = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											&mphysicalSystem, msceneManager,
-											0.2,
-											ChVector<>(-5, 1.0+bi*2.0,  0),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(2,2, 14) );
-		mrigidBody1->GetBody()->SetMaterialSurface(mmaterial);
-		mrigidBody1->setMaterialTexture(0,	cubeMap);
-
-		ChBodySceneNode* mrigidBody2;
-		mrigidBody2 = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											&mphysicalSystem, msceneManager,
-											0.2,
-											ChVector<>( 5, 1.0+bi*2.0,  0),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(2,2, 14) );
-		mrigidBody2->GetBody()->SetMaterialSurface(mmaterial);
-		mrigidBody2->setMaterialTexture(0,	cubeMap);
-
-		ChBodySceneNode* mrigidBody3;
-		mrigidBody3 = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											&mphysicalSystem, msceneManager,
-											0.2,
-											ChVector<>(0, 3.0+bi*2.0,  5),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(14,2, 2) );
-		mrigidBody3->GetBody()->SetMaterialSurface(mmaterial);
-		mrigidBody3->setMaterialTexture(0,	cubeMap);
-
-		ChBodySceneNode* mrigidBody4;
-		mrigidBody4 = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											&mphysicalSystem, msceneManager,
-											0.2,
-											ChVector<>(0, 3.0+bi*2.0,  -5),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(14,2, 2) );
-		mrigidBody4->GetBody()->SetMaterialSurface(mmaterial);
-		mrigidBody4->setMaterialTexture(0,	cubeMap);
-	}
-	*/
-
 	// Create the floor using
 	// fixed rigid body of 'box' type:
 
-	mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											&mphysicalSystem, msceneManager,
-											200.0,
-											ChVector<>(0,-2,0),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(550,4,550) );
-	mrigidBody->GetBody()->SetBodyFixed(true);
-	mrigidBody->GetBody()->SetMaterialSurface(mmaterial);
+	ChSharedPtr<ChBodyEasyBox> mrigidFloor(new ChBodyEasyBox(
+											250, 4, 250, // x,y,z size
+											1000,		// density
+											true,		// collide enable?
+											true));		// visualization?
+	mrigidFloor->SetPos(ChVector<>(0,-2,0));
+	mrigidFloor->SetMaterialSurface(mmaterial);
+	mrigidFloor->SetBodyFixed(true);
+	
+	mphysicalSystem.Add(mrigidFloor);
 
 
+	
 	// Create a ball that will collide with wall
-	double mradius = 4;
-	double density = 1.01;
-	double mmass = (4./3.)*CH_C_PI*pow(mradius,3)*density; 
-	GetLog() << "Ball mass = " << mmass << "\n";
-	double minert = (2./5.)* mmass * pow(mradius,2);
+	ChSharedPtr<ChBodyEasySphere> mrigidBall(new ChBodyEasySphere(
+											4,			// radius
+											8000,		// density
+											true,		// collide enable?
+											true));		// visualization?
+	mrigidBall->SetPos(ChVector<>(0,-2,0));
+	mrigidBall->SetMaterialSurface(mmaterial);
+	mrigidBall->SetPos(ChVector<>(0, 3, -8));
+	mrigidBall->SetPos_dt(ChVector<>(0,0,16)); // set initial speed 
+	mrigidBall->GetMaterialSurface()->SetFriction(0.4f); // use own (not shared) matrial properties
+	mrigidBall->GetMaterialSurface()->SetCompliance(0.0);
+	mrigidBall->GetMaterialSurface()->SetComplianceT(0.0);
+	mrigidBall->GetMaterialSurface()->SetDampingF(0.2f);
 
-	mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easySphere(
-										&mphysicalSystem, msceneManager,
-										mmass, // mass
-										ChVector<>(0, 3, -8), // pos
-										mradius, // radius
-										20,  // hslices, for rendering
-										15); // vslices, for rendering
+	mphysicalSystem.Add(mrigidBall);
 
-	// set moment of inertia (more realistic than default 1,1,1).
-	mrigidBody->GetBody()->SetInertiaXX(ChVector<>(minert,minert,minert));
-	mrigidBody->GetBody()->SetPos_dt(ChVector<>(0,0,16));
-	mrigidBody->GetBody()->GetMaterialSurface()->SetFriction(0.4f);
-	mrigidBody->GetBody()->GetMaterialSurface()->SetCompliance(0.0);
-	mrigidBody->GetBody()->GetMaterialSurface()->SetComplianceT(0.0);
-	mrigidBody->GetBody()->GetMaterialSurface()->SetDampingF(0.2);
+	// optional, attach a texture for better visualization
+	ChSharedPtr<ChTexture> mtextureball(new ChTexture());
+	mtextureball->SetTextureFilename("../data/bluwhite.png");
+	mrigidBall->AddAsset(mtextureball);
 
-	// Some aesthetics for 3d view..
-	mrigidBody->addShadowVolumeSceneNode();
-	mrigidBody->setMaterialTexture(0,	sphereMap);
 
 }
 
 
+// Create a bunch of ChronoENGINE rigid bodies that
+// represent bricks in a Jenga tower
+
+void create_jengatower_bodies(ChSystem& mphysicalSystem)
+{
+	// Create a material that will be shared between bricks
+	ChSharedPtr<ChMaterialSurface> mmaterial(new ChMaterialSurface);
+	mmaterial->SetFriction(0.4f);
+	mmaterial->SetCompliance (0.0000005f);
+	mmaterial->SetComplianceT(0.0000005f);
+	mmaterial->SetDampingF(0.2f);
+
+	// Create bricks
+	for (int bi = 0; bi < 12; bi+=2) 
+	{ 
+		ChSharedPtr<ChBodyEasyBox> mrigidBody1(new ChBodyEasyBox(
+											2, 2, 14,   // x,y,z size
+											 100,		// density
+											true,		// collide enable?
+											true));		// visualization?
+		mrigidBody1->SetPos( ChVector<>(-5, 1.0+bi*2.0,  0) );
+		mrigidBody1->SetMaterialSurface(mmaterial); // use shared surface properties	
+		mphysicalSystem.Add(mrigidBody1);
+
+
+		ChSharedPtr<ChBodyEasyBox> mrigidBody2(new ChBodyEasyBox(
+											2, 2, 14,   // x,y,z size
+											 100,		// density
+											true,		// collide enable?
+											true));		// visualization?
+		mrigidBody2->SetPos( ChVector<>( 5, 1.0+bi*2.0,  0) );
+		mrigidBody2->SetMaterialSurface(mmaterial); // use shared surface properties	
+		mphysicalSystem.Add(mrigidBody2);
+
+
+		ChSharedPtr<ChBodyEasyBox> mrigidBody3(new ChBodyEasyBox(
+											14, 2, 2,   // x,y,z size
+											 100,		// density
+											true,		// collide enable?
+											true));		// visualization?
+		mrigidBody3->SetPos( ChVector<>(0, 3.0+bi*2.0,  5) );
+		mrigidBody3->SetMaterialSurface(mmaterial); // use shared surface properties	
+		mphysicalSystem.Add(mrigidBody3);
+
+
+		ChSharedPtr<ChBodyEasyBox> mrigidBody4(new ChBodyEasyBox(
+											14, 2, 2,   // x,y,z size
+											 100,		// density
+											true,		// collide enable?
+											true));		// visualization?
+		mrigidBody4->SetPos( ChVector<>(0, 3.0+bi*2.0,  -5) );
+		mrigidBody4->SetMaterialSurface(mmaterial); // use shared surface properties	
+		mphysicalSystem.Add(mrigidBody4);
+	}
+
+	// Create the floor using
+	// fixed rigid body of 'box' type:
+
+	ChSharedPtr<ChBodyEasyBox> mrigidFloor(new ChBodyEasyBox(
+											250, 4, 250, // x,y,z size
+											1000,		// density
+											true,		// collide enable?
+											true));		// visualization?
+	mrigidFloor->SetPos(ChVector<>(0,-2,0));
+	mrigidFloor->SetMaterialSurface(mmaterial);
+	mrigidFloor->SetBodyFixed(true);
+	
+	mphysicalSystem.Add(mrigidFloor);
+
+
+	
+	// Create a ball that will collide with tower
+	ChSharedPtr<ChBodyEasySphere> mrigidBall(new ChBodyEasySphere(
+											4,			// radius
+											1000,		// density
+											true,		// collide enable?
+											true));		// visualization?
+	mrigidBall->SetPos(ChVector<>(0,-2,0));
+	mrigidBall->SetMaterialSurface(mmaterial);
+	mrigidBall->SetPos(ChVector<>(0, 3, -8));
+	mrigidBall->SetPos_dt(ChVector<>(0,0,2)); // set initial speed 
+	mrigidBall->GetMaterialSurface()->SetFriction(0.4f); // use own (not shared) matrial properties
+	mrigidBall->GetMaterialSurface()->SetCompliance(0.0);
+	mrigidBall->GetMaterialSurface()->SetComplianceT(0.0);
+	mrigidBall->GetMaterialSurface()->SetDampingF(0.2f);
+
+	mphysicalSystem.Add(mrigidBall);
+
+	// optional, attach a texture for better visualization
+	ChSharedPtr<ChTexture> mtextureball(new ChTexture());
+	mtextureball->SetTextureFilename("../data/bluwhite.png");
+	mrigidBall->AddAsset(mtextureball);
+
+
+}
  
 
  
@@ -202,23 +246,35 @@ int main(int argc, char* argv[])
 
 	// Create the Irrlicht visualization (open the Irrlicht device, 
 	// bind a simple user interface, etc. etc.)
-	ChIrrAppInterface application(&mphysicalSystem, L"Bricks test",core::dimension2d<u32>(800,600),false, true); 
+	ChIrrApp application(&mphysicalSystem, L"Bricks test",core::dimension2d<u32>(800,600),false, true); 
 
  
 	// Easy shortcuts to add camera, lights, logo and sky in Irrlicht scene:
 	ChIrrWizard::add_typical_Logo  (application.GetDevice());
 	ChIrrWizard::add_typical_Sky   (application.GetDevice());
-	ChIrrWizard::add_typical_Lights(application.GetDevice(), core::vector3df(70.f, 120.f, -90.f), core::vector3df(30.f, 80.f, 60.f), 590,  400);
+	ChIrrWizard::add_typical_Lights(application.GetDevice(), core::vector3df(70.f, 120.f, -90.f), core::vector3df(30.f, 80.f, 60.f), 290,  190);
 	ChIrrWizard::add_typical_Camera(application.GetDevice(), core::vector3df(-15,14,-30), core::vector3df(0,5,0)); 
 
 	// 
-	// HERE YOU CREATE THE MECHANICAL SYSTEM OF CHRONO... 
+	// HERE YOU POPULATE THE MECHANICAL SYSTEM OF CHRONO... 
 	// 
 
  
 	// Create all the rigid bodies.
-	create_some_falling_items(mphysicalSystem, application.GetSceneManager(), application.GetVideoDriver());
-  
+    create_wall_bodies (mphysicalSystem);
+	//create_jengatower_bodies (mphysicalSystem);
+
+
+	// Use this function for adding a ChIrrNodeAsset to all items
+	// If you need a finer control on which item really needs a visualization proxy in 
+	// Irrlicht, just use application.AssetBind(myitem); on a per-item basis.
+	application.AssetBindAll();
+
+	// Use this function for 'converting' into Irrlicht meshes the assets 
+	// into Irrlicht-visualizable meshes
+	application.AssetUpdateAll();
+
+
   
 	// Prepare the physical system for the simulation 
 
