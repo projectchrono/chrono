796a83e7bc01cd0c0b153e9128244fd73bd75da1
Alessandro Tasora <tasora@ied.unipr.it>
2014-07-02 11:35:01 +0200

    New constraint: ChShaftsGearboxAngled. It implements a gearbox with given transmission ratio between 1D input and 1D output, but with different directions in 3D space. Basically it defines a transmission ratio between two 1D entities of ChShaft type, and transmits the reaction of the gearbox to a 3D body that acts as the  support truss. A typical example is the case of a gearbox with bevel gears, where input shaft and output shaft are at 90°. Note that the more basic ChShaftsGear can do the same, except that it does not provide a way to transmit reaction to a 3D truss body.

---
+++ src/physics/ChShaftsGearboxAngled.cpp
@@ -0,0 +1,250 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2010, 2012 Alessandro Tasora
+// Copyright (c) 2013 Project Chrono
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+
+
+#include "physics/ChShaftsGearboxAngled.h"
+#include "physics/ChSystem.h"
+#include "physics/ChShaft.h"
+
+#include "core/ChMemory.h" // must be last include (memory leak debugger). In .cpp only.
+
+namespace chrono
+{
+
+
+// Register into the object factory, to enable run-time
+// dynamic creation and persistence
+ChClassRegister<ChShaftsGearboxAngled> a_registration_ChShaftsGearboxAngled;
+
+
+
+
+
+ChShaftsGearboxAngled::ChShaftsGearboxAngled ()
+{
+	this->t0=1;
+
+	SetTransmissionRatio(1);
+
+	this->torque_react= 0;
+	this->cache_li_speed =0.f;
+	this->cache_li_pos = 0.f;
+
+	this->shaft1 = 0;
+	this->shaft2 = 0;
+	this->body = 0;
+
+	this->shaft_dir1 = VECT_X;
+	this->shaft_dir2 = VECT_X;
+
+	SetIdentifier(CHGLOBALS().GetUniqueIntID()); // mark with unique ID
+
+}
+
+
+ChShaftsGearboxAngled::~ChShaftsGearboxAngled ()
+{
+	
+}
+
+void ChShaftsGearboxAngled::Copy(ChShaftsGearboxAngled* source)
+{
+		// copy the parent class data...
+	ChPhysicsItem::Copy(source);
+
+		// copy class data
+	t0 = source->t0;
+	shaft_dir1 = source->shaft_dir1;
+	shaft_dir2 = source->shaft_dir2;
+
+	torque_react = source->torque_react;
+	cache_li_speed = source->cache_li_speed;
+	cache_li_pos = source->cache_li_pos;
+	this->shaft1 = 0;
+	this->shaft2 = 0;
+	this->body = 0;
+	
+}
+
+
+int ChShaftsGearboxAngled::Initialize(ChSharedPtr<ChShaft> mshaft1, ///< first (input) shaft to join
+						   ChSharedPtr<ChShaft> mshaft2, ///< second  (output) shaft to join
+						   ChSharedPtr<ChBodyFrame>  mbody,  ///< 3D body to use as truss (also carrier, if rotates as in planetary gearboxes) 
+						   ChVector<>& mdir1,			 ///< the direction of the first shaft on 3D body defining the gearbox truss
+						   ChVector<>& mdir2			 ///< the direction of the first shaft on 3D body defining the gearbox truss
+						   )
+{
+	ChShaft* mm1 = mshaft1.get_ptr();
+	ChShaft* mm2 = mshaft2.get_ptr();
+	ChBodyFrame* mm3 = mbody.get_ptr();
+	assert(mm1 && mm2 && mm3);
+	assert(mm1 != mm2);
+	assert(mm1 != mm3);
+	assert(mm3 != mm2);
+	assert((mm1->GetSystem() == mm2->GetSystem()) &&( mm1->GetSystem() == mm3->GetSystem()) );
+
+	this->shaft1 = mm1;
+	this->shaft2 = mm2;
+	this->body = mm3;
+	this->shaft_dir1 = mdir1;
+	this->shaft_dir2 = mdir2;
+
+	this->constraint.SetVariables(&mm1->Variables(), &mm2->Variables(), &mm3->Variables());
+
+	this->SetSystem(this->shaft1->GetSystem());
+	return true;
+}
+
+
+void ChShaftsGearboxAngled::Update (double mytime)
+{
+		// Inherit time changes of parent class
+	ChPhysicsItem::Update(mytime);
+	
+		// update class data
+	// ...
+}
+
+
+
+
+////////// LCP INTERFACES ////
+
+
+void ChShaftsGearboxAngled::InjectConstraints(ChLcpSystemDescriptor& mdescriptor)
+{
+	//if (!this->IsActive())
+	//	return;
+
+	mdescriptor.InsertConstraint(&constraint);
+}
+
+void ChShaftsGearboxAngled::ConstraintsBiReset()
+{
+	constraint.Set_b_i(0.);
+}
+ 
+void ChShaftsGearboxAngled::ConstraintsBiLoad_C(double factor, double recovery_clamp, bool do_clamp)
+{
+	//if (!this->IsActive())
+	//	return;
+
+	double res = 0; // no residual
+
+	constraint.Set_b_i(constraint.Get_b_i() +  factor * res);
+}
+
+void ChShaftsGearboxAngled::ConstraintsBiLoad_Ct(double factor)
+{
+	//if (!this->IsActive())
+	//	return;
+
+	// nothing
+}
+
+/*
+void ChShaftsGearboxAngled::ConstraintsFbLoadForces(double factor)
+{
+	// no forces
+}
+*/
+
+void ChShaftsGearboxAngled::ConstraintsLoadJacobians()
+{
+		// compute jacobians
+	constraint.Get_Cq_a()->SetElement(0,0, (float)this->t0);
+	constraint.Get_Cq_b()->SetElement(0,0, (float)-1.0);
+
+	//ChVector<> jacw = this->body->TrasformDirectionParentToLocal(this->t0*this->shaft_dir1 - this->shaft_dir2);
+	ChVector<> jacw = (this->t0*this->shaft_dir1 - this->shaft_dir2);
+	
+	this->constraint.Get_Cq_c()->ElementN(0)=0;
+	this->constraint.Get_Cq_c()->ElementN(1)=0;
+	this->constraint.Get_Cq_c()->ElementN(2)=0;
+	this->constraint.Get_Cq_c()->ElementN(3)=(float)jacw.x ;
+	this->constraint.Get_Cq_c()->ElementN(4)=(float)jacw.y ;
+	this->constraint.Get_Cq_c()->ElementN(5)=(float)jacw.z ;
+}
+ 
+
+void ChShaftsGearboxAngled::ConstraintsFetch_react(double factor)
+{
+	// From constraints to react vector:
+	this->torque_react = constraint.Get_l_i() * factor;  
+}
+
+// Following functions are for exploiting the contact persistence
+
+void  ChShaftsGearboxAngled::ConstraintsLiLoadSuggestedSpeedSolution()
+{
+	constraint.Set_l_i(this->cache_li_speed);
+}
+
+void  ChShaftsGearboxAngled::ConstraintsLiLoadSuggestedPositionSolution()
+{
+	constraint.Set_l_i(this->cache_li_pos);
+}
+
+void  ChShaftsGearboxAngled::ConstraintsLiFetchSuggestedSpeedSolution()
+{
+	this->cache_li_speed = (float)constraint.Get_l_i();
+}
+
+void  ChShaftsGearboxAngled::ConstraintsLiFetchSuggestedPositionSolution()
+{
+	this->cache_li_pos =  (float)constraint.Get_l_i();
+}
+
+
+
+//////// FILE I/O
+
+void ChShaftsGearboxAngled::StreamOUT(ChStreamOutBinary& mstream)
+{
+			// class version number
+	mstream.VersionWrite(1);
+
+		// serialize parent class too
+	ChPhysicsItem::StreamOUT(mstream);
+
+		// stream out all member data
+	mstream << this->t0;
+	mstream << this->shaft_dir1;
+	mstream << this->shaft_dir2;
+	
+}
+
+void ChShaftsGearboxAngled::StreamIN(ChStreamInBinary& mstream)
+{
+		// class version number
+	int version = mstream.VersionRead();
+
+		// deserialize parent class too
+	ChPhysicsItem::StreamIN(mstream);
+
+		// deserialize class
+	mstream >> this->t0;
+	mstream >> this->shaft_dir1;
+	mstream >> this->shaft_dir2;
+	
+}
+
+
+
+
+
+
+} // END_OF_NAMESPACE____
+
+
+/////////////////////
---
+++ src/physics/ChShaftsGearboxAngled.h
@@ -0,0 +1,205 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2010-2012 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+#ifndef CHSHAFTSGEARBOXANGLED_H
+#define CHSHAFTSGEARBOXANGLED_H
+
+
+
+#include "physics/ChPhysicsItem.h"
+#include "physics/ChBodyFrame.h"
+#include "physics/ChShaft.h"
+#include "lcp/ChLcpConstraintThreeGeneric.h"
+
+
+
+namespace chrono
+{
+
+// Forward references (for parent hierarchy pointer)
+
+class ChShaft;
+class ChBodyFrame;
+
+///  Class for defining a gearbox with 1D input and 1D output, but with
+///  different directions in 3D space. Basically it defines a 
+///  transmission ratio between two 1D entities of ChShaft type, and
+///  transmits the reaction of the gearbox to a 3D body that acts as the 
+///  support truss. 
+///  A typical example is the case of a gearbox with bevel gears,
+///  where input shaft and output shaft are at 90°.
+///  Note that the more basic ChShaftsGear can do the same, except
+///  that it does not provide a way to transmit reaction
+///  to a truss body.
+
+class ChApi ChShaftsGearboxAngled : public ChPhysicsItem {
+
+						// Chrono simulation of RTTI, needed for serialization
+	CH_RTTI(ChShaftsGearboxAngled,ChPhysicsItem);
+
+private:
+			//
+	  		// DATA
+			//
+
+	double t0;		
+
+	double torque_react;					
+	
+						// used as an interface to the LCP solver.
+	ChLcpConstraintThreeGeneric constraint;
+
+	float cache_li_speed;	// used to cache the last computed value of multiplier (solver warm starting)
+	float cache_li_pos;		// used to cache the last computed value of multiplier (solver warm starting)	
+
+	ChShaft* shaft1;
+	ChShaft* shaft2;
+	ChBodyFrame*  body;
+
+	ChVector<> shaft_dir1;
+	ChVector<> shaft_dir2;
+
+public:
+
+			//
+	  		// CONSTRUCTORS
+			//
+
+				/// Constructor.
+	ChShaftsGearboxAngled ();
+				/// Destructor
+	~ChShaftsGearboxAngled ();
+
+				/// Copy from another ChShaftsGearboxAngled. 
+	void Copy(ChShaftsGearboxAngled* source);
+
+
+			//
+	  		// FLAGS
+			//
+
+			//
+	  		// FUNCTIONS
+			//
+
+				/// Number of scalar costraints 
+	virtual int GetDOC_c  () {return 1;}
+
+
+			// Override/implement LCP system functions of ChPhysicsItem
+			// (to assembly/manage data for LCP system solver
+
+	virtual void InjectConstraints(ChLcpSystemDescriptor& mdescriptor);
+	virtual void ConstraintsBiReset();
+	virtual void ConstraintsBiLoad_C(double factor=1., double recovery_clamp=0.1, bool do_clamp=false);
+	virtual void ConstraintsBiLoad_Ct(double factor=1.);
+	//virtual void ConstraintsFbLoadForces(double factor=1.);
+	virtual void ConstraintsLoadJacobians();
+	virtual void ConstraintsLiLoadSuggestedSpeedSolution();
+	virtual void ConstraintsLiLoadSuggestedPositionSolution();
+	virtual void ConstraintsLiFetchSuggestedSpeedSolution();
+	virtual void ConstraintsLiFetchSuggestedPositionSolution();
+	virtual void ConstraintsFetch_react(double factor=1.);
+
+
+			   // Other functions
+
+				/// Use this function after gear creation, to initialize it, given  
+				/// two shafts to join, and the 3D body that acts as a truss and
+				/// receives the reaction torque of the gearbox.
+				/// Each shaft and body must belong to the same ChSystem.
+				/// Shafts directions are considered in local body coordinates.
+	virtual int Initialize(ChSharedPtr<ChShaft> mshaft1, ///< first (input) shaft to join
+						   ChSharedPtr<ChShaft> mshaft2, ///< second  (output) shaft to join
+						   ChSharedPtr<ChBodyFrame>  mbody,  ///< 3D body to use as truss (also carrier, if rotates as in planetary gearboxes) 
+						   ChVector<>& mdir1,			 ///< the direction of the first shaft on 3D body defining the gearbox truss
+						   ChVector<>& mdir2			 ///< the direction of the first shaft on 3D body defining the gearbox truss
+						   );
+
+				/// Get the first shaft (carrier wheel)
+	ChShaft* GetShaft1() {return shaft1;}
+				/// Get the second shaft 
+	ChShaft* GetShaft2() {return shaft2;}
+				/// Get the third shaft 
+	ChBodyFrame* GetBodyTruss() {return body;}
+
+
+				/// Set the transmission ratio t, as in w2=t*w1, or t=w2/w1 , or  t*w1 - w2 = 0.
+				/// For example, t=1 for equal bevel gears, etc.
+	void   SetTransmissionRatio(double mt0) { t0= mt0;}
+		
+				/// Get the transmission ratio t, as in w2=t*w1, or t=w2/w1.
+	double GetTransmissionRatio() { return t0;}
+
+
+				/// Set the direction of shaft 1 (input) respect to 3D body, as a 
+				/// normalized vector expressed in the coordinates of the body.
+				/// The shaft applies only torque, about this axis.
+	void  SetShaftDirection1(ChVector<> md) {shaft_dir1 = Vnorm(md);}
+
+				/// Set the direction of shaft 2 (output) respect to 3D body, as a 
+				/// normalized vector expressed in the coordinates of the body.
+				/// The shaft applies only torque, about this axis.
+	void  SetShaftDirection2(ChVector<> md) {shaft_dir2 = Vnorm(md);}
+
+				/// Get the direction of the shaft 1 (input) respect to 3D body, as a 
+				/// normalized vector expressed in the coordinates of the body. 
+	ChVector<>  GetShaftDirection1() {return shaft_dir1;}
+
+				/// Get the direction of the shaft 2 (input) respect to 3D body, as a 
+				/// normalized vector expressed in the coordinates of the body. 
+	ChVector<>  GetShaftDirection2() {return shaft_dir2;}
+
+
+
+				/// Get the reaction torque considered as applied to the 1st axis.
+	double GetTorqueReactionOn1() {return (this->t0*torque_react);}
+
+				/// Get the reaction torque considered as applied to the 2nd axis.
+	double GetTorqueReactionOn2() {return (-1.0*torque_react);}
+
+				/// Get the reaction torque considered as applied to the body
+				/// (the truss of the gearbox), expressed in local body coordinates.
+	ChVector<> GetTorqueReactionOnBody() {return (this->shaft_dir1*this->t0 - this->shaft_dir2)*torque_react;}
+
+			//
+			// UPDATE FUNCTIONS
+			//
+
+				/// Update all auxiliary data of the gear transmission at given time
+	virtual void Update (double mytime);
+	
+
+
+			//
+			// STREAMING
+			//
+
+
+				/// Method to allow deserializing a persistent binary archive (ex: a file)
+				/// into transient data.
+	void StreamIN(ChStreamInBinary& mstream);
+
+				/// Method to allow serializing transient data into a persistent
+				/// binary archive (ex: a file).
+	void StreamOUT(ChStreamOutBinary& mstream);
+
+};
+
+
+
+
+
+
+} // END_OF_NAMESPACE____
+
+
+#endif
