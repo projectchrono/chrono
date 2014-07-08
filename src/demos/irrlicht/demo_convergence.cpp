94b2c1a1a0dcf0a34cbc0a399fe6c195dcc665c1
Alessandro Tasora <tasora@ied.unipr.it>
2014-07-08 18:50:32 +0200

    demo_convergence.cpp largely refactored: - use the new 'asset' system approach to visualization, - use the new ChBodyEasyXXX stuff that automatically computes masses and inertias. The Irrlicht stuff is not intrusive anymore.

--- src/demos/irrlicht/demo_convergence.cpp
+++ src/demos/irrlicht/demo_convergence.cpp
@@ -30,13 +30,11 @@
  
 #include "physics/ChApidll.h" 
 #include "physics/ChSystem.h"
-#include "irrlicht_interface/ChBodySceneNode.h"
-#include "irrlicht_interface/ChBodySceneNodeTools.h" 
-#include "irrlicht_interface/ChIrrAppInterface.h"
+#include "physics/ChBodyEasy.h"
+#include "assets/ChTexture.h"
+#include "irrlicht_interface/ChIrrApp.h"
 #include "core/ChRealtimeStep.h"
 #include "lcp/ChLcpIterativeMINRES.h"
-
-#include <irrlicht.h>
  
 
 
@@ -45,8 +43,7 @@
 using namespace chrono;
 
 // Use the main namespaces of Irrlicht
-using namespace irr;
-         
+using namespace irr;    
 using namespace core;
 using namespace scene; 
 using namespace video;
@@ -55,7 +52,7 @@ using namespace gui;
 
 // Static data used for this simple demo
 
-std::vector<ChBodySceneNode*> mspheres;
+std::vector< ChSharedPtr<ChBody> > mspheres;
 
 double STATIC_COMPLIANCE = 0.1*   (10./1000.)/500; // as 1/K, in m/N. es: 10mm/500N
 
@@ -64,11 +61,7 @@ double STATIC_COMPLIANCE = 0.1*   (10./1000.)/500; // as 1/K, in m/N. es: 10mm/5
 
 void create_items(ChIrrAppInterface& application) 
 {
-	ChBodySceneNode* mrigidBody;
-
-	// Create some spheres in a vertical stack, and put them into 'parent' level
-
-	video::ITexture* sphereMap = application.GetVideoDriver()->getTexture("../data/bluwhite.png");
+	// Create some spheres in a vertical stack
 
 	bool do_wall = false;
 	bool do_stack = true;
@@ -97,34 +90,41 @@ void create_items(ChIrrAppInterface& application)
 			if (do_oddmass && bi==(nbodies-1))
 				sphrad = sphrad*pow(oddfactor, 1./3.);
 			double dens= 1000;
-			double sphmass = dens * (4./3.) * CH_C_PI * pow(sphrad,3);
-			double sphinertia = (2./5.) * sphmass * pow(sphrad,2);
+
+			ChSharedPtr<ChBody> mrigidBody;
 
 			if (do_spheres)
-				mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easySphere(
-												application.GetSystem(), application.GetSceneManager(),
-												sphmass,
-												ChVector<>(0.5, sphrad+level, 0.7),
-												sphrad);
-			else
-				mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-														application.GetSystem(), application.GetSceneManager(),
-														sphmass,
-														ChVector<>(0.5, sphrad+level, 0.7),
-														ChQuaternion<>(1,0,0,0), 
-														ChVector<>(sphrad,sphrad,sphrad) );
-	   
-			mrigidBody->GetBody()->SetInertiaXX(ChVector<>(sphinertia,sphinertia,sphinertia));
-			mrigidBody->GetBody()->SetFriction(0.5f); 
-			mrigidBody->GetBody()->SetImpactC(0.0f); 
-			mrigidBody->addShadowVolumeSceneNode();
+			{
+				mrigidBody = ChSharedPtr<ChBodyEasySphere>(new ChBodyEasySphere(
+											sphrad,		// radius
+											dens,		// density
+											true,		// collide enable?
+											true));		// visualization?
+				mrigidBody->SetPos(ChVector<>(0.5, sphrad+level, 0.7));
+				mrigidBody->AddAsset( ChSharedPtr<ChTexture>(new ChTexture("../data/bluwhite.png")) );
 
-			mrigidBody->setMaterialTexture(0,	sphereMap);
+				application.GetSystem()->Add(mrigidBody);
+			}
+			else
+			{
+				mrigidBody = ChSharedPtr<ChBodyEasyBox>(new ChBodyEasyBox(
+											sphrad,sphrad,sphrad, // x,y,z size
+											dens,		// density
+											true,		// collide enable?
+											true));		// visualization?
+				mrigidBody->SetPos(ChVector<>(0.5, sphrad+level, 0.7));
+				mrigidBody->AddAsset( ChSharedPtr<ChTexture>(new ChTexture("../data/cubetexture_bluwhite.png")) );
+
+				application.GetSystem()->Add(mrigidBody);
+			}
+
+			mrigidBody->SetFriction(0.5f); 
+			mrigidBody->SetImpactC(0.0f); 
 
 			mspheres.push_back(mrigidBody);
 
 			level   +=sphrad*2;
-			totmass +=sphmass;
+			totmass +=mrigidBody->GetMass();
 		}
 
 		GetLog() << "Expected contact force at bottom F=" << (totmass *application.GetSystem()->Get_G_acc().y)  << "\n";
@@ -137,14 +137,16 @@ void create_items(ChIrrAppInterface& application)
 			{ 
 				for (int ui = 0; ui < 15; ui++)  // N. of hor. bricks
 				{ 
-					mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-														application.GetSystem(), application.GetSceneManager(),
-														0.8,
-														ChVector<>(-8+ui*4.0+2*(bi%2),  1.0+bi*2.0, -5+ ai*6),
-														ChQuaternion<>(1,0,0,0), 
-														ChVector<>(3.96,2,4) );
-					mrigidBody->GetBody()->SetFriction(0.4f);
-					mrigidBody->setMaterialTexture(0,	sphereMap);
+					ChSharedPtr<ChBodyEasyBox> mrigidWall (new ChBodyEasyBox(
+											3.96,2,4,		// radius
+											dens,		// density
+											true,		// collide enable?
+											true));		// visualization?
+					mrigidWall->SetPos(ChVector<>(-8+ui*4.0+2*(bi%2),  1.0+bi*2.0, -5+ ai*6));
+					mrigidWall->SetFriction(0.4f);
+					mrigidWall->AddAsset( ChSharedPtr<ChTexture>(new ChTexture("../data/cubetexture_bluwhite.png")) );
+
+					application.GetSystem()->Add(mrigidWall);
 				}
 			}
 		}
@@ -153,61 +155,37 @@ void create_items(ChIrrAppInterface& application)
 	{
 		double sphrad = 0.2;
 		double dens= 1000;
-		double sphmass = dens * (4./3.) * CH_C_PI * pow(sphrad,3);
-		double sphinertia = (2./5.) * sphmass * pow(sphrad,2);
-
 		double hfactor = 100;
-		mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easySphere(
-												application.GetSystem(), application.GetSceneManager(),
-												sphmass*hfactor,
-												ChVector<>(0.5, sphrad+0.1, -1),
-												sphrad);
-		mrigidBody->GetBody()->SetInertiaXX(ChVector<>(sphinertia*hfactor,sphinertia*hfactor,sphinertia*hfactor));
-		mrigidBody->addShadowVolumeSceneNode();
-		mrigidBody->setMaterialTexture(0,	sphereMap);
+
+		ChSharedPtr<ChBodyEasySphere> mrigidHeavy(new ChBodyEasySphere(
+											sphrad,		 // radius
+											dens*hfactor,// density
+											true,		 // collide enable?
+											true));		 // visualization?
+		mrigidHeavy->SetPos(ChVector<>(0.5, sphrad+0.1, -1));
+		mrigidHeavy->AddAsset( ChSharedPtr<ChTexture>(new ChTexture("../data/pinkwhite.png")) );
+
+		application.GetSystem()->Add(mrigidHeavy);
 
 		GetLog() << "Expected contact deformation at side sphere=" << 
-			(sphmass*hfactor *application.GetSystem()->Get_G_acc().y)*STATIC_COMPLIANCE  << "\n";
+			(mrigidHeavy->GetMass() *application.GetSystem()->Get_G_acc().y)*STATIC_COMPLIANCE  << "\n";
 	}
 
 
 	// Create the floor using a fixed rigid body of 'box' type:
 
-	mrigidBody = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											application.GetSystem(), application.GetSceneManager(),
-											1.0,
-											ChVector<>(0,-2,0),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(50,4,50) );
-	mrigidBody->GetBody()->SetBodyFixed(true);
-	mrigidBody->GetBody()->SetFriction(0.6f);
+	ChSharedPtr<ChBodyEasyBox> mrigidFloor (new ChBodyEasyBox(
+											50,4,50,	// radius
+											dens,		// density
+											true,		// collide enable?
+											true));		// visualization?
+	mrigidFloor->SetPos( ChVector<>(0,-2,0) );
+	mrigidFloor->SetBodyFixed(true);
+	mrigidFloor->SetFriction(0.6f);
+	mrigidFloor->AddAsset( ChSharedPtr<ChTexture>(new ChTexture("../data/concrete.jpg")) );
 
-	video::ITexture* cubeMap = application.GetVideoDriver()->getTexture("../data/concrete.jpg");
-	mrigidBody->setMaterialTexture(0,	cubeMap);
+	application.GetSystem()->Add(mrigidFloor);
 
-/*
-	// Create rotating stuff
-	ChBodySceneNode* rotatingBody = (ChBodySceneNode*)addChBodySceneNode_easyBox(
-											application.GetSystem(), application.GetSceneManager(),
-											1.0,
-											ChVector<>(0,12,0),
-											ChQuaternion<>(1,0,0,0), 
-											ChVector<>(10,2.5,1) ); 
-	rotatingBody->GetBody()->SetMass(100);
-	rotatingBody->GetBody()->SetInertiaXX(ChVector<>(500,500,500));
-	rotatingBody->GetBody()->SetFriction(0.4f);
-	rotatingBody->addShadowVolumeSceneNode();
-
-	// .. an engine between mixer and truss	
-	ChSharedPtr<ChLinkEngine> my_motor(new ChLinkEngine);
-	my_motor->Initialize(rotatingBody->GetBody(), mrigidBody->GetBody(), 
-				ChCoordsys<>(ChVector<>(0,15,0),
-							 Q_from_AngAxis(CH_C_PI_2, VECT_X)) );
-	my_motor->Set_eng_mode(ChLinkEngine::ENG_MODE_SPEED);
-	if (ChFunction_Const* mfun = dynamic_cast<ChFunction_Const*>(my_motor->Get_spe_funct()))
-		mfun->Set_yconst(CH_C_PI/2.0); // speed w=90°/s
-	application.GetSystem()->AddLink(my_motor);
-*/
 
 } 
      
@@ -215,16 +193,18 @@ void create_items(ChIrrAppInterface& application)
 
 // Function that forces all spheres in the 'parent' level to be on the same vertical
 // axis, without needing any constraint (for simplifying the solver benchmark).
+// Also impose no rotation.
 
 void align_spheres(ChIrrAppInterface& application)
 {
-	for (int i = 0; i< mspheres.size(); ++i)
+	for (unsigned int i = 0; i< mspheres.size(); ++i)
 	{
-		ChBodySceneNode* body = mspheres[i];
-		ChVector<> mpos = body->GetBody()->GetPos();
+		ChSharedPtr<ChBody> body = mspheres[i];
+		ChVector<> mpos = body->GetPos();
 		mpos.x = 0.5;
 		mpos.z = 0.7;
-		body->GetBody()->SetPos(mpos);
+		body->SetPos(mpos);
+		body->SetRot(QUNIT);
 	}
 }
    
@@ -241,7 +221,7 @@ int main(int argc, char* argv[])
 
 	// Create the Irrlicht visualization (open the Irrlicht device, 
 	// bind a simple user interface, etc. etc.)
-	ChIrrAppInterface application(&mphysicalSystem, L"Critical cases for solver convergence",core::dimension2d<u32>(800,600),false,true);
+	ChIrrApp application(&mphysicalSystem, L"Critical cases for solver convergence",core::dimension2d<u32>(800,600),false,true);
 
 
 	// Easy shortcuts to add camera, lights, logo and sky in Irrlicht scene:
@@ -275,6 +255,12 @@ int main(int argc, char* argv[])
 	// Tell the system to use the callback above, per each created contact!
 	mphysicalSystem.SetCustomCollisionPointCallback(&mycontact_callback);
  
+
+	// Use this function for adding a ChIrrNodeAsset to all already created items (ex. a floor, a wall, etc.)
+	// Otherwise use application.AssetBind(myitem); on a per-item basis.
+	application.AssetBindAll();
+	application.AssetUpdateAll();
+
 	
 	// Modify some setting of the physical system for the simulation, if you want
 
