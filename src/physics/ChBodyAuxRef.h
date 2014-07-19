f722cd5039dbd0347852f1e2c25320658f8c9177
Radu Serban <serban@engr.wisc.edu>
2014-07-18 23:29:00 -0500

    Cleanup of ChBodyDEM and ChBodyAuxRef.

--- src/physics/ChBodyAuxRef.h
+++ src/physics/ChBodyAuxRef.h
@@ -12,30 +12,11 @@
 #ifndef CHBODYAUXREF_H
 #define CHBODYAUXREF_H
 
-//////////////////////////////////////////////////
-//
-//   ChBodyAuxRef.h
-//
-//   Class for rigid bodie with an auxiliary reference
-//   that is separated from the COG.
-//
-//   HEADER file for CHRONO,
-//	 Multibody dynamics engine
-//
-// ------------------------------------------------
-//             www.deltaknowledge.com
-// ------------------------------------------------
-///////////////////////////////////////////////////
-
 #include "physics/ChBody.h"
-
 
 
 namespace chrono
 {
-
-
-
 
 /// Class for rigid bodies with an auxiliary reference that can be used
 /// for the collision shapes and marker positions; that reference can be
@@ -46,83 +27,77 @@ namespace chrono
 
 class ChApi ChBodyAuxRef : public ChBody {
 
-						// Chrono simulation of RTTI, needed for serialization
-	CH_RTTI(ChBodyAuxRef,ChBody);
+            // Chrono simulation of RTTI, needed for serialization
+  CH_RTTI(ChBodyAuxRef,ChBody);
 
-	
+  
 private:
-			//
-	  		// DATA
-			//
-	ChFrameMoving<> auxref_to_cog;	// auxiliary REF location, relative to COG
-	ChFrameMoving<> auxref_to_abs;	// for speeding up code: REF location relative to abs coords (needs Update() )
+      //
+      // DATA
+      //
+  ChFrameMoving<> auxref_to_cog;  // auxiliary REF location, relative to COG
+  ChFrameMoving<> auxref_to_abs;  // for speeding up code: REF location relative to abs coords (needs Update() )
 
 public:
 
-			//
-	  		// CONSTRUCTORS
-			//
+      //
+      // CONSTRUCTORS
+      //
 
-				/// Build a rigid body.
-	ChBodyAuxRef ();
-				/// Destructor
-	~ChBodyAuxRef ();
+        /// Build a rigid body.
+  ChBodyAuxRef ();
+        /// Destructor
+  ~ChBodyAuxRef ();
 
-				/// Copy from another ChBodyAuxRef. 
-				/// NOTE: all settings of the body are copied, but the
-				/// child hierarchy of ChForces and ChMarkers (if any) are NOT copied.
-	void Copy(ChBodyAuxRef* source);
-
-
-				/// Get the location of the auxiliary reference respect to COG.
-				/// Viceversa, if you need to know the COG respect to auxiliary 
-				/// reference, use GetREF_to_COG().GetInverse() 
-	virtual const ChFrame<>& GetFrame_REF_to_COG() {return auxref_to_cog;};
-
-				/// Set the location of the auxiliary reference respect to COG,
-				/// and do not move the body absolute COG (the COG is fixed).
-	virtual void SetFrame_REF_to_COG(const ChFrame<>& mloc) {auxref_to_cog = mloc;};
-
-				/// Set the location of the COG respect to the auxiliary reference,
-				/// and move the body absolute COG (the REF is fixed).
-				/// Note! the position of contained ChMarker objects, if any, is 
-				/// not changed respect to the reference!
-	virtual void SetFrame_COG_to_REF(const ChFrame<>& mloc);
-
-				/// Set the absolute location of the auxiliary reference, 
-				/// moving the entire body. The body COG is rigidly moved as well.
-	virtual void SetFrame_REF_to_abs(const ChFrame<>& mfra);
-
-				/// Get the rigid body coordinate system that is used for
-				/// defining the collision shapes and the ChMarker objects, respect
-				/// to the absolute system.
-				/// In this ChBodyAuxRef class, differently form ChBody, this is
-				/// not necessarily the same reference of GetFrame_COG_to_abs().
-	virtual const ChFrame<>& GetFrame_REF_to_abs() const {return auxref_to_abs;}
-
-				/// Update all auxiliary data of the rigid body and of
-				/// its children (markers, forces..)
-	virtual void Update ();
+        /// Copy from another ChBodyAuxRef. 
+        /// NOTE: all settings of the body are copied, but the
+        /// child hierarchy of ChForces and ChMarkers (if any) are NOT copied.
+  void Copy(ChBodyAuxRef* source);
 
 
+        /// Get the location of the auxiliary reference respect to COG.
+        /// Viceversa, if you need to know the COG respect to auxiliary 
+        /// reference, use GetREF_to_COG().GetInverse() 
+  virtual const ChFrame<>& GetFrame_REF_to_COG() {return auxref_to_cog;};
 
-			//
-			// STREAMING
-			//
+        /// Set the location of the auxiliary reference respect to COG,
+        /// and do not move the body absolute COG (the COG is fixed).
+  virtual void SetFrame_REF_to_COG(const ChFrame<>& mloc) {auxref_to_cog = mloc;};
 
-				/// Method to allow deserializing a persistent binary archive (ex: a file)
-				/// into transient data.
-	void StreamIN(ChStreamInBinary& mstream);
+        /// Set the location of the COG respect to the auxiliary reference,
+        /// and move the body absolute COG (the REF is fixed).
+        /// Note! the position of contained ChMarker objects, if any, is 
+        /// not changed respect to the reference!
+  virtual void SetFrame_COG_to_REF(const ChFrame<>& mloc);
 
-				/// Method to allow serializing transient data into a persistent
-				/// binary archive (ex: a file).
-	void StreamOUT(ChStreamOutBinary& mstream);
+        /// Set the absolute location of the auxiliary reference, 
+        /// moving the entire body. The body COG is rigidly moved as well.
+  virtual void SetFrame_REF_to_abs(const ChFrame<>& mfra);
 
+        /// Get the rigid body coordinate system that is used for
+        /// defining the collision shapes and the ChMarker objects, respect
+        /// to the absolute system.
+        /// In this ChBodyAuxRef class, differently form ChBody, this is
+        /// not necessarily the same reference of GetFrame_COG_to_abs().
+  virtual const ChFrame<>& GetFrame_REF_to_abs() const {return auxref_to_abs;}
+
+        /// Update all auxiliary data of the rigid body and of
+        /// its children (markers, forces..)
+  virtual void Update();
+
+      //
+      // STREAMING
+      //
+
+        /// Method to allow deserializing a persistent binary archive (ex: a file)
+        /// into transient data.
+  void StreamIN(ChStreamInBinary& mstream);
+
+        /// Method to allow serializing transient data into a persistent
+        /// binary archive (ex: a file).
+  void StreamOUT(ChStreamOutBinary& mstream);
 
 };
-
-
-
 
 
 } // END_OF_NAMESPACE____
--- src/physics/ChBodyDEM.h
+++ src/physics/ChBodyDEM.h
@@ -13,102 +13,72 @@
 #ifndef CHBODYDEM_H
 #define CHBODYDEM_H
 
-//////////////////////////////////////////////////
-//
-//   ChBodyDEM.h
-//
-//   Class for rigid bodies, that is rigid moving
-//   parts with mass and collision geometry 
-//   which interact through DEM approach.
-//
-//   HEADER file for CHRONO,
-//   Multibody dynamics engine
-//
-// ------------------------------------------------
-//             www.projectchrono.org
-// ------------------------------------------------
-///////////////////////////////////////////////////
-
-
 #include "physics/ChBody.h"
 #include "physics/ChPhysicsItem.h"
 #include "physics/ChMaterialSurfaceDEM.h"
 
-
-
 namespace chrono
 {
 
-using namespace collision;
-
-
-
-///
-/// Class for rigid bodies. A rigid body is an entity which
-/// can move in 3D space, and can be constrained to other rigid
-/// bodies using ChLink objects.
-/// These objects have mass and inertia properties. A shape can also
-/// be associated to the body, for collision detection.
-///
+/// Class for rigid bodies for DEM-based contact.
+/// A rigid body is an entity which can move in 3D space, and can be constrained
+/// to other rigid bodies using ChLink objects.
+/// These objects have mass and inertia properties. A shape can also be
+/// associated to the body, for collision detection.
 
 class ChApi ChBodyDEM: public ChBody {
 
-	// Chrono simulation of RTTI, needed for serialization
-	CH_RTTI(ChBodyDEM, ChBody);
+  // Chrono simulation of RTTI, needed for serialization
+  CH_RTTI(ChBodyDEM, ChBody);
 
 
 private:
-			//
-			// DATA
-			//
+      //
+      // DATA
+      //
 
-	// data for surface contact and impact (can be shared):
-	ChSharedPtr<ChMaterialSurfaceDEM> matsurfaceDEM;
+  // data for surface contact and impact (can be shared):
+  ChSharedPtr<ChMaterialSurfaceDEM> matsurfaceDEM;
 
 public:
 
-			//
-			// CONSTRUCTORS
-			//
+      //
+      // CONSTRUCTORS
+      //
 
-	ChBodyDEM();
-	ChBodyDEM(ChCollisionModel* new_collision_model);
-	~ChBodyDEM() {}
+  ChBodyDEM();
+  ChBodyDEM(ChCollisionModel* new_collision_model);
+  ~ChBodyDEM() {}
 
-	/// Copy from another body
-	void Copy(ChBodyDEM* source);
+  /// Copy from another body
+  void Copy(ChBodyDEM* source);
 
-			//
-			// FUNCTIONS
-			//
+      //
+      // FUNCTIONS
+      //
 
-	const ChSharedPtr<ChMaterialSurfaceDEM>& GetMaterialSurfaceDEM() const {return matsurfaceDEM;}
-	ChSharedPtr<ChMaterialSurfaceDEM>& GetMaterialSurfaceDEM() {return matsurfaceDEM;}
-	void SetMaterialSurfaceDEM(ChSharedPtr<ChMaterialSurfaceDEM>& mnewsurf) {matsurfaceDEM = mnewsurf;}
+  const ChSharedPtr<ChMaterialSurfaceDEM>& GetMaterialSurfaceDEM() const {return matsurfaceDEM;}
+  ChSharedPtr<ChMaterialSurfaceDEM>& GetMaterialSurfaceDEM() {return matsurfaceDEM;}
+  void SetMaterialSurfaceDEM(const ChSharedPtr<ChMaterialSurfaceDEM>& mnewsurf) {matsurfaceDEM = mnewsurf;}
 
-	/// accumulate force on this body, or reset the force to zero
-	//void AccumulateForce(ChVector<> ff) {Xforce+=ff;}
-	//void ResetBodyForce() {Xforce = VNULL;}
-	//void AccumulateTorque(ChVector<> tt) {Xtorque+=tt;}
-	//void ResetBodyTorque() {Xtorque = VNULL;}
 
-			//
-			// STREAMING
-			//
+      //
+      // STREAMING
+      //
 
-	/// Method to allow deserializing a persistent binary archive (ex: a file)
-	/// into transient data.
-	void StreamIN(ChStreamInBinary& mstream);
+  /// Method to allow deserializing a persistent binary archive (ex: a file)
+  /// into transient data.
+  void StreamIN(ChStreamInBinary& mstream);
 
-	/// Method to allow serializing transient data into a persistent
-	/// binary archive (ex: a file).
-	void StreamOUT(ChStreamOutBinary& mstream);
+  /// Method to allow serializing transient data into a persistent
+  /// binary archive (ex: a file).
+  void StreamOUT(ChStreamOutBinary& mstream);
 
-	/// Save data, including child markers and child forces
-	int StreamOUTall(ChStreamOutBinary& m_file);
+  /// Save data, including child markers and child forces
+  int StreamOUTall(ChStreamOutBinary& m_file);
 
-	/// Read data, including child markers and child forces
-	int StreamINall(ChStreamInBinary&  m_file);
+  /// Read data, including child markers and child forces
+  int StreamINall(ChStreamInBinary&  m_file);
 
 };
 
