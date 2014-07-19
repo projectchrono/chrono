7f4fb33ee30e0d64d08e59aadbe64c4147f174a8
Radu Serban <serban@engr.wisc.edu>
2014-07-19 18:09:39 -0500

    Cleanup `ChMaterial.h` and `ChVsualization.h`
    - remove using directives
    - use cont references where appropriate
    - reformatting (untabify)

--- src/assets/ChMaterial.h
+++ src/assets/ChMaterial.h
@@ -3,12 +3,12 @@
 
 ///////////////////////////////////////////////////
 //
-//   ChColor.h
+//   ChMaterial.h
 //
 //   Class for storing a color as an object asset
 //
 //   HEADER file for CHRONO,
-//	 Multibody dynamics engine
+//   Multibody dynamics engine
 //
 ///////////////////////////////////////////////////
 
@@ -17,71 +17,74 @@
 #include <string>
 #include <vector>
 
-using namespace std;
 namespace chrono {
+
 enum ChMaterialType {
-	CH_MATERIAL_DIFFUSE, CH_MATERIAL_PHONG, CH_MATERIAL_CONDUCTOR, CH_MATERIAL_PLASTIC
+  CH_MATERIAL_DIFFUSE,
+  CH_MATERIAL_PHONG,
+  CH_MATERIAL_CONDUCTOR,
+  CH_MATERIAL_PLASTIC
 };
-/*
- #define CH_MATERIAL_DIFFUSE=0;
- #define CH_MATERIAL_PHONG=1;
- #define CH_MATERIAL_CONDUCTOR=2;
- #define CH_MATERIAL_PLASTIC=3;
- */
+
 struct material_option {
-	string type, parameter, value;
+  std::string type, parameter, value;
 };
 
 class ChApi ChMaterial {
 public:
 
-	ChMaterial();
-	~ChMaterial();
+  ChMaterial();
+  ~ChMaterial();
 
-	bool IsVisible() const {
-		return visible;
-	}
-	void SetVisible(bool mv) {
-		visible = mv;
-	}
+  bool IsVisible() const   {return visible;}
+  void SetVisible(bool mv) {visible = mv;}
 
-// Get the color of the surface. This information could be used by visualization postprocessing.
-	ChColor GetColor() const {
-		return color;
-	}
-// Set the color of the surface. This information could be used by visualization postprocessing.
-	void SetColor(const ChColor& mc) {
-		color = mc;
-	}
+  // Get the color of the surface.
+  // This information could be used by visualization postprocessing.
+  const ChColor& GetColor() const {
+    return color;
+  }
 
-// Get the fading amount, 0..1.
-// If =0, no transparency of surface, it =1 surface is completely transparent.
-	float GetFading() const {
-		return fading;
-	}
-// Set the fading amount, 0..1.
-// If =0, no transparency of surface, it =1 surface is completely transparent.
-	void SetFading(const float mc) {
-		fading = mc;
-	}
+  // Set the color of the surface.
+  // This information could be used by visualization postprocessing.
+  void SetColor(const ChColor& mc) {
+    color = mc;
+  }
 
-	void SetType(const ChMaterialType type) {
-		material_type = type;
-	}
+  // Get the fading amount, 0..1.
+  // If =0, no transparency of surface, it =1 surface is completely transparent.
+  float GetFading() const {
+    return fading;
+  }
 
-	void SetOption(string type, string parameter, string value) {
-		material_option temp;
-		temp.type = type;
-		temp.parameter = parameter;
-		temp.value = value;
-		options.push_back(temp);
-	}
-	ChColor color; //color of material
-	float fading; //transparency of material
-	ChMaterialType material_type;
-	vector<material_option> options;
-	bool visible;
+  // Set the fading amount, 0..1.
+  // If =0, no transparency of surface, it =1 surface is completely transparent.
+  void SetFading(const float mc) {
+    fading = mc;
+  }
+
+  void SetType(const ChMaterialType type) {
+    material_type = type;
+  }
+
+  void SetOption(const std::string& type,
+                 const std::string& parameter,
+                 const std::string& value) {
+    material_option temp;
+    temp.type = type;
+    temp.parameter = parameter;
+    temp.value = value;
+    options.push_back(temp);
+  }
+
+  ChColor color; //color of material
+  float fading; //transparency of material
+  ChMaterialType material_type;
+  std::vector<material_option> options;
+  bool visible;
 
 };
+
 }
+
 #endif
--- src/assets/ChVisualization.h
+++ src/assets/ChVisualization.h
@@ -20,18 +20,14 @@
 //   visualization (rendering, post processing, etc.)
 //
 //   HEADER file for CHRONO,
-//	 Multibody dynamics engine
+//   Multibody dynamics engine
 //
-// ------------------------------------------------
-//             www.deltaknowledge.com
-// ------------------------------------------------
 ///////////////////////////////////////////////////
 
 
 #include "assets/ChAsset.h"
 #include "assets/ChColor.h"
 #include "core/ChMath.h"
-using namespace chrono;
 
 namespace chrono
 {
@@ -42,55 +38,48 @@ namespace chrono
 class ChApi ChVisualization : public ChAsset {
 
 protected:
-				//
-	  			// DATA
-				//
-	bool	visible;
-	ChColor color;	
-	float	fading;
+        //
+        // DATA
+        //
+  bool    visible;
+  ChColor color;
+  float   fading;
 
 public:
-				//
-	  			// CONSTRUCTORS
-				//
+        //
+        // CONSTRUCTORS
+        //
 
-	ChVisualization () : visible(true), color(1,1,1,0), fading(0) {};
+  ChVisualization () : visible(true), color(1,1,1,0), fading(0) {}
 
-	virtual ~ChVisualization () {};
+  virtual ~ChVisualization () {};
 
-				//
-	  			// FUNCTIONS
-				//
+        //
+        // FUNCTIONS
+        //
 
-	bool IsVisible() const {return visible;}
-	void SetVisible(bool mv) {visible = mv;}
+  bool IsVisible() const {return visible;}
+  void SetVisible(bool mv) {visible = mv;}
 
-		// Get the color of the surface. This information could be used by visualization postprocessing. 
-	ChColor GetColor() const {return color;}
-		// Set the color of the surface. This information could be used by visualization postprocessing.
-	void SetColor(const ChColor& mc) {color = mc;}
+    // Get the color of the surface. This information could be used by visualization postprocessing. 
+  ChColor GetColor() const {return color;}
+    // Set the color of the surface. This information could be used by visualization postprocessing.
+  void SetColor(const ChColor& mc) {color = mc;}
 
-		// Get the fading amount, 0..1. 
-		// If =0, no transparency of surface, it =1 surface is completely transparent.
-	float GetFading() const {return fading;}
-		// Set the fading amount, 0..1. 
-		// If =0, no transparency of surface, it =1 surface is completely transparent.
-	void SetFading(const float mc) {fading = mc;}
+    // Get the fading amount, 0..1. 
+    // If =0, no transparency of surface, it =1 surface is completely transparent.
+  float GetFading() const {return fading;}
+    // Set the fading amount, 0..1. 
+    // If =0, no transparency of surface, it =1 surface is completely transparent.
+  void SetFading(const float mc) {fading = mc;}
 
-				//
-		  		// DATA
-				//
-	/// Rotation of Asset
-	ChMatrix33<> Rot;
-	/// Position of Asset
-	ChVector<> Pos;
+        //
+        // DATA
+        //
+  ChMatrix33<> Rot;     /// Rotation of Asset
+  ChVector<> Pos;       /// Position of Asset
 };
 
-
-
-
-//////////////////////////////////////////////////////
-//////////////////////////////////////////////////////
 
 
 } // END_OF_NAMESPACE____
