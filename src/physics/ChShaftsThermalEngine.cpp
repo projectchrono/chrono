479f175d4b854de549be86249d1fcfaf7ec307dd
Alessandro Tasora <tasora@ied.unipr.it>
2014-07-26 12:14:07 +0200

    New "thermal engine" class, defining a torque curve and a throttle modulation. The computed torque is applied to shaft 1, say the crankshaft, and the negative torque is applied to shaft 2, say the motor block, that gets the reaction. Note that the throttle model is simplified, it simply scales the value of the torque curve linearly, as Tfinal = T(w)*throttle In future we may add a 2D map as Tfinal=T(w,throttle).

---
+++ src/physics/ChShaftsThermalEngine.cpp
@@ -0,0 +1,127 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2010 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+
+
+#include "physics/ChShaftsThermalEngine.h"
+#include "physics/ChSystem.h"
+#include "physics/ChShaft.h"
+
+#include "core/ChMemory.h" // must be last include (memory leak debugger). In .cpp only.
+
+namespace chrono
+{
+
+
+// Register into the object factory, to enable run-time
+// dynamic creation and persistence
+ChClassRegister<ChShaftsThermalEngine> a_registration_ChShaftsThermalEngine;
+
+
+
+//////////////////////////////////////
+//////////////////////////////////////
+
+
+
+ChShaftsThermalEngine::ChShaftsThermalEngine ()
+{
+	this->throttle = 1;
+	this->error_backward = false;
+
+	// default torque curve= constant zero. User will provide better fx.
+	this->Tw = ChSharedPtr<ChFunction> (new ChFunction_Const(0));
+
+	SetIdentifier(CHGLOBALS().GetUniqueIntID()); // mark with unique ID
+}
+
+
+ChShaftsThermalEngine::~ChShaftsThermalEngine ()
+{
+	
+}
+
+void ChShaftsThermalEngine::Copy(ChShaftsThermalEngine* source)
+{
+		// copy the parent class data...
+	ChShaftsTorqueBase::Copy(source);
+
+		// copy class data
+	throttle = source->throttle;
+	error_backward = source->error_backward;
+	this->Tw = ChSharedPtr<ChFunction>(Tw->new_Duplicate()); // deep copy
+}
+
+
+
+double ChShaftsThermalEngine::ComputeTorque()
+{
+		// COMPUTE THE TORQUE HERE!
+	double mw = this->GetRelativeRotation_dt();
+
+	if (mw <0) 
+		this->error_backward = true;
+	else
+		this->error_backward = false;
+
+	// get the actual torque from torque curve
+	double mT = Tw->Get_y( mw );		
+
+	// modulate it with throttle
+	double modulated_T = mT * this->throttle; 
+
+	return modulated_T;
+}
+
+
+
+//////// FILE I/O
+
+void ChShaftsThermalEngine::StreamOUT(ChStreamOutBinary& mstream)
+{
+			// class version number
+	mstream.VersionWrite(1);
+
+		// serialize parent class too
+	ChShaftsTorqueBase::StreamOUT(mstream);
+
+		// stream out all member data
+	mstream << this->throttle;
+	mstream << this->error_backward;
+	mstream.AbstractWrite(this->Tw.get_ptr()); //***TODO*** proper serialize for ChSharedPtr
+}
+
+void ChShaftsThermalEngine::StreamIN(ChStreamInBinary& mstream)
+{
+		// class version number
+	int version = mstream.VersionRead();
+
+		// deserialize parent class too
+	ChShaftsTorqueBase::StreamIN(mstream);
+
+		// deserialize class data
+	mstream >> this->throttle;
+	mstream >> this->error_backward;
+
+	ChFunction* newfun;
+	mstream.AbstractReadCreate(&newfun); //***TODO*** proper deserialize for ChSharedPtr
+	this->Tw = ChSharedPtr<ChFunction>(newfun);
+}
+
+
+
+
+
+
+} // END_OF_NAMESPACE____
+
+
+/////////////////////
---
+++ src/physics/ChShaftsThermalEngine.h
@@ -0,0 +1,117 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2010 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+#ifndef CHSHAFTHERMALENGINE_H
+#define CHSHAFTHERMALENGINE_H
+
+
+#include "physics/ChShaftsTorqueBase.h"
+#include "physics/ChFunction.h"
+
+
+namespace chrono
+{
+
+
+///  Class for defining a thermal engine 
+///  between two one-degree-of-freedom parts, that is,
+///  shafts that can be used to build 1D models
+///  of power trains. 
+///  The first shaft is the 'crankshaft' to whom the 
+///  torque is applied, the second is the botor block,
+///  that receives the negative torque.
+
+class ChApi ChShaftsThermalEngine : public ChShaftsTorqueBase {
+
+						// Chrono simulation of RTTI, needed for serialization
+	CH_RTTI(ChShaftsThermalEngine,ChShaftsTorqueBase);
+
+private:
+			//
+	  		// DATA
+			//
+
+	ChSharedPtr<ChFunction> Tw; // torque as function of angular vel.		
+	double throttle;
+
+	bool error_backward;
+
+public:
+
+			//
+	  		// CONSTRUCTORS
+			//
+
+				/// Constructor.
+	ChShaftsThermalEngine ();
+				/// Destructor
+	~ChShaftsThermalEngine ();
+
+				/// Copy from another ChShaftsThermalEngine. 
+	void Copy(ChShaftsThermalEngine* source);
+
+
+			//
+	  		// FUNCTIONS
+			//
+
+				/// Set the torque curve T(w), function of angular speed between shaft1 and shaft2.
+				/// Output units: [Nm]  , input units: [rad/s]
+	void SetTorqueCurve( ChSharedPtr<ChFunction> mf) { Tw = mf;}
+				/// Get the torque curve T(w).
+	ChSharedPtr<ChFunction> GetTorqueCurve() {return Tw;}
+
+				/// Set the current throttle value 's' in [0,1] range. If s=1,
+				/// the torque is exactly T=T(w), otherwise it is linearly
+				/// scaled as T=T(w)*s.
+				/// This is a simplified model of real torque modulation, but
+				/// nough for a basic model. An advanced approach would require a 2D map T(w,s)
+	void SetThrottle( double mt) { this->throttle = mt;}
+				/// Get the current throttle value
+	double GetThrottle() {return this->throttle;}
+
+				/// Tell if the engine is rotating backward.
+				/// If so, it's an erroneous situation
+	bool IsRotatingBackward() {return this->error_backward;}
+
+
+			//
+			// UPDATE FUNCTIONS
+			//
+
+				/// This is the function that actually contains the 
+				/// formula for computing T=T(w,throttle)
+	virtual double ComputeTorque();
+
+
+			//
+			// STREAMING
+			//
+
+
+				/// Method to allow deserializing a persistent binary archive (ex: a file)
+				/// into transient data.
+	void StreamIN(ChStreamInBinary& mstream);
+
+				/// Method to allow serializing transient data into a persistent
+				/// binary archive (ex: a file).
+	void StreamOUT(ChStreamOutBinary& mstream);
+
+};
+
+
+
+
+
+} // END_OF_NAMESPACE____
+
+
+#endif
