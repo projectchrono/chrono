724d80fbb904fbf87a477f99a1ffc163b49a6b58
Radu Serban <serban@engr.wisc.edu>
2014-07-28 16:58:46 -0500

    Move implementation of ChIrrApp methods to cpp file.

--- src/unit_IRRLICHT/ChIrrApp.cpp
+++ src/unit_IRRLICHT/ChIrrApp.cpp
@@ -10,14 +10,80 @@
 //
 
 
-#include "ChIrrApp.h"
+#include "unit_IRRLICHT/ChIrrApp.h"
 
 
 namespace irr
 {
 
+ChIrrApp::ChIrrApp(chrono::ChSystem*      psystem,
+                   const wchar_t*         title,
+                   core::dimension2d<u32> dimens,
+                   bool                   do_fullscreen,
+                   bool                   do_shadows,
+                   video::E_DRIVER_TYPE   mydriver)
+: ChIrrAppInterface(psystem,title,dimens,do_fullscreen,do_shadows,mydriver)
+{
+  mconverter = new scene::ChIrrAssetConverter(*this);
+}
 
+ChIrrApp::~ChIrrApp()
+{
+  if (mconverter)
+    delete mconverter;
+}
 
+void ChIrrApp::AssetBind(chrono::ChSharedPtr<chrono::ChPhysicsItem> mitem)
+{
+  GetAssetConverter()->Bind(mitem);
+}
+
+void ChIrrApp::AssetBindAll()
+{
+  GetAssetConverter()->BindAll();
+}
+
+void ChIrrApp::AssetUpdate(chrono::ChSharedPtr<chrono::ChPhysicsItem> mitem)
+{
+  GetAssetConverter()->Update(mitem);
+}
+
+void ChIrrApp::AssetUpdateAll()
+{
+  GetAssetConverter()->UpdateAll();
+}
+
+void ChIrrApp::AddShadow(chrono::ChSharedPtr<chrono::ChPhysicsItem> mitem)
+{
+  chrono::ChSharedPtr<chrono::ChIrrNodeAsset> myirrasset;
+  myirrasset = GetAssetConverter()->GetIrrNodeAsset(mitem);
+  if (!myirrasset.IsNull())
+  {
+    _recurse_add_shadow(myirrasset->GetIrrlichtNode());
+  }
+}
+
+void ChIrrApp::AddShadowAll()
+{
+  chrono::ChSystem::IteratorPhysicsItems miter = this->GetSystem()->IterBeginPhysicsItems();
+  while(miter.HasItem())
+  {
+    AddShadow(*miter);
+    ++miter;
+  }
+}
+
+void ChIrrApp::_recurse_add_shadow(scene::ISceneNode* mnode)
+{
+  scene::ISceneNodeList::ConstIterator it = mnode->getChildren().begin();
+  for (; it != mnode->getChildren().end(); ++it)
+  {
+    _recurse_add_shadow((*it));
+  }
+  // add shadow only to leaves
+  if (mnode->getChildren().getSize()==0)
+    GetEffects()->addShadowToNode(mnode);
+}
 
 
 } // END_OF_NAMESPACE____
