94347ca48308875950a1171b38018123f87aa255
Radu Serban <serban@engr.wisc.edu>
2014-07-29 17:21:03 -0500

    Make ChVisualization constructor protected.
    - ChVisualization objects should only be constructed through derived classes.
    - Cleanup.

--- src/assets/ChVisualization.h
+++ src/assets/ChVisualization.h
@@ -12,76 +12,55 @@
 #ifndef CHVISUALIZATION_H
 #define CHVISUALIZATION_H
 
-///////////////////////////////////////////////////
-//
-//   ChVisualization.h
-//
-//   Base class for assets that define something about
-//   visualization (rendering, post processing, etc.)
-//
-//   HEADER file for CHRONO,
-//   Multibody dynamics engine
-//
-///////////////////////////////////////////////////
-
 
 #include "assets/ChAsset.h"
 #include "assets/ChColor.h"
 #include "core/ChMath.h"
 
-namespace chrono
-{
-/// Base class for assets that define something about
-/// visualization (rendering, post processing, etc.)
-/// It contains basic information about color and visibility.
+
+namespace chrono {
+
+/// Base class for assets that define something about visualization (rendering,
+/// post processing, etc.)
+/// It contains basic information about position, color, and visibility.
 
 class ChApi ChVisualization : public ChAsset {
 
+public:
+  virtual ~ChVisualization() {}
+
+    /// Get/Set visible status flag.
+  bool IsVisible() const {return visible;}
+  void SetVisible(bool mv) {visible = mv;}
+
+    /// Get/Set the color of the surface. This information could be used by
+    /// visualization postprocessing.
+  const ChColor& GetColor() const {return color;}
+  void SetColor(const ChColor& mc) {color = mc;}
+
+    /// Get/Set the fading amount, 0 <= fading <= 1.
+    /// If fading = 0, no transparency of surface,
+    /// If fading = 1, surface is completely transparent.
+  float GetFading() const {return fading;}
+  void SetFading(const float mc) {fading = mc;}
+
+    // DATA
+  ChVector<>   Pos;     /// Position of Asset
+  ChMatrix33<> Rot;     /// Rotation of Asset
+
 protected:
-        //
-        // DATA
-        //
+    /// Constructor.
+    /// Protected because ChVisualization should not be constructed directly.
+  ChVisualization() : visible(true), fading(0) {}
+
   bool    visible;
   ChColor color;
   float   fading;
 
-public:
-        //
-        // CONSTRUCTORS
-        //
-
-  ChVisualization () : visible(true), color(1,1,1,0), fading(0) {}
-
-  virtual ~ChVisualization () {};
-
-        //
-        // FUNCTIONS
-        //
-
-  bool IsVisible() const {return visible;}
-  void SetVisible(bool mv) {visible = mv;}
-
-    // Get the color of the surface. This information could be used by visualization postprocessing. 
-  ChColor GetColor() const {return color;}
-    // Set the color of the surface. This information could be used by visualization postprocessing.
-  void SetColor(const ChColor& mc) {color = mc;}
-
-    // Get the fading amount, 0..1. 
-    // If =0, no transparency of surface, it =1 surface is completely transparent.
-  float GetFading() const {return fading;}
-    // Set the fading amount, 0..1. 
-    // If =0, no transparency of surface, it =1 surface is completely transparent.
-  void SetFading(const float mc) {fading = mc;}
-
-        //
-        // DATA
-        //
-  ChMatrix33<> Rot;     /// Rotation of Asset
-  ChVector<> Pos;       /// Position of Asset
 };
 
 
-
 } // END_OF_NAMESPACE____
+
 
 #endif
