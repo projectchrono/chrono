847b1bea868bc10d7afcc67691a2d72c65f15c6a
Radu Serban <serban@engr.wisc.edu>
2014-07-29 17:19:46 -0500

    Cleanup.

--- src/assets/ChColorAsset.h
+++ src/assets/ChColorAsset.h
@@ -15,63 +15,40 @@
 
 #include "assets/ChAsset.h"
 #include "assets/ChColor.h"
-#include <string>
-#include <vector>
 
 
 namespace chrono {
 
-
-
-
-/// Base class for assets that carry basic informations about
-/// the surface color for visualization assets.
-/// Here we assume that a ChColorAsset 'paints'
-/// all the assets of the list (or in same ChAssetLevel).
-/// It can be used when exporting to postprocessing or
-/// in OpenGL views. The postprocessor must be able to
-/// recognize it and implement the proper translation.
+/// Base class for assets that carry basic informations about the surface color
+/// for visualization assets. Here we assume that a ChColorAsset 'paints' all
+/// the assets of the list (or in same ChAssetLevel). It can be used when
+/// exporting to postprocessing or in OpenGL views. The postprocessor must be
+/// able to recognize it and implement the proper translation.
 
 class ChApi ChColorAsset : public ChAsset {
 
 public:
+  ChColorAsset() : fading(0) {}
+  ChColorAsset(float mR, float mG, float mB, float mA = 0) : color(mR, mG, mB, mA), fading(0) {}
 
-	ChColorAsset() 
-	{
-		fading = 0;
-	};
+    /// Get/Set the color of the surface. This information could be used by
+    /// visualization postprocessing.
+  const ChColor& GetColor() const {return color;}
+  void SetColor(const ChColor& mc) {color = mc;}
 
-	ChColorAsset(float mR, float mG, float mB, float mA = 0)
-	{
-		color = ChColor(mR, mG, mB, mA);
-	}
+    /// Get/Set the fading amount, 0 <= fading <= 1.
+    /// If fading = 0, no transparency of surface,
+    /// If fading = 1, surface is completely transparent.
+  float GetFading() const {return fading;}
+  void SetFading(const float mc) {fading = mc;}
 
-	~ChColorAsset() {};
-
-		/// Get the color of the surface. This information could be used by visualization postprocessing.
-	ChColor GetColor() const {
-		return color;
-	}
-		/// Set the color of the surface. This information could be used by visualization postprocessing.
-	void SetColor(const ChColor& mc) {
-		color = mc;
-	}
-
-		/// Get the fading amount, 0..1.
-		/// If =0, no transparency of surface, it =1 surface is completely transparent.
-	float GetFading() const {
-		return fading;
-	}
-		/// Set the fading amount, 0..1.
-		/// If =0, no transparency of surface, it =1 surface is completely transparent.
-	void SetFading(const float mc) {
-		fading = mc;
-	}
-
-	ChColor color; //color of material
-	float fading; //transparency of material
-
+private:
+  ChColor color;  //color of material
+  float fading;   //transparency of material
 };
 
-}
+
+} // END_OF_NAMESPACE____
+
+
 #endif
