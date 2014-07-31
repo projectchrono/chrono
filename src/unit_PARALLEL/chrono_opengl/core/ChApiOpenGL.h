7f5daf6079e3105476b19785a1342599754df66d
Radu Serban <serban@engr.wisc.edu>
2014-07-31 12:59:50 -0500

    Minor cleanup of variable names in CMake files.

--- src/unit_PARALLEL/chrono_opengl/core/ChApiOpenGL.h
+++ src/unit_PARALLEL/chrono_opengl/core/ChApiOpenGL.h
@@ -23,7 +23,7 @@
 // marked as exported). Otherwise, just do not define it if you
 // link the library to your code, and the symbols will be imported.
 
-#if defined(CH_API_COMPILE_UTILS_OPENGL)
+#if defined(CH_API_COMPILE_OPENGL)
 	#define CH_UTILS_OPENGL_API ChApiEXPORT
 #else
 	#define CH_UTILS_OPENGL_API ChApiINPORT
