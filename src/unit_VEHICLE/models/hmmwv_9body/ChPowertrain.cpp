461a780a944938491e5e854576dee39ffc5814fa
Radu Serban <serban@engr.wisc.edu>
2014-08-03 12:43:58 -0500

    Add base class ChPowertrain.

---
+++ src/unit_VEHICLE/models/hmmwv_9body/ChPowertrain.cpp
@@ -0,0 +1,33 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Justin Madsen
+// =============================================================================
+//
+// Base class for a vehicle powertrain.
+//
+// =============================================================================
+
+#include "ChPowertrain.h"
+
+
+namespace chrono {
+
+
+ChPowertrain::ChPowertrain(ChVehicle*   car,
+                           ChDriveType  type)
+: m_car(car),
+  m_type(type)
+{
+}
+
+
+}  // end namespace chrono
---
+++ src/unit_VEHICLE/models/hmmwv_9body/ChPowertrain.h
@@ -0,0 +1,56 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Justin Madsen
+// =============================================================================
+//
+// Base class for a vehicle powertrain.
+//
+// =============================================================================
+
+#ifndef CH_POWERTRAIN_H
+#define CH_POWERTRAIN_H
+
+#include "core/ChShared.h"
+#include "core/ChVector.h"
+#include "physics/ChBody.h"
+
+#include "ChVehicle.h"
+
+namespace chrono {
+
+enum ChDriveType {
+  FWD,
+  RWD,
+  AWD
+};
+
+class ChPowertrain : public ChShared {
+public:
+
+  ChPowertrain(ChVehicle* car, ChDriveType type);
+
+  virtual ~ChPowertrain() {}
+
+  virtual double GetWheelTorque(ChWheelId which) const = 0;
+
+  virtual void Update(double time, double throttle) = 0;
+
+protected:
+  ChVehicle*    m_car;
+  ChDriveType   m_type;
+};
+
+
+} // end namespace chrono
+
+
+#endif
