8315644e65ff5a54f369572b64b29681e8225ea0
Hammad Mazhar <hammad.maz@gmail.com>
2014-08-05 17:25:04 -0500

    update cmake install paths so that includes are put into a separate directory called "chrono" cleanup cmake files for different units units write include files to proper locations cleanup testing framework, still a lot of work to be done for this...

---
+++ src/unit_TESTING/ChTestConfig.h
@@ -0,0 +1,28 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2010 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+//////////////////////////////////////////////////
+//   
+//   ChTestConfig.h
+//   
+//   File to configure tests, the number of times
+//   they are run for timing etc
+//
+//   HEADER file for CHRONO,
+//	 Multibody dynamics engine
+//
+// ------------------------------------------------
+//             www.deltaknowledge.com
+// ------------------------------------------------
+///////////////////////////////////////////////////
+
+#define LOOPCOUNT 1000
+#define CYCLES 1000
---
+++ src/unit_TESTING/core/test_ChVector.cpp
@@ -0,0 +1,12 @@
+
+#include "../ChTestConfig.h"
+#include <core/ChVector.h>
+
+using namespace chrono;
+
+
+int main(){
+///TODO ADD TEST
+
+	return 0;
+}
