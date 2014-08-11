bae9d995328f1aa06a218c5845eceb894b70ff50
Hammad Mazhar <hammad.maz@gmail.com>
2014-08-11 13:43:09 -0500

    Updated benchmark to also run the GetUniqueIntID function

--- src/unit_TESTING/benchmark/benchmark_atomic.cpp
+++ src/unit_TESTING/benchmark/benchmark_atomic.cpp
@@ -1,4 +1,5 @@
 #include "../ChTestConfig.h"
+#include "physics/ChGlobal.h"
 #if defined(__APPLE__)
 #include <libkern/OSAtomic.h>
 #endif
@@ -9,7 +10,7 @@ using namespace chrono;
 static volatile int first = 100000;
 
 int main() {
-   ChTimer<double> OSX, GNU;
+   ChTimer<double> OSX, GNU, CHRONO;
 #if defined(__APPLE__)
    OSX.start();
    for (int j = 0; j < 100; j++) {
@@ -35,5 +36,16 @@ int main() {
    double result_GNU = GNU();
    cout << "__sync_add_and_fetch: " << result_GNU << " " << result_GNU / 100000000.0 << endl;
 #endif
+
+   CHRONO.start();
+   for (int j = 0; j < 100; j++) {
+      for (int i = 0; i < 1000000; i++) {
+         GetUniqueIntID();
+      }
+   }
+   CHRONO.stop();
+   double result_CHRONO = CHRONO();
+   cout << "Chrono GetUniqueIntID: " << result_CHRONO << " " << result_CHRONO / 100000000.0 << endl;
+
    return 0;
 }
