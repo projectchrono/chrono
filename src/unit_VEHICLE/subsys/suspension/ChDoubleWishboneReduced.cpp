a12f671f37a30a73d535a15419cf44f7a203b3ac
Hammad Mazhar <hammad.maz@gmail.com>
2014-08-22 09:50:17 -0500

    fix warning for GCC : returning reference to temporary

--- src/unit_VEHICLE/subsys/suspension/ChDoubleWishboneReduced.cpp
+++ src/unit_VEHICLE/subsys/suspension/ChDoubleWishboneReduced.cpp
@@ -148,8 +148,8 @@ ChDoubleWishboneReduced::Initialize(ChSharedBodyPtr   chassis,
   if (m_driven) {
     m_axle->SetInertia(getAxleInertia());
     chassis->GetSystem()->Add(m_axle);
-
-    m_axle_to_spindle->Initialize(m_axle, m_spindle, ChVector<>(0, 1, 0));
+    ChVector<> m_direction(0, 1, 0);
+    m_axle_to_spindle->Initialize(m_axle, m_spindle, m_direction);
     chassis->GetSystem()->Add(m_axle_to_spindle);
   }
 
