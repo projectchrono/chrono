b02da435a4eed228bfe4c95e17ed6b5d15a6e8c6
Daniel Melanz <daniel.melanz@gmail.com>
2014-08-25 16:53:19 -0500

    Added HMMWV9 files to get a starting point for HMMWVFULL.

---
+++ src/unit_VEHICLE/models/hmmwv_full/HMMWV9_SimplePowertrain.cpp
@@ -0,0 +1,45 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Justin Madsen
+// =============================================================================
+//
+// Simple powertrain model for the HMMWV vehicle.
+// - RWD only
+// - trivial speed-torque curve
+// - no differential
+//
+// =============================================================================
+
+#include "HMMWV9_SimplePowertrain.h"
+
+using namespace chrono;
+
+namespace hmmwv9 {
+
+// -----------------------------------------------------------------------------
+// Static variables
+// -----------------------------------------------------------------------------
+const double HMMWV9_SimplePowertrain::m_max_torque = 2400 / 8.851;
+const double HMMWV9_SimplePowertrain::m_max_speed  = 2000;
+const double HMMWV9_SimplePowertrain::m_conic_tau = 0.2;
+const double HMMWV9_SimplePowertrain::m_gear_tau = 0.3;
+
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+HMMWV9_SimplePowertrain::HMMWV9_SimplePowertrain(HMMWV9_Vehicle* car)
+: ChSimplePowertrain(car)
+{
+}
+
+
+} // end namespace hmmwv9
---
+++ src/unit_VEHICLE/models/hmmwv_full/HMMWV9_Vehicle.h
@@ -0,0 +1,92 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Justin Madsen
+// =============================================================================
+//
+// HMMWV 9-body vehicle model...
+//
+// =============================================================================
+
+#ifndef HMMWV9_VEHICLE_H
+#define HMMWV9_VEHICLE_H
+
+#include "core/ChCoordsys.h"
+#include "physics/ChSystem.h"
+
+#include "subsys/ChVehicle.h"
+
+#include "HMMWV9.h"
+#include "HMMWV9_DoubleWishbone.h"
+#include "HMMWV9_Wheel.h"
+#include "HMMWV9_SimplePowertrain.h"
+#include "HMMWV9_Powertrain.h"
+
+namespace hmmwv9 {
+
+// Forward reference
+class HMMWV9_SimplePowertrain;
+class HMMWV9_Powertrain;
+
+class HMMWV9_Vehicle : public chrono::ChVehicle {
+public:
+
+  HMMWV9_Vehicle(const bool                   fixed = false,
+                 VisualizationType            chassisVis = NONE,
+                 VisualizationType            wheelVis = PRIMITIVES);
+
+  ~HMMWV9_Vehicle();
+
+  virtual chrono::ChSharedBodyPtr GetWheelBody(chrono::ChWheelId which) const;
+
+  virtual const chrono::ChVector<>& GetWheelPos(chrono::ChWheelId which) const;
+  virtual const chrono::ChQuaternion<>& GetWheelRot(chrono::ChWheelId which) const;
+  virtual const chrono::ChVector<>& GetWheelLinVel(chrono::ChWheelId which) const;
+  virtual chrono::ChVector<> GetWheelAngVel(chrono::ChWheelId which) const;
+  virtual double GetWheelOmega(chrono::ChWheelId which) const;
+
+  virtual void Initialize(const chrono::ChCoordsys<>& chassisPos);
+  virtual void Update(double                      time,
+                      double                      throttle,
+                      double                      steering,
+                      const chrono::ChTireForces& tire_forces);
+
+  static void ExportMeshPovray(const std::string& out_dir);
+
+private:
+
+  chrono::ChSharedPtr<HMMWV9_DoubleWishboneFront>   m_front_right_susp;
+  chrono::ChSharedPtr<HMMWV9_DoubleWishboneFront>   m_front_left_susp;
+  chrono::ChSharedPtr<HMMWV9_DoubleWishboneRear>    m_rear_right_susp;
+  chrono::ChSharedPtr<HMMWV9_DoubleWishboneRear>    m_rear_left_susp;
+
+  chrono::ChSharedPtr<HMMWV9_Wheel> m_front_right_wheel;
+  chrono::ChSharedPtr<HMMWV9_Wheel> m_front_left_wheel;
+  chrono::ChSharedPtr<HMMWV9_Wheel> m_rear_right_wheel;
+  chrono::ChSharedPtr<HMMWV9_Wheel> m_rear_left_wheel;
+
+  ////chrono::ChSharedPtr<HMMWV9_SimplePowertrain>  m_powertrain;
+  chrono::ChSharedPtr<HMMWV9_Powertrain>  m_powertrain;
+
+  // Chassis visualization mesh
+  static const std::string m_chassisMeshName;
+  static const std::string m_chassisMeshFile;
+
+  // Chassis mass properties
+  static const double  m_chassisMass;
+  static const chrono::ChVector<>  m_chassisInertia;
+};
+
+
+} // end namespace hmmwv9
+
+
+#endif
