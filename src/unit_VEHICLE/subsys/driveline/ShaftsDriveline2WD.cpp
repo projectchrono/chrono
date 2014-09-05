fd5f11e6545292007bcd9b31c8d97f4bf0dc8d7e
Radu Serban <serban@engr.wisc.edu>
2014-09-05 18:12:48 -0500

    Add ShaftsDriveline2WD, a concrete class that implements a ChShaftsDriveline2WD using data in JSON format.

---
+++ src/unit_VEHICLE/subsys/driveline/ShaftsDriveline2WD.cpp
@@ -0,0 +1,79 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// 2WD driveline model template based on ChShaft objects using data from file
+// (JSON format).
+//
+// =============================================================================
+
+#include "subsys/driveline/ShaftsDriveline2WD.h"
+
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+
+using namespace rapidjson;
+
+namespace chrono {
+
+
+// -----------------------------------------------------------------------------
+// This utility function returns a ChVector from the specified JSON array
+// -----------------------------------------------------------------------------
+static ChVector<> loadVector(const Value& a)
+{
+  assert(a.IsArray());
+  assert(a.Size() == 3);
+
+  return ChVector<>(a[0u].GetDouble(), a[1u].GetDouble(), a[2u].GetDouble());
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+ShaftsDriveline2WD::ShaftsDriveline2WD(ChVehicle*         car,
+                                       const std::string& filename)
+: ChShaftsDriveline2WD(car)
+{
+  FILE* fp = fopen(filename.c_str(), "r");
+
+  char readBuffer[65536];
+  FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+  fclose(fp);
+
+  Document d;
+  d.ParseStream(is);
+
+  // Read top-level data.
+  assert(d.HasMember("Type"));
+  assert(d.HasMember("Template"));
+  assert(d.HasMember("Name"));
+
+  // Get shaft directions.
+  assert(d.HasMember("Shaft Direction"));
+  SetMotorBlockDirection(loadVector(d["Shaft Direction"]["Motor Block"]));
+  SetAxleDirection(loadVector(d["Shaft Direction"]["Axle"]));
+
+  // Read shaft inertias.
+  assert(d.HasMember("Shaft Inertia"));
+  m_driveshaft_inertia = d["Shaft Inertia"]["Driveshaft"].GetDouble();
+  m_differentialbox_inertia = d["Shaft Inertia"]["Differential Box"].GetDouble();
+
+  // Read gear ratios.
+  assert(d.HasMember("Gear Ratio"));
+  m_conicalgear_ratio = d["Gear Ratio"]["Conical Gear"].GetDouble();
+  m_differential_ratio = d["Gear Ratio"]["Differential"].GetDouble();
+}
+
+
+} // end namespace chrono
---
+++ src/unit_VEHICLE/subsys/driveline/ShaftsDriveline2WD.h
@@ -0,0 +1,59 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// 2WD driveline model template based on ChShaft objects using data from file
+// (JSON format).
+//
+// =============================================================================
+
+#ifndef SHAFTS_DRIVELINE_2WD_H
+#define SHAFTS_DRIVELINE_2WD_H
+
+#include "subsys/ChApiSubsys.h"
+#include "subsys/driveline/ChShaftsDriveline2WD.h"
+
+
+namespace chrono {
+
+
+class CH_SUBSYS_API ShaftsDriveline2WD : public ChShaftsDriveline2WD
+{
+public:
+
+  ShaftsDriveline2WD(ChVehicle*         car,
+                     const std::string& filename);
+
+  ~ShaftsDriveline2WD() {}
+
+  virtual double GetDriveshaftInertia() const      { return m_driveshaft_inertia; }
+  virtual double GetDifferentialBoxInertia() const { return m_differentialbox_inertia; }
+
+  virtual double GetConicalGearRatio() const       { return m_conicalgear_ratio; }
+  virtual double GetDifferentialRatio() const      { return m_differential_ratio; }
+
+private:
+  // Shaft inertias.
+  double  m_driveshaft_inertia;
+  double  m_differentialbox_inertia;
+
+  // Gear ratios.
+  double  m_conicalgear_ratio;
+  double  m_differential_ratio;
+};
+
+
+} // end namespace chrono
+
+
+#endif
