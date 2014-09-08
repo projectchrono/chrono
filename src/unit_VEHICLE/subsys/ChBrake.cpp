5d7ddba094d49e30701ed8807bf6b77c64667f2d
Alessandro Tasora <tasora@ied.unipr.it>
2014-09-08 11:19:07 +0200

    Base class for braking systems for wheels

---
+++ src/unit_VEHICLE/subsys/ChBrake.cpp
@@ -0,0 +1,30 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Alessandro Tasora
+// =============================================================================
+//
+// Base class for a vehicle driveline.
+//
+// =============================================================================
+
+#include "subsys/ChBrake.h"
+
+
+namespace chrono {
+
+
+ChBrake::ChBrake()
+{
+}
+
+
+}  // end namespace chrono
