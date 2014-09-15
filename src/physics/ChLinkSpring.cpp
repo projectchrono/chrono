713d6889d75d4c91a1081debaecf926896d80d47
Radu Serban <serban@engr.wisc.edu>
2014-09-15 13:06:25 -0500

    Update ChLinkSpring to use ChSharedPtr to handle ChFunctions.

--- src/physics/ChLinkSpring.cpp
+++ src/physics/ChLinkSpring.cpp
@@ -9,240 +9,163 @@
 // and at http://projectchrono.org/license-chrono.txt.
 //
 
-///////////////////////////////////////////////////
-//
-//   ChLinkSpring.cpp
-//
-// ------------------------------------------------
-//             www.deltaknowledge.com
-// ------------------------------------------------
-///////////////////////////////////////////////////
 
- 
-  
- 
 #include "physics/ChLinkSpring.h"
-#include "core/ChMemory.h" // must be last include (memory leak debugger). 
+#include "core/ChMemory.h" // must be last include (memory leak debugger)
 
 
 namespace chrono
 {
 
 
-
-
-////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-//
-//   CLASS FOR SPRING LINKS
-//
-//
-
 // Register into the object factory, to enable run-time
 // dynamic creation and persistence
 ChClassRegister<ChLinkSpring> a_registration_ChLinkSpring;
 
 
-                // BUILDERS
 ChLinkSpring::ChLinkSpring ()
 {
-    spr_restlength = 0;     // custom variables
-    spr_k = 100;
-    spr_r = 5;
-    spr_f = 0;
+  spr_restlength = 0;
+  spr_k = 100;
+  spr_r = 5;
+  spr_f = 0;
 
-    mod_f_time =   new ChFunction_Const (1);
-    mod_k_d =   new ChFunction_Const (1);
-    mod_k_speed =   new ChFunction_Const (1);
-    mod_r_d =   new ChFunction_Const (1);
-    mod_r_speed =   new ChFunction_Const (1);
+  mod_f_time = ChSharedPtr<ChFunction>(new ChFunction_Const(1));
+  mod_k_d = ChSharedPtr<ChFunction>(new ChFunction_Const(1));
+  mod_k_speed = ChSharedPtr<ChFunction>(new ChFunction_Const(1));
+  mod_r_d = ChSharedPtr<ChFunction>(new ChFunction_Const(1));
+  mod_r_speed = ChSharedPtr<ChFunction>(new ChFunction_Const(1));
 
-    spr_react = 0.0;
+  spr_react = 0.0;
 }
 
 
-            // DESTROYER
 ChLinkSpring::~ChLinkSpring ()
 {
-    // delete custom instanced data...
-    if (mod_f_time) delete mod_f_time;
-    if (mod_k_d) delete mod_k_d;
-    if (mod_k_speed) delete mod_k_speed;
-    if (mod_r_d) delete mod_r_d;
-    if (mod_r_speed) delete mod_r_speed;
 }
 
 
 void ChLinkSpring::Copy(ChLinkSpring* source)
 {
-    // first copy the parent class data...
-    //
-    ChLinkMarkers::Copy(source);
+  // first copy the parent class data...
+  ChLinkMarkers::Copy(source);
 
-    // copy custom data:
-    spr_restlength = source->spr_restlength;
-    spr_f = source->spr_f;
-    spr_k = source->spr_k;
-    spr_r = source->spr_r;
-    spr_react = source->spr_react;
-    if (mod_f_time) delete mod_f_time;
-         mod_f_time =   source->mod_f_time->new_Duplicate();
-    if (mod_k_d) delete mod_k_d;
-         mod_k_d =   source->mod_k_d->new_Duplicate();
-    if (mod_k_speed) delete mod_k_speed;
-         mod_k_speed =   source->mod_k_speed->new_Duplicate();
-    if (mod_r_d) delete mod_r_d;
-         mod_r_d =   source->mod_r_d->new_Duplicate();
-    if (mod_r_speed) delete mod_r_speed;
-         mod_r_speed =   source->mod_r_speed->new_Duplicate();
+  // copy custom data:
+  spr_restlength = source->spr_restlength;
+  spr_f = source->spr_f;
+  spr_k = source->spr_k;
+  spr_r = source->spr_r;
+  spr_react = source->spr_react;
+
+  mod_f_time = ChSharedPtr<ChFunction>(source->mod_f_time->new_Duplicate());
+  mod_k_d = ChSharedPtr<ChFunction>(source->mod_k_d->new_Duplicate());
+  mod_k_speed = ChSharedPtr<ChFunction>(source->mod_k_speed->new_Duplicate());
+  mod_r_d = ChSharedPtr<ChFunction>(source->mod_r_d->new_Duplicate());
+  mod_r_speed = ChSharedPtr<ChFunction>(source->mod_r_speed->new_Duplicate());
 }
 
 
 ChLink* ChLinkSpring::new_Duplicate ()
 {
-    ChLinkSpring* m_l;
-    m_l = new ChLinkSpring;  // inherited classes should write here: m_l = new MyInheritedLink;
-    m_l->Copy(this);
-    return (m_l);
+  ChLinkSpring* m_l;
+  m_l = new ChLinkSpring;  // inherited classes should write here: m_l = new MyInheritedLink;
+  m_l->Copy(this);
+  return (m_l);
 }
 
 
-
-
-void ChLinkSpring::Initialize(ChSharedPtr<ChBody> mbody1,   ///< first body to link
-						   ChSharedPtr<ChBody> mbody2,		 ///< second body to link
-						   bool pos_are_relative,///< true: following posit. are considered relative to bodies. false: pos.are absolute
-						   ChVector<> mpos1,	 ///< position of distance endpoint, for 1st body (rel. or abs., see flag above)
-						   ChVector<> mpos2,	 ///< position of distance endpoint, for 2nd body (rel. or abs., see flag above) 
-						   bool auto_rest_length,///< if true, initializes the imposed distance as the distance between mpos1 and mpos2
-						   double mrest_length   ///< imposed distance (no need to define, if auto_distance=true.)
-						   )
+void ChLinkSpring::Initialize(ChSharedPtr<ChBody> mbody1,           // first body to link
+                              ChSharedPtr<ChBody> mbody2,           // second body to link
+                              bool                pos_are_relative, // true: following posit. are considered relative to bodies. false: pos.are absolute
+                              ChVector<>          mpos1,            // position of distance endpoint, for 1st body (rel. or abs., see flag above)
+                              ChVector<>          mpos2,            // position of distance endpoint, for 2nd body (rel. or abs., see flag above) 
+                              bool                auto_rest_length, // if true, initializes the imposed distance as the distance between mpos1 and mpos2
+                              double              mrest_length      // imposed distance (no need to define, if auto_distance=true.)
+                              )
 {
-	// first, initialize as all constraint with markers. In this case, create the two markers also!.
-	ChLinkMarkers::Initialize(mbody1,mbody2, CSYSNORM);
+  // First, initialize as all constraint with markers.
+  // In this case, create the two markers also!.
+  ChLinkMarkers::Initialize(mbody1, mbody2, CSYSNORM);
 
-	if (pos_are_relative)
-	{
-		this->marker1->Impose_Rel_Coord(ChCoordsys<>(mpos1,QUNIT));
-		this->marker2->Impose_Rel_Coord(ChCoordsys<>(mpos2,QUNIT));
-	}
-	else
-	{
-		this->marker1->Impose_Abs_Coord(ChCoordsys<>(mpos1,QUNIT));
-		this->marker2->Impose_Abs_Coord(ChCoordsys<>(mpos2,QUNIT));
-	}
-	 
-	ChVector<> AbsDist = this->marker1->GetAbsCoord().pos - this->marker2->GetAbsCoord().pos;
-	this->dist = AbsDist.Length();
+  if (pos_are_relative)
+  {
+    marker1->Impose_Rel_Coord(ChCoordsys<>(mpos1, QUNIT));
+    marker2->Impose_Rel_Coord(ChCoordsys<>(mpos2, QUNIT));
+  }
+  else
+  {
+    marker1->Impose_Abs_Coord(ChCoordsys<>(mpos1, QUNIT));
+    marker2->Impose_Abs_Coord(ChCoordsys<>(mpos2, QUNIT));
+  }
 
-	if (auto_rest_length)
-	{
-		this->spr_restlength = this->dist;
-	}
-	else
-	{
-		this->spr_restlength = mrest_length; 
-	}
+  ChVector<> AbsDist = marker1->GetAbsCoord().pos - marker2->GetAbsCoord().pos;
+  dist = AbsDist.Length();
+
+  spr_restlength = auto_rest_length ? dist : mrest_length;
 }
 
 
-
-
-
-
-void ChLinkSpring::Set_mod_f_time (ChFunction* m_funct)
+void ChLinkSpring::UpdateForces(double mytime)
 {
-    if (mod_f_time) delete mod_f_time;
-    mod_f_time = m_funct;
+  // Inherit force computation:
+  // also base class can add its own forces.
+  ChLinkMarkers::UpdateForces(mytime);
+
+  spr_react = 0.0;
+  Vector m_force;
+  double deform = Get_SpringDeform();
+
+  spr_react = spr_f * mod_f_time->Get_y(ChTime);
+  spr_react -= (spr_k * mod_k_d->Get_y(deform) * mod_k_speed->Get_y(dist_dt)) * (deform);
+  spr_react -= (spr_r * mod_r_d->Get_y(deform) * mod_r_speed->Get_y(dist_dt)) * (dist_dt);
+
+  m_force = Vmul(Vnorm(relM.pos), spr_react);
+
+  C_force = Vadd(C_force, m_force);
 }
-void ChLinkSpring::Set_mod_k_d    (ChFunction* m_funct)
-{
-    if (mod_k_d) delete mod_k_d;
-    mod_k_d = m_funct;
-}
-void ChLinkSpring::Set_mod_r_d    (ChFunction* m_funct)
-{
-    if (mod_r_d) delete mod_r_d;
-    mod_r_d = m_funct;
-}
-void ChLinkSpring::Set_mod_k_speed(ChFunction* m_funct)
-{
-    if (mod_k_speed) delete mod_k_speed;
-    mod_k_speed = m_funct;
-}
-void ChLinkSpring::Set_mod_r_speed(ChFunction* m_funct)
-{
-    if (mod_r_speed) delete mod_r_speed;
-    mod_r_speed = m_funct;
-}
-
-
-void ChLinkSpring::UpdateForces (double mytime)
-{
-    // Inherit force computation:
-    // also base class can add its own forces.
-    ChLinkMarkers::UpdateForces(mytime);
-
-    spr_react = 0.0;
-    Vector m_force;
-    double deform = Get_SpringDeform();
-
-    spr_react  =  spr_f * mod_f_time->Get_y(ChTime);
-    spr_react -= (spr_k * mod_k_d->Get_y(deform) * mod_k_speed->Get_y(dist_dt)) * (deform);
-    spr_react -= (spr_r * mod_r_d->Get_y(deform) * mod_r_speed->Get_y(dist_dt)) * (dist_dt);
-
-    m_force = Vmul (Vnorm(relM.pos), spr_react);
-
-    C_force = Vadd(C_force, m_force);  
-}
-
 
 
 void ChLinkSpring::StreamOUT(ChStreamOutBinary& mstream)
 {
-			// class version number
-	mstream.VersionWrite(1);
-		// serialize parent class too
-	ChLinkMarkers::StreamOUT(mstream);
+  // Class version number
+  mstream.VersionWrite(1);
 
-		// stream out all member data
-	mstream << spr_restlength;
-    mstream << spr_f;
-    mstream << spr_k;
-    mstream << spr_r;
-	mstream.AbstractWrite(mod_f_time);
-	mstream.AbstractWrite(mod_k_d);
-    mstream.AbstractWrite(mod_k_speed);
-    mstream.AbstractWrite(mod_r_d);
-    mstream.AbstractWrite(mod_r_speed);
+  // Serialize parent class too
+  ChLinkMarkers::StreamOUT(mstream);
+
+  // Stream out all member data
+  mstream << spr_restlength;
+  mstream << spr_f;
+  mstream << spr_k;
+  mstream << spr_r;
+  mstream.AbstractWrite(mod_f_time.get_ptr());
+  mstream.AbstractWrite(mod_k_d.get_ptr());
+  mstream.AbstractWrite(mod_k_speed.get_ptr());
+  mstream.AbstractWrite(mod_r_d.get_ptr());
+  mstream.AbstractWrite(mod_r_speed.get_ptr());
 }
+
 
 void ChLinkSpring::StreamIN(ChStreamInBinary& mstream)
 {
-		// class version number
-	int version = mstream.VersionRead();
-		// deserialize parent class too
-	ChLinkMarkers::StreamIN(mstream);
+  // Class version number
+  int version = mstream.VersionRead();
 
-		// stream in all member data
-	ChFunction* ffoo;
-	mstream >> spr_restlength;
-    mstream >> spr_f;
-    mstream >> spr_k;
-    mstream >> spr_r;
-	mstream.AbstractReadCreate(&ffoo);		Set_mod_f_time(ffoo);
-	mstream.AbstractReadCreate(&ffoo);		Set_mod_k_d(ffoo);
-    mstream.AbstractReadCreate(&ffoo);		Set_mod_k_speed(ffoo);
-    mstream.AbstractReadCreate(&ffoo);		Set_mod_r_d(ffoo);
-    mstream.AbstractReadCreate(&ffoo);		Set_mod_r_speed(ffoo);
+  // Deserialize parent class
+  ChLinkMarkers::StreamIN(mstream);
+
+  // Stream in all member data
+  ChFunction* newfun;
+  mstream >> spr_restlength;
+  mstream >> spr_f;
+  mstream >> spr_k;
+  mstream >> spr_r;
+  mstream.AbstractReadCreate(&newfun);    mod_f_time  = ChSharedPtr<ChFunction>(newfun);
+  mstream.AbstractReadCreate(&newfun);    mod_k_d     = ChSharedPtr<ChFunction>(newfun);
+  mstream.AbstractReadCreate(&newfun);    mod_k_speed = ChSharedPtr<ChFunction>(newfun);
+  mstream.AbstractReadCreate(&newfun);    mod_r_d     = ChSharedPtr<ChFunction>(newfun);
+  mstream.AbstractReadCreate(&newfun);    mod_r_speed = ChSharedPtr<ChFunction>(newfun);
 }
-
-
-
-
-
-///////////////////////////////////////////////////////////////
-
 
 
 } // END_OF_NAMESPACE____
--- src/physics/ChLinkSpring.h
+++ src/physics/ChLinkSpring.h
@@ -12,31 +12,13 @@
 #ifndef CHLINKSPRING_H
 #define CHLINKSPRING_H
 
-///////////////////////////////////////////////////
-//
-//   ChLinkSpring.h
-//
-//
-//   Classes for spring-dampers joints.
-//
-//   This class is inherited from the base ChLink()
-//   class, used by all joints in 3D.
-//
-//   HEADER file for CHRONO,
-//	 Multibody dynamics engine
-//
-// ------------------------------------------------
-//             www.deltaknowledge.com
-// ------------------------------------------------
-///////////////////////////////////////////////////
-
 
 #include "physics/ChLinkMarkers.h"
 
 
-
 namespace chrono
 {
+
 ///
 /// Class for spring-damper systems, acting along the polar
 /// distance of two markers
@@ -44,105 +26,106 @@ namespace chrono
 
 class ChApi ChLinkSpring : public ChLinkMarkers {
 
-	CH_RTTI(ChLinkSpring,ChLinkMarkers);
+  CH_RTTI(ChLinkSpring,ChLinkMarkers);
 
 protected:
-	double spr_restlength;
-	double spr_k;
-	double spr_r;
-	double spr_f;
-	ChFunction* mod_f_time;		// f(t)
-	ChFunction* mod_k_d;		// k(d)
-	ChFunction* mod_r_d;		// r(d)
-	ChFunction* mod_r_speed;	// k(speed)
-	ChFunction* mod_k_speed;	// r(speed)
-	double spr_react;		// resulting force in dist. coord / readonly
+  double spr_restlength;
+  double spr_k;
+  double spr_r;
+  double spr_f;
+  ChSharedPtr<ChFunction> mod_f_time;   // f(t)
+  ChSharedPtr<ChFunction> mod_k_d;      // k(d)
+  ChSharedPtr<ChFunction> mod_r_d;      // r(d)
+  ChSharedPtr<ChFunction> mod_r_speed;  // k(speed)
+  ChSharedPtr<ChFunction> mod_k_speed;  // r(speed)
+  double spr_react;   // resulting force in dist. coord / readonly
+
 public:
 
-				//
-	  			// FUNCTIONS
-				//
-						// builders and destroyers
-	ChLinkSpring ();
-	virtual ~ChLinkSpring ();
-	virtual void Copy(ChLinkSpring* source);
-	virtual ChLink* new_Duplicate ();	// always return base link class pointer, Link*
+  //
+  // FUNCTIONS
+  //
 
-	virtual int GetType	() {return LNK_SPRING;}
+      // builders and destroyers
+  ChLinkSpring();
+  virtual ~ChLinkSpring();
+  virtual void Copy(ChLinkSpring* source);
+  virtual ChLink* new_Duplicate();  // always return base link class pointer
 
-						// data fetch/store
-	double Get_SpringRestLength() {return spr_restlength;};
-	void   Set_SpringRestLength(double m_r) {spr_restlength = m_r;};
-	double Get_SpringDeform() {return (dist - spr_restlength);};
-	double Get_SpringK() {return spr_k;};
-	void   Set_SpringK(double m_r) {spr_k = m_r;};
-	double Get_SpringR() {return spr_r;};
-	void   Set_SpringR(double m_r) {spr_r = m_r;};
-	double Get_SpringF() {return spr_f;};
-	void   Set_SpringF(double m_r) {spr_f = m_r;};
-	double Get_SpringReact() {return spr_react;};
+  virtual int GetType() { return LNK_SPRING; }
 
-	ChFunction* Get_mod_f_time() {return mod_f_time;};
-	ChFunction* Get_mod_k_d() {return mod_k_d;};
-	ChFunction* Get_mod_r_d() {return mod_r_d;};
-	ChFunction* Get_mod_k_speed() {return mod_k_speed;};
-	ChFunction* Get_mod_r_speed() {return mod_r_speed;};
-	void Set_mod_f_time	(ChFunction* m_funct);
-	void Set_mod_k_d	(ChFunction* m_funct);
-	void Set_mod_r_d	(ChFunction* m_funct);
-	void Set_mod_k_speed(ChFunction* m_funct);
-	void Set_mod_r_speed(ChFunction* m_funct);
+      // data fetch/store
+  double Get_SpringRestLength() { return spr_restlength; }
+  double Get_SpringDeform()     { return (dist - spr_restlength); }
+  double Get_SpringK()          { return spr_k; }
+  double Get_SpringR()          { return spr_r; }
+  double Get_SpringF()          { return spr_f; }
+  double Get_SpringReact()      { return spr_react; }
 
-					/// Specialized initialization for springs, given the two bodies to be connected, the
-					/// positions of the two anchor endpoints of the spring (each expressed
-					/// in body or abs. coordinates) and the imposed rest length of the spring.
-					/// NOTE! As in ChLinkMarkers::Initialize(), the two markers are automatically 
-					/// created and placed inside the two connected bodies.
-	void Initialize(ChSharedPtr<ChBody> mbody1,	///< first body to link
-					ChSharedPtr<ChBody> mbody2, ///< second body to link
-					bool pos_are_relative,	///< true: following posit. are considered relative to bodies. false: pos.are absolute
-					ChVector<> mpos1,		///< position of spring endpoint, for 1st body (rel. or abs., see flag above)
-					ChVector<> mpos2,		///< position of spring endpoint, for 2nd body (rel. or abs., see flag above) 
-					bool auto_rest_length =true,///< if true, initializes the rest-length as the distance between mpos1 and mpos2
-					double mrest_length =0   ///< imposed rest_length (no need to define, if auto_rest_length=true.)
-					);
+  void Set_SpringRestLength(double m_r) { spr_restlength = m_r; }
+  void Set_SpringK(double m_r)          { spr_k = m_r; }
+  void Set_SpringR(double m_r)          { spr_r = m_r; }
+  void Set_SpringF(double m_r)          { spr_f = m_r; }
 
-					/// Get the 1st spring endpoint (expressed in Body1 coordinate system)
-	ChVector<> GetEndPoint1Rel() {return marker1->GetPos();}
-					/// Set the 1st spring endpoint (expressed in Body1 coordinate system)
-	void       SetEndPoint1Rel(const ChVector<>& mset) { marker1->Impose_Rel_Coord(ChCoordsys<>(mset,QUNIT));}
-					/// Get the 1st spring endpoint (expressed in absolute coordinate system)
-	ChVector<> GetEndPoint1Abs() {return marker1->GetAbsCoord().pos;}
-					/// Set the 1st spring endpoint (expressed in absolute coordinate system)
-	void       SetEndPoint1Abs(ChVector<>& mset) { marker1->Impose_Abs_Coord(ChCoordsys<>(mset,QUNIT));}
+  ChSharedPtr<ChFunction> Get_mod_f_time()  { return mod_f_time; }
+  ChSharedPtr<ChFunction> Get_mod_k_d()     { return mod_k_d; }
+  ChSharedPtr<ChFunction> Get_mod_r_d()     { return mod_r_d; }
+  ChSharedPtr<ChFunction> Get_mod_k_speed() { return mod_k_speed; }
+  ChSharedPtr<ChFunction> Get_mod_r_speed() { return mod_r_speed; }
 
-					/// Get the 2nd spring endpoint (expressed in Body2 coordinate system)
-	ChVector<> GetEndPoint2Rel() {return marker2->GetPos();};
-					/// Set the 2nd spring endpoint (expressed in Body2 coordinate system)
-	void       SetEndPoint2Rel(const ChVector<>& mset) { marker2->Impose_Rel_Coord(ChCoordsys<>(mset,QUNIT));}
-					/// Get the 1st spring endpoint (expressed in absolute coordinate system)
-	ChVector<> GetEndPoint2Abs() {return marker2->GetAbsCoord().pos;}
-					/// Set the 1st spring endpoint (expressed in absolute coordinate system)
-	void       SetEndPoint2Abs(ChVector<>& mset) { marker2->Impose_Abs_Coord(ChCoordsys<>(mset,QUNIT));}
+  void Set_mod_f_time(ChSharedPtr<ChFunction> mf)  { mod_f_time = mf; }
+  void Set_mod_k_d(ChSharedPtr<ChFunction> mf)     { mod_k_d = mf; }
+  void Set_mod_r_d(ChSharedPtr<ChFunction> mf)     { mod_r_d = mf; }
+  void Set_mod_k_speed(ChSharedPtr<ChFunction> mf) { mod_k_speed = mf; }
+  void Set_mod_r_speed(ChSharedPtr<ChFunction> mf) { mod_r_speed = mf; }
 
-			//
-			// UPDATING FUNCTIONS
-			//
+      /// Specialized initialization for springs, given the two bodies to be connected,
+      /// the positions of the two anchor endpoints of the spring (each expressed
+      /// in body or abs. coordinates) and the imposed rest length of the spring.
+      /// NOTE! As in ChLinkMarkers::Initialize(), the two markers are automatically 
+      /// created and placed inside the two connected bodies.
+  void Initialize(ChSharedPtr<ChBody> mbody1,   ///< first body to link
+                  ChSharedPtr<ChBody> mbody2,   ///< second body to link
+                  bool pos_are_relative,        ///< true: following pos. are considered relative to bodies. false: pos.are absolute
+                  ChVector<> mpos1,             ///< position of spring endpoint, for 1st body (rel. or abs., see flag above)
+                  ChVector<> mpos2,             ///< position of spring endpoint, for 2nd body (rel. or abs., see flag above) 
+                  bool auto_rest_length = true, ///< if true, initializes the rest-length as the distance between mpos1 and mpos2
+                  double mrest_length = 0       ///< imposed rest_length (no need to define, if auto_rest_length=true.)
+                  );
 
-						/// Inherits, then also adds the spring custom forces to
-						/// the C_force and C_torque. 
-	virtual void UpdateForces (double mytime);
+      /// Get the 1st spring endpoint (expressed in Body1 coordinate system)
+  ChVector<> GetEndPoint1Rel() { return marker1->GetPos(); }
+      /// Set the 1st spring endpoint (expressed in Body1 coordinate system)
+  void       SetEndPoint1Rel(const ChVector<>& mset) { marker1->Impose_Rel_Coord(ChCoordsys<>(mset, QUNIT)); }
+      /// Get the 1st spring endpoint (expressed in absolute coordinate system)
+  ChVector<> GetEndPoint1Abs() { return marker1->GetAbsCoord().pos; }
+      /// Set the 1st spring endpoint (expressed in absolute coordinate system)
+  void       SetEndPoint1Abs(ChVector<>& mset) { marker1->Impose_Abs_Coord(ChCoordsys<>(mset, QUNIT)); }
 
+      /// Get the 2nd spring endpoint (expressed in Body2 coordinate system)
+  ChVector<> GetEndPoint2Rel() { return marker2->GetPos(); };
+      /// Set the 2nd spring endpoint (expressed in Body2 coordinate system)
+  void       SetEndPoint2Rel(const ChVector<>& mset) { marker2->Impose_Rel_Coord(ChCoordsys<>(mset, QUNIT)); }
+      /// Get the 1st spring endpoint (expressed in absolute coordinate system)
+  ChVector<> GetEndPoint2Abs() { return marker2->GetAbsCoord().pos; }
+      /// Set the 1st spring endpoint (expressed in absolute coordinate system)
+  void       SetEndPoint2Abs(ChVector<>& mset) { marker2->Impose_Abs_Coord(ChCoordsys<>(mset, QUNIT)); }
 
-			//
-			// STREAMING
-			//
+  //
+  // UPDATING FUNCTIONS
+  //
+
+      /// Inherits, then also adds the spring custom forces to
+      /// the C_force and C_torque. 
+  virtual void UpdateForces(double mytime);
 
 
-	virtual void StreamIN(ChStreamInBinary& mstream);
-	virtual void StreamOUT(ChStreamOutBinary& mstream);
+  //
+  // STREAMING
+  //
 
-
+  virtual void StreamIN(ChStreamInBinary& mstream);
+  virtual void StreamOUT(ChStreamOutBinary& mstream);
 };
 
 
