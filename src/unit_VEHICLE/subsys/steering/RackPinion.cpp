1819153e6ef5e5f2eb8b7863d0c284513b77ec6a
Radu Serban <serban@engr.wisc.edu>
2014-10-04 10:52:30 -0500

    Add concrete rack-pinion steering class, constructed from a JSON specification file.

---
+++ src/unit_VEHICLE/subsys/steering/RackPinion.cpp
@@ -0,0 +1,77 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Rack-pinion steering model constructed with data from file (JSON format).
+//
+// =============================================================================
+
+#include "subsys/steering/RackPinion.h"
+#include "utils/ChUtilsInputOutput.h"
+#include "utils/ChUtilsData.h"
+
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+
+using namespace rapidjson;
+
+namespace chrono {
+
+
+// -----------------------------------------------------------------------------
+// This utility function returns a ChVector from the specified JSON array
+// -----------------------------------------------------------------------------
+static ChVector<> loadVector(const Value& a)
+{
+  assert(a.IsArray());
+  assert(a.Size() == 3);
+
+  return ChVector<>(a[0u].GetDouble(), a[1u].GetDouble(), a[2u].GetDouble());
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+RackPinion::RackPinion(const std::string& filename)
+: ChRackPinion("")
+{
+  FILE* fp = fopen(filename.c_str(), "r");
+
+  char readBuffer[65536];
+  FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+  fclose(fp);
+
+  Document d;
+  d.ParseStream(is);
+
+  // Read top-level data
+  assert(d.HasMember("Type"));
+  assert(d.HasMember("Template"));
+  assert(d.HasMember("Name"));
+
+  SetName(d["Name"].GetString());
+
+  // Read steering link data
+  m_steeringLinkMass = d["Steering Link"]["Mass"].GetDouble();
+  m_steeringLinkInertia = loadVector(d["Steering Link"]["Inertia"]);
+  m_steeringLinkCOM = d["Steering Link"]["COM"].GetDouble();
+  m_steeringLinkRadius = d["Steering Link"]["Radius"].GetDouble();
+  m_steeringLinkLength = d["Steering Link"]["Length"].GetDouble();
+
+  // Pinion radius
+  m_pinionRadius = d["Pinion"]["Radius"].GetDouble();
+  m_maxAngle = d["Pinion"]["Maximum Angle"].GetDouble();
+}
+
+
+}  // end namespace chrono
---
+++ src/unit_VEHICLE/subsys/steering/RackPinion.h
@@ -0,0 +1,61 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Rack-pinion steering model constructed with data from file (JSON format).
+//
+// =============================================================================
+
+#ifndef RACK_PINION_H
+#define RACK_PINION_H
+
+#include "subsys/ChApiSubsys.h"
+#include "subsys/steering/ChRackPinion.h"
+
+namespace chrono {
+
+
+class CH_SUBSYS_API RackPinion : public ChRackPinion
+{
+public:
+
+  RackPinion(const std::string& filename);
+  ~RackPinion() {}
+
+  virtual double GetSteeringLinkMass() const               { return m_steeringLinkMass; }
+  virtual const ChVector<>& GetSteeringLinkInertia() const { return m_steeringLinkInertia; }
+  virtual double GetSteeringLinkCOM() const                { return m_steeringLinkCOM; }
+  virtual double GetSteeringLinkRadius() const             { return m_steeringLinkRadius; }
+  virtual double GetSteeringLinkLength() const             { return m_steeringLinkLength; }
+
+  virtual double GetPinionRadius() const                   { return m_pinionRadius; }
+
+  virtual double GetMaxAngle() const                       { return m_maxAngle; }
+
+private:
+
+  double      m_steeringLinkMass;
+  ChVector<>  m_steeringLinkInertia;
+  double      m_steeringLinkCOM;
+  double      m_steeringLinkRadius;
+  double      m_steeringLinkLength;
+
+  double      m_pinionRadius;
+  double      m_maxAngle;
+};
+
+
+} // end namespace chrono
+
+
+#endif
