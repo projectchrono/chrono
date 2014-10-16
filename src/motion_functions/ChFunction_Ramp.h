0a8c1f27069d8d240e037387bbfe1cc156216ce3
Hammad Mazhar <hammad.maz@gmail.com>
2014-10-16 16:50:41 -0500

    Line ChFunction_Const, make functions from the base class virtual. The destructor needs to be virtual so that we can dynamic cast

--- src/motion_functions/ChFunction_Ramp.h
+++ src/motion_functions/ChFunction_Ramp.h
@@ -51,28 +51,28 @@ private:
 public:
 	ChFunction_Ramp () {y0 = 0; ang=1;};
 	ChFunction_Ramp (double m_y0, double m_ang)  {y0 = m_y0; ang = m_ang;};
-	~ChFunction_Ramp () {};
-	void Copy (ChFunction_Ramp* source);
-	ChFunction* new_Duplicate ();
+	virtual ~ChFunction_Ramp () {};
+	virtual  void Copy (ChFunction_Ramp* source);
+	virtual ChFunction* new_Duplicate ();
 
-	int Get_Type () {return (FUNCT_RAMP);}			
+	virtual int Get_Type () {return (FUNCT_RAMP);}
 	
 			// Custom data
 
 				/// The value for x=0;
-	void	Set_y0   (double m_y0)   {y0 = m_y0;};	
-	double	Get_y0 ()  {return y0;};
+	void   Set_y0   (double m_y0)   {y0 = m_y0;};
+	double Get_y0 ()  {return y0;};
 
 				/// The angular coefficient.
-	void	Set_ang  (double m_ang)  {ang = m_ang;};
-	double	Get_ang () {return ang;};
+	void	 Set_ang  (double m_ang)  {ang = m_ang;};
+	double Get_ang () {return ang;};
 
 
 			// Override the Get_y(), Get_y_dx etc. functions with analytical formulas.
 
-	double Get_y      (double x) {return (y0 + (x*ang))  ;};
-	double Get_y_dx   (double x) {return (ang)			 ;};
-	double Get_y_dxdx (double x) {return  0				 ;};
+	virtual double Get_y      (double x) {return (y0 + (x*ang))  ;};
+	virtual double Get_y_dx   (double x) {return (ang)			    ;};
+	virtual double Get_y_dxdx (double x) {return  0				    ;};
 
 			// Expose the parameters which can be used for optimizations, as Javascript vars.
 
