e785063974184ef62c0a9da2014538f69ab6e3ab
armanpazouki <apazouki@gmail.com>
2014-10-21 13:12:29 -0500

    Modifiying the slight compressiblitiy which I don't know where the heck has it come from

--- src/chrono_fsi/main.cpp
+++ src/chrono_fsi/main.cpp
@@ -1972,9 +1972,9 @@ int main() {
 		paramsH.bodyForce4 = R4(0.1,0,0,0);//R4(3.2e-3,0,0,0);// R4(0);;// /*Re = 100 */ //R4(3.2e-4, 0, 0, 0);/*Re = 100 */
 			paramsH.rho0 = 1000;
 			paramsH.mu0 = .001;
-		paramsH.v_Max = 50e-3;//18e-3;//1.5;//2e-1; /*0.2 for Re = 100 */ //2e-3;
+		paramsH.v_Max = 75e-3;//18e-3;//1.5;//2e-1; /*0.2 for Re = 100 */ //2e-3;
 			paramsH.EPS_XSPH = .5f;
-		paramsH.dT = 1e-4;//.001; //sph alone: .01 for Re 10;
+		paramsH.dT = 0.5e-4;//.001; //sph alone: .01 for Re 10;
 			paramsH.tFinal = 400;//20 * paramsH.dT; //400
 			paramsH.timePause = .0005 * paramsH.tFinal; // keep it as small as possible. the time step will be 1/10 * dT
 			paramsH.timePauseRigidFlex = .01 * paramsH.tFinal;
