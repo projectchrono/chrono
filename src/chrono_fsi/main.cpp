32ecdfb8fc4733b2d7d269c1dd4a95b234dfcfe2
armanpazouki <apazouki@gmail.com>
2014-11-06 18:27:17 -0600

    2D channel, but does not work

--- src/chrono_fsi/main.cpp
+++ src/chrono_fsi/main.cpp
@@ -1546,11 +1546,11 @@ int2 CreateFluidMarkers(thrust::host_vector<real3> & mPosRad,
 				real_ penDist = 0;
 				bool flag = true;
 				///penDist = IsInsideCurveOfSerpentineBeta(posRad);
-				penDist = IsInsideSerpentine(posRad);
+				///penDist = IsInsideSerpentine(posRad);
 				//*** paramsH.straightChannelBoundaryMin   should be taken care of
 				//*** paramsH.straightChannelBoundaryMax   should be taken care of
 				///penDist = IsInsideStraightChannel(posRad);
-				///penDist = IsInsideStraightChannel_XZ(posRad);
+				penDist = IsInsideStraightChannel_XZ(posRad);
 				///penDist = IsInsideTube(posRad);
 				///penDist = IsInsideStepTube(posRad);
 
@@ -2014,7 +2014,7 @@ int main() {
 			//***		paramsH.numBodies;
 			//***		paramsH.boxDims;
 		paramsH.sizeScale = 1; //don't change it.
-		paramsH.HSML = 0.0001;
+		paramsH.HSML = 0.00004;
 		paramsH.MULT_INITSPACE = 1.0;
 			paramsH.NUM_BOUNDARY_LAYERS = 3;
 			paramsH.toleranceZone = paramsH.NUM_BOUNDARY_LAYERS * (paramsH.HSML * paramsH.MULT_INITSPACE);
@@ -2030,10 +2030,10 @@ int main() {
 			paramsH.mu0 = .001;
 		paramsH.v_Max = 50e-3;//18e-3;//1.5;//2e-1; /*0.2 for Re = 100 */ //2e-3;
 			paramsH.EPS_XSPH = .5f;
-		paramsH.dT = 1e-4;//.001; //sph alone: .01 for Re 10;
+		paramsH.dT = 1e-5;//.001; //sph alone: .01 for Re 10;
 			paramsH.tFinal = 400;//20 * paramsH.dT; //400
-			paramsH.timePause = .0005 * paramsH.tFinal;//.0001 * paramsH.tFinal; // keep it as small as possible. the time step will be 1/10 * dT
-			paramsH.timePauseRigidFlex = .05 * paramsH.tFinal;
+			paramsH.timePause = 0;//.0005 * paramsH.tFinal;//.0001 * paramsH.tFinal; // keep it as small as possible. the time step will be 1/10 * dT
+			paramsH.timePauseRigidFlex = 0;//.05 * paramsH.tFinal;
 			paramsH.kdT = 5;
 			paramsH.gammaBB = 0.5;
 		// *** cMax cMin, see below
@@ -2042,8 +2042,8 @@ int main() {
 		// ************
 			paramsH.binSize0; // will be changed
 			paramsH.rigidRadius; //will be changed
-		paramsH.densityReinit = 0; //0: no re-initialization, 1: with initialization
-		paramsH.contactBoundary = 1; // 0: straight channel, 1: serpentine
+		paramsH.densityReinit = 1; //0: no re-initialization, 1: with initialization
+		paramsH.contactBoundary = 0; // 0: straight channel, 1: serpentine
 
 		flexParams.E = 2.0e5;
 		flexParams.r = paramsH.HSML * paramsH.MULT_INITSPACE * (paramsH.NUM_BCE_LAYERS - 1);
@@ -2068,19 +2068,19 @@ int main() {
 		//*** initialize straight channel
 //		paramsH.straightChannelBoundaryMin = R3(0, 0, 0) * paramsH.sizeScale;
 //		paramsH.straightChannelBoundaryMax = R3(paramsH.nPeriod * distance + 0, 1, 1) * paramsH.sizeScale;
-//		paramsH.straightChannelBoundaryMin = R3(0, 0, 0); //2D channel
-//		paramsH.straightChannelBoundaryMax = R3(1 * mm, .2 * mm, 1 * mm) * paramsH.sizeScale;
-		paramsH.straightChannelBoundaryMin = R3(0, 0, 0); //3D channel
-		paramsH.straightChannelBoundaryMax = R3(2 * mm, 1 * mm, 3 * mm) * paramsH.sizeScale;
+		paramsH.straightChannelBoundaryMin = R3(0, 0, 0); //2D channel
+		paramsH.straightChannelBoundaryMax = R3(1 * mm, .2 * mm, 1 * mm) * paramsH.sizeScale;
+//		paramsH.straightChannelBoundaryMin = R3(0, 0, 0); //3D channel
+//		paramsH.straightChannelBoundaryMax = R3(2 * mm, 1 * mm, 3 * mm) * paramsH.sizeScale;
 		//********************************************************************************************************
 		//**  reminiscent of the past******************************************************************************
 		//paramsH.cMax = R3( paramsH.nPeriod * 4.6 + 0, 1.5,  4.0) * paramsH.sizeScale;  //for only CurvedSerpentine (w/out straight part)
 		//**  end of reminiscent of the past   ******************************************************************
-		paramsH.cMin = R3(0, -2 * paramsH.toleranceZone, -4.5 * mm); 							//for serpentine
-		paramsH.cMax = R3( paramsH.nPeriod * sPeriod + r3_2.x + 2 * r4_2.x + r6_2.x + x_FirstChannel + 2 * x_SecondChannel, 1.5 * mm,  r6_2.y + 2 * paramsH.toleranceZone);  //for serpentine
+//		paramsH.cMin = R3(0, -2 * paramsH.toleranceZone, -4.5 * mm); 							//for serpentine
+//		paramsH.cMax = R3( paramsH.nPeriod * sPeriod + r3_2.x + 2 * r4_2.x + r6_2.x + x_FirstChannel + 2 * x_SecondChannel, 1.5 * mm,  r6_2.y + 2 * paramsH.toleranceZone);  //for serpentine
 
-//		paramsH.cMin = R3(0, 0, -2 * paramsH.toleranceZone);						// 2D channel
-//		paramsH.cMax = R3( 1 * mm, 0.2 * mm,  1 * mm + 2 * paramsH.toleranceZone);
+		paramsH.cMin = R3(0, 0, -2 * paramsH.toleranceZone);						// 2D channel
+		paramsH.cMax = R3( 1 * mm, 0.2 * mm,  1 * mm + 2 * paramsH.toleranceZone);
 
 //		paramsH.cMin = R3(0, -2 * paramsH.toleranceZone, -2 * paramsH.toleranceZone);						// 3D channel
 //		paramsH.cMax = R3( 2 * mm, 1 * mm + 2 * paramsH.toleranceZone,  3 * mm + 2 * paramsH.toleranceZone);
@@ -2104,7 +2104,7 @@ int main() {
 				(paramsH.cMax.z - paramsH.cMin.z) / side0.z);
 		paramsH.binSize0 = (binSize3.x > binSize3.y) ? binSize3.x : binSize3.y;
 	//	paramsH.binSize0 = (paramsH.binSize0 > binSize3.z) ? paramsH.binSize0 : binSize3.z;
-		paramsH.binSize0 = binSize3.x; //for effect of distance. Periodic BC in x direction. we do not care about paramsH.cMax y and z.
+		paramsH.binSize0 = binSize3.y; //for effect of distance. Periodic BC in x direction. we do not care about paramsH.cMax y and z.
 		paramsH.cMax = paramsH.cMin + paramsH.binSize0 * R3(side0);
 		paramsH.boxDims = paramsH.cMax - paramsH.cMin;
 		printf("sizes *** nP * sP + xFistChannel + xSecondChannel %f \n ", 7 * sPeriod + (r3_2.x + 2 * r4_2.x + r6_2.x) + x_FirstChannel + x_SecondChannel);
@@ -2113,7 +2113,7 @@ int main() {
 
 		//************************** modify pressure ***************************
 //		paramsH.deltaPress = paramsH.rho0 * paramsH.boxDims * paramsH.bodyForce3;  //did not work as I expected
-		paramsH.deltaPress = .9 * paramsH.boxDims * paramsH.bodyForce3;
+		paramsH.deltaPress = 1 * paramsH.boxDims * paramsH.bodyForce3;
 
 
 		printf("***** deltaP %f %f %f\n", paramsH.deltaPress.x, paramsH.deltaPress.y, paramsH.deltaPress.z);
