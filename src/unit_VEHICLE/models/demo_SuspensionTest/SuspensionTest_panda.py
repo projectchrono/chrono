8fd73ff59b2c57de94f6f3814ec8ce70fcbb0d6e
justin <jcmadsen@wisc.edu>
2014-11-13 16:45:31 -0600

    SuspensionTest saved data postprocessed plots using python.

--- src/unit_VEHICLE/models/demo_SuspensionTest/SuspensionTest_panda.py
+++ src/unit_VEHICLE/models/demo_SuspensionTest/SuspensionTest_panda.py
@@ -42,10 +42,20 @@ class SuspensionTest_panda:
             DF_curr = pd.read_csv(filename_list[i], header=0, sep=',')   # index_col=0,
             self._DF_list.append(DF_curr)
             self._leg_list.append(leg_list[i])
-     
+    
+    # @brief put the input values on the RHS y-axis
+    # returns the secondary y-axis
+    def _plot_inputs_twinx(self, ax, DF):
+        # create a 2nd axis for the steer and shaker inputs
+        ax_RHS = ax.twinx()
+        ax_RHS.plot(DF['time'], DF['steer'], 'r--',linewidth = 1.5, label = 'steer')
+        ax_RHS.plot(DF['time'], DF['postDisp_L'], 'g--',linewidth = 1.5, label = 'postDisp_L')
+        ax_RHS.plot(DF['time'], DF['postDisp_R'], 'b--',linewidth = 1.5, label = 'postDisp_R')
+        return ax_RHS
+    
     # @brief plot the inputs to the suspension test rig (steer and post displacement)       
     def plot_input(self):
-        figF = plt.figure()
+        # figF = plt.figure()
         
         df_in_list = []
         inDat = ['time','steer','postDisp_L','postDisp_R']
@@ -65,11 +75,83 @@ class SuspensionTest_panda:
              ax.plot(df_in_list[p]['time'], df_in_list[p]['postDisp_R'], linewidth = 1.5, label = 'PostDisp_R')
         
         ax.set_xlabel('time [s]')
-        ax.set_ylabel('steer [-], displacement [m]')
-        ax.legend(loc='upper left')
+        ax.set_ylabel('steer [-], displacement [in]')
+        ax.legend(loc='upper right')
         ax.set_title('test rig inputs')  
         
-     
+    # @brief plot the kingpin angle and offset, with the inputs overlayed on each
+    def plot_kingpin(self,file_number=0):
+        fig, axarr = plt.subplots(2,sharex=True)
+        
+        inDat = ['time','steer','postDisp_L','postDisp_R','KA_L','KA_R','Koff_L','Koff_R']
+        # add files to the DF list
+        DF = pd.DataFrame(self._DF_list[file_number], columns= inDat)
+            
+        DF.plot(ax = axarr[0], linewidth=1.5,x='time',y=['KA_L','KA_R'])
+        ax_rhs_top = self._plot_inputs_twinx(axarr[0],DF)
+        DF.plot(ax = axarr[1], linewidth=1.5,x='time',y=['Koff_L','Koff_R'])
+        ax_rhs_bot = self._plot_inputs_twinx(axarr[1],DF)
+    
+        # label axes, legends
+        axarr[0].set_xlabel('time [s]')
+        axarr[0].set_ylabel('Kingpin angle [deg]')
+        axarr[1].set_ylabel('offset [inches]')
+        ax_rhs_top.set_ylabel('steer [-], displacement [in]')
+        ax_rhs_bot.set_ylabel('steer [-], displacement [in]')
+        
+        axarr[0].legend(loc='upper left')
+        axarr[1].legend(loc='lower left')
+        # ax_rhs_top.legend(loc='lower right')
+        ax_rhs_bot.legend(loc='upper right')
+        axarr[0].set_title('Kingpin angle and offset')  
+        
+         
+    # @brief plot the caster angle and offset
+    def plot_caster(self,file_number=0):
+        fig, axarr = plt.subplots(2,sharex=True)
+        
+        inDat = ['time','steer','postDisp_L','postDisp_R','CA_L','CA_R','Coff_L','Coff_R']
+        # add files to the DF list
+        DF = pd.DataFrame(self._DF_list[file_number], columns= inDat)
+            
+        DF.plot(ax = axarr[0], linewidth=1.5,x='time',y=['CA_L','CA_R'])
+        ax_rhs_top = self._plot_inputs_twinx(axarr[0],DF)
+        DF.plot(ax = axarr[1], linewidth=1.5,x='time',y=['Coff_L','Coff_R'])
+        ax_rhs_bot = self._plot_inputs_twinx(axarr[1],DF)
+    
+        # label axes, legends
+        axarr[0].set_xlabel('time [s]')
+        axarr[0].set_ylabel('Caster angle [deg]')
+        axarr[1].set_ylabel('offset [inches]')
+        ax_rhs_top.set_ylabel('steer [-], displacement [in]')
+        ax_rhs_bot.set_ylabel('steer [-], displacement [in]')
+        
+        axarr[0].legend(loc='upper left')
+        axarr[1].legend(loc='lower left')
+        # ax_rhs_top.legend(loc='lower right')
+        ax_rhs_bot.legend(loc='center right')
+        axarr[0].set_title('Caster angle and offset')  
+        
+    # @brief plot the toe angle
+    def plot_toe(self,file_number=0):
+        # fig = plt.figure()
+        
+        inDat = ['time','steer','postDisp_L','postDisp_R','TA_L','TA_R']
+        # add files to the DF list
+        DF = pd.DataFrame(self._DF_list[file_number], columns= inDat)
+            
+        axis = DF.plot(linewidth=1.5,x='time',y=['TA_L','TA_R'])
+        ax_rhs = self._plot_inputs_twinx(axis,DF)
+    
+        # label axes, legends
+        axis.set_xlabel('time [s]')
+        axis.set_ylabel('Toe angle [deg]')
+
+        axis.set_title('Toe angle')
+        ax_rhs.set_ylabel('steer [-], displacement [in]')        
+        axis.legend(loc='upper left')
+        ax_rhs.legend(loc='center right')
+
       
 if __name__ == '__main__':
     
@@ -93,9 +175,10 @@ if __name__ == '__main__':
     sTest = SuspensionTest_panda([data_dir+steer_data], ["dPost = 0"])
     sTest.plot_input()
     
-    # sTest.plot_kingpin()
-    # sTest.plot_caster()
-    # sTest.plot_toe()
+    sTest.plot_kingpin()
+    sTest.plot_caster()
+    sTest.plot_toe()
+    
     # sTest.plot_spring()
 
 py.show()
