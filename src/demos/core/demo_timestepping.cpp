07782ab22247b72895b13d2f93e8f19003c1cd05
Alessandro Tasora <tasora@ied.unipr.it>
2014-11-27 20:00:06 +0100

    Improved demos

--- src/demos/core/demo_timestepping.cpp
+++ src/demos/core/demo_timestepping.cpp
@@ -70,13 +70,16 @@ int main(int argc, char* argv[])
 				}
 		};
 
+							// File to dump results
+		ChStreamOutAsciiFile log_file1("log_timestepper_1.dat");
+
 
 							// Create and object from your custom integrable class:
 		MyIntegrable mintegrable;
 
-							// Create a time-integrator:
+							// Create a time-integrator, class: Eulero explicit
 		ChTimestepperEuleroExpl mystepper(mintegrable);
-		//ChTimestepperRungeKuttaExpl mystepper(mintegrable);
+
 
 							// Execute the time integration
 		while (mystepper.GetTime() <4)
@@ -87,18 +90,19 @@ int main(int argc, char* argv[])
 			GetLog() << " T = " << mystepper.GetTime() 
 					 << "  x="  << mystepper.Y()(0) 
 					 << "  x_exact="<< exact_solution << "\n";
+			log_file1 << mystepper.GetTime() << ", " << mystepper.Y()(0) << ", " << exact_solution << "\n";
 		}
 
 	}
 
 
-	if (false)
+	if (true)
 	{
 		//
 		// EXAMPLE 2:
 		//
  
-		GetLog() << " Example 2: integrate 2nd order oscillator: M*ddx/dtdt + R*dx/dt + K*w = F(t) \n";
+		GetLog() << "\n\n Example 2: integrate 2nd order oscillator: M*ddx/dtdt + R*dx/dt + K*w = F(t) with a 1st order integrator. \n";
 
 
 						// Define a class inherited from ChIntegrable,
@@ -162,22 +166,150 @@ int main(int argc, char* argv[])
 				}
 		};
 
+		// File to dump results
+		ChStreamOutAsciiFile log_file2("log_timestepper_2.dat");
+
+
+		// Try integrator Eulero explicit
 
 							// Create and object from your custom integrable class:
 		MyIntegrable mintegrable;
-
 							// Create a time-integrator:
 		ChTimestepperEuleroExpl mystepper(mintegrable);
+
+
+		// Try integrator Runge Kutta 4st  explicit
+
+							// Create and object from your custom integrable class:
+		MyIntegrable mintegrable_rk;
+							// Create a time-integrator, class: Runge Kutta 4 explicit
+		ChTimestepperRungeKuttaExpl mystepper_rk(mintegrable_rk);
+
+
+
 
 							// Execute the time integration
 		while (mystepper.GetTime() <1)
 		{
 			mystepper.Advance(0.01);
+			mystepper_rk.Advance(0.01);
 
 			GetLog() << " T = " << mystepper.GetTime() << "  x=" << mystepper.Y()(0) << "  v=" << mystepper.Y()(1) << "\n";
+			log_file2 << mystepper.GetTime() << ", " << mystepper.Y()(0)    << ", " << mystepper.Y()(1) 
+				                             << ", " << mystepper_rk.Y()(0) << ", " << mystepper_rk.Y()(1) << "\n";
 		}
 
 	}
+
+
+
+
+	if (true)
+	{
+		//
+		// EXAMPLE 3:
+		//
+
+		GetLog() << "\n\n Example 3: integrate 2nd order oscillator: M*ddx/dtdt + R*dx/dt + K*w = F(t) with a 2nd order integrator. \n";
+
+
+		// Define a class inherited from ChIntegrableIIorder,
+		// it will represent the differential equations
+		// by implementing the StateSolveA() function, and few other interfaces.
+		// Compared to previous example 2, this is inherited from ChIntegrableIIorder,
+		// so one can use more advanced integrators such as ChTimestepperEuleroSemiImplicit,
+		// that can exploit the II order nature of the problem. Anyway, also all I order
+		// integrators can still be used.
+		class MyIntegrable : public ChIntegrableIIorder
+		{
+		private:
+			double M;
+			double K;
+			double R;
+			double T;
+			double mx;
+			double mv;
+		public:
+			MyIntegrable()
+			{
+				T = 0;
+				M = 10;
+				K = 30;
+				R = 1;
+				mx = 0;
+				mv = 0;
+			}
+
+			/// the number of coordinates in the state, x position part:
+			virtual int GetNcoords_x() { return 1; }
+
+			/// system -> state
+			virtual void StateGather(ChState& x, ChStateDelta& v, double& mT)
+			{
+				x(0) = mx;
+				v(0) = mv;
+				mT = T;
+			};
+
+			/// state -> system  
+			virtual void StateScatter(const ChState& x, const ChStateDelta& v, const double mT)
+			{
+				mx = x(0);
+				mv = v(0);
+				T = mT;
+			};
+
+			/// compute  dy/dt=f(y,t) 
+			virtual void StateSolveA(ChStateDelta& Dv,		///< result: computed Dv
+				ChVectorDynamic<>& L,	///< result: computed lagrangian multipliers, if any
+				const ChState& x,		///< current state, x
+				const ChStateDelta& v,	///< current state, v
+				const double T,		///< current time T
+				const double dt,	///< timestep (if needed)
+				bool force_state_scatter = true ///< if false, y and T are not scattered to the system, assuming that someone has done StateScatter just before
+				)
+			{
+				if (force_state_scatter)
+					StateScatter(x, v, T);
+				double F = sin(T * 4) * 2;
+				Dv(0) = dt*(1. / M) * (F - K*mx - R*mv);
+			}
+		};
+
+
+		// Create a file to dump results
+		ChStreamOutAsciiFile log_file3("log_timestepper_3.dat");
+
+
+		// Create and object from your custom integrable class:
+		MyIntegrable mintegrable1;
+		MyIntegrable mintegrable2;
+		MyIntegrable mintegrable3;
+
+		// Create few time-integrators to be compared:
+		ChTimestepperEuleroExpl		     mystepper1(mintegrable1);
+		ChTimestepperEuleroExplIIorder   mystepper2(mintegrable2);
+		ChTimestepperEuleroSemiImplicit  mystepper3(mintegrable3);
+
+		
+		// Execute the time integration
+		while (mystepper1.GetTime() <1)
+		{
+			mystepper1.Advance(0.01);
+			mystepper2.Advance(0.01);
+			mystepper3.Advance(0.01);
+
+			GetLog() << "T = " << mystepper1.GetTime() << "  x=" << mystepper1.Y()(0) << "  v=" << mystepper1.Y()(1) << "\n";
+			log_file3 << mystepper1.GetTime()	<< ", " << mystepper1.Y()(0) << ", " << mystepper1.Y()(1)
+												<< ", " << mystepper2.X()(0) << ", " << mystepper2.V()(0)
+												<< ", " << mystepper3.X()(0) << ", " << mystepper3.V()(0)
+												<< "\n";
+		}
+
+	}
+
+
+
 
 
 	GetLog() << "\n  CHRONO execution terminated.";
