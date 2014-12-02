b3e3e68a58b42922c308c69223132939b5c1d98f
justin <jcmadsen@wisc.edu>
2014-12-02 17:29:39 -0600

    add variables read in from JSON file

--- src/tracked_vehicles/subsys/trackSystem/TrackSystem.h
+++ src/tracked_vehicles/subsys/trackSystem/TrackSystem.h
@@ -36,20 +36,54 @@ public:
 
   TrackSystem(const std::string& filename);
   TrackSystem(const rapidjson::Document& d);
-  virtual ~TrackSystem() {}
+  ~TrackSystem() {}
 
 
+  void Initialize(ChSharedPtr<ChBodyAuxRef> chassis,
+				 const ChVector<>&         location,
+				 const ChQuaternion<>&     rotation));
+  
 private:
 
+  // private functions
   void Create(const rapidjson::Document& d);
 
-
+  // private variables
   ChSharedPtr<DriveGear> m_driveGear;
   ChSharedPtr<Idler>	m_idler;
   ChSharedPtr<TrackChain> m_chain;
   std::vector<ChSharedPtr<TorsionArmSuspension>> m_suspensions;
-  
   std::vector<ChSharedPtr<ChBody>> m_supportRollers;
+  
+  // idler
+  double m_idlerMass;
+  ChVector<> m_idlerInertia;
+  ChVector<> m_idlerPos
+  double m_idlerRadius;
+  double m_idlerWidth;
+  double m_idler_K;
+  double m_idler_C;
+  
+  // drive gear
+  double m_gearMass;
+  ChVector<> m_gearPos;
+  double m_gearRadius;
+  ChVector<> m_gearInertia;
+  
+  // Support rollers
+  double m_rollerMass;
+  double m_rollerRadius;
+  double m_rollerWidth;
+  ChVector<> m_rollerInertia;
+  std::vector<ChVector<> > m_rollerLocs;
+  
+  // suspension
+  std::string m_suspensionFilename;
+  std::vector<ChVector<> > m_suspensionLocs;
+  
+  // Track Chain
+  std::string m_trackChainFilename;
+  
 };
 
 
