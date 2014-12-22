223db50d7f8aa7203e5f39fb869225a26c859797
justin <jcmadsen@wisc.edu>
2014-12-22 11:25:21 -0600

    Initialize function added

--- src/demos/trackedVehicle/subsys/driveGear/DriveGear.cpp
+++ src/demos/trackedVehicle/subsys/driveGear/DriveGear.cpp
@@ -18,7 +18,19 @@
 
 #include "DriveGear.h"
 
+#include "assets/ChCylinderShape.h"
+#include "assets/ChTriangleMeshShape.h"
+#include "assets/ChTexture.h"
+#include "assets/ChColorAsset.h"
+
+#include "utils/ChUtilsData.h"
+#include "utils/ChUtilsInputOutput.h"
+
 namespace chrono {
+
+// static variables
+const std::string DriveGear::m_meshName = "wheel_L_POV_geom";
+const std::string DriveGear::m_meshFile = utils::GetModelDataFile("hmmwv/wheel_L.obj");
 
 
 DriveGear::DriveGear(double mass, const ChVector<>& inertia, double rad, double width)
@@ -29,5 +41,44 @@ DriveGear::DriveGear(double mass, const ChVector<>& inertia, double rad, double 
   m_gear->SetInertiaXX(inertia);
 }
 
+void DriveGear::Initialize(ChSharedPtr<ChBodyAuxRef> chassis, const ChVector<>& pos, const ChQuaternion<>& rot)
+{
+
+  // 0 = none, 1 = primitive, 2 = mesh
+  m_visType = 0;
+
+  // Attach visualization
+  switch (m_visType) {
+  case 1:
+  {
+    ChSharedPtr<ChCylinderShape> cyl(new ChCylinderShape);
+    cyl->GetCylinderGeometry().rad = m_radius;
+    cyl->GetCylinderGeometry().p1 = ChVector<>(0, m_width / 2, 0);
+    cyl->GetCylinderGeometry().p2 = ChVector<>(0, -m_width / 2, 0);
+    m_gear->AddAsset(cyl);
+
+    ChSharedPtr<ChTexture> tex(new ChTexture);
+    tex->SetTextureFilename(GetChronoDataFile("bluwhite.png"));
+    m_gear->AddAsset(tex);
+
+    break;
+  }
+  case 2:
+  {
+    geometry::ChTriangleMeshConnected trimesh;
+    trimesh.LoadWavefrontMesh(getMeshFile(), false, false);
+
+    ChSharedPtr<ChTriangleMeshShape> trimesh_shape(new ChTriangleMeshShape);
+    trimesh_shape->SetMesh(trimesh);
+    trimesh_shape->SetName(getMeshName());
+    m_gear->AddAsset(trimesh_shape);
+
+    ChSharedPtr<ChColorAsset> mcolor(new ChColorAsset(0.3f, 0.3f, 0.3f));
+    m_gear->AddAsset(mcolor);
+
+    break;
+  }
+  }
+}
 
 } // end namespace chrono
