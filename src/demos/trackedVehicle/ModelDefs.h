079b16cf334d8983e74c913590a53466bf99e14a
justin <jcmadsen@wisc.edu>
2014-12-24 13:01:29 -0600

    model definitions for setting visual and collision types

---
+++ src/demos/trackedVehicle/ModelDefs.h
@@ -0,0 +1,35 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Justin Madsen, Radu Serban
+// =============================================================================
+//	Common enums for user input to public TrackedVehicle functions
+// =============================================================================
+
+#ifndef MODEL_DEFS_H
+#define MODEL_DEFS_H
+
+// #include "ChronoT_config.h"
+
+enum struct VisualizationType {
+  NONE,
+  PRIMITIVES,
+  MESH
+};
+
+enum struct CollisionType {
+  NONE,
+  PRIMITIVES,
+  MESH,
+  CONVEXHULL
+};
+
+#endif
