5dc19e40667e3b0872738f3eb30ca1aed8e722cd
Hammad Mazhar <hammad.maz@gmail.com>
2015-01-08 15:58:18 -0600

    Preliminary update to the base solver class
    Features not supported have been commented out for now

--- src/unit_PARALLEL/chrono_parallel/solver/ChSolverParallel.h
+++ src/unit_PARALLEL/chrono_parallel/solver/ChSolverParallel.h
@@ -19,173 +19,134 @@
 #ifndef CHSOLVERPARALLEL_H
 #define CHSOLVERPARALLEL_H
 
-#include "chrono_parallel/ChParallelDefines.h"
 #include "chrono_parallel/ChBaseParallel.h"
 #include "chrono_parallel/ChDataManager.h"
 #include "chrono_parallel/math/ChParallelMath.h"
-#include "chrono_parallel/math/ChThrustLinearAlgebra.h"
 #include "chrono_parallel/constraints/ChConstraintRigidRigid.h"
+#include "chrono_parallel/constraints/ChConstraintRigidFluid.h"
+#include "chrono_parallel/constraints/ChConstraintFluidFluid.h"
 #include "chrono_parallel/constraints/ChConstraintBilateral.h"
-#include "chrono_parallel/collision/ChCNarrowphaseMPR.h"
+
 namespace chrono {
 class CH_PARALLEL_API ChSolverParallel : public ChBaseParallel {
  public:
-   ChSolverParallel();
-   ~ChSolverParallel();
+  ChSolverParallel();
+  virtual ~ChSolverParallel();
 
-   // At the beginning of the step reset the size/indexing variables,
-   // resize for new contact list and clear temporary accumulation variables
-   void Setup(ChParallelDataManager *data_container_  //pointer to data container
-   );
+  // At the beginning of the step reset the size/indexing variables,
+  // resize for new contact list and clear temporary accumulation variables
+  void Setup(ChParallelDataManager* data_container_    // pointer to data container
+             );
 
-   // Project the lagrange multipliers (gamma) onto the friction cone.
-   void Project(real* gamma  //Lagrange Multipliers
-   );
+  // Project the lagrange multipliers (gamma) onto the friction cone.
+  void Project(real* gamma    // Lagrange Multipliers
+               );
 
-   // Project a single contact
-   void Project_Single(int index,  //index of contact
-                       real* gamma  //Lagrange Multipliers
-                       );
+  // Project a single contact
+  void Project_Single(int index,     // index of contact
+                      real* gamma    // Lagrange Multipliers
+                      );
 
-   // Compute the first half of the shur matrix vector multiplication (N*x)
-   // Perform M_invDx=M^-1*D*x
-   void shurA(real*x  //Vector that N is multiplied by
-   );
+  // Compute the first half of the shur matrix vector multiplication (N*x)
+  // Perform M_invDx=M^-1*D*x
+  void shurA(blaze::DynamicVector<real>& x, blaze::DynamicVector<real>& out    // Vector that N is multiplied by
+             );
 
-   // Compute second half of shur matrix vector multiplication Nx=D*M_invDx
-   void shurB(real* x,   //Vector that contains M_invDx
-              real* out  //N*x
-              );
+  // Compute rhs value with relaxation term
+  void ComputeSRhs(custom_vector<real>& gamma, const custom_vector<real>& rhs, custom_vector<real3>& vel_data, custom_vector<real3>& omg_data, custom_vector<real>& b);
 
-   // Compute rhs value with relaxation term
-   void ComputeSRhs(
-                    custom_vector<real>& gamma,
-                    const custom_vector<real>& rhs,
-                    custom_vector<real3>& vel_data,
-                    custom_vector<real3>& omg_data,
-                    custom_vector<real>& b);
+  //   // Perform M^-1*D*gamma and compute the linear and rotational velocity
+  //   vectors
+  //   void ComputeImpulses(
+  //                        custom_vector<real>& gamma,
+  //                        custom_vector<real3>& vel_data,
+  //                        custom_vector<real3>& omg_data);
 
-   // Perform M^-1*D*gamma and increment the linear and rotational velocity vectors
-   void ComputeImpulses();
+  // Function that performs time integration to get the new positions
+  // Used when contacts need to be updated within the solver
+  // Function is similar to compute impulses
+  void UpdatePosition(custom_vector<real>& x    // Lagrange multipliers
+                      );
 
-   // Perform M^-1*D*gamma and compute the linear and rotational velocity vectors
-   void ComputeImpulses(
-                        custom_vector<real>& gamma,
-                        custom_vector<real3>& vel_data,
-                        custom_vector<real3>& omg_data);
+  // Rerun the narrowphase to get the new contact list, broadphase is not run
+  // again here
+  // This assumes that the positions did not drastically change.
+  void UpdateContacts();
 
-   // Function that performs time integration to get the new positions
-   // Used when contacts need to be updated within the solver
-   // Function is similar to compute impulses
-   void UpdatePosition(custom_vector<real>& x   //Lagrange multipliers
-   );
+  // Compute the full shur matrix vector product (N*x) where N=D^T*M^-1*D
+  void ShurProduct(const blaze::DynamicVector<real>& x,    // Vector that will be multiplied by N
+                   blaze::DynamicVector<real>& AX          // Output Result
+                   );
 
-   // Rerun the narrowphase to get the new contact list, broadphase is not run again here
-   // This assumes that the positions did not drastically change.
-   void UpdateContacts();
+  // Compute the shur matrix vector product only for the bilaterals (N*x)
+  // where N=D^T*M^-1*D
+  template <class T>
+  void ShurBilaterals(T& x, blaze::DynamicVector<real>& output) {
+    blaze::SparseSubmatrix<CompressedMatrix<real> > D_T_sub =
+        blaze::submatrix(data_container->host_data.D_T, data_container->num_unilaterals, 0, data_container->num_bilaterals, data_container->num_bodies * 6);
 
-   // Compute the full shur matrix vector product (N*x) where N=D^T*M^-1*D
-   void ShurProduct(
-                    custom_vector<real>& x,  //Vector that will be multiplied by N
-                    custom_vector<real>& AX  //Output Result
-                    );
+    blaze::SparseSubmatrix<CompressedMatrix<real> > M_invD_sub =
+        blaze::submatrix(data_container->host_data.M_invD, 0, data_container->num_unilaterals, data_container->num_bodies * 6, data_container->num_bilaterals);
+    output = D_T_sub * M_invD_sub * x;
+  }
 
-   // Compute the shur matrix vector product only for the bilaterals (N*x)
-   // where N=D^T*M^-1*D
-   void ShurBilaterals(
-                       custom_vector<real> &x,   //Vector that will be multiplied by N
-                       custom_vector<real> & AX  //Output Result
-                       );
+  // Call this function with an associated solver type to solve the system
+  virtual void Solve() = 0;
 
-   // Call this function with an associated solver type to solve the system
-   virtual void Solve()=0;
+  // Perform velocity stabilization on bilateral constraints
+  uint SolveStab(const uint max_iter,                                     // Maximum number of iterations
+                 const uint size,                                         // Number of unknowns
+                 const blaze::DenseSubvector<DynamicVector<real> >& b,    // Rhs vector
+                 blaze::DenseSubvector<DynamicVector<real> >& x           // The vector of unknowns
+                 );
 
-   // Perform velocity stabilization on bilateral constraints
-   uint SolveStab(const uint max_iter,           // Maximum number of iterations
-                  const uint size,               // Number of unknowns
-                  const custom_vector<real> &b,  // Rhs vector
-                  custom_vector<real> &x         // The vector of unknowns
-                  );
+  // Get the number of iterations perfomed by the solver
+  int GetIteration() { return current_iteration; }
+  // Get the current residual
+  real GetResidual() { return residual; }
 
-   // Get the number of iterations perfomed by the solver
-   int GetIteration() {
-      return current_iteration;
-   }
-   // Get the current residual
-   real GetResidual() {
-      return residual;
-   }
-   real GetObjective(custom_vector<real> & x,
-                     custom_vector<real> & b) {
-      custom_vector<real> Nl(x.size());
-      // f_p = 0.5*l_candidate'*N*l_candidate - l_candidate'*b  = l_candidate'*(0.5*Nl_candidate - b);
-      ShurProduct(x, Nl);     // 1)  g_tmp = N*l_candidate ...        #### MATR.MULTIPLICATION!!!###
-      SEAXMY(0.5, Nl, b, Nl);  // 2) 0.5*N*l_candidate-b_shur
-      return Dot(x, Nl);      // 3)  mf_p  = l_candidate'*(0.5*N*l_candidate-b_shur)
+  real GetObjectiveBlaze(blaze::DynamicVector<real>& x, blaze::DynamicVector<real>& b) {
+    blaze::DynamicVector<real> Nl(x.size());
+    // f_p = 0.5*l_candidate'*N*l_candidate - l_candidate'*b  =
+    // l_candidate'*(0.5*Nl_candidate - b);
+    Nl = data_container->host_data.D_T * (data_container->host_data.M_invD * x);    // 1)  g_tmp = N*l_candidate
+                                                                                    // ...        ####
+                                                                                    // MATR.MULTIPLICATION!!!###
+    Nl = 0.5 * Nl - b;                                                              // 2) 0.5*N*l_candidate-b_shur
+    return (x, Nl);                                                                 // 3)  mf_p  = l_candidate'*(0.5*N*l_candidate-b_shur)
+  }
 
-   }
-   real GetObjectiveBlaze(blaze::DynamicVector<real> & x,
-                          blaze::DynamicVector<real> & b) {
-      blaze::DynamicVector<real> Nl(x.size());
-      // f_p = 0.5*l_candidate'*N*l_candidate - l_candidate'*b  = l_candidate'*(0.5*Nl_candidate - b);
-      Nl = data_container->host_data.D_T * (data_container->host_data.M_invD * x);  // 1)  g_tmp = N*l_candidate ...        #### MATR.MULTIPLICATION!!!###
-      Nl = 0.5 * Nl - b;          // 2) 0.5*N*l_candidate-b_shur
-      return (x, Nl);            // 3)  mf_p  = l_candidate'*(0.5*N*l_candidate-b_shur)
-   }
-   real GetObjective() {
-      custom_vector<real> Nl(data_container->host_data.gamma_data.size());
-      // f_p = 0.5*l_candidate'*N*l_candidate - l_candidate'*b  = l_candidate'*(0.5*Nl_candidate - b);
-      ShurProduct(data_container->host_data.gamma_data, Nl);    // 1)  g_tmp = N*l_candidate ...        #### MATR.MULTIPLICATION!!!###
-      SEAXMY(0.5, Nl, data_container->host_data.rhs_data, Nl);  // 2) 0.5*N*l_candidate-b_shur
-      return Dot(data_container->host_data.gamma_data, Nl);     // 3)  obj  = l_candidate'*(0.5*N*l_candidate-b_shur)
+  real Res4Blaze(blaze::DynamicVector<real>& x, blaze::DynamicVector<real>& b) {
+    real gdiff = .1;
+    blaze::DynamicVector<real> inside = x - gdiff * (data_container->host_data.D_T * (data_container->host_data.M_invD * x) - b);
+    Project(inside.data());
+    blaze::DynamicVector<real> temp = (x - inside) / (x.size() * gdiff);
+    return sqrt((temp, temp));
+  }
 
-   }
-   real Res4Blaze(blaze::DynamicVector<real> & x,
-                  blaze::DynamicVector<real> & b) {
-      real gdiff = .1;
-      blaze::DynamicVector<real> inside = x - gdiff * (data_container->host_data.D_T * (data_container->host_data.M_invD * x) - b);
-      Project(inside.data());
-      blaze::DynamicVector<real> temp = (x - inside) / (x.size() * gdiff);
-      return sqrt((temp, temp));
-   }
+  void AtIterationEnd(real maxd, real maxdeltalambda, int iter) {
+    maxd_hist.push_back(maxd);
+    maxdeltalambda_hist.push_back(maxdeltalambda);
+    iter_hist.push_back(iter);
+  }
 
-   real GetKE() {
-      real kinetic_energy = 0;
+  // Set the maximum number of iterations for all solvers
+  void SetMaxIterations(const int max_iteration_value) { max_iteration = max_iteration_value; }
 
-      for (int i = 0; i < data_container->num_bodies; i++) {
-         real v = length(data_container->host_data.vel_data[i]);
-         kinetic_energy += 0.5 * 1.0 / data_container->host_data.mass_data[i] * v * v;
-      }
-      return kinetic_energy;
+  int current_iteration;    // The current iteration number of the solver
+  int max_iteration;        // The maximum number of iterations that the solver will
+                            // perform
+  int total_iteration;      // The total number of iterations performed, this
+                            // variable accumulates
+  real residual;            // Current residual for the solver
+  real objective_value;
 
-   }
+  // These three variables are used to store the convergence history of the
+  // solver
+  custom_vector<real> maxd_hist, maxdeltalambda_hist, iter_hist;
 
-   void AtIterationEnd(real maxd,
-                       real maxdeltalambda,
-                       int iter) {
-      maxd_hist.push_back(maxd);
-      maxdeltalambda_hist.push_back(maxdeltalambda);
-      iter_hist.push_back(iter);
-   }
-
-
-   // Set the maximum number of iterations for all solvers
-   void SetMaxIterations(const int max_iteration_value) {
-      max_iteration = max_iteration_value;
-   }
-
-   int current_iteration;  // The current iteration number of the solver
-   int max_iteration;     // The maximum number of iterations that the solver will perform
-   int total_iteration;   // The total number of iterations performed, this variable accumulates
-   real residual;         // Current residual for the solver
-   real objective_value;
-
-   //These three variables are used to store the convergence history of the solver
-   thrust::host_vector<real> maxd_hist, maxdeltalambda_hist, iter_hist;
-
-   ChConstraintRigidRigid *rigid_rigid;
-   ChConstraintBilateral *bilateral;
-
+  ChConstraintRigidRigid* rigid_rigid;
+  ChConstraintBilateral* bilateral;
 };
 }
 
