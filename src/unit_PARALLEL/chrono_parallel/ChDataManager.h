490ac71bed3b69e6c4c49c82fb0a04bb50d1f78c
Hammad Mazhar <hammad.maz@gmail.com>
2015-01-08 14:03:28 -0600

    Remove unnecessary variables as most of them will be transitioned into the blaze data structures

--- src/unit_PARALLEL/chrono_parallel/ChDataManager.h
+++ src/unit_PARALLEL/chrono_parallel/ChDataManager.h
@@ -32,7 +32,7 @@ using blaze::DynamicVector;
 namespace chrono {
 
 struct host_container {
-   //collision data
+   // Collision data
    thrust::host_vector<real3> ObA_rigid;
    thrust::host_vector<real3> ObB_rigid;
    thrust::host_vector<real3> ObC_rigid;
@@ -42,44 +42,40 @@ struct host_container {
    thrust::host_vector<uint> id_rigid;
    thrust::host_vector<real3> aabb_rigid;
 
-   //contact data
+   // Contact data
    thrust::host_vector<real3> norm_rigid_rigid;
-   thrust::host_vector<real3> old_norm_rigid_rigid;
    thrust::host_vector<real3> cpta_rigid_rigid;
    thrust::host_vector<real3> cptb_rigid_rigid;
    thrust::host_vector<real> dpth_rigid_rigid;
    thrust::host_vector<real> erad_rigid_rigid;
    thrust::host_vector<int2> bids_rigid_rigid;
    thrust::host_vector<long long> pair_rigid_rigid;
-   thrust::host_vector<long long> old_pair_rigid_rigid;
 
+   // This vector holds the friction information as a triplet
+   // x - Sliding friction
+   // y - Rolling friction
+   // z - Spinning Friction
+   // This is precomputed at every timestep for all contacts in parallel
+   // Improves performance and reduces conditionals later on
    thrust::host_vector<real3> fric_rigid_rigid;
+   // Holds the cohesion value for each contact, similar to friction this is
+   // precomputed for all contacts in parallel
+   thrust::host_vector<real> coh_rigid_rigid;
 
-   thrust::host_vector<real> gamma_data;
-   thrust::host_vector<real> old_gamma_data;
-
-   //object data
-   thrust::host_vector<real3> vel_data, vel_new_data;
-   thrust::host_vector<real3> omg_data, omg_new_data;
+   // Object data
    thrust::host_vector<real3> pos_data, pos_new_data;
    thrust::host_vector<real4> rot_data, rot_new_data;
    thrust::host_vector<real3> inr_data;
-   thrust::host_vector<real3> frc_data;
-   thrust::host_vector<real3> trq_data;
    thrust::host_vector<bool> active_data;
    thrust::host_vector<bool> collide_data;
    thrust::host_vector<real> mass_data;
 
-   // shaft data
+   // Shaft data
    thrust::host_vector<real> shaft_rot;    // shaft rotation angles
    thrust::host_vector<real> shaft_omg;    // shaft angular velocities
    thrust::host_vector<real> shaft_trq;    // shaft torques
    thrust::host_vector<real> shaft_inr;    // shaft inverse inertias
    thrust::host_vector<bool> shaft_active; // shaft active (not sleeping nor fixed) flags
-
-   thrust::host_vector<real3> lim_data;
-   thrust::host_vector<real3> gyr_data;
-   thrust::host_vector<real> bin_number;
 
    // Material properties (DVI)
    thrust::host_vector<real3> fric_data;
@@ -92,24 +88,13 @@ struct host_container {
    thrust::host_vector<real> alpha;            // Dissipation factor (Hunt-Crossley)
    thrust::host_vector<real> cr;               // Coefficient of restitution
 
-   thrust::host_vector<real> rhs_data;
-   thrust::host_vector<real> diag;
-   thrust::host_vector<real3> QXYZ_data, QUVW_data;
-
-   thrust::host_vector<real3> JXYZA_bilateral, JXYZB_bilateral;
-   thrust::host_vector<real3> JUVWA_bilateral, JUVWB_bilateral;
-   thrust::host_vector<real> residual_bilateral;
-   thrust::host_vector<real> correction_bilateral;
-   thrust::host_vector<int2> bids_bilateral;
-   thrust::host_vector<real> gamma_bilateral;
-
    //This matrix, if used will hold D^TxM^-1xD in sparse form
    CompressedMatrix<real> Nshur;
    //The D Matrix hold the Jacobian for the entire system
    CompressedMatrix<real> D;
    //D_T is the transpose of the D matrix, note that D_T is actually computed
    //first and D is taken as the transpose. This is due to the way that blaze
-   //handles sparse matrix alloction, it is easier to do it on a per row basis
+   //handles sparse matrix allocation, it is easier to do it on a per row basis
    CompressedMatrix<real> D_T;
    //M_inv is the inver mass matrix, This matrix, if holding the full inertia
    //tensor is block diagonal
@@ -126,12 +111,13 @@ struct host_container {
    DynamicVector<real> M_invk; //result of M_inv multiplied by vector of forces
    DynamicVector<real> gamma; //THe unknowns we are solving for
    DynamicVector<real> v; //This vector holds the velocities for all objects
-   DynamicVector<real> hf;//This vector holds h*forces, h is timestep
+   DynamicVector<real> hf;//This vector holds h*forces, h is time step
    DynamicVector<real> rhs_bilateral;
    //While E is the compliance matrix, in reality it is completely diagonal
    //therefore it is stored in a vector for performance reasons
    DynamicVector<real> E;
-   //DynamicVector<real> gamma_bilateral;
+   //Vector that stores the gamma values for just the bilaterals
+   DynamicVector<real> gamma_bilateral;
 
 };
 
