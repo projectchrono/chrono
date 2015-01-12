6850cbc92124e71e28a6ae39ddafce1efcf889d5
Radu Serban <serban@engr.wisc.edu>
2015-01-12 00:19:35 -0600

    Remove unnecessary code.

--- src/unit_PARALLEL/chrono_parallel/collision/ChContactContainerParallel.cpp
+++ src/unit_PARALLEL/chrono_parallel/collision/ChContactContainerParallel.cpp
@@ -24,10 +24,6 @@ namespace chrono {
 using namespace collision;
 using namespace geometry;
 
-// Register into the object factory, to enable run-time
-// dynamic creation and persistence
-ChClassRegister<ChContactContainerParallel> a_registration_ChContactContainerGPU;
-
 ChContactContainerParallel::ChContactContainerParallel(ChParallelDataManager* dc)
 : data_container(dc)
 {
