f0cd6e3753d6425ef60ea047f02774feca54c614
Radu Serban <serban@engr.wisc.edu>
2015-01-13 12:48:21 -0600

    Streamline the code for bilateral Jacobian in preparation for including additional constraint types.

--- src/unit_PARALLEL/chrono_parallel/constraints/ChConstraintBilateral.cpp
+++ src/unit_PARALLEL/chrono_parallel/constraints/ChConstraintBilateral.cpp
@@ -6,114 +6,104 @@ using namespace chrono;
 using blaze::DenseSubvector;
 using blaze::subvector;
 
-void ChConstraintBilateral::Build_b() {
-  DenseSubvector<DynamicVector<real> > b = subvector(data_container->host_data.b, data_container->num_unilaterals, data_container->num_bilaterals);
-  std::vector<ChLcpConstraint*>& mconstraints = (*data_container->lcp_system_descriptor).GetConstraintsList();
+void ChConstraintBilateral::Build_b()
+{
+  std::vector<ChLcpConstraint*>& mconstraints = data_container->lcp_system_descriptor->GetConstraintsList();
+
 #pragma omp parallel for
   for (int index = 0; index < num_bilaterals; index++) {
     int cntr = data_container->host_data.bilateral_mapping[index];
     ChLcpConstraintTwoBodies* mbilateral = (ChLcpConstraintTwoBodies*)(mconstraints[cntr]);
-    b[index] = mbilateral->Get_b_i();
+    data_container->host_data.b[index + num_unilaterals] = mbilateral->Get_b_i();
   }
 }
 
-void ChConstraintBilateral::Build_E() {
+void ChConstraintBilateral::Build_E()
+{
 #pragma omp parallel for
   for (int index = 0; index < num_bilaterals; index++) {
     data_container->host_data.E[index + num_unilaterals] = 0;
   }
 }
 
-void ChConstraintBilateral::Build_D() {
+void ChConstraintBilateral::Build_D()
+{
   data_container->host_data.gamma_bilateral.resize(num_bilaterals);
 
-  ChLcpSystemDescriptor* lcp_sys = data_container->lcp_system_descriptor;
-  std::vector<ChLcpConstraint*>& mconstraints = (*lcp_sys).GetConstraintsList();
+  // Grab the list of all bilateral constraints present in the system
+  // (note that this includes possibly inactive constraints)
+  std::vector<ChLcpConstraint*>& mconstraints = data_container->lcp_system_descriptor->GetConstraintsList();
 
+  // Loop over the active constraints and fill in the rows of the Jacobian,
+  // taking into account the type of each constraint.
   CompressedMatrix<real>& D_T = data_container->host_data.D_T;
 #pragma omp parallel for
   for (int index = 0; index < num_bilaterals; index++) {
     int cntr = data_container->host_data.bilateral_mapping[index];
+    int type = data_container->host_data.bilateral_type[cntr];
+    int row = index + num_unilaterals;
+
     ChLcpConstraintTwoBodies* mbilateral = (ChLcpConstraintTwoBodies*)(mconstraints[cntr]);
+    mconstraints[cntr]->Update_auxiliary();
 
     int idA = ((ChBody*)((ChLcpVariablesBody*)(mbilateral->GetVariables_a()))->GetUserData())->GetId();
     int idB = ((ChBody*)((ChLcpVariablesBody*)(mbilateral->GetVariables_b()))->GetUserData())->GetId();
+    int colA = idA * 6;
+    int colB = idB * 6;
 
-    mconstraints[cntr]->Update_auxiliary();
+    D_T(row, colA + 0) = mbilateral->Get_Cq_a()->GetElementN(0);
+    D_T(row, colA + 1) = mbilateral->Get_Cq_a()->GetElementN(1);
+    D_T(row, colA + 2) = mbilateral->Get_Cq_a()->GetElementN(2);
 
-    int2 body_id = I2(idA, idB);
+    D_T(row, colA + 3) = mbilateral->Get_Cq_a()->GetElementN(3);
+    D_T(row, colA + 4) = mbilateral->Get_Cq_a()->GetElementN(4);
+    D_T(row, colA + 5) = mbilateral->Get_Cq_a()->GetElementN(5);
 
-    D_T(index + num_unilaterals, body_id.x* 6 + 0) = mbilateral->Get_Cq_a()->GetElementN(0);
-    D_T(index + num_unilaterals, body_id.x* 6 + 1) = mbilateral->Get_Cq_a()->GetElementN(1);
-    D_T(index + num_unilaterals, body_id.x* 6 + 2) = mbilateral->Get_Cq_a()->GetElementN(2);
+    D_T(row, colB + 0) = mbilateral->Get_Cq_b()->GetElementN(0);
+    D_T(row, colB + 1) = mbilateral->Get_Cq_b()->GetElementN(1);
+    D_T(row, colB + 2) = mbilateral->Get_Cq_b()->GetElementN(2);
 
-    D_T(index + num_unilaterals, body_id.x* 6 + 3) = mbilateral->Get_Cq_a()->GetElementN(3);
-    D_T(index + num_unilaterals, body_id.x* 6 + 4) = mbilateral->Get_Cq_a()->GetElementN(4);
-    D_T(index + num_unilaterals, body_id.x* 6 + 5) = mbilateral->Get_Cq_a()->GetElementN(5);
-
-    D_T(index + num_unilaterals, body_id.y* 6 + 0) = mbilateral->Get_Cq_b()->GetElementN(0);
-    D_T(index + num_unilaterals, body_id.y* 6 + 1) = mbilateral->Get_Cq_b()->GetElementN(1);
-    D_T(index + num_unilaterals, body_id.y* 6 + 2) = mbilateral->Get_Cq_b()->GetElementN(2);
-
-    D_T(index + num_unilaterals, body_id.y* 6 + 3) = mbilateral->Get_Cq_b()->GetElementN(3);
-    D_T(index + num_unilaterals, body_id.y* 6 + 4) = mbilateral->Get_Cq_b()->GetElementN(4);
-    D_T(index + num_unilaterals, body_id.y* 6 + 5) = mbilateral->Get_Cq_b()->GetElementN(5);
+    D_T(row, colB + 3) = mbilateral->Get_Cq_b()->GetElementN(3);
+    D_T(row, colB + 4) = mbilateral->Get_Cq_b()->GetElementN(4);
+    D_T(row, colB + 5) = mbilateral->Get_Cq_b()->GetElementN(5);
   }
 }
 
-void ChConstraintBilateral::GenerateSparsity() {
+void ChConstraintBilateral::GenerateSparsity()
+{
+  // Grab the list of all bilateral constraints present in the system
+  // (note that this includes possibly inactive constraints)
+  std::vector<ChLcpConstraint*>& mconstraints = data_container->lcp_system_descriptor->GetConstraintsList();
+
+  // Loop over the active constraints and fill in the sparsity pattern of the
+  // Jacobian, taking into account the type of each constraint.
+  // Note that the data for a Blaze compressed matrix must be filled in increasing
+  // order of the column index for each row.
   CompressedMatrix<real>& D_T = data_container->host_data.D_T;
-  ChLcpSystemDescriptor* lcp_sys = data_container->lcp_system_descriptor;
-  std::vector<ChLcpConstraint*>& mconstraints = (*lcp_sys).GetConstraintsList();
 
   for (int index = 0; index < num_bilaterals; index++) {
-    //Here the active bilateral mapping that was generated during the update
-    //bilateral phase is used
     int cntr = data_container->host_data.bilateral_mapping[index];
+    int type = data_container->host_data.bilateral_type[cntr];
+    int row = index + num_unilaterals;
 
     ChLcpConstraintTwoBodies* mbilateral = (ChLcpConstraintTwoBodies*)(mconstraints[cntr]);
 
     int idA = ((ChBody*)((ChLcpVariablesBody*)(mbilateral->GetVariables_a()))->GetUserData())->GetId();
     int idB = ((ChBody*)((ChLcpVariablesBody*)(mbilateral->GetVariables_b()))->GetUserData())->GetId();
-    int2 body_id = I2(idA, idB);
-
-    // Blaze the data needs to be appended in increasing column order for each
-    // row, this check ensures that the data is written properly
-
+    int col1 = idA * 6;
+    int col2 = idB * 6;
     if (idA > idB) {
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 0, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 1, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 2, 1);
-
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 3, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 4, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 5, 1);
-
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 0, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 1, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 2, 1);
-
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 3, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 4, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 5, 1);
-    } else {
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 0, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 1, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 2, 1);
-
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 3, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 4, 1);
-      D_T.append(index + num_unilaterals, body_id.x * 6 + 5, 1);
-
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 0, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 1, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 2, 1);
-
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 3, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 4, 1);
-      D_T.append(index + num_unilaterals, body_id.y * 6 + 5, 1);
+      col1 = idB * 6;
+      col2 = idA * 6;
     }
-    D_T.finalize(index + num_unilaterals);
+
+    D_T.append(row, col1 + 0, 1); D_T.append(row, col1 + 1, 1); D_T.append(row, col1 + 2, 1);
+    D_T.append(row, col1 + 3, 1); D_T.append(row, col1 + 4, 1); D_T.append(row, col1 + 5, 1);
+
+    D_T.append(row, col2 + 0, 1); D_T.append(row, col2 + 1, 1); D_T.append(row, col2 + 2, 1);
+    D_T.append(row, col2 + 3, 1); D_T.append(row, col2 + 4, 1); D_T.append(row, col2 + 5, 1);
+
+    D_T.finalize(row);
   }
 }
 
