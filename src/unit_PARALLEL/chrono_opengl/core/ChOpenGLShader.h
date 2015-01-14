c23bb8c09ebe872a59ddc7e368f632163538bc3d
Hammad Mazhar <hammad.maz@gmail.com>
2015-01-14 17:08:23 +0100

    Cleanup and optimize the shader class

--- src/unit_PARALLEL/chrono_opengl/core/ChOpenGLShader.h
+++ src/unit_PARALLEL/chrono_opengl/core/ChOpenGLShader.h
@@ -22,61 +22,42 @@
 #define CHOPENGLSHADER_H
 
 #include "chrono_opengl/core/ChOpenGLBase.h"
+#include <glm/glm.hpp>
 
 namespace chrono {
 namespace opengl {
 
 class CH_OPENGL_API ChOpenGLShader : public ChOpenGLBase {
  public:
-   ChOpenGLShader();
-   void TakeDown();
-   void Use();
-   bool CompileStrings(
-         std::string shader_name,
-         const char * vertex_shader,
-         const char * fragment_shader);
-   bool CompileFiles(
-         std::string vertex_shader_file,
-         std::string fragment_shader_file);
-   void CompleteInit();
+  ChOpenGLShader();
+  void TakeDown();
+  void Use();
+  bool CompileStrings(std::string shader_name, const char* vertex_shader, const char* fragment_shader);
+  bool CompileFiles(std::string vertex_shader_file, std::string fragment_shader_file);
+  void CompleteInit();
 
-   virtual bool InitializeFiles(
-         std::string vertex_shader_file,
-         std::string fragment_shader_file);
-   virtual bool InitializeStrings(
-         std::string shader_name,
-         const char * vertex_shader_file,
-         const char * fragment_shader_file);
-   virtual void CustomSetup();
-   void CommonSetup(
-         const GLfloat * projection,
-         const GLfloat * view);
-   void SetTime(
-         float _time);
-   void SetCamera(
-         glm::vec3 campos);
-   GLuint GetUniformLocation(
-         std::string name);
-   GLuint view_matrix_handle;
-   GLuint projection_matrix_handle;
-   GLuint time_handle, camera_handle;
-   GLuint vertex_shader_id;
-   GLuint fragment_shader_id;
-   GLuint program_id;
-   bool LoadShader(
-         const std::string file_name,
-         GLuint shader_id);
-   bool LoadShaderString(
-         const char * shader_string,
-         GLuint shader_id);
-   std::string GetShaderLog(
-         GLuint shader_id);
-   void CheckGlProgram(
-         GLuint prog);
-   float time;
-   glm::vec3 camera_pos;
+  virtual bool InitializeFiles(std::string vertex_shader_file, std::string fragment_shader_file);
+  virtual bool InitializeStrings(std::string shader_name, const char* vertex_shader_file, const char* fragment_shader_file);
+  virtual void CustomSetup();
+  void CommonSetup(const GLfloat* projection, const GLfloat* view);
+  void SetTime(const float& _time);
+  void SetCamera(const glm::vec3& campos);
+  void SetViewport(const glm::ivec2& viewport);
+  GLuint GetUniformLocation(std::string name);
+  GLuint view_matrix_handle;
+  GLuint projection_matrix_handle;
+  GLuint time_handle, camera_handle, viewport_size_handle;
+  GLuint vertex_shader_id;
+  GLuint fragment_shader_id;
+  GLuint program_id;
+  bool LoadShader(const std::string file_name, GLuint shader_id);
+  bool LoadShaderString(const char* shader_string, GLuint shader_id);
+  std::string GetShaderLog(GLuint shader_id);
+  void CheckGlProgram(GLuint prog);
+  float time;
+  glm::vec3 camera_pos;
+  glm::ivec2 viewport_size;
 };
-
 }
 }
-#endif  // END of CHOPENGLSHADER_H
+#endif    // END of CHOPENGLSHADER_H
