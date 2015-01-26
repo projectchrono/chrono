86102beae4382976374be2e7f6ec18e00a62725d
justin <jcmadsen@wisc.edu>
2015-01-26 09:46:32 -0600

    only use ->SetFamilyMaskNoCollisionWithFamily() once per collision shape

--- src/demos/trackVehicle/subsys/trackVehicle/TrackVehicle.cpp
+++ src/demos/trackVehicle/subsys/trackVehicle/TrackVehicle.cpp
@@ -256,9 +256,8 @@ void TrackVehicle::AddCollisionGeometry()
   // set the collision family
   m_chassis->GetCollisionModel()->SetFamily( (int)CollisionFam::HULL );
   // don't collide with rolling elements or tracks
-  m_chassis->GetCollisionModel()->SetFamilyMaskNoCollisionWithFamily( (int)(CollisionFam::GEARS) );
-  m_chassis->GetCollisionModel()->SetFamilyMaskNoCollisionWithFamily( (int)(CollisionFam::WHEELS) );
-  m_chassis->GetCollisionModel()->SetFamilyMaskNoCollisionWithFamily( (int)(CollisionFam::SHOES) );
+  // m_chassis->GetCollisionModel()->SetFamilyMaskNoCollisionWithFamily( (int)(CollisionFam::WHEELS) );
+  // m_chassis->GetCollisionModel()->SetFamilyMaskNoCollisionWithFamily( (int)(CollisionFam::SHOES) );
 
   m_chassis->GetCollisionModel()->BuildModel();
 }
