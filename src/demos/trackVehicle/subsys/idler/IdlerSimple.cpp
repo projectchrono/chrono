bacecc1880403b535e5ee10b1e829f90bde13394
justin <jcmadsen@wisc.edu>
2015-01-29 08:34:35 -0600

    idler tensioner preload should be larger

--- src/demos/trackVehicle/subsys/idler/IdlerSimple.cpp
+++ src/demos/trackVehicle/subsys/idler/IdlerSimple.cpp
@@ -45,8 +45,8 @@ const std::string IdlerSimple::m_meshFile = utils::GetModelDataFile("M113/Idler_
 const double IdlerSimple::m_radius = 0.255;
 const double IdlerSimple::m_width = 0.166;
 const double IdlerSimple::m_widthGap = 0.092; 
-const double IdlerSimple::m_springK = 100000;
-const double IdlerSimple::m_springC = 1000;
+const double IdlerSimple::m_springK = 150000;
+const double IdlerSimple::m_springC = 1500;
 const double IdlerSimple::m_springRestLength = 1.0;
 
 
@@ -147,7 +147,7 @@ void IdlerSimple::Initialize(ChSharedPtr<ChBodyAuxRef> chassis,
 
   // init shock, add to system
   // put the second marker some length in front of marker1, based on desired preload
-  double preLoad = 20000; // [N]
+  double preLoad = 30000; // [N]
   // chassis spring attachment point is towards the center of the vehicle
   ChVector<> pos_chassis_abs = local_Csys.pos;
   if(local_Csys.pos.x < 0 ) 
