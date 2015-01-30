37367d57a215467454deb2d5684442e30fa3154f
Radu Serban <serban@engr.wisc.edu>
2015-01-30 12:22:02 -0600

    Use collision families to explicitly disable collision between bin and mixer body.

--- src/unit_PARALLEL/tests/mixerDEM.cpp
+++ src/unit_PARALLEL/tests/mixerDEM.cpp
@@ -89,6 +89,8 @@ void AddContainer(ChSystemParallelDEM* sys) {
    utils::AddBoxGeometry(bin.get_ptr(), ChVector<>(hthick, hdim.y, hdim.z), ChVector<>(hdim.x + hthick, 0, hdim.z));
    utils::AddBoxGeometry(bin.get_ptr(), ChVector<>(hdim.x, hthick, hdim.z), ChVector<>(0, -hdim.y - hthick, hdim.z));
    utils::AddBoxGeometry(bin.get_ptr(), ChVector<>(hdim.x, hthick, hdim.z), ChVector<>(0, hdim.y + hthick, hdim.z));
+   bin->GetCollisionModel()->SetFamily(1);
+   bin->GetCollisionModel()->SetFamilyMaskNoCollisionWithFamily(2);
    bin->GetCollisionModel()->BuildModel();
 
    sys->AddBody(bin);
@@ -107,6 +109,7 @@ void AddContainer(ChSystemParallelDEM* sys) {
 
    mixer->GetCollisionModel()->ClearModel();
    utils::AddBoxGeometry(mixer.get_ptr(), hsize);
+   mixer->GetCollisionModel()->SetFamily(2);
    mixer->GetCollisionModel()->BuildModel();
 
    sys->AddBody(mixer);
