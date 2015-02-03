27a2bab0a64dfda58da94518095c6cb91978db67
justin <jcmadsen@wisc.edu>
2015-02-03 10:04:17 -0600

    asdf

--- src/demos/trackVehicle/demo_track_M113.cpp
+++ src/demos/trackVehicle/demo_track_M113.cpp
@@ -127,7 +127,7 @@ int main(int argc, char* argv[])
 
   // The vehicle inherits ChSystem. Input chassis visual and collision type
 	TrackVehicle vehicle("Justins M113 model", 
-    VisualizationType::NONE,
+    VisualizationType::MESH,
     CollisionType::PRIMITIVES);
   
   // set the chassis REF at the specified initial config.
