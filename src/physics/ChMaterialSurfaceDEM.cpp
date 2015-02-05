55fc7e6bc42a7cc6be74a85f330ea711497782a4
Radu Serban <serban@engr.wisc.edu>
2015-02-05 17:16:45 -0600

    Add user-specified stiffness and damping coefficients in the DEM material.

--- src/physics/ChMaterialSurfaceDEM.cpp
+++ src/physics/ChMaterialSurfaceDEM.cpp
@@ -10,15 +10,6 @@
 // and at http://projectchrono.org/license-chrono.txt.
 //
 
-///////////////////////////////////////////////////
-//
-//   ChMaterialSurfaceDEM.cpp
-//
-// ------------------------------------------------
-//             www.projectchrono.org
-// ------------------------------------------------
-///////////////////////////////////////////////////
-
 #include <cmath>
 #include <algorithm>
 
@@ -26,9 +17,38 @@
 #include "physics/ChSystem.h"
 #include "physics/ChBodyDEM.h"
 
-namespace chrono
-{
 
+namespace chrono {
+
+
+// Constructors for a DEM material
+ChMaterialSurfaceDEM::ChMaterialSurfaceDEM()
+: young_modulus(2e5),
+  poisson_ratio(0.3f),
+  static_friction(0.6f),
+  sliding_friction(0.6f),
+  restitution(0.4f),
+  cohesion(0),
+  kn(2e5),
+  kt(2e5),
+  gn(40),
+  gt(20)
+{
+}
+
+ChMaterialSurfaceDEM::ChMaterialSurfaceDEM(const ChMaterialSurfaceDEM& other)
+{
+  young_modulus = other.young_modulus;
+  poisson_ratio = other.poisson_ratio;
+  static_friction = other.static_friction;
+  sliding_friction = other.sliding_friction;
+  restitution = other.restitution;
+  cohesion = other.cohesion;
+  kn = other.kn;
+  kt = other.kt;
+  gn = other.gn;
+  gt = other.gt;
+}
 
 // Calculate composite material properties as a combination of the physical
 // properties of the two specified materials.
@@ -36,27 +56,29 @@ ChCompositeMaterialDEM
 ChMaterialSurfaceDEM::CompositeMaterial(const ChSharedPtr<ChMaterialSurfaceDEM>& mat1,
                                         const ChSharedPtr<ChMaterialSurfaceDEM>& mat2)
 {
-	ChCompositeMaterialDEM mat;
+  ChCompositeMaterialDEM mat;
 
-	float inv_E = (1 - mat1->poisson_ratio * mat1->poisson_ratio) / mat1->young_modulus
-	            + (1 - mat2->poisson_ratio * mat2->poisson_ratio) / mat2->young_modulus;
-	float inv_G = 2 * (2 + mat1->poisson_ratio) * (1 - mat1->poisson_ratio) / mat1->young_modulus
-	            + 2 * (2 + mat2->poisson_ratio) * (1 - mat2->poisson_ratio) / mat2->young_modulus;
-	
-	mat.E_eff = 1 / inv_E;
-	mat.G_eff = 1 / inv_G;
+  float inv_E = (1 - mat1->poisson_ratio * mat1->poisson_ratio) / mat1->young_modulus
+    + (1 - mat2->poisson_ratio * mat2->poisson_ratio) / mat2->young_modulus;
+  float inv_G = 2 * (2 + mat1->poisson_ratio) * (1 - mat1->poisson_ratio) / mat1->young_modulus
+    + 2 * (2 + mat2->poisson_ratio) * (1 - mat2->poisson_ratio) / mat2->young_modulus;
 
-	mat.mu_eff    = std::min<float>(mat1->static_friction, mat2->static_friction);
+  mat.E_eff = 1 / inv_E;
+  mat.G_eff = 1 / inv_G;
 
-	mat.cr_eff    = (mat1->restitution + mat2->restitution) / 2;
+  mat.mu_eff = std::min<float>(mat1->static_friction, mat2->static_friction);
 
-	mat.cohesion_eff = std::min<float>(mat1->cohesion, mat2->cohesion);
+  mat.cr_eff = (mat1->restitution + mat2->restitution) / 2;
 
-	return mat;
+  mat.cohesion_eff = std::min<float>(mat1->cohesion, mat2->cohesion);
+
+  mat.kn = (mat1->kn + mat2->kn) / 2;
+  mat.kt = (mat1->kt + mat2->kt) / 2;
+  mat.gn = (mat1->gn + mat2->gn) / 2;
+  mat.gt = (mat1->gt + mat2->gt) / 2;
+
+  return mat;
 }
 
 
-} // END_OF_NAMESPACE____
-
-
-// eof
+} // end namespace chrono
