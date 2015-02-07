dbefa817c9adbf2338c40f63fc0e12d384012cc0
Hammad Mazhar <hammad.maz@gmail.com>
2015-02-06 21:04:25 -0600

    Vis materials are now in their own header

--- src/unit_PARALLEL/chrono_opengl/ChOpenGLViewer.cpp
+++ src/unit_PARALLEL/chrono_opengl/ChOpenGLViewer.cpp
@@ -14,6 +14,7 @@
 // =============================================================================
 #include "chrono_parallel/ChApiParallel.h"
 #include "chrono_opengl/ChOpenGLViewer.h"
+#include "chrono_opengl/ChOpenGLMaterials.h"
 
 #include "chrono_parallel/physics/ChNodeFluid.h"
 
@@ -97,37 +98,10 @@ void ChOpenGLViewer::TakeDown() {
 }
 
 bool ChOpenGLViewer::Initialize() {
-
-  ChOpenGLMaterial white(glm::vec3(0, 0, 0), glm::vec3(1, 1, 1), glm::vec3(1, 1, 1));
-  ChOpenGLMaterial red(glm::vec3(0, 0, 0), glm::vec3(1, 0, 0), glm::vec3(1, 1, 1));
-  ChOpenGLMaterial river(glm::vec3(0, 0, 0), glm::vec3(52.0 / 255.0, 152.0 / 255.0, 219.0 / 255.0), glm::vec3(1, 1, 1));
-  ChOpenGLMaterial darkriver(glm::vec3(0, 0, 0), glm::vec3(77.0 / 255.0, 114.0 / 255.0, 130.0 / 255.0), glm::vec3(1, 1, 1));
-  ChOpenGLMaterial brightriver(glm::vec3(0, 0, 0), glm::vec3(146.0 / 255.0, 214.0 / 255.0, 255.0 / 255.0), glm::vec3(1, 1, 1));
-
-  float ambient = .5;
-
-  ChOpenGLMaterial slate(glm::vec3(85.0f, 98.0f, 112.0f) / 255.0f * ambient, glm::vec3(85.0f, 98.0f, 112.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial pacifica(glm::vec3(78.0f, 205.0f, 196.0f) / 255.0f * ambient, glm::vec3(78.0f, 205.0f, 196.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial apple(glm::vec3(199.0f, 244.0f, 100.0f) / 255.0f * ambient, glm::vec3(199.0f, 244.0f, 100.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial cherry(glm::vec3(255.0f, 107.0f, 107.0f) / 255.0f * ambient, glm::vec3(255.0f, 107.0f, 107.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial pillow(glm::vec3(196.0f, 77.0f, 88.0f) / 255.0f * ambient, glm::vec3(196.0f, 77.0f, 88.0f) / 255.0f, glm::vec3(1, 1, 1));
-
-  ChOpenGLMaterial elated(glm::vec3(255.0f, 171.0f, 25.0f) / 255.0f * ambient, glm::vec3(255.0f, 171.0f, 25.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial greyslate(glm::vec3(158.0f, 158.0f, 158.0f) / 255.0f * ambient, glm::vec3(158.0f, 158.0f, 158.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial darkred(glm::vec3(193.0f, 21.0f, 21.0f) / 255.0f * ambient, glm::vec3(193.0f, 21.0f, 21.0f) / 255.0f, glm::vec3(1, 1, 1));
-
-  ChOpenGLMaterial t1(glm::vec3(236.0f, 208.0f, 120.0f) / 255.0f * ambient, glm::vec3(236.0f, 208.0f, 120.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial t2(glm::vec3(217.0f, 91.0f, 67.0f) / 255.0f * ambient, glm::vec3(217.0f, 91.0f, 67.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial t3(glm::vec3(192.0f, 41.0f, 66.0f) / 255.0f * ambient, glm::vec3(192.0f, 41.0f, 66.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial t4(glm::vec3(84.0f, 36.0f, 55.0f) / 255.0f * ambient, glm::vec3(84.0f, 36.0f, 55.0f) / 255.0f, glm::vec3(1, 1, 1));
-  ChOpenGLMaterial t5(glm::vec3(83.0f, 119.0f, 122.0f) / 255.0f * ambient, glm::vec3(83.0f, 119.0f, 122.0f) / 255.0f, glm::vec3(1, 1, 1));
-
-  ChOpenGLMaterial text_mat(glm::vec3(0, 0, 0), glm::vec3(100.0f, 145.0f, 170.0f) / 255.0f, glm::vec3(1, 1, 1));
-
+// Initialize all of the shaders and compile them
   if (!main_shader.InitializeStrings("phong", phong_vert, phong_frag)) {
     return 0;
   }
-
   if (!cloud_shader.InitializeStrings("cloud", cloud_vert, cloud_frag)) {
     return 0;
   }
@@ -141,6 +115,7 @@ bool ChOpenGLViewer::Initialize() {
     return 0;
   }
 
+  //Setup the generic shapes and load them from file
   sphere.Initialize("../resources/sphere.obj", slate, &main_shader);
   box.Initialize("../resources/box.obj", t3, &main_shader);
   cylinder.Initialize("../resources/cylinder.obj", apple, &main_shader);
