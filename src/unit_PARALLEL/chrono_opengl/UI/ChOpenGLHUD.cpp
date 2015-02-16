a2cf2c46daef2677c1dac5ef0f6f31e01266dd67
Hammad Mazhar <hammad.maz@gmail.com>
2015-02-15 23:07:16 -0600

    HUD now uses a better layout mechanism that will scale to the window

--- src/unit_PARALLEL/chrono_opengl/UI/ChOpenGLHUD.cpp
+++ src/unit_PARALLEL/chrono_opengl/UI/ChOpenGLHUD.cpp
@@ -27,12 +27,12 @@ using namespace collision;
 namespace opengl {
 using namespace glm;
 
-
 #define LEFT -.95
-#define TOP  .95
-#define RIGHT .6
+#define TOP .95
+#define BOTTOM -.95
+#define RIGHT .55
 #define CENTER 0
-#define SPACING sy*50.0
+#define SPACING sy * 35.0
 #define SCALE .001
 ChOpenGLHUD::ChOpenGLHUD() : ChOpenGLBase() {
   time_total = time_text = time_geometry = 0;
@@ -57,38 +57,34 @@ void ChOpenGLHUD::Update(const glm::ivec2& window_size, const float& dpi, const 
   int screen_width = window_size.x;
   int screen_height = window_size.y;
 
-
   float virtual_width = 1920;
   float virtual_height = 1080;
 
-  float targetAspectRatio = virtual_width/virtual_height;
+  float targetAspectRatio = virtual_width / virtual_height;
 
   // figure out the largest area that fits in this resolution at the desired aspect ratio
-  int width = screen_width ;
+  int width = screen_width;
   int height = (int)(width / targetAspectRatio + 0.5f);
 
-  if (height > screen_height )
-  {
-    //std::cout<<"It doesn't fit our height, we must switch to pillarbox then "<<height<<std::endl;;
-      height = screen_height ;
-      width = (int)(height * targetAspectRatio + 0.5f);
+  if (height > screen_height) {
+    // std::cout<<"It doesn't fit our height, we must switch to pillarbox then "<<height<<std::endl;;
+    height = screen_height;
+    width = (int)(height * targetAspectRatio + 0.5f);
   }
 
-  // set up the new viewport centered in the backbuffer
-  int vp_x = (screen_width  / 2) - (width / 2);
-  int vp_y = (screen_height / 2) - (height/ 2);
+  //  // set up the new viewport centered in the backbuffer
+  //  int vp_x = (screen_width  / 2) - (width / 2);
+  //  int vp_y = (screen_height / 2) - (height/ 2);
 
+  aspect = float(width) / float(height);
+  sx = SCALE;             // * (float)((float)(screen_width) / (float)virtual_width);
+  sy = SCALE * aspect;    //(float)((float)(screen_height) / (float)virtual_height) ;
 
-  aspect = float(width)/float(height);
-  sx = SCALE;// * (float)((float)(screen_width) / (float)virtual_width);
-  sy = SCALE * aspect;//(float)((float)(screen_height) / (float)virtual_height) ;
-
-  z_x = vp_x/float(width)-1;
-  z_y = vp_y/float(height)-1;
-
-
-  std::cout<<window_size.x<<" "<<window_size.y<<" "<<vp_x<<" "<<vp_y<<" "<<z_x<<" "<<z_y<<" "<<height<<" "<<width<<std::endl;
-
+  //  z_x = vp_x/float(width)-1;
+  //  z_y = vp_y/float(height)-1;
+  //
+  //
+  //  std::cout<<window_size.x<<" "<<window_size.y<<" "<<vp_x<<" "<<vp_y<<" "<<z_x<<" "<<z_y<<" "<<height<<" "<<width<<std::endl;
 
   text.Update();
 
@@ -96,12 +92,6 @@ void ChOpenGLHUD::Update(const glm::ivec2& window_size, const float& dpi, const 
   time_geometry = .5 * timer->GetTime("geometry") + .5 * time_geometry;
   time_total = .5 * timer->GetTime("render") + .5 * time_total;
   fps = frame_per_sec;
-
-
-
-
-
-
 }
 
 void ChOpenGLHUD::TakeDown() {
@@ -112,28 +102,28 @@ void ChOpenGLHUD::TakeDown() {
 
 void ChOpenGLHUD::GenerateHelp() {
 
-  text.Render("Press h to exit help", -.95, 0.925 - SPACING * 0, sx, sy);
-  text.Render("W: Forward", -.95, 0.925 - SPACING * 1, sx, sy);
-  text.Render("A: Strafe Left", -.95, 0.925 - SPACING * 2, sx, sy);
-  text.Render("S: Back", -.95, 0.925 - SPACING * 3, sx, sy);
-  text.Render("D: Strafe Right", -.95, 0.925 - SPACING * 4, sx, sy);
-  text.Render("Q: Down", -.95, 0.925 - SPACING * 5, sx, sy);
-  text.Render("E: Up", -.95, 0.925 - SPACING * 6, sx, sy);
+  text.Render("Press h to exit help", LEFT, TOP - SPACING * 0, sx, sy);
+  text.Render("W: Forward", LEFT, TOP - SPACING * 1, sx, sy);
+  text.Render("A: Strafe Left", LEFT, 0.925 - SPACING * 2, sx, sy);
+  text.Render("S: Back", LEFT, TOP - SPACING * 3, sx, sy);
+  text.Render("D: Strafe Right", LEFT, TOP - SPACING * 4, sx, sy);
+  text.Render("Q: Down", LEFT, TOP - SPACING * 5, sx, sy);
+  text.Render("E: Up", LEFT, TOP - SPACING * 6, sx, sy);
 
-  text.Render("Mouse Look (Click and hold left mouse button)", -.95, 0.925 - SPACING * 7, sx, sy);
+  text.Render("Mouse Look (Click and hold left mouse button)", LEFT, TOP - SPACING * 7, sx, sy);
 
-  text.Render("1: Point Cloud (default)", -.95, 0.925 - SPACING * 9, sx, sy);
-  text.Render("2: Wireframe (slow)", -.95, 0.925 - SPACING * 10, sx, sy);
-  text.Render("3: Solid", -.95, 0.925 - SPACING * 11, sx, sy);
+  text.Render("1: Point Cloud (default)", LEFT, TOP - SPACING * 9, sx, sy);
+  text.Render("2: Wireframe (slow)", LEFT, TOP - SPACING * 10, sx, sy);
+  text.Render("3: Solid", LEFT, TOP - SPACING * 11, sx, sy);
 
-  text.Render("C: Show/Hide Contacts (DVI only)", -.95, 0.925 - SPACING * 13, sx, sy);
+  text.Render("C: Show/Hide Contacts (DVI only)", LEFT, TOP - SPACING * 13, sx, sy);
 
-  text.Render("Space: Pause Simulation (not rendering)", -.95, 0.925 - SPACING * 15, sx, sy);
-  text.Render("P: Pause Rendering (not simulating)", -.95, 0.925 - SPACING * 16, sx, sy);
-  text.Render(".: Single Step ", -.95, 0.925 - SPACING * 18, sx, sy);
-  text.Render("B: Enable/Disable AABB ", -.95, 0.925 - SPACING * 20, sx, sy);
+  text.Render("Space: Pause Simulation (not rendering)", LEFT, TOP - SPACING * 15, sx, sy);
+  text.Render("P: Pause Rendering (not simulating)", LEFT, TOP - SPACING * 16, sx, sy);
+  text.Render(".: Single Step ", LEFT, TOP - SPACING * 18, sx, sy);
+  text.Render("B: Enable/Disable AABB ", LEFT, TOP - SPACING * 20, sx, sy);
 
-  text.Render("Escape: Exit ", -.95, 0.925 - SPACING * 30, sx, sy);
+  text.Render("Escape: Exit ", LEFT, TOP - SPACING * 30, sx, sy);
 }
 
 void ChOpenGLHUD::GenerateCamera() {
@@ -152,9 +142,9 @@ void ChOpenGLHUD::GenerateSystem(ChSystem* physics_system) {
   int average_contacts_per_body = num_bodies > 0 ? num_contacts / num_bodies : 0;
 
   sprintf(buffer, "MODEL INFO");
-  text.Render(buffer, RIGHT, 0.925 - SPACING * 0, sx, sy);
+  text.Render(buffer, RIGHT, TOP - SPACING * 0, sx, sy);
   sprintf(buffer, "BODIES     %04d", num_bodies);
-  text.Render(buffer, RIGHT, 0.925 - SPACING * 1, sx, sy);
+  text.Render(buffer, RIGHT, TOP - SPACING * 1, sx, sy);
   int num_shapes = 0;
   if (ChSystemParallelDVI* parallel_system = dynamic_cast<ChSystemParallelDVI*>(physics_system)) {
     num_shapes = parallel_system->data_manager->num_shapes;
@@ -164,31 +154,31 @@ void ChOpenGLHUD::GenerateSystem(ChSystem* physics_system) {
   }
 
   sprintf(buffer, "AABB       %04d", num_shapes);
-  text.Render(buffer, RIGHT, 0.925 - SPACING * 2, sx, sy);
+  text.Render(buffer, RIGHT, TOP - SPACING * 2, sx, sy);
 
   sprintf(buffer, "CONTACTS   %04d", num_contacts);
-  text.Render(buffer, RIGHT, 0.925 - SPACING * 3, sx, sy);
+  text.Render(buffer, RIGHT, TOP - SPACING * 3, sx, sy);
   sprintf(buffer, "AVGCONPB   %04d", average_contacts_per_body);
-  text.Render(buffer, RIGHT, 0.925 - SPACING * 4, sx, sy);
+  text.Render(buffer, RIGHT, TOP - SPACING * 4, sx, sy);
 
   sprintf(buffer, "TIMING INFO");
-  text.Render(buffer, RIGHT, -0.925 + SPACING * 11, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 11, sx, sy);
   sprintf(buffer, "STEP     %04f", physics_system->GetTimerStep());
-  text.Render(buffer, RIGHT, -0.925 + SPACING * 10, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 10, sx, sy);
   sprintf(buffer, "BROAD    %04f", physics_system->GetTimerCollisionBroad());
-  text.Render(buffer, RIGHT, -0.925 + SPACING * 9, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 9, sx, sy);
   sprintf(buffer, "NARROW   %04f", physics_system->GetTimerCollisionNarrow());
-  text.Render(buffer, RIGHT, -0.925 + SPACING * 8, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 8, sx, sy);
   sprintf(buffer, "SOLVE    %04f", physics_system->GetTimerLcp());
-  text.Render(buffer, RIGHT, -0.925 + SPACING * 7, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 7, sx, sy);
   sprintf(buffer, "UPDATE   %04f", physics_system->GetTimerUpdate());
-  text.Render(buffer, RIGHT, -0.925 + SPACING * 6, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 6, sx, sy);
 }
 
 void ChOpenGLHUD::GenerateSolver(ChSystem* physics_system) {
 
   sprintf(buffer, "SOLVER INFO");
-  text.Render(buffer, .6, 0.925 - SPACING * 6, sx, sy);
+  text.Render(buffer, RIGHT, TOP - SPACING * 6, sx, sy);
 
   double iters = ((ChLcpIterativeSolver*)(physics_system->GetLcpSolverSpeed()))->GetTotalIterations();
   if (iters > 0) {
@@ -196,18 +186,18 @@ void ChOpenGLHUD::GenerateSolver(ChSystem* physics_system) {
     double dlambda = ((ChLcpIterativeSolver*)(physics_system->GetLcpSolverSpeed()))->GetDeltalambdaHistory().back();
 
     sprintf(buffer, "ITERS    %04d", int(iters));
-    text.Render(buffer, .6, 0.925 - SPACING * 7, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 7, sx, sy);
     sprintf(buffer, "RESIDUAL %04f", residual);
-    text.Render(buffer, .6, 0.925 - SPACING * 8, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 8, sx, sy);
     sprintf(buffer, "CORRECT  %04f", dlambda);
-    text.Render(buffer, .6, 0.925 - SPACING * 9, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 9, sx, sy);
   } else {
     sprintf(buffer, "ITERS    %04d", 0);
-    text.Render(buffer, .6, 0.925 - SPACING * 7, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 7, sx, sy);
     sprintf(buffer, "RESIDUAL %04f", 0);
-    text.Render(buffer, .6, 0.925 - SPACING * 8, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 8, sx, sy);
     sprintf(buffer, "CORRECT  %04f", 0);
-    text.Render(buffer, .6, 0.925 - SPACING * 9, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 9, sx, sy);
   }
 }
 
@@ -221,19 +211,19 @@ void ChOpenGLHUD::GenerateCD(ChSystem* physics_system) {
     real3 center = (min_pt + max_pt) * .5;
     int max_aabb_per_bin = parallel_sys->data_manager->measures.collision.max_aabb_per_bin;
     sprintf(buffer, "COLLISION INFO");
-    text.Render(buffer, .6, 0.925 - SPACING * 11, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 11, sx, sy);
     sprintf(buffer, "DIMS  [%d,%d,%d]", grid_size.x, grid_size.y, grid_size.z);
-    text.Render(buffer, .6, 0.925 - SPACING * 12, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 12, sx, sy);
     sprintf(buffer, "MAX   %d", max_aabb_per_bin);
-    text.Render(buffer, .6, 0.925 - SPACING * 13, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 13, sx, sy);
     sprintf(buffer, "SX    %f", bin_size_vec.x);
-    text.Render(buffer, .6, 0.925 - SPACING * 14, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 14, sx, sy);
     sprintf(buffer, "SY    %f", bin_size_vec.y);
-    text.Render(buffer, .6, 0.925 - SPACING * 15, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 15, sx, sy);
     sprintf(buffer, "SZ    %f", bin_size_vec.z);
-    text.Render(buffer, .6, 0.925 - SPACING * 16, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 16, sx, sy);
     sprintf(buffer, "RIGID %d", parallel_sys->data_manager->num_contacts);
-    text.Render(buffer, .6, 0.925 - SPACING * 17, sx, sy);
+    text.Render(buffer, RIGHT, TOP - SPACING * 17, sx, sy);
   } else {
     // ChCollisionSystemBullet* collision_system = (ChCollisionSystemBullet*)physics_system->GetCollisionSystem();
     // btDbvtBroadphase * broadphase = (btDbvtBroadphase* ) collision_system->GetBulletCollisionWorld()->getBroadphase();
@@ -243,15 +233,15 @@ void ChOpenGLHUD::GenerateCD(ChSystem* physics_system) {
 void ChOpenGLHUD::GenerateRenderer() {
 
   sprintf(buffer, "RENDER INFO");
-  text.Render(buffer, .6, -0.925 + SPACING * 4, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 4, sx, sy);
   sprintf(buffer, "GEOMETRY %04f", time_geometry);
-  text.Render(buffer, .6, -0.925 + SPACING * 3, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 3, sx, sy);
   sprintf(buffer, "TEXT     %04f", time_text);
-  text.Render(buffer, .6, -0.925 + SPACING * 2, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 2, sx, sy);
   sprintf(buffer, "TOTAL    %04f", time_total);
-  text.Render(buffer, .6, -0.925 + SPACING * 1, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 1, sx, sy);
   sprintf(buffer, "FPS      %04d", int(fps));
-  text.Render(buffer, .6, -0.925 + SPACING * 0, sx, sy);
+  text.Render(buffer, RIGHT, BOTTOM + SPACING * 0, sx, sy);
 }
 
 void ChOpenGLHUD::GenerateStats(ChSystem* physics_system) {
@@ -262,13 +252,11 @@ void ChOpenGLHUD::GenerateStats(ChSystem* physics_system) {
   sprintf(buffer, "TIME:  %04f  | %04f", physics_system->GetChTime(), physics_system->GetStep());
   text.Render(buffer, LEFT, TOP, sx, sy);
 
-
-
   GenerateCamera();
-//  GenerateSystem(physics_system);
-//  GenerateSolver(physics_system);
-//  GenerateCD(physics_system);
-//  GenerateRenderer();
+  GenerateSystem(physics_system);
+  GenerateSolver(physics_system);
+  GenerateCD(physics_system);
+  GenerateRenderer();
 
   //  if (ChSystemParallelDVI* parallel_sys = dynamic_cast<ChSystemParallelDVI*>(physics_system)) {
   //    sprintf(buffer, "TimerA:  %04f", parallel_sys->data_manager->system_timer.GetTime("ChSolverParallel_solverA"));
