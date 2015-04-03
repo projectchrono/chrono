1eec85302e096e975cde297d614dc145f6be67d7
Alessandro Tasora <tasora@ied.unipr.it>
2015-04-04 01:14:11 +0200

    This is a wrapper for the ChLinePath, to be associated to bodies as visualization assets.

---
+++ src/assets/ChPathShape.h
@@ -0,0 +1,67 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2013 Project Chrono
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be 
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+#ifndef CHPATHSHAPE_H
+#define CHPATHSHAPE_H
+
+
+#include "assets/ChVisualization.h"
+#include "geometry/ChCLinePath.h"
+#include "geometry/ChCLineSegment.h"
+#include "geometry/ChCLineArc.h"
+
+namespace chrono {
+
+/// Class for referencing a ChLinePath that can be 
+/// visualized in some way.
+
+class ChApi ChPathShape: public ChVisualization {
+
+	protected:
+		//
+		// DATA
+		//
+		geometry::ChLinePath gpath;
+
+	public:
+		//
+		// CONSTRUCTORS
+		//
+
+		ChPathShape() {
+		}
+		;
+		ChPathShape(geometry::ChLinePath& mpath) :
+				gpath(mpath) {
+		}
+		;
+
+		virtual ~ChPathShape() {
+		}
+		;
+
+		//
+		// FUNCTIONS
+		//
+
+		// Access the sphere geometry
+		geometry::ChLinePath& GetPathGeometry() {
+			return gpath;
+		}
+
+};
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+}// END_OF_NAMESPACE____
+
+#endif
