6741e2e8b449756b274c1d3404a31aa9ec56dae2
Hammad Mazhar <hammad.maz@gmail.com>
2015-04-13 20:04:56 -0500

    add subtraction operator for uint3 and int3

--- src/unit_PARALLEL/chrono_parallel/math/other_types.h
+++ src/unit_PARALLEL/chrono_parallel/math/other_types.h
@@ -79,6 +79,14 @@ static inline uint3 _make_uint3(const uint& a, const uint& b, const uint& c) {
   return t;
 }
 
+static inline int3 operator-(const int3& a, const int3& b) {
+  return I3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+static inline uint3 operator-(const uint3& a, const uint3& b) {
+  return U3(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
 static inline int clamp(const int& a, const int& clamp_min, const int& clamp_max) {
   if (a < clamp_min) {
     return clamp_min;
