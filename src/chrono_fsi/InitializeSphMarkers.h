55263bbc0012489a6c67e3f899e3baced169262d
armanpazouki <apazouki@gmail.com>
2015-04-21 19:34:21 -0500

    Wall boundary using BCE included. Turning it on (using wall BCE instead of contact) or off (using contact instead of BCE) is controlled by a macro define in the fsi_hmmwv file, called useWallBce (true: use, fale: don't use bce)

--- src/chrono_fsi/InitializeSphMarkers.h
+++ src/chrono_fsi/InitializeSphMarkers.h
@@ -13,6 +13,8 @@
 #include "SPHCudaUtils.h"
 #include "MyStructs.cuh"
 
+#include "chrono_parallel/physics/ChSystemParallel.h"
+
 int2 CreateFluidMarkers(
 		thrust::host_vector<Real3> & posRadH,
 		thrust::host_vector<Real4> & velMasH,
@@ -20,6 +22,17 @@ int2 CreateFluidMarkers(
 		thrust::host_vector<uint> & bodyIndex,
 		const SimParams & paramsH,
 		Real & sphMarkerMass);
+
+void CreateBCE_On_Box(
+		thrust::host_vector<Real3> & posRadBCE,
+		thrust::host_vector<Real4> & velMasBCE,
+		thrust::host_vector<Real4> & rhoPresMuBCE,
+		const SimParams & paramsH,
+		const Real & sphMarkerMass,
+		const chrono::ChVector<>& size,
+		const chrono::ChVector<>& pos = chrono::ChVector<>(0, 0, 0),
+		const chrono::ChQuaternion<>& rot = chrono::ChQuaternion<>(1, 0, 0, 0),
+		int face = 12);
 
 void SetNumObjects(
 		NumberOfObjects & numObjects,
