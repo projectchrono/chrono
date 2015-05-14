8285590f195b9ed3cb5d050450d7d642a8e392ef
Alessandro Tasora <tasora@ied.unipr.it>
2015-05-14 07:01:32 +0200

    Forcing OpenMP to parallelize loops in matrix operations only if the size of the matrix is quite large, otherwise one gets a performance penalty because of thread management.

--- src/core/ChMatrix.h
+++ src/core/ChMatrix.h
@@ -36,8 +36,8 @@
 // Too low values could slow down the execution because sometimes the overhead of
 // multithreading setup is larger than the effective computation; in general optimal
 // values depend on processor architecture etc.
-#define CH_OMP_MATRLIGHT 4
-#define CH_OMP_MATR 4
+#define CH_OMP_MATRLIGHT 400
+#define CH_OMP_MATR 400
 
 namespace chrono {
 
