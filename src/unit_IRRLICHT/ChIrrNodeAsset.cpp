77bdbfdc5f05f9a8eebf15f636b40c564ff100ae
Alessandro Tasora <tasora@ied.unipr.it>
2015-06-18 15:40:05 +0200

    Serialization and class factory enabled also for the asset in the unit_IRRLICHT

---
+++ src/unit_IRRLICHT/ChIrrNodeAsset.cpp
@@ -0,0 +1,21 @@
+//
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2012 Alessandro Tasora
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file at the top level of the distribution
+// and at http://projectchrono.org/license-chrono.txt.
+//
+
+
+#include "unit_IRRLICHT/ChIrrNodeAsset.h"
+
+namespace chrono {
+
+// Register into the object factory, to enable run-time
+// dynamic creation and persistence
+ChClassRegister<ChIrrNodeAsset> a_registration_ChIrrNodeAsset;
+
+}  // END_OF_NAMESPACE____
