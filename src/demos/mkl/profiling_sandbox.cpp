0c3bbfe7b8943d823a93081023e6fe27d1781427
DarioMangoni <dariomangoni@gmail.com>
2015-06-27 16:31:43 +0200

    reformatted profiling

--- src/demos/mkl/profiling_sandbox.cpp
+++ src/demos/mkl/profiling_sandbox.cpp
@@ -1,7 +1,7 @@
-#include <lcp/ChLcpSystemDescriptor.h>
 #include <iostream>
 #include <cstdlib>
 #include <VSPerf.h>
+#include <lcp/ChLcpSystemDescriptor.h>
 #include <unit_MKL/ChCSR3matrix.h>
 #include <lcp/ChLcpMatrixTool.h>
 
@@ -11,48 +11,54 @@ using namespace chrono;
 
 int main(){
 	// Initialize matrix (CSR3) and its pointer
-	const unsigned int n = 10;
+	const unsigned int n = 750;
 	std::vector<int> resizeSize(n);
 	for (unsigned int i = 1; i < n; i++){
 		resizeSize[i] = n;
 	}
 	ChEigenMatrix Zcsr(n, n, resizeSize);
-	ChSparseMatrixBase* Zcsr_ptr = &Zcsr;
 	
 	// Set Pointer to Member
 	ChLcpMatrixTool MatToolCSR;
 	MatToolCSR.SetMatrixTools(&Zcsr);
 
+	// Set Pointer to Base
+	ChSparseMatrixBase* Zcsr_ptr = &Zcsr;
 
-	// Different calls...
-	cout << "Eigen Matrix" << endl;
-	(MatToolCSR.output_matrix->*MatToolCSR.MatrixFunctions.SetElementPtr)(1, 1, 3.4);
-	cout << "PtrMember: " << Zcsr.GetElement(1, 1) << endl;
-	//vZcsr_ptr->SetElement(1, 1, 7.9);
-	//cout << "Base->Member " << Zcsr.GetElement(1, 1) << endl;
 
-	//cout << "Zcsr_ptr->GetElement(1, 1): " << Zcsr_ptr->GetElement(1, 1) << endl;
+	StartProfile(PROFILE_GLOBALLEVEL, PROFILE_CURRENTID);
 
-	cout << endl;
+	for (int cont = 0; cont < 1; cont++){
+
+		CommentMarkProfile(75000, "START PtrToMember");
+		for (auto i = 0; i < n; i++)
+			for (auto j = 0; j < n; j++)
+			{
+				(MatToolCSR.output_matrix->*MatToolCSR.MatrixFunctions.SetElementPtr)(i, j, i + j);
+			}
+		CommentMarkProfile(75001, "STOP PtrToMember");
+
+		CommentMarkProfile(75002, "START VirtualCall");
+		for (auto i = 0; i < n; i++)
+			for (auto j = 0; j < n; j++)
+			{
+				Zcsr_ptr->SetElement(i, j, i + j);
+			}
+		CommentMarkProfile(75003, "STOP VirtualCall");
+
+		//cout << cont << endl;
+	}
+
+	StopProfile(PROFILE_GLOBALLEVEL, PROFILE_CURRENTID);
 
 	//////////////////////////////////////////////////////////////////
 
-	// Initialize matrix (ChSparseMatrix)
-	ChSparseMatrix ZLL(n, n, 1);
-	ChSparseMatrixBase* ZLL_ptr = &ZLL;
+	//// Initialize matrix (ChSparseMatrix)
+	//ChSparseMatrix ZLL(n, n, 1);
+	//ChSparseMatrixBase* ZLL_ptr = &ZLL;
 
-	// Set Pointer to Member
-	ChLcpMatrixTool MatToolLL;
-	MatToolLL.SetMatrixTools(&ZLL);
-
-
-	// Different calls...
-	cout << "LinkedList Matrix" << endl;
-	(MatToolLL.output_matrix->*MatToolLL.MatrixFunctions.SetElementPtr)(1, 1, 9.5);
-	cout << "PtrMember: " << ZLL.GetElement(1, 1) << endl;
-	//ZLL_ptr->SetElement(1, 1, 1.6);
-	//cout << "Base->Member: " << ZLL.GetElement(1, 1) << endl;
-
-	//cout << "ZLL_ptr->GetElement(1, 1): " << ZLL_ptr->GetElement(1, 1) << endl;
+	//// Set Pointer to Member
+	//ChLcpMatrixTool MatToolLL;
+	//MatToolLL.SetMatrixTools(&ZLL);
 
 }
