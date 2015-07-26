7df30196e187c41468de44bfed1ba5ba5f164279
Radu Serban <serban@engr.wisc.edu>
2015-07-26 10:34:44 +0200

    Update ChcontactContainerParallel to represent a collection of ChContactTuples.
    Note that this contact container is only used for reporting geometric information and therefore need not differentiate between DVI and DEM contact.
    Currently, only contacts between rigid bodies are considered.

--- src/unit_PARALLEL/chrono_parallel/collision/ChContactContainerParallel.h
+++ src/unit_PARALLEL/chrono_parallel/collision/ChContactContainerParallel.h
@@ -3,31 +3,45 @@
 
 #include <list>
 
-#include "physics/ChContactContainer.h"
+#include "physics/ChContactContainerBase.h"
+#include "physics/ChContactTuple.h"
 
 #include "chrono_parallel/ChApiParallel.h"
 #include "chrono_parallel/ChDataManager.h"
 
 namespace chrono {
-/// Class representing a container of many contacts,
-/// implemented as a typical linked list of ChContactGPUsimple
-/// objects.
-/// This contact container must be used for the preliminar CUDA solver
-/// that was developed by Ale & Dan, but in future will be
-/// replaced by ChContactContainerGPU, and advanced container
-/// that does not use linked lists of cpu objects but rather
-/// keeps all contact data as GPU buffers on the GPU device.
 
-class CH_PARALLEL_API ChContactContainerParallel : public ChContactContainer {
-  CH_RTTI(ChContactContainerParallel, ChContactContainer);
+/// Class representing a container of many contacts, implemented as a linked list of contact tuples.
+/// Notes:
+/// * This container is used only for reporting geometric information about the contact pairs
+///   and is therefore suitable for both DVI and DEM systems.
+/// * Currently, only contacts between rigid bodies are considered
+
+class CH_PARALLEL_API ChContactContainerParallel : public ChContactContainerBase {
+  CH_RTTI(ChContactContainerParallel, ChContactContainerBase);
 
  public:
-  ChContactContainerParallel(ChParallelDataManager* dc);
+  typedef ChContactTuple<ChContactable_1vars<6>, ChContactable_1vars<6> > ChContact_6_6;
 
-  virtual ~ChContactContainerParallel();
+  ChContactContainerParallel(ChParallelDataManager* dc);
+  ~ChContactContainerParallel();
+
   virtual int GetNcontacts() { return data_manager->num_rigid_contacts; }
 
+  virtual void RemoveAllContacts();
+  virtual void BeginAddContact();
+  virtual void AddContact(const collision::ChCollisionInfo& mcontact);
+  virtual void EndAddContact();
+
+  /// Return the list of contacts between rigid bodies
+  const std::list<ChContact_6_6*>& GetContactList() const { return contactlist_6_6; }
+
   ChParallelDataManager* data_manager;
+
+ private:
+  int n_added_6_6;
+  std::list<ChContact_6_6*> contactlist_6_6;
+  std::list<ChContact_6_6*>::iterator lastcontact_6_6;
 };
 }
 
