4c0b2451454c95f71ea885a515f225118419e8f6
Radu Serban <serban@engr.wisc.edu>
2015-09-29 15:26:39 +0200

    Add wrapper classes for modeling an entire HMMWV vehicle assembly.
    - These convenience classes encapsulate the HMMWV vehicle itself (full or reduced models), the powertrain, and the tires
    - Use one of the derived classes (HMMWV_Full or HMMWV_Reduced)
    - Modified demo_HMMWV and demo_HMMWV9 to use these new classes

--- src/demos/vehicle/demo_HMMWV/demo_HMMWV.cpp
+++ src/demos/vehicle/demo_HMMWV/demo_HMMWV.cpp
@@ -12,52 +12,33 @@
 // Authors: Radu Serban, Justin Madsen
 // =============================================================================
 //
-// Main driver function for the HMMWV full model, using rigid tire-terrain
-// contact.
-//
-// If using the Irrlicht interface, driver inputs are obtained from the keyboard.
+// Main driver function for the HMMWV full model.
 //
 // The vehicle reference frame has Z up, X towards the front of the vehicle, and
 // Y pointing to the left.
 //
 // =============================================================================
 
-#include <vector>
-
 #include "chrono/core/ChFileutils.h"
 #include "chrono/core/ChStream.h"
 #include "chrono/core/ChRealtimeStep.h"
 #include "chrono/physics/ChSystem.h"
-#include "chrono/physics/ChLinkDistance.h"
 #include "chrono/utils/ChUtilsInputOutput.h"
 
 #include "chrono_vehicle/ChConfigVehicle.h"
-#include "chrono_vehicle/ChVehicleModelData.h"
 #include "chrono_vehicle/terrain/RigidTerrain.h"
-#include "chrono_vehicle/tire/ChPacejkaTire.h"
-
-#include "chrono_irrlicht/ChIrrApp.h"
 #include "chrono_vehicle/driver/ChIrrGuiDriver.h"
+#include "chrono_vehicle/utils/ChVehicleIrrApp.h"
 
-#include "ModelDefs.h"
-#include "hmmwv/vehicle/HMMWV_Vehicle.h"
-#include "hmmwv/powertrain/HMMWV_Powertrain.h"
-#include "hmmwv/powertrain/HMMWV_SimplePowertrain.h"
-#include "hmmwv/tire/HMMWV_RigidTire.h"
-#include "hmmwv/tire/HMMWV_LugreTire.h"
-
-// DEBUGGING:  Uncomment the following line to print shock data
-//#define DEBUG_LOG
+#include "hmmwv/HMMWV.h"
 
 using namespace chrono;
 using namespace hmmwv;
 
 // =============================================================================
 
-// Initial vehicle position
+// Initial vehicle location and orientation
 ChVector<> initLoc(0, 0, 1.0);
-
-// Initial vehicle orientation
 ChQuaternion<> initRot(1, 0, 0, 0);
 // ChQuaternion<> initRot(0.866025, 0, 0, 0.5);
 // ChQuaternion<> initRot(0.7071068, 0, 0, 0.7071068);
@@ -103,137 +84,40 @@ bool povray_output = false;
 // =============================================================================
 
 int main(int argc, char* argv[]) {
-    // --------------------------
-    // Create the various modules
-    // --------------------------
+    // --------------
+    // Create systems
+    // --------------
 
-    // Create the HMMWV vehicle
-    HMMWV_Vehicle vehicle(false, RWD, PRIMITIVES, PRIMITIVES);
+    // Create the HMMWV vehicle, set parameters, and initialize
+    HMMWV_Full my_hmmwv;
+    my_hmmwv.SetChassisFixed(false);
+    my_hmmwv.SetChassisVis(PRIMITIVES);
+    my_hmmwv.SetwheelVis(PRIMITIVES);
+    my_hmmwv.SetInitPosition(ChCoordsys<>(initLoc, initRot));
+    my_hmmwv.SetPowertrainType(powertrain_model);
+    my_hmmwv.SetDriveType(RWD);
+    my_hmmwv.SetTireType(tire_model);
+    my_hmmwv.SetTireStepSize(tire_step_size);
+    my_hmmwv.Initialize();
 
-    vehicle.Initialize(ChCoordsys<>(initLoc, initRot));
-
-    // Create the ground
-    RigidTerrain terrain(vehicle.GetSystem(), terrainHeight, terrainLength, terrainWidth, 0.8);
+    // Create the terrain
+    RigidTerrain terrain(my_hmmwv.GetSystem(), terrainHeight, terrainLength, terrainWidth, 0.8);
     // terrain.AddMovingObstacles(10);
     terrain.AddFixedObstacles();
 
-    // Create and initialize the powertrain system
-    ChSharedPtr<ChPowertrain> powertrain;
-
-    switch (powertrain_model) {
-        case SHAFTS: {
-            ChSharedPtr<HMMWV_Powertrain> ptrain(new HMMWV_Powertrain);
-
-            ptrain->Initialize(vehicle.GetChassis(), vehicle.GetDriveshaft());
-            powertrain = ptrain;
-
-            break;
-        }
-        case SIMPLE: {
-            ChSharedPtr<HMMWV_SimplePowertrain> ptrain(new HMMWV_SimplePowertrain);
-
-            ptrain->Initialize();
-            powertrain = ptrain;
-
-            break;
-        }
-    }
-
-    // Create and initialize the tires
-    ChSharedPtr<ChTire> tire_front_right;
-    ChSharedPtr<ChTire> tire_front_left;
-    ChSharedPtr<ChTire> tire_rear_right;
-    ChSharedPtr<ChTire> tire_rear_left;
-
-    switch (tire_model) {
-        case RIGID: {
-            ChSharedPtr<HMMWV_RigidTire> tire_FL(new HMMWV_RigidTire("FL", 0.7f));
-            ChSharedPtr<HMMWV_RigidTire> tire_FR(new HMMWV_RigidTire("FR", 0.7f));
-            ChSharedPtr<HMMWV_RigidTire> tire_RL(new HMMWV_RigidTire("RL", 0.7f));
-            ChSharedPtr<HMMWV_RigidTire> tire_RR(new HMMWV_RigidTire("RR", 0.7f));
-
-            tire_FL->Initialize(vehicle.GetWheelBody(FRONT_LEFT));
-            tire_FR->Initialize(vehicle.GetWheelBody(FRONT_RIGHT));
-            tire_RL->Initialize(vehicle.GetWheelBody(REAR_LEFT));
-            tire_RR->Initialize(vehicle.GetWheelBody(REAR_RIGHT));
-
-            tire_front_left = tire_FL;
-            tire_front_right = tire_FR;
-            tire_rear_left = tire_RL;
-            tire_rear_right = tire_RR;
-
-            break;
-        }
-        case LUGRE: {
-            ChSharedPtr<HMMWV_LugreTire> tire_FL(new HMMWV_LugreTire("FL"));
-            ChSharedPtr<HMMWV_LugreTire> tire_FR(new HMMWV_LugreTire("FR"));
-            ChSharedPtr<HMMWV_LugreTire> tire_RL(new HMMWV_LugreTire("RL"));
-            ChSharedPtr<HMMWV_LugreTire> tire_RR(new HMMWV_LugreTire("RR"));
-
-            tire_FL->Initialize();
-            tire_FR->Initialize();
-            tire_RL->Initialize();
-            tire_RR->Initialize();
-
-            tire_FL->SetStepsize(tire_step_size);
-            tire_FR->SetStepsize(tire_step_size);
-            tire_RL->SetStepsize(tire_step_size);
-            tire_RR->SetStepsize(tire_step_size);
-
-            tire_front_left = tire_FL;
-            tire_front_right = tire_FR;
-            tire_rear_left = tire_RL;
-            tire_rear_right = tire_RR;
-
-            break;
-        }
-        case PACEJKA: {
-            std::string param_file = vehicle::GetDataFile("hmmwv/tire/HMMWV_pacejka.tir");
-
-            ChSharedPtr<ChPacejkaTire> tire_FL(new ChPacejkaTire("FL", param_file));
-            ChSharedPtr<ChPacejkaTire> tire_FR(new ChPacejkaTire("FR", param_file));
-            ChSharedPtr<ChPacejkaTire> tire_RL(new ChPacejkaTire("RL", param_file));
-            ChSharedPtr<ChPacejkaTire> tire_RR(new ChPacejkaTire("RR", param_file));
-
-            tire_FL->Initialize(LEFT, false);
-            tire_FR->Initialize(RIGHT, false);
-            tire_RL->Initialize(LEFT, true);
-            tire_RR->Initialize(RIGHT, true);
-
-            tire_FL->SetStepsize(tire_step_size);
-            tire_FR->SetStepsize(tire_step_size);
-            tire_RL->SetStepsize(tire_step_size);
-            tire_RR->SetStepsize(tire_step_size);
-
-            tire_front_left = tire_FL;
-            tire_front_right = tire_FR;
-            tire_rear_left = tire_RL;
-            tire_rear_right = tire_RR;
-
-            break;
-        }
-    }
-
-    // -------------------------------------
     // Create the vehicle Irrlicht interface
-    // -------------------------------------
-
-    ChVehicleIrrApp app(vehicle, *powertrain.get_ptr(), L"HMMWV Demo");
-
+    ChVehicleIrrApp app(my_hmmwv.GetVehicle(), my_hmmwv.GetPowertrain(), L"HMMWV Demo");
     app.SetSkyBox();
     app.AddTypicalLights(irr::core::vector3df(30.f, -30.f, 100.f), irr::core::vector3df(30.f, 50.f, 100.f), 250, 130);
     app.SetChaseCamera(trackPoint, 6.0, 0.5);
-
     app.SetTimestep(step_size);
-
     app.AssetBindAll();
     app.AssetUpdateAll();
 
     // Create the interactive driver system
-    ChIrrGuiDriver driver(app, vehicle, *powertrain.get_ptr());
+    ChIrrGuiDriver driver(app, my_hmmwv.GetVehicle(), my_hmmwv.GetPowertrain());
 
     // Set the time response for steering and throttle keyboard inputs.
-    // NOTE: this is not exact, since we do not render quite at the specified FPS.
     double steering_time = 1.0;  // time to go from 0 to +1 (or from 0 to -1)
     double throttle_time = 1.0;  // time to go from 0 to +1
     double braking_time = 0.3;   // time to go from 0 to +1
@@ -256,8 +140,7 @@ int main(int argc, char* argv[]) {
             std::cout << "Error creating directory " << pov_dir << std::endl;
             return 1;
         }
-
-        vehicle.ExportMeshPovray(out_dir);
+        my_hmmwv.ExportMeshPovray(out_dir);
     }
 
     if (driver_output) {
@@ -270,35 +153,23 @@ int main(int argc, char* argv[]) {
 
     if (debug_output) {
         GetLog() << "\n\n============ System Configuration ============\n";
-        vehicle.LogHardpointLocations();
+        my_hmmwv.LogHardpointLocations();
     }
 
-    // Inter-module communication data
-    ChTireForces tire_forces(4);
-    ChWheelState wheel_states[4];
-    double driveshaft_speed;
-    double powertrain_torque;
-    double throttle_input;
-    double steering_input;
-    double braking_input;
-
-    // Number of simulation steps between two 3D view render frames
+    // Number of simulation steps between miscellaneous events
     int render_steps = (int)std::ceil(render_step_size / step_size);
-
-    // Number of simulation steps between two debug logging frames
     int debug_steps = (int)std::ceil(debug_step_size / step_size);
-
-    // Number of simulation steps between two driver output frames
     int driver_steps = (int)std::ceil(driver_step_size / step_size);
 
     // Initialize simulation frame counter and simulation time
+    ChRealtimeStepTimer realtime_timer;
     int step_number = 0;
     int render_frame = 0;
     double time = 0;
 
-    ChRealtimeStepTimer realtime_timer;
-
     while (app.GetDevice()->run()) {
+        time = my_hmmwv.GetSystem()->GetChTime();
+
         // Render scene and output POV-Ray data
         if (step_number % render_steps == 0) {
             app.BeginScene(true, true, irr::video::SColor(255, 140, 161, 192));
@@ -308,74 +179,40 @@ int main(int argc, char* argv[]) {
             if (povray_output) {
                 char filename[100];
                 sprintf(filename, "%s/data_%03d.dat", pov_dir.c_str(), render_frame + 1);
-                utils::WriteShapesPovray(vehicle.GetSystem(), filename);
+                utils::WriteShapesPovray(my_hmmwv.GetSystem(), filename);
             }
 
             render_frame++;
         }
 
+        // Debug logging
         if (debug_output && step_number % debug_steps == 0) {
             GetLog() << "\n\n============ System Information ============\n";
             GetLog() << "Time = " << time << "\n\n";
-            vehicle.DebugLog(DBG_SPRINGS | DBG_SHOCKS | DBG_CONSTRAINTS);
+            my_hmmwv.DebugLog(DBG_SPRINGS | DBG_SHOCKS | DBG_CONSTRAINTS);
         }
 
-        // Collect output data from modules (for inter-module communication)
-        throttle_input = driver.GetThrottle();
-        steering_input = driver.GetSteering();
-        braking_input = driver.GetBraking();
-
-        powertrain_torque = powertrain->GetOutputTorque();
-
-        tire_forces[FRONT_LEFT.id()] = tire_front_left->GetTireForce();
-        tire_forces[FRONT_RIGHT.id()] = tire_front_right->GetTireForce();
-        tire_forces[REAR_LEFT.id()] = tire_rear_left->GetTireForce();
-        tire_forces[REAR_RIGHT.id()] = tire_rear_right->GetTireForce();
-
-        driveshaft_speed = vehicle.GetDriveshaftSpeed();
-
-        wheel_states[FRONT_LEFT.id()] = vehicle.GetWheelState(FRONT_LEFT);
-        wheel_states[FRONT_RIGHT.id()] = vehicle.GetWheelState(FRONT_RIGHT);
-        wheel_states[REAR_LEFT.id()] = vehicle.GetWheelState(REAR_LEFT);
-        wheel_states[REAR_RIGHT.id()] = vehicle.GetWheelState(REAR_RIGHT);
-
-        // Update modules (process inputs from other modules)
-        time = vehicle.GetSystem()->GetChTime();
-
-        driver.Update(time);
+        // Driver output
         if (driver_output && step_number % driver_steps == 0) {
             driver.Log(time);
         }
 
+        // Collect output data from modules (for inter-module communication)
+        double throttle_input = driver.GetThrottle();
+        double steering_input = driver.GetSteering();
+        double braking_input = driver.GetBraking();
+
+        // Update modules (process inputs from other modules)
+        driver.Update(time);
         terrain.Update(time);
-
-        tire_front_left->Update(time, wheel_states[FRONT_LEFT.id()], terrain);
-        tire_front_right->Update(time, wheel_states[FRONT_RIGHT.id()], terrain);
-        tire_rear_left->Update(time, wheel_states[REAR_LEFT.id()], terrain);
-        tire_rear_right->Update(time, wheel_states[REAR_RIGHT.id()], terrain);
-
-        powertrain->Update(time, throttle_input, driveshaft_speed);
-
-        vehicle.Update(time, steering_input, braking_input, powertrain_torque, tire_forces);
-
+        my_hmmwv.Update(time, steering_input, braking_input, throttle_input, terrain);
         app.Update(driver.GetInputModeAsString(), steering_input, throttle_input, braking_input);
 
         // Advance simulation for one timestep for all modules
         double step = realtime_timer.SuggestSimulationStep(step_size);
-
         driver.Advance(step);
-
         terrain.Advance(step);
-
-        tire_front_left->Advance(step);
-        tire_front_right->Advance(step);
-        tire_rear_right->Advance(step);
-        tire_rear_left->Advance(step);
-
-        powertrain->Advance(step);
-
-        vehicle.Advance(step);
-
+        my_hmmwv.Advance(step);
         app.Advance(step);
 
         // Increment frame number
---
+++ src/demos/vehicle/hmmwv/HMMWV.h
@@ -0,0 +1,126 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// wrapper classes for modeling an entire HMMWV vehicle assembly
+// (including the vehicle itself, the powertrain, and the tires).
+//
+// =============================================================================
+
+#ifndef HMMWV_H
+#define HMMWV_H
+
+#include <string>
+
+#include "chrono_vehicle/tire/ChPacejkaTire.h"
+
+#include "ModelDefs.h"
+#include "hmmwv/vehicle/HMMWV_Vehicle.h"
+#include "hmmwv/vehicle/HMMWV_VehicleReduced.h"
+#include "hmmwv/powertrain/HMMWV_Powertrain.h"
+#include "hmmwv/powertrain/HMMWV_SimplePowertrain.h"
+#include "hmmwv/tire/HMMWV_RigidTire.h"
+#include "hmmwv/tire/HMMWV_LugreTire.h"
+
+namespace hmmwv {
+
+class HMMWV {
+  public:
+    virtual ~HMMWV();
+
+    void SetChassisFixed(bool val) { m_fixed = val; }
+
+    void SetDriveType(DrivelineType val) { m_driveType = val; }
+    void SetPowertrainType(PowertrainModelType val) { m_powertrainType = val; }
+    void SetTireType(TireModelType val) { m_tireType = val; }
+
+    void SetChassisVis(VisualizationType val) { m_chassisVis = val; }
+    void SetwheelVis(VisualizationType val) { m_wheelVis = val; }
+
+    void SetInitPosition(const chrono::ChCoordsys<>& pos) { m_initPos = pos; }
+
+    void SetTireStepSize(double step_size) { m_tire_step_size = step_size; }
+    void SetPacejkaParamfile(const std::string& filename) { m_pacejkaParamFile = filename; }
+
+    chrono::ChSystem* GetSystem() const { return m_vehicle->GetSystem(); }
+    chrono::ChVehicle& GetVehicle() const { return *m_vehicle; }
+    chrono::ChPowertrain& GetPowertrain() const { return *m_powertrain; }
+
+    void Initialize();
+
+    void Update(double time,
+                double steering_input,
+                double braking_input,
+                double throttle_input,
+                const chrono::ChTerrain& terrain);
+
+    void Advance(double step);
+
+  protected:
+    // Protected constructor -- this class cannot be instantiated by itself.
+    HMMWV();
+
+    virtual chrono::ChVehicle* CreateVehicle() = 0;
+
+    bool m_fixed;
+    VisualizationType m_chassisVis;
+    VisualizationType m_wheelVis;
+
+    DrivelineType m_driveType;
+    PowertrainModelType m_powertrainType;
+    TireModelType m_tireType;
+
+    double m_tire_step_size;
+    std::string m_pacejkaParamFile;
+
+    chrono::ChCoordsys<> m_initPos;
+
+    chrono::ChVehicle* m_vehicle;
+    chrono::ChPowertrain* m_powertrain;
+    chrono::ChTire* m_tireFL;
+    chrono::ChTire* m_tireFR;
+    chrono::ChTire* m_tireRR;
+    chrono::ChTire* m_tireRL;
+};
+
+class HMMWV_Full : public HMMWV {
+  public:
+    HMMWV_Full() {}
+    ~HMMWV_Full() {}
+
+    void ExportMeshPovray(const std::string& out_dir) { ((HMMWV_Vehicle*)m_vehicle)->ExportMeshPovray(out_dir); }
+    void LogHardpointLocations() { ((HMMWV_Vehicle*)m_vehicle)->LogHardpointLocations(); }
+    void DebugLog(int what) { ((HMMWV_Vehicle*)m_vehicle)->DebugLog(what); }
+
+  private:
+    virtual chrono::ChVehicle* CreateVehicle() override {
+        return new HMMWV_Vehicle(m_fixed, m_driveType, m_chassisVis, m_wheelVis);
+    }
+};
+
+class HMMWV_Reduced : public HMMWV {
+  public:
+    HMMWV_Reduced() {}
+    ~HMMWV_Reduced() {}
+
+    void ExportMeshPovray(const std::string& out_dir) { ((HMMWV_VehicleReduced*)m_vehicle)->ExportMeshPovray(out_dir); }
+
+  private:
+    virtual chrono::ChVehicle* CreateVehicle() override {
+        return new HMMWV_VehicleReduced(m_fixed, m_driveType, m_chassisVis, m_wheelVis);
+    }
+};
+
+}  // end namespace hmmwv
+
+#endif
