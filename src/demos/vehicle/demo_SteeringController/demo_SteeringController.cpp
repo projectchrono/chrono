87cc279354a212c1e0481ec845ea99428ea80439
Radu Serban <serban@engr.wisc.edu>
2015-10-01 18:02:57 +0200

    Update the demo_SteeringController.
    - use the full HMMWV vehicle
    - add POV-Ray output
    - add debug logging
    - update to use new mechanism for visualizing the path

--- src/demos/vehicle/demo_SteeringController/demo_SteeringController.cpp
+++ src/demos/vehicle/demo_SteeringController/demo_SteeringController.cpp
@@ -19,37 +19,35 @@
 //
 // =============================================================================
 
-#include <vector>
-
+#include "chrono/core/ChFileutils.h"
 #include "chrono/core/ChRealtimeStep.h"
-#include "chrono/geometry/ChCLineBezier.h"
-#include "chrono/assets/ChLineShape.h"
 
 #include "chrono_vehicle/ChVehicleModelData.h"
-#include "chrono_vehicle/vehicle/Vehicle.h"
-#include "chrono_vehicle/powertrain/SimplePowertrain.h"
-#include "chrono_vehicle/tire/RigidTire.h"
-#include "chrono_vehicle/tire/LugreTire.h"
 #include "chrono_vehicle/terrain/RigidTerrain.h"
 #include "chrono_vehicle/utils/ChVehicleIrrApp.h"
 #include "chrono_vehicle/driver/ChIrrGuiDriver.h"
 
 #include "ModelDefs.h"
+#include "hmmwv/HMMWV.h"
 #include "generic/Generic_PathFollowerDriver.h"
 
 using namespace chrono;
+using namespace hmmwv;
 using namespace geometry;
 
 // =============================================================================
 // Problem parameters
 
-// Type of tire model (RIGID or LUGRE)
+// Type of tire model (RIGID, LUGRE, FIALA)
 TireModelType tire_model = RIGID;
+
+// Type of powertrain model (SHAFTS, SIMPLE)
+PowertrainModelType powertrain_model = SHAFTS;
 
 // Input file names for the path-follower driver model
 std::string steering_controller_file("generic/driver/SteeringController.json");
 std::string speed_controller_file("generic/driver/SpeedController.json");
-//std::string path_file("paths/curve.txt");
+// std::string path_file("paths/curve.txt");
 std::string path_file("paths/ISO_double_lane_change.txt");
 
 // JSON file names for vehicle model, tire models, and (simple) powertrain
@@ -59,7 +57,7 @@ std::string lugretire_file("generic/tire/LugreTire.json");
 std::string simplepowertrain_file("generic/powertrain/SimplePowertrain.json");
 
 // Initial vehicle location and orientation
-ChVector<> initLoc(-125, -125, 0.6);
+ChVector<> initLoc(-125, -125, 0.5);
 ChQuaternion<> initRot(1, 0, 0, 0);
 
 // Desired vehicle speed (m/s)
@@ -70,14 +68,26 @@ double terrainHeight = 0;
 double terrainLength = 300.0;  // size in X direction
 double terrainWidth = 300.0;   // size in Y direction
 
-// Simulation step size
-double step_size = 2e-3;
-
-// Render FPS
-double fps = 60;
-
 // Point on chassis tracked by the chase camera
 ChVector<> trackPoint(0.0, 0.0, 1.75);
+
+// Simulation step size
+double step_size = 1e-3;
+double tire_step_size = step_size;
+
+// Render FPS
+double fps = 30;
+
+// Debug logging
+bool debug_output = false;
+double debug_fps = 10;
+
+// Output directories
+const std::string out_dir = "../STEERING_CONTROLLER";
+const std::string pov_dir = out_dir + "/POVRAY";
+
+// POV-Ray output
+bool povray_output = true;
 
 // =============================================================================
 
@@ -155,48 +165,25 @@ class ChDriverSelector : public irr::IEventReceiver {
 // =============================================================================
 
 int main(int argc, char* argv[]) {
-    // --------------------------
-    // Create the various systems
-    // --------------------------
+    // ------------------------------
+    // Create the vehicle and terrain
+    // ------------------------------
 
-    // Create and initialize the vehicle system
-    Vehicle vehicle(vehicle::GetDataFile(vehicle_file));
-    vehicle.Initialize(ChCoordsys<>(initLoc, initRot));
-    ////vehicle.GetChassis()->SetBodyFixed(true);
+    // Create the HMMWV vehicle, set parameters, and initialize
+    HMMWV_Full my_hmmwv;
+    my_hmmwv.SetChassisFixed(false);
+    my_hmmwv.SetChassisVis(PRIMITIVES);
+    my_hmmwv.SetWheelVis(PRIMITIVES);
+    my_hmmwv.SetInitPosition(ChCoordsys<>(initLoc, initRot));
+    my_hmmwv.SetPowertrainType(powertrain_model);
+    my_hmmwv.SetDriveType(RWD);
+    my_hmmwv.SetTireType(tire_model);
+    my_hmmwv.SetTireStepSize(tire_step_size);
+    my_hmmwv.Initialize();
 
     // Create the terrain
-    RigidTerrain terrain(vehicle.GetSystem(), terrainHeight, terrainLength, terrainWidth, 0.9, GetChronoDataFile("textures/tile4.jpg"), 200, 200);
-
-    // Create and initialize the powertrain system
-    SimplePowertrain powertrain(vehicle::GetDataFile(simplepowertrain_file));
-    powertrain.Initialize();
-
-    // Create and initialize the tires
-    int num_axles = vehicle.GetNumberAxles();
-    int num_wheels = 2 * num_axles;
-
-    std::vector<ChSharedPtr<ChTire> > tires(num_wheels);
-
-    switch (tire_model) {
-        case RIGID: {
-            std::vector<ChSharedPtr<RigidTire> > tires_rigid(num_wheels);
-            for (int i = 0; i < num_wheels; i++) {
-                tires_rigid[i] = ChSharedPtr<RigidTire>(new RigidTire(vehicle::GetDataFile(rigidtire_file)));
-                tires_rigid[i]->Initialize(vehicle.GetWheelBody(i));
-                tires[i] = tires_rigid[i];
-            }
-            break;
-        }
-        case LUGRE: {
-            std::vector<ChSharedPtr<LugreTire> > tires_lugre(num_wheels);
-            for (int i = 0; i < num_wheels; i++) {
-                tires_lugre[i] = ChSharedPtr<LugreTire>(new LugreTire(vehicle::GetDataFile(lugretire_file)));
-                tires_lugre[i]->Initialize(vehicle.GetWheelBody(i));
-                tires[i] = tires_lugre[i];
-            }
-            break;
-        }
-    }
+    RigidTerrain terrain(my_hmmwv.GetSystem(), terrainHeight, terrainLength, terrainWidth, 0.9,
+                         GetChronoDataFile("textures/tile4.jpg"), 200, 200);
 
     // ----------------------
     // Create the Bezier path
@@ -205,33 +192,24 @@ int main(int argc, char* argv[]) {
     ChBezierCurve* path = ChBezierCurve::read(vehicle::GetDataFile(path_file));
     ////path->write("my_path.txt");
 
-    // To visualize the path, create a fixed body and add the path as its asset
-    ChSharedPtr<ChBody> road(new ChBody);
-    road->SetBodyFixed(true);
-    vehicle.GetSystem()->AddBody(road);
-    ChSharedPtr<ChLineShape> path_asset(new ChLineShape);
-    path_asset->SetLineGeometry(ChSharedPtr<ChLineBezier>(new ChLineBezier(path)));
-    path_asset->SetColor(ChColor(0.0f, 0.8f, 0.0f));
-    road->AddAsset(path_asset);
-
     // ---------------------------------------
     // Create the vehicle Irrlicht application
     // ---------------------------------------
 
-    ChVehicleIrrApp app(vehicle, powertrain, L"Steering Controller Demo", irr::core::dimension2d<irr::u32>(800, 640));
+    ChVehicleIrrApp app(my_hmmwv.GetVehicle(), my_hmmwv.GetPowertrain(), L"Steering Controller Demo",
+                        irr::core::dimension2d<irr::u32>(800, 640));
 
     app.SetHUDLocation(500, 20);
     app.SetSkyBox();
     app.AddTypicalLogo();
-    app.AddTypicalLights(irr::core::vector3df(-150.f, -150.f, 200.f), irr::core::vector3df(-150.f, 150.f, 200.f), 100, 100);
-    app.AddTypicalLights(irr::core::vector3df(150.f, -150.f, 200.f), irr::core::vector3df(150.0f, 150.f, 200.f), 100, 100);
+    app.AddTypicalLights(irr::core::vector3df(-150.f, -150.f, 200.f), irr::core::vector3df(-150.f, 150.f, 200.f), 100,
+                         100);
+    app.AddTypicalLights(irr::core::vector3df(150.f, -150.f, 200.f), irr::core::vector3df(150.0f, 150.f, 200.f), 100,
+                         100);
     app.EnableGrid(false);
     app.SetChaseCamera(trackPoint, 6.0, 0.5);
 
     app.SetTimestep(step_size);
-
-    app.AssetBindAll();
-    app.AssetUpdateAll();
 
     // Visualization of controller points (sentinel & target)
     irr::scene::IMeshSceneNode* ballS = app.GetSceneManager()->addSphereSceneNode(0.1f);
@@ -244,77 +222,108 @@ int main(int argc, char* argv[]) {
     // -------------------------
 
     // Create both a GUI driver and a path-follower and allow switching between them
-    ChIrrGuiDriver driver_gui(app, vehicle, powertrain);
+    ChIrrGuiDriver driver_gui(app, my_hmmwv.GetVehicle(), my_hmmwv.GetPowertrain());
 
     /*
-    Generic_PathFollowerDriver driver_follower(vehicle, path);
+    Generic_PathFollowerDriver driver_follower(my_hmmwv.GetVehicle(), path);
     driver_follower.GetSteeringController().SetLookAheadDistance(5);
     driver_follower.GetSteeringController().SetGains(0.5, 0, 0);
     driver_follower.GetSpeedController().SetGains(0.4, 0, 0);
     */
-    Generic_PathFollowerDriver driver_follower(vehicle, vehicle::GetDataFile(steering_controller_file),
-                                               vehicle::GetDataFile(speed_controller_file), path);
+    Generic_PathFollowerDriver driver_follower(my_hmmwv.GetVehicle(), vehicle::GetDataFile(steering_controller_file),
+                                               vehicle::GetDataFile(speed_controller_file), path, "my_path");
 
     driver_follower.SetDesiredSpeed(target_speed);
 
     // Create and register a custom Irrlicht event receiver to allow selecting the
     // current driver model.
-    ChDriverSelector selector(vehicle, &driver_follower, &driver_gui);
+    ChDriverSelector selector(my_hmmwv.GetVehicle(), &driver_follower, &driver_gui);
     app.SetUserEventReceiver(&selector);
+
+    // Finalize construction of visualization assets
+    app.AssetBindAll();
+    app.AssetUpdateAll();
+
+    // -----------------
+    // Initialize output
+    // -----------------
+
+    if (povray_output) {
+        if (ChFileutils::MakeDirectory(out_dir.c_str()) < 0) {
+            std::cout << "Error creating directory " << out_dir << std::endl;
+            return 1;
+        }
+        if (ChFileutils::MakeDirectory(pov_dir.c_str()) < 0) {
+            std::cout << "Error creating directory " << pov_dir << std::endl;
+            return 1;
+        }
+        my_hmmwv.ExportMeshPovray(out_dir);
+        driver_follower.ExportPathPovray(out_dir);
+    }
 
     // ---------------
     // Simulation loop
     // ---------------
 
-    // Inter-module communication data
-    ChTireForces tire_forces(num_wheels);
-    ChWheelStates wheel_states(num_wheels);
-    double driveshaft_speed;
-    double powertrain_torque;
-    double throttle_input;
-    double steering_input;
-    double braking_input;
+    // Driver location in vehicle local frame
+    ChVector<> driver_pos = my_hmmwv.GetVehicle().GetLocalDriverCoordsys().pos;
 
-    ChRealtimeStepTimer realtime_timer;
+    // Number of simulation steps between miscellaneous events
     double render_step_size = 1 / fps;
     int render_steps = (int)std::ceil(render_step_size / step_size);
-    int step_number = 0;
+    double debug_step_size = 1 / debug_fps;
+    int debug_steps = (int)std::ceil(debug_step_size / step_size);
+
+    // Initialize simulation frame counter and simulation time
+    ChRealtimeStepTimer realtime_timer;
+    int sim_frame = 0;
+    int render_frame = 0;
 
     while (app.GetDevice()->run()) {
+        double time = my_hmmwv.GetSystem()->GetChTime();
+
         // Update sentinel and target location markers for the path-follower controller.
         // Note that we do this whether or not we are currently using the path-follower driver.
         const ChVector<>& pS = driver_follower.GetSteeringController().GetSentinelLocation();
         const ChVector<>& pT = driver_follower.GetSteeringController().GetTargetLocation();
-        ballS->setPosition(irr::core::vector3df(pS.x, pS.y, pS.z));
-        ballT->setPosition(irr::core::vector3df(pT.x, pT.y, pT.z));
+        ballS->setPosition(irr::core::vector3df((irr::f32)pS.x, (irr::f32)pS.y, (irr::f32)pS.z));
+        ballT->setPosition(irr::core::vector3df((irr::f32)pT.x, (irr::f32)pT.y, (irr::f32)pT.z));
 
-        // Render scene
-        if (step_number % render_steps == 0) {
+        // Render scene and output POV-Ray data
+        if (sim_frame % render_steps == 0) {
             app.BeginScene(true, true, irr::video::SColor(255, 140, 161, 192));
             app.DrawAll();
             app.EndScene();
+
+            if (povray_output) {
+                char filename[100];
+                sprintf(filename, "%s/data_%03d.dat", pov_dir.c_str(), render_frame + 1);
+                utils::WriteShapesPovray(my_hmmwv.GetSystem(), filename);
+            }
+
+            render_frame++;
+        }
+
+        // Debug logging
+        if (debug_output && sim_frame % debug_steps == 0) {
+            ChVector<> driver_acc = my_hmmwv.GetVehicle().GetVehicleAcceleration(driver_pos);
+            GetLog() << "driver acceleration:  " << driver_acc.x << "  " << driver_acc.y << "  " << driver_acc.z
+                     << "\n";
+            ChVector<> acc = my_hmmwv.GetVehicle().GetChassis()->GetPos_dtdt();
+            GetLog() << "CG acceleration:      " << acc.x << "  " << acc.y << "  " << acc.z << "\n";
+            GetLog() << "\n";
         }
 
         // Collect output data from modules (for inter-module communication)
-        throttle_input = selector.GetDriver()->GetThrottle();
-        steering_input = selector.GetDriver()->GetSteering();
-        braking_input = selector.GetDriver()->GetBraking();
-        powertrain_torque = powertrain.GetOutputTorque();
-        driveshaft_speed = vehicle.GetDriveshaftSpeed();
-        for (int i = 0; i < num_wheels; i++) {
-            tire_forces[i] = tires[i]->GetTireForce();
-            wheel_states[i] = vehicle.GetWheelState(i);
-        }
+        double throttle_input = selector.GetDriver()->GetThrottle();
+        double steering_input = selector.GetDriver()->GetSteering();
+        double braking_input = selector.GetDriver()->GetBraking();
 
         // Update modules (process inputs from other modules)
-        double time = vehicle.GetChTime();
         driver_follower.Update(time);
         driver_gui.Update(time);
-        powertrain.Update(time, throttle_input, driveshaft_speed);
-        vehicle.Update(time, steering_input, braking_input, powertrain_torque, tire_forces);
         terrain.Update(time);
-        for (int i = 0; i < num_wheels; i++)
-            tires[i]->Update(time, wheel_states[i], terrain);
+        my_hmmwv.Update(time, steering_input, braking_input, throttle_input, terrain);
         std::string msg = selector.UsingGUI() ? "GUI driver" : "Follower driver";
         app.Update(msg, steering_input, throttle_input, braking_input);
 
@@ -322,15 +331,12 @@ int main(int argc, char* argv[]) {
         double step = realtime_timer.SuggestSimulationStep(step_size);
         driver_follower.Advance(step);
         driver_gui.Advance(step);
-        powertrain.Advance(step);
-        vehicle.Advance(step);
         terrain.Advance(step);
-        for (int i = 0; i < num_wheels; i++)
-            tires[i]->Advance(step);
+        my_hmmwv.Advance(step);
         app.Advance(step);
 
-        // Increment step number
-        step_number++;
+        // Increment simulation frame number
+        sim_frame++;
     }
 
     return 0;
