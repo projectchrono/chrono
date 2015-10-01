277048b1b6577f5d6383b9be90475aea40f6eb38
Radu Serban <serban@engr.wisc.edu>
2015-10-01 18:00:54 +0200

    Add functionality for visualizing the path in the generic path-follower driver model

--- src/demos/vehicle/generic/Generic_PathFollowerDriver.cpp
+++ src/demos/vehicle/generic/Generic_PathFollowerDriver.cpp
@@ -16,24 +16,48 @@
 // =============================================================================
 
 #include "chrono/core/ChMathematics.h"
+#include "chrono/geometry/ChCLineBezier.h"
+#include "chrono/utils/ChUtilsInputOutput.h"
 
 #include "generic/Generic_PathFollowerDriver.h"
 
 using namespace chrono;
 
-Generic_PathFollowerDriver::Generic_PathFollowerDriver(ChVehicle& vehicle, ChBezierCurve* path)
-    : m_vehicle(vehicle), m_steeringPID(path), m_target_speed(0) {
-    m_steeringPID.Reset(vehicle);
-    m_speedPID.Reset(vehicle);
+Generic_PathFollowerDriver::Generic_PathFollowerDriver(ChVehicle& vehicle,
+                                                       ChBezierCurve* path,
+                                                       const std::string& path_name)
+    : m_vehicle(vehicle), m_steeringPID(path), m_pathName(path_name), m_target_speed(0) {
+    Create();
 }
 
 Generic_PathFollowerDriver::Generic_PathFollowerDriver(ChVehicle& vehicle,
                                                        const std::string& steering_filename,
                                                        const std::string& speed_filename,
-                                                       ChBezierCurve* path)
-    : m_vehicle(vehicle), m_steeringPID(steering_filename, path), m_speedPID(speed_filename), m_target_speed(0) {
-    m_steeringPID.Reset(vehicle);
-    m_speedPID.Reset(vehicle);
+                                                       ChBezierCurve* path,
+                                                       const std::string& path_name)
+    : m_vehicle(vehicle),
+      m_steeringPID(steering_filename, path),
+      m_speedPID(speed_filename),
+      m_pathName(path_name),
+      m_target_speed(0) {
+    Create();
+}
+
+void Generic_PathFollowerDriver::Create() {
+    // Reset the steering and speed controllers
+    m_steeringPID.Reset(m_vehicle);
+    m_speedPID.Reset(m_vehicle);
+
+    // Create a fixed body to carry a visualization asset for the path
+    ChSharedPtr<ChBody> road(new ChBody);
+    road->SetBodyFixed(true);
+    m_vehicle.GetSystem()->AddBody(road);
+    ChSharedPtr<ChLineShape> path_asset(new ChLineShape);
+    path_asset->SetLineGeometry(
+        ChSharedPtr<geometry::ChLineBezier>(new geometry::ChLineBezier(m_steeringPID.GetPath())));
+    path_asset->SetColor(ChColor(0.0f, 0.8f, 0.0f));
+    path_asset->SetName(m_pathName);
+    road->AddAsset(path_asset);
 }
 
 void Generic_PathFollowerDriver::Reset() {
@@ -64,4 +88,8 @@ void Generic_PathFollowerDriver::Advance(double step) {
     double out_steering = m_steeringPID.Advance(m_vehicle, step);
     ChClampValue(out_steering, -1.0, 1.0);
     m_steering = out_steering;
+}
+
+void Generic_PathFollowerDriver::ExportPathPovray(const std::string& out_dir) {
+    utils::WriteCurvePovray(*m_steeringPID.GetPath(), m_pathName, out_dir, 0.04, ChColor(0.8f, 0.5f, 0.0f));
 }
