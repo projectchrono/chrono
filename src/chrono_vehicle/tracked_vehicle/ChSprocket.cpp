8cbaa53449228dec7a5f705bf848659cbdf4b8c9
Radu Serban <serban@engr.wisc.edu>
2015-10-23 12:41:19 +0200

    Full implementation of sprocket subsystem

--- src/chrono_vehicle/tracked_vehicle/ChSprocket.cpp
+++ src/chrono_vehicle/tracked_vehicle/ChSprocket.cpp
@@ -20,10 +20,82 @@
 //
 // =============================================================================
 
+#include "chrono/assets/ChLineShape.h"
+
 #include "chrono_vehicle/tracked_vehicle/ChSprocket.h"
 
 namespace chrono {
 namespace vehicle {
+
+void ChSprocket::Initialize(ChSharedPtr<ChBodyAuxRef> chassis, const ChVector<>& location) {
+    // The sprocket reference frame is aligned with that of the chassis and centered at the
+    // specified location.
+    ////ChFrame<> sprocket_to_abs(location);
+    ////sprocket_to_abs.ConcatenatePreTransformation(chassis->GetFrame_REF_to_abs());
+    ChVector<> loc = chassis->GetFrame_REF_to_abs().TransformPointLocalToParent(location);
+    ChQuaternion<> chassisRot = chassis->GetFrame_REF_to_abs().GetRot();
+    ChQuaternion<> y2z = Q_from_AngX(CH_C_PI_2);
+    ChMatrix33<> rot_y2z(y2z);
+
+    // Create and initialize the gear body (same orientation as the chassis).
+    m_gear = ChSharedPtr<ChBody>(new ChBody(chassis->GetSystem()->GetContactMethod()));
+    m_gear->SetNameString(m_name + "_gear");
+    m_gear->SetPos(loc);
+    m_gear->SetRot(chassisRot);
+    m_gear->SetMass(getGearMass());
+    m_gear->SetInertiaXX(getGearInertia());
+    chassis->GetSystem()->AddBody(m_gear);
+
+    // Add collision shape to gear body.
+    double sep = getSeparation();
+    ChSharedPtr<geometry::ChLinePath> profile = getProfile();
+    m_gear->SetCollide(true);
+    m_gear->GetCollisionModel()->SetSafeMargin(0.02);
+    m_gear->GetCollisionModel()->ClearModel();
+    m_gear->GetCollisionModel()->Add2Dpath(*profile.get_ptr(), ChVector<>(0, 0, sep / 2), rot_y2z);
+    m_gear->GetCollisionModel()->Add2Dpath(*profile.get_ptr(), ChVector<>(0, 0, -sep / 2), rot_y2z);
+    m_gear->GetCollisionModel()->BuildModel();
+
+    // Create and initialize the revolute joint between chassis and gear.
+    ChCoordsys<> rev_csys(loc, chassisRot * y2z);
+    m_revolute = ChSharedPtr<ChLinkLockRevolute>(new ChLinkLockRevolute);
+    m_revolute->SetNameString(m_name + "_revolute");
+    m_revolute->Initialize(chassis, m_gear, rev_csys);
+
+    // Create and initialize the axle shaft and its connection to the gear. Note that the
+    // gear rotates about the Y axis.
+    m_axle = ChSharedPtr<ChShaft>(new ChShaft);
+    m_axle->SetNameString(m_name + "_axle");
+    m_axle->SetInertia(getAxleInertia());
+    chassis->GetSystem()->Add(m_axle);
+
+    m_axle_to_spindle = ChSharedPtr<ChShaftsBody>(new ChShaftsBody);
+    m_axle_to_spindle->SetNameString(m_name + "_axle_to_spindle");
+    m_axle_to_spindle->Initialize(m_axle, m_gear, ChVector<>(0, -1, 0));
+    chassis->GetSystem()->Add(m_axle_to_spindle);
+}
+
+void ChSprocket::AddGearVisualization(const ChColor& color) {
+    ChQuaternion<> y2z = Q_from_AngX(CH_C_PI_2);
+    ChMatrix33<> rot_y2z(y2z);
+
+    double sep = getSeparation();
+    ChSharedPtr<geometry::ChLinePath> profile = getProfile();
+
+    ChSharedPtr<ChLineShape> asset_1(new ChLineShape);
+    asset_1->SetLineGeometry(profile);
+    asset_1->Pos = ChVector<>(0, 0, sep / 2);
+    asset_1->Rot = rot_y2z;
+    asset_1->SetColor(color);
+    m_gear->AddAsset(asset_1);
+
+    ChSharedPtr<ChLineShape> asset_2(new ChLineShape);
+    asset_2->SetLineGeometry(profile);
+    asset_2->Pos = ChVector<>(0, 0, -sep / 2);
+    asset_2->Rot = rot_y2z;
+    asset_2->SetColor(color);
+    m_gear->AddAsset(asset_2);
+}
 
 void ChSprocket::ApplyAxleTorque(double torque) {
     //// TODO: is this really needed?
--- src/chrono_vehicle/tracked_vehicle/ChSprocket.h
+++ src/chrono_vehicle/tracked_vehicle/ChSprocket.h
@@ -26,10 +26,14 @@
 #include <vector>
 
 #include "chrono/core/ChShared.h"
+#include "chrono/physics/ChSystem.h"
 #include "chrono/physics/ChBody.h"
+#include "chrono/physics/ChBodyAuxRef.h"
 #include "chrono/physics/ChLinkLock.h"
 #include "chrono/physics/ChShaft.h"
 #include "chrono/physics/ChShaftsBody.h"
+#include "chrono/geometry/ChCLinePath.h"
+#include "chrono/assets/ChColor.h"
 
 #include "chrono_vehicle/ChApiVehicle.h"
 #include "chrono_vehicle/ChSubsysDefs.h"
@@ -54,6 +58,9 @@ class CH_VEHICLE_API ChSprocket : public ChShared {
     /// Set the name identifier for this sprocket subsystem.
     void SetName(const std::string& name) { m_name = name; }
 
+    /// Get the number of teeth of the gear.
+    virtual int GetNumTeeth() const = 0;
+
     /// Get a handle to the road wheel body.
     ChSharedPtr<ChBody> GetWheel() const { return m_gear; }
 
@@ -66,15 +73,45 @@ class CH_VEHICLE_API ChSprocket : public ChShared {
     /// Get the angular speed of the axle.
     double GetAxleSpeed() const { return m_axle->GetPos_dt(); }
 
+    /// Initialize this sprocket subsystem.
+    /// The sprocket subsystem is initialized by attaching it to the specified
+    /// chassis body at the specified location (with respect to and expressed in
+    /// the reference frame of the chassis).
+    virtual void Initialize(ChSharedPtr<ChBodyAuxRef> chassis,  ///< [in] handle to the chassis body
+                            const ChVector<>& location          ///< [in] location relative to the chassis frame
+                            );
+
     /// Apply the provided motor torque.
     /// The given torque is applied to the axle. This function provides the interface
     /// to the drivetrain subsystem (intermediated by the vehicle system).
     void ApplyAxleTorque(double torque  ///< [in] value of applied torque
                          );
 
-  protected:
-    std::string m_name;  ///< name of the subsystem
+    /// Add visualization of the tooth profiles.
+    /// This (optional) function should be called only after a call to Initialize().
+    void AddGearVisualization(const ChColor& color);
 
+  protected:
+    /// Return the mass of the gear body.
+    virtual double getGearMass() const = 0;
+
+    /// Return the moments of inertia of the gear body.
+    virtual const ChVector<>& getGearInertia() = 0;
+
+    /// Return the inertia of the axle shaft.
+    virtual double getAxleInertia() const = 0;
+
+    /// Return the distance between the two gear profiles.
+    virtual double getSeparation() const = 0;
+
+    /// Return the 2D gear profile.
+    /// The gear profile, a ChLinePath geometric object, is made up of an arbitrary number
+    /// of sub-paths of type ChLineArc or ChLineSegment sub-lines. These must be added in
+    /// clockwise order, and the end of sub-path i must be coincident with beginning of
+    /// sub-path i+1.
+    virtual ChSharedPtr<geometry::ChLinePath> getProfile() = 0;
+
+    std::string m_name;                           ///< name of the subsystem
     ChSharedPtr<ChBody> m_gear;                   ///< handle to the sprocket gear body
     ChSharedPtr<ChShaft> m_axle;                  ///< handle to gear shafts
     ChSharedPtr<ChShaftsBody> m_axle_to_spindle;  ///< handle to gear-shaft connector
