8347a73b9a0a30f7fa86455dbbd14854f5928be3
Radu Serban <serban@engr.wisc.edu>
2015-10-25 11:10:30 +0100

    Add draft implementation for torsion bar suspension with linear-damper

---
+++ src/chrono_vehicle/tracked_vehicle/suspension/ChLinearDamperRWAssembly.cpp
@@ -0,0 +1,115 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Base class for a torsion-bar suspension system using linear dampers (template
+// definition).
+//
+// =============================================================================
+
+#include "chrono/assets/ChCylinderShape.h"
+#include "chrono/assets/ChColorAsset.h"
+
+#include "chrono_vehicle/tracked_vehicle/suspension/ChLinearDamperRWAssembly.h"
+
+namespace chrono {
+namespace vehicle {
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+ChLinearDamperRWAssembly::ChLinearDamperRWAssembly(const std::string& name) : ChRoadWheelAssembly(name) {
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void ChLinearDamperRWAssembly::Initialize(ChSharedPtr<ChBodyAuxRef> chassis, const ChVector<>& location) {
+    // Invoke the base class implementation
+    ChRoadWheelAssembly::Initialize(chassis, location);
+
+    // Express the suspension reference frame in the absolute coordinate system.
+    ChFrame<> susp_to_abs(location);
+    susp_to_abs.ConcatenatePreTransformation(chassis->GetFrame_REF_to_abs());
+
+    // Transform all points and directions to absolute frame.
+    std::vector<ChVector<> > points(NUM_POINTS);
+
+    for (int i = 0; i < NUM_POINTS; i++) {
+        ChVector<> rel_pos = GetLocation(static_cast<PointId>(i));
+        points[i] = susp_to_abs.TransformPointLocalToParent(rel_pos);
+    }
+
+    // Create the trailing arm body
+    // Create and initialize spindle body (same orientation as the chassis)
+    m_arm = ChSharedPtr<ChBody>(new ChBody(chassis->GetSystem()->GetContactMethod()));
+    m_arm->SetNameString(m_name + "_arm");
+    m_arm->SetPos(points[ARM]);
+    m_arm->SetRot(susp_to_abs.GetRot());
+    m_arm->SetMass(GetArmMass());
+    m_arm->SetInertiaXX(GetArmInertia());
+    AddVisualizationArm(points[ARM], susp_to_abs.GetPos(), points[ARM_CHASSIS]);
+    chassis->GetSystem()->AddBody(m_arm);
+
+    // Create and initialize the revolute joint between arm and chassis.
+    // The axis of rotation is the y axis of the suspension reference frame.
+    m_revolute = ChSharedPtr<ChLinkLockRevolute>(new ChLinkLockRevolute);
+    m_revolute->SetNameString(m_name + "_revolute");
+    m_revolute->Initialize(chassis, m_arm,
+                           ChCoordsys<>(points[ARM_CHASSIS], susp_to_abs.GetRot() * Q_from_AngX(CH_C_PI_2)));
+    // Add torsional spring associated with m_revolute
+    m_revolute->SetForce_Rz(GetTorsionForceFunction());
+    chassis->GetSystem()->AddLink(m_revolute);
+
+    // Create and initialize the tensioner force element.
+    m_shock = ChSharedPtr<ChLinkSpringCB>(new ChLinkSpringCB);
+    m_shock->SetNameString(m_name + "_shock");
+    m_shock->Initialize(chassis, m_arm, false, points[SHOCK_C], points[SHOCK_A]);
+    m_shock->Set_SpringCallback(GetShockForceCallback());
+    chassis->GetSystem()->AddLink(m_shock);
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void ChLinearDamperRWAssembly::AddVisualizationArm(const ChVector<>& pt_A,
+                                                   const ChVector<>& pt_AW,
+                                                   const ChVector<>& pt_AC) {
+    static const double threshold2 = 1e-6;
+    double radius = GetArmVisRadius();
+
+    // Express hardpoint locations in body frame.
+    ChVector<> p_A = m_arm->TransformPointParentToLocal(pt_A);
+    ChVector<> p_AC = m_arm->TransformPointParentToLocal(pt_AC);
+    ChVector<> p_AW = m_arm->TransformPointParentToLocal(pt_AW);
+
+    if ((p_A - p_AW).Length2() > threshold2) {
+        ChSharedPtr<ChCylinderShape> cyl(new ChCylinderShape);
+        cyl->GetCylinderGeometry().p1 = p_A;
+        cyl->GetCylinderGeometry().p2 = p_AW;
+        cyl->GetCylinderGeometry().rad = radius;
+        m_arm->AddAsset(cyl);
+    }
+
+    if ((p_A - p_AC).Length2() > threshold2) {
+        ChSharedPtr<ChCylinderShape> cyl(new ChCylinderShape);
+        cyl->GetCylinderGeometry().p1 = p_A;
+        cyl->GetCylinderGeometry().p2 = p_AC;
+        cyl->GetCylinderGeometry().rad = radius;
+        m_arm->AddAsset(cyl);
+    }
+
+    ChSharedPtr<ChColorAsset> col(new ChColorAsset);
+    col->SetColor(ChColor(0.2f, 0.6f, 0.3f));
+    m_arm->AddAsset(col);
+}
+
+}  // end namespace vehicle
+}  // end namespace chrono
---
+++ src/chrono_vehicle/tracked_vehicle/suspension/ChLinearDamperRWAssembly.h
@@ -0,0 +1,97 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Base class for a torsion-bar suspension system using linear dampers (template
+// definition).
+//
+// =============================================================================
+
+#ifndef CH_DOUBLE_IDLER_H
+#define CH_DOUBLE_IDLER_H
+
+#include "chrono/physics/ChLinkSpringCB.h"
+#include "chrono/physics/ChLinkForce.h"
+
+#include "chrono_vehicle/ChApiVehicle.h"
+#include "chrono_vehicle/ChSubsysDefs.h"
+
+#include "chrono_vehicle/tracked_vehicle/ChRoadWheelAssembly.h"
+
+namespace chrono {
+namespace vehicle {
+
+///
+///
+///
+class ChLinearDamperRWAssembly : public ChRoadWheelAssembly {
+  public:
+    ChLinearDamperRWAssembly(const std::string& name  ///< [in] name of the subsystem
+                             );
+
+    virtual ~ChLinearDamperRWAssembly() {}
+
+    /// Return a handle to the carrier body.
+    virtual ChSharedPtr<ChBody> GetCarrier() const override { return m_arm; }
+
+    /// Initialize this suspension subsystem.
+    /// The suspension subsystem is initialized by attaching it to the specified
+    /// chassis body at the specified location (with respect to and expressed in
+    /// the reference frame of the chassis). It is assumed that the suspension
+    /// reference frame is always aligned with the chassis reference frame.
+    virtual void Initialize(ChSharedPtr<ChBodyAuxRef> chassis,  ///< [in] handle to the chassis body
+                            const ChVector<>& location          ///< [in] location relative to the chassis frame
+                            ) override;
+
+  protected:
+    /// Identifiers for the various hardpoints.
+    enum PointId {
+        ARM,          ///< arm location
+        ARM_CHASSIS,  ///< arm, connection point to chassis
+        SHOCK_A,      ///< shock, arm location
+        SHOCK_C,      ///< shock, chassis location
+        NUM_POINTS
+    };
+
+    /// Return the location of the specified hardpoint.
+    /// The returned location must be expressed in the idler subsystem reference frame.
+    virtual const ChVector<> GetLocation(PointId which) = 0;
+
+    /// Return the mass of the arm body.
+    virtual double GetArmMass() const = 0;
+    /// Return the moments of inertia of the arm body.
+    virtual const ChVector<>& GetArmInertia() const = 0;
+    /// Return a visualization radius for the arm body.
+    virtual double GetArmVisRadius() const = 0;
+
+    /// Return the funtion for torsion force
+    virtual ChTorsionForce* GetTorsionForceFunction() const = 0;
+
+    /// Return the callback function for shock force.
+    virtual ChSpringForceCallback* GetShockForceCallback() const = 0;
+
+    ChSharedPtr<ChBody> m_arm;                   ///< handle to the trailing arm body
+    ChSharedPtr<ChLinkLockRevolute> m_revolute;  ///< handle to the revolute joint arm-chassis
+    ChSharedPtr<ChLinkSpringCB> m_shock;         ///< handle to the shock link
+
+  private:
+    void AddVisualizationArm(const ChVector<>& pt_A,   ///< arm location (in global frame)
+                             const ChVector<>& pt_AW,  ///< connection to wheel (in global frame)
+                             const ChVector<>& pt_AC   ///< connection to chassis (in global frame)
+                             );
+};
+
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
