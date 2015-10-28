fb2f074b3a5125865e0ad3eb8d89883b506042bb
Antonio Recuero <recuero@wisc.edu>
2015-10-27 22:22:04 -0500

    Apply Clang format to unit test

--- src/unit_tests/fea/test_ANCFShell_Iso.cpp
+++ src/unit_tests/fea/test_ANCFShell_Iso.cpp
@@ -17,11 +17,11 @@
 // Successful execution of this unit test may validate: this element's elastic, isotropic
 // force formulation and numerical integration implementations.
 //
-// The reference file data was validated by matching the steady-state response of the 
-// flat shell tip (i.e. only steady-state was validated) in the paper by Yamashita, Valkeapaa, 
-// Jayakumar, and Sugiyama, "Continuum Mechanics Based Bilinear Shear Deformable Shell 
+// The reference file data was validated by matching the steady-state response of the
+// flat shell tip (i.e. only steady-state was validated) in the paper by Yamashita, Valkeapaa,
+// Jayakumar, and Sugiyama, "Continuum Mechanics Based Bilinear Shear Deformable Shell
 // Element Using Absolute Nodal Coordinate Formulation", ASME Journal of Computational and
-// Nonlinear Dynamics, 10(5), 051012 (Sep 01, 2015). See its Figure 4.  
+// Nonlinear Dynamics, 10(5), 051012 (Sep 01, 2015). See its Figure 4.
 //
 // Gravity must be disabled; only a constant load of -50 N at a corner is used. Only
 // 10 time steps are checked by default. User can increase this value up to 4000.
@@ -42,26 +42,26 @@
 using namespace chrono;
 using namespace fea;
 
-const double precision = 1e-5; // Used to accept/reject implementation
+const double precision = 1e-5;  // Used to accept/reject implementation
 
 int main(int argc, char* argv[]) {
-	// Utils to open/read files: Load reference solution ("golden") file
-	ChMatrixDynamic<> FileInputMat(4000, 2);
-	std::string shell_validation_file = GetChronoDataPath() + "testing/" + "UT_ANCFShellIso.txt";
-	std::ifstream fileMid(shell_validation_file);
-	if (!fileMid.is_open()) {
-		fileMid.open(shell_validation_file);
-	}
-	if (!fileMid) {
-		std::cout << "Cannot open file.\n";
-		exit(1);
-	}
-	for (int x = 0; x < 4000; x++) {
-		fileMid >> FileInputMat[x][0] >> FileInputMat[x][1];
-	}
-	fileMid.close();
+    // Utils to open/read files: Load reference solution ("golden") file
+    ChMatrixDynamic<> FileInputMat(4000, 2);
+    std::string shell_validation_file = GetChronoDataPath() + "testing/" + "UT_ANCFShellIso.txt";
+    std::ifstream fileMid(shell_validation_file);
+    if (!fileMid.is_open()) {
+        fileMid.open(shell_validation_file);
+    }
+    if (!fileMid) {
+        std::cout << "Cannot open file.\n";
+        exit(1);
+    }
+    for (int x = 0; x < 4000; x++) {
+        fileMid >> FileInputMat[x][0] >> FileInputMat[x][1];
+    }
+    fileMid.close();
 
-	// Definition of the model
+    // Definition of the model
     ChSystem my_system;
 
     // The physical system: it contains all physical objects.
@@ -69,8 +69,8 @@ int main(int argc, char* argv[]) {
     // of elements and their referenced nodes.
     ChSharedPtr<ChMesh> my_mesh(new ChMesh);
     int numFlexBody = 1;
-	const int num_steps = 10; // Number of time steps for unit test (range 1 to 4000)
-	const double time_step = 0.001; // Time step
+    const int num_steps = 10;        // Number of time steps for unit test (range 1 to 4000)
+    const double time_step = 0.001;  // Time step
     // Geometry of the plate
     double plate_lenght_x = 1;
     double plate_lenght_y = 1;
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
     ChMatrixDynamic<double> COORDFlex(TotalNumNodes, 6);
     ChMatrixDynamic<double> VELCYFlex(TotalNumNodes, 6);
     ChMatrixDynamic<int> NumNodes(TotalNumElements, 4);
-    ChMatrixDynamic<int> LayNum(TotalNumElements, 1); // Only one layer in this unit test
+    ChMatrixDynamic<int> LayNum(TotalNumElements, 1);  // Only one layer in this unit test
     ChMatrixDynamic<int> NDR(TotalNumNodes, 6);
     ChMatrixDynamic<double> ElemLengthXY(TotalNumElements, 2);
     ChMatrixNM<double, 10, 12> MPROP;
@@ -110,8 +110,8 @@ int main(int argc, char* argv[]) {
         LayNum(i, 0) = 1;
         // Node number of the 4 nodes which creates element i.
         // The nodes are distributed this way. First in the x direction for constant
-        // y when max x is reached go to the next level for y by doing the same  
-		// distribution but for y+1 and keep doing until y limit is reached. Node
+        // y when max x is reached go to the next level for y by doing the same
+        // distribution but for y+1 and keep doing until y limit is reached. Node
         // number start from 1.
         NumNodes(i, 0) = (i / (numDiv_x)) * (N_x) + i % numDiv_x;
         NumNodes(i, 1) = (i / (numDiv_x)) * (N_x) + i % numDiv_x + 1;
@@ -183,18 +183,18 @@ int main(int argc, char* argv[]) {
     //!------------ Input Material Data----------------!
     //!------------------------------------------------!
     for (int i = 0; i < MaxMNUM; i++) {
-		double nu_coef = 0.3;
-        MTYPE = 2; // The user must use orthotropic input (MTYPE=2) to introduce isotropic material
-		// properties for this unit test
+        double nu_coef = 0.3;
+        MTYPE = 2;  // The user must use orthotropic input (MTYPE=2) to introduce isotropic material
+                    // properties for this unit test
 
         if (MTYPE == 2) {
-			MPROP(i, 0) = 500;      // Density [kg/m3]
-			MPROP(i, 1) = 2.1E+08;  // Ex//
-			MPROP(i, 2) = 2.1E+08;  // Ey
-			MPROP(i, 3) = 2.1E+08;  // Ez
-			// Additional information for the Type 2 of Material.
-			MPROP(i, 4) = 0.3;            // nuxy
-			MPROP(i, 5) = 0.3;            // nuxz
+            MPROP(i, 0) = 500;      // Density [kg/m3]
+            MPROP(i, 1) = 2.1E+08;  // Ex//
+            MPROP(i, 2) = 2.1E+08;  // Ey
+            MPROP(i, 3) = 2.1E+08;  // Ez
+            // Additional information for the Type 2 of Material.
+            MPROP(i, 4) = 0.3;                                    // nuxy
+            MPROP(i, 5) = 0.3;                                    // nuxz
             MPROP(i, 6) = 0.3;                                    // nuyz
             MPROP(i, 7) = MPROP(i, 1) / 2.0 / (1 + MPROP(i, 6));  // Gxy
             MPROP(i, 8) = MPROP(i, 1) / 2.0 / (1 + MPROP(i, 6));  // Gxz
@@ -204,7 +204,7 @@ int main(int argc, char* argv[]) {
     ChSharedPtr<ChContinuumElastic> mmaterial(new ChContinuumElastic);
     // Adding the nodes to the mesh
     int i = 0;
-	
+
     while (i < TotalNumNodes) {
         ChSharedPtr<ChNodeFEAxyzD> node(
             new ChNodeFEAxyzD(ChVector<>(COORDFlex(i, 0), COORDFlex(i, 1), COORDFlex(i, 2)),
@@ -218,11 +218,10 @@ int main(int argc, char* argv[]) {
     }
 
     ChSharedPtr<ChNodeFEAxyzD> nodetip(my_mesh->GetNode(TotalNumNodes - 1).DynamicCastTo<ChNodeFEAxyzD>());
-	// A random node just to try
-	ChSharedPtr<ChNodeFEAxyzD> noderand(my_mesh->GetNode(TotalNumNodes/2).DynamicCastTo<ChNodeFEAxyzD>());
-	// A constrained node
-	ChSharedPtr<ChNodeFEAxyzD> noderclamped(my_mesh->GetNode(0).DynamicCastTo<ChNodeFEAxyzD>());
-
+    // A random node just to try
+    ChSharedPtr<ChNodeFEAxyzD> noderand(my_mesh->GetNode(TotalNumNodes / 2).DynamicCastTo<ChNodeFEAxyzD>());
+    // A constrained node
+    ChSharedPtr<ChNodeFEAxyzD> noderclamped(my_mesh->GetNode(0).DynamicCastTo<ChNodeFEAxyzD>());
 
     int elemcount = 0;
     while (elemcount < TotalNumElements) {
@@ -281,33 +280,32 @@ int main(int argc, char* argv[]) {
         my_mesh->AddElement(element);
         elemcount++;
     }
-	// Switch off mesh class gravity
-	my_mesh->SetAutomaticGravity(false);
+    // Switch off mesh class gravity
+    my_mesh->SetAutomaticGravity(false);
     // This is mandatory
     my_mesh->SetupInitial();
     // Remember to add the mesh to the system!
     my_system.Add(my_mesh);
     // Perform a dynamic time integration:
     my_system.SetLcpSolverType(ChSystem::LCP_ITERATIVE_MINRES);  // <- NEEDED because other solvers can't
-                                                               // handle stiffness matrices
-	chrono::ChLcpIterativeMINRES* msolver = (chrono::ChLcpIterativeMINRES*)my_system.GetLcpSolverSpeed();
-	msolver->SetDiagonalPreconditioning(true);
-	my_system.SetIterLCPwarmStarting(true);  // this helps a lot to speedup convergence in this class of problems
-	my_system.SetIterLCPmaxItersSpeed(100);
-	my_system.SetIterLCPmaxItersStab(100);
-	my_system.SetTolForce(1e-6);
+                                                                 // handle stiffness matrices
+    chrono::ChLcpIterativeMINRES* msolver = (chrono::ChLcpIterativeMINRES*)my_system.GetLcpSolverSpeed();
+    msolver->SetDiagonalPreconditioning(true);
+    my_system.SetIterLCPwarmStarting(true);  // this helps a lot to speedup convergence in this class of problems
+    my_system.SetIterLCPmaxItersSpeed(100);
+    my_system.SetIterLCPmaxItersStab(100);
+    my_system.SetTolForce(1e-6);
 
-	/*ChLcpMklSolver * mkl_solver_stab = new ChLcpMklSolver; // MKL Solver option
-	ChLcpMklSolver * mkl_solver_speed = new ChLcpMklSolver;
-	my_system.ChangeLcpSolverStab(mkl_solver_stab);
-	my_system.ChangeLcpSolverSpeed(mkl_solver_speed);
-	mkl_solver_stab->SetProblemSizeLock(true);
-	mkl_solver_stab->SetSparsityPatternLock(false);
-	my_system.Update();*/
-	
+    /*ChLcpMklSolver * mkl_solver_stab = new ChLcpMklSolver; // MKL Solver option
+    ChLcpMklSolver * mkl_solver_speed = new ChLcpMklSolver;
+    my_system.ChangeLcpSolverStab(mkl_solver_stab);
+    my_system.ChangeLcpSolverSpeed(mkl_solver_speed);
+    mkl_solver_stab->SetProblemSizeLock(true);
+    mkl_solver_stab->SetSparsityPatternLock(false);
+    my_system.Update();*/
 
     // INT_HHT or INT_EULER_IMPLICIT
-	my_system.SetIntegrationType(ChSystem::INT_HHT);
+    my_system.SetIntegrationType(ChSystem::INT_HHT);
 
     ChSharedPtr<ChTimestepperHHT> mystepper = my_system.GetTimestepper().DynamicCastTo<ChTimestepperHHT>();
     mystepper->SetAlpha(0.0);
@@ -316,40 +314,37 @@ int main(int argc, char* argv[]) {
     mystepper->SetMode(ChTimestepperHHT::POSITION);
     mystepper->SetScaling(true);
 
-	utils::Data m_data; 
-	m_data.resize(2);
-	for (size_t col = 0; col < 2; col++)
-	m_data[col].resize(num_steps);
-	utils::CSV_writer csv(" ");
-	std::ifstream file2("UT_ANCFShellIso.txt");
+    utils::Data m_data;
+    m_data.resize(2);
+    for (size_t col = 0; col < 2; col++)
+        m_data[col].resize(num_steps);
+    utils::CSV_writer csv(" ");
+    std::ifstream file2("UT_ANCFShellIso.txt");
 
-	ChVector<> mforce(0, 0, 0);
-	mforce(2) = -50;
-	nodetip->SetForce(mforce);
+    ChVector<> mforce(0, 0, 0);
+    mforce(2) = -50;
+    nodetip->SetForce(mforce);
 
-	for (unsigned int it = 0; it < num_steps; it++) {
-		nodetip->SetForce(mforce);
-		my_system.DoStepDynamics(time_step);
-		std::cout << "Time t = " << my_system.GetChTime() << "s \n";
-		// Checking tip Z displacement
-		double AbsVal = abs(nodetip->pos.z - FileInputMat[it][1]);
-		if (AbsVal > precision) {
-			std::cout << "Unit test check failed \n";
-			return 1;
-		}
-	}
-		std::cout << "Unit test check succeeded \n";
-		// std::cout << "Clamped z = " << noderclamped->pos.z << "\n";
-		// std::cout << "nodetip->pos.z = " << nodetip->pos.z << "\n";
-		// std::cout << "mystepper->GetNumIterations()= " << mystepper->GetNumIterations() << "\n";
+    for (unsigned int it = 0; it < num_steps; it++) {
+        nodetip->SetForce(mforce);
+        my_system.DoStepDynamics(time_step);
+        std::cout << "Time t = " << my_system.GetChTime() << "s \n";
+        // Checking tip Z displacement
+        double AbsVal = abs(nodetip->pos.z - FileInputMat[it][1]);
+        if (AbsVal > precision) {
+            std::cout << "Unit test check failed \n";
+            return 1;
+        }
+    }
+    std::cout << "Unit test check succeeded \n";
+    // std::cout << "Clamped z = " << noderclamped->pos.z << "\n";
+    // std::cout << "nodetip->pos.z = " << nodetip->pos.z << "\n";
+    // std::cout << "mystepper->GetNumIterations()= " << mystepper->GetNumIterations() << "\n";
 
-		// Code snippet to generate golden file
-		/*m_data[0][it] = my_system.GetChTime();
-		m_data[1][it] = nodetip->pos.z;
-		csv << m_data[0][it] << m_data[1][it]  << std::endl;
-		csv.write_to_file("UT_ANCFShellIso.txt");*/ 
-	 return 0;}
-		
-
-   
-
+    // Code snippet to generate golden file
+    /*m_data[0][it] = my_system.GetChTime();
+    m_data[1][it] = nodetip->pos.z;
+    csv << m_data[0][it] << m_data[1][it]  << std::endl;
+    csv.write_to_file("UT_ANCFShellIso.txt");*/
+    return 0;
+}
