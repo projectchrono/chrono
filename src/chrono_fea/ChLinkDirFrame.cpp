a0085a84797f3d2d7cf1533ebc79d18e796f78df
Radu Serban <serban@engr.wisc.edu>
2015-11-21 17:13:42 +0100

    Fix bug in clamping constraints for ChLinkDirFrame

--- src/chrono_fea/ChLinkDirFrame.cpp
+++ src/chrono_fea/ChLinkDirFrame.cpp
@@ -188,8 +188,8 @@ void ChLinkDirFrame::IntLoadConstraint_C(
 	ChVector<> cres = res * c;
 
 	if (do_clamp) {
-		cres.x = ChMin(ChMax(cres.y, -recovery_clamp), recovery_clamp);
-		cres.y = ChMin(ChMax(cres.z, -recovery_clamp), recovery_clamp);
+		cres.y = ChMin(ChMax(cres.y, -recovery_clamp), recovery_clamp);
+		cres.z = ChMin(ChMax(cres.z, -recovery_clamp), recovery_clamp);
 	}
 	Qc(off_L+0) += cres.y;
 	Qc(off_L+1) += cres.z;
