3c298afe440f004ee14eeed78ee308ceedbc7907
Hammad Mazhar <hammad.maz@gmail.com>
2015-12-01 18:22:11 -0600

    Remove fluid node and add a fluid container

---
+++ src/chrono_parallel/physics/ChFluidContainer.cpp
@@ -0,0 +1,107 @@
+
+#include <stdlib.h>
+#include <algorithm>
+
+#include "chrono_parallel/physics/ChSystemParallel.h"
+#include "core/ChLinearAlgebra.h"
+#include <chrono_parallel/physics/ChFluidContainer.h>
+
+namespace chrono {
+
+using namespace collision;
+using namespace geometry;
+
+//////////////////////////////////////
+//////////////////////////////////////
+
+/// CLASS FOR A 3DOF FLUID NODE
+
+ChFluidContainer::ChFluidContainer(ChSystemParallel* physics_system) {
+    system = physics_system;
+    system->AddFluid(this);
+}
+
+ChFluidContainer::~ChFluidContainer() {}
+
+ChFluidContainer::ChFluidContainer(const ChFluidContainer& other) : ChPhysicsItem(other) {
+    this->system = other.system;
+}
+
+ChFluidContainer& ChFluidContainer::operator=(const ChFluidContainer& other) {
+    if (&other == this)
+        return *this;
+
+    ChPhysicsItem::operator=(other);
+    return *this;
+}
+
+void ChFluidContainer::AddFluid(const std::vector<real3>& positions, const std::vector<real3>& velocities) {
+    host_vector<real3>& pos_fluid = system->data_manager->host_data.pos_fluid;
+    host_vector<real3>& vel_fluid = system->data_manager->host_data.vel_fluid;
+
+    pos_fluid.insert(pos_fluid.end(), positions.begin(), positions.end());
+    vel_fluid.insert(vel_fluid.end(), velocities.begin(), velocities.end());
+    // In case the number of velocities provided were not enough, resize to the number of fluid bodies
+    vel_fluid.resize(pos_fluid.size());
+    system->data_manager->num_fluid_bodies = pos_fluid.size();
+}
+void ChFluidContainer::Update(double ChTime) {
+    uint num_fluid_bodies = system->data_manager->num_fluid_bodies;
+    uint num_rigid_bodies = system->data_manager->num_rigid_bodies;
+    uint num_shafts = system->data_manager->num_shafts;
+    host_vector<real3>& pos_fluid = system->data_manager->host_data.pos_fluid;
+    host_vector<real3>& vel_fluid = system->data_manager->host_data.vel_fluid;
+    ChVector<> g_acc = system->Get_G_acc();
+    real3 h_gravity = system->GetStep() * system->data_manager->settings.fluid.mass * R3(g_acc.x, g_acc.y, g_acc.z);
+    for (int i = 0; i < num_fluid_bodies; i++) {
+        real3 vel = vel_fluid[i];
+        system->data_manager->host_data.v[num_rigid_bodies * 6 + num_shafts + i * 3 + 0] = vel.x;
+        system->data_manager->host_data.v[num_rigid_bodies * 6 + num_shafts + i * 3 + 1] = vel.y;
+        system->data_manager->host_data.v[num_rigid_bodies * 6 + num_shafts + i * 3 + 2] = vel.z;
+
+        system->data_manager->host_data.hf[num_rigid_bodies * 6 + num_shafts + i * 3 + 0] = h_gravity.x;
+        system->data_manager->host_data.hf[num_rigid_bodies * 6 + num_shafts + i * 3 + 1] = h_gravity.y;
+        system->data_manager->host_data.hf[num_rigid_bodies * 6 + num_shafts + i * 3 + 2] = h_gravity.z;
+    }
+}
+
+void ChFluidContainer::UpdatePosition(double ChTime) {
+    uint num_fluid_bodies = system->data_manager->num_fluid_bodies;
+    uint num_rigid_bodies = system->data_manager->num_rigid_bodies;
+    uint num_shafts = system->data_manager->num_shafts;
+
+    host_vector<real3>& pos_fluid = system->data_manager->host_data.pos_fluid;
+    host_vector<real3>& vel_fluid = system->data_manager->host_data.vel_fluid;
+
+    for (int i = 0; i < num_fluid_bodies; i++) {
+        real3 vel;
+        vel.x = system->data_manager->host_data.v[num_rigid_bodies * 6 + num_shafts + i * 3 + 0];
+        vel.y = system->data_manager->host_data.v[num_rigid_bodies * 6 + num_shafts + i * 3 + 1];
+        vel.z = system->data_manager->host_data.v[num_rigid_bodies * 6 + num_shafts + i * 3 + 2];
+
+        real speed = vel.length();
+        if (speed > system->data_manager->settings.fluid.max_velocity) {
+            vel = vel * system->data_manager->settings.fluid.max_velocity / speed;
+        }
+        vel_fluid[i] = vel;
+        pos_fluid[i] += vel * system->GetStep();
+    }
+}
+
+real3 ChFluidContainer::GetPos(int i) {
+    return system->data_manager->host_data.pos_fluid[i];
+}
+void ChFluidContainer::SetPos(const int& i, const real3& mpos) {
+    system->data_manager->host_data.pos_fluid[i] = mpos;
+}
+
+real3 ChFluidContainer::GetPos_dt(int i) {
+    return system->data_manager->host_data.vel_fluid[i];
+}
+void ChFluidContainer::SetPos_dt(const int& i, const real3& mposdt) {
+    system->data_manager->host_data.vel_fluid[i] = mposdt;
+}
+
+}  // END_OF_NAMESPACE____
+
+/////////////////////
---
+++ src/chrono_parallel/physics/ChFluidContainer.h
@@ -0,0 +1,56 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Hammad Mazhar
+// =============================================================================
+//
+// Description: This file defines a fluid node, it is based on a 3DOF NodeXYZ
+// element. This class is similar to ChMatterSPH but is meant to be a bit more
+// general.
+// =============================================================================
+
+#pragma once
+
+#include <math.h>
+
+#include "chrono_parallel/ChParallelDefines.h"
+#include "chrono_parallel/math/real.h"
+#include "chrono_parallel/ChDataManager.h"
+namespace chrono {
+
+// Forward references (for parent hierarchy pointer)
+class ChSystemParallel;
+
+class CH_PARALLEL_API ChFluidContainer : public ChPhysicsItem {
+  public:
+    ChFluidContainer(ChSystemParallel* system);
+    ~ChFluidContainer();
+
+    ChFluidContainer(const ChFluidContainer& other);             // Copy constructor
+    ChFluidContainer& operator=(const ChFluidContainer& other);  // Assignment operator
+
+    void AddFluid(const std::vector<real3>& positions, const std::vector<real3>& velocities);
+    void Update(double ChTime);
+    void UpdatePosition(double ChTime);
+    // Position of the node - in absolute csys.
+    real3 GetPos(int i);
+    // Position of the node - in absolute csys.
+    void SetPos(const int& i, const real3& mpos);
+
+    // Velocity of the node - in absolute csys.
+    real3 GetPos_dt(int i);
+    // Velocity of the node - in absolute csys.
+    void SetPos_dt(const int& i, const real3& mposdt);
+
+  private:
+    ChSystemParallel* system;
+};
+}
