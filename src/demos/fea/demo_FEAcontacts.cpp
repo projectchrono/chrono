ed814398fdfa3a6fedfc084c4b052fa332d769ef
Alessandro Tasora <tasora@ied.unipr.it>
2015-12-12 21:56:59 +0100

    Improved demo for mesh-mesh collision with FEA (using DEM-p), where the tetrahedron(s) use the new mesh collision system (previously only the collision cloud system was used, so it was not possible to do collisions between meshes) NOTE: WORK IN PROGRESS - there are some weird behaviours in mesh-mesh contacts, they disappear sometimes, and the mesh-bullet contacts have a 'sticky' persistence that should not be here....

--- src/demos/fea/demo_FEAcontacts.cpp
+++ src/demos/fea/demo_FEAcontacts.cpp
@@ -20,6 +20,7 @@
 #include "chrono/physics/ChBodyEasy.h"
 #include "chrono/physics/ChLoadContainer.h"
 #include "chrono/lcp/ChLcpIterativeMINRES.h"
+#include "chrono/geometry/ChCTriangleMeshConnected.h"
 
 #include "chrono_fea/ChElementTetra_4.h"
 #include "chrono_fea/ChMesh.h"
@@ -31,6 +32,7 @@
 
 
 using namespace chrono;
+using namespace geometry;
 using namespace fea;
 using namespace irr;
 
@@ -54,6 +56,11 @@ int main(int argc, char* argv[]) {
     // CREATE THE PHYSICAL SYSTEM
     //
 
+
+    //collision::ChCollisionModel::SetDefaultSuggestedEnvelope(0.0); // not needed, already 0 when using ChSystemDEM
+    collision::ChCollisionModel::SetDefaultSuggestedMargin(0.005); // max inside penetration - if not enough stiffness in material: troubles
+
+
     // Create the surface material, containing information
     // about friction etc.
 
@@ -63,32 +70,74 @@ int main(int argc, char* argv[]) {
     mysurfmaterial->SetGn(2200);
     mysurfmaterial->SetGt(2200);
 
-    // RIGID BODIES
-    // Create some rigid bodies, for instance a floor and two bouncing items:
 
-    ChSharedPtr<ChBodyEasyBox> mfloor (new ChBodyEasyBox(3,0.1,3,2700, true));
-    mfloor->SetBodyFixed(true);
-    mfloor->SetMaterialSurface(mysurfmaterial);
-    my_system.Add(mfloor);
+    // Create a floor:
+
+    bool do_mesh_collision_floor = true;
+
+    ChTriangleMeshConnected mmeshbox;
+    mmeshbox.LoadWavefrontMesh(GetChronoDataFile("cube.obj"),true,true);
+
+    if(do_mesh_collision_floor) {
+
+        // floor as a triangle mesh surface:
+        ChSharedPtr<ChBody> mfloor(new ChBody);
+        mfloor->SetPos(ChVector<>(0, -1, 0));
+        mfloor->SetBodyFixed(true);
+        mfloor->SetMaterialSurface(mysurfmaterial);
+        my_system.Add(mfloor);
+
+        mfloor->GetCollisionModel()->ClearModel();
+        mfloor->GetCollisionModel()->AddTriangleMesh(mmeshbox,false, false, VNULL, ChMatrix33<>(1), 0.004);
+        mfloor->GetCollisionModel()->BuildModel();
+        mfloor->SetCollide(true);
+
+        ChSharedPtr<ChTriangleMeshShape> masset_meshbox(new ChTriangleMeshShape());
+        masset_meshbox->SetMesh(mmeshbox);
+        mfloor->AddAsset(masset_meshbox);
+
+        ChSharedPtr<ChTexture> masset_texture(new ChTexture());
+        masset_texture->SetTextureFilename(GetChronoDataFile("concrete.jpg"));
+        mfloor->AddAsset(masset_texture);
+        
+    }
+    else {
+        // floor as a simple collision primitive:
+
+        ChSharedPtr<ChBodyEasyBox> mfloor (new ChBodyEasyBox(2,0.1,2,2700, true));
+        mfloor->SetBodyFixed(true);
+        mfloor->SetMaterialSurface(mysurfmaterial);
+        my_system.Add(mfloor);
+
+        ChSharedPtr<ChTexture> masset_texture(new ChTexture());
+        masset_texture->SetTextureFilename(GetChronoDataFile("concrete.jpg"));
+        mfloor->AddAsset(masset_texture);
+    }
+
+    
+
+    // two falling objects:
 
     ChSharedPtr<ChBodyEasyBox> mcube (new ChBodyEasyBox(0.1,0.1,0.1,2700, true));
-    mcube->SetPos(ChVector<>(1,0.5,1));
+    mcube->SetPos(ChVector<>(0.6,0.5,0.6));
     mcube->SetMaterialSurface(mysurfmaterial);
     my_system.Add(mcube);
 
     ChSharedPtr<ChBodyEasySphere> msphere (new ChBodyEasySphere(0.1,2700, true));
-    msphere->SetPos(ChVector<>(1.2,0.5,1));
+    msphere->SetPos(ChVector<>(0.8,0.5,0.6));
     msphere->SetMaterialSurface(mysurfmaterial);
     my_system.Add(msphere);
 
-   
-    // FINITE ELEMENT MESH
-    // Create a mesh, that is a container for groups
-    // of FEA elements and their referenced nodes.
+
+    //
+    // Example 1: tetrahedrons, with collisions
+    // 
+
+    // Create a mesh. We will use it for tetahedrons.
 
     ChSharedPtr<ChMesh> my_mesh(new ChMesh);
 
-    // 1) a FEA tetahedron:
+    // 1) a FEA tetahedron(s):
 
     // Create a material, that must be assigned to each solid element in the mesh,
     // and set its parameters
@@ -98,25 +147,55 @@ int main(int argc, char* argv[]) {
     mmaterial->Set_RayleighDampingK(0.003);
     mmaterial->Set_density(1000);
 
-    // Creates the nodes for the tetahedron
-    ChSharedPtr<ChNodeFEAxyz> mnode1(new ChNodeFEAxyz(ChVector<>(0, 0.2, 0)));
-    ChSharedPtr<ChNodeFEAxyz> mnode2(new ChNodeFEAxyz(ChVector<>(0, 0.2, 0.2)));
-    ChSharedPtr<ChNodeFEAxyz> mnode3(new ChNodeFEAxyz(ChVector<>(0, 0.4, 0)));
-    ChSharedPtr<ChNodeFEAxyz> mnode4(new ChNodeFEAxyz(ChVector<>(0.2, 0.2, 0)));
+    for (int i=0; i<1; ++i) {
+        // Creates the nodes for the tetahedron
+        ChVector<> offset(i*0.12,i*0.1, i*0.03);
+        ChSharedPtr<ChNodeFEAxyz> mnode1(new ChNodeFEAxyz(ChVector<>(0,   0.1, 0  )+offset));
+        ChSharedPtr<ChNodeFEAxyz> mnode2(new ChNodeFEAxyz(ChVector<>(0,   0.1, 0.2)+offset));
+        ChSharedPtr<ChNodeFEAxyz> mnode3(new ChNodeFEAxyz(ChVector<>(0,   0.3, 0  )+offset));
+        ChSharedPtr<ChNodeFEAxyz> mnode4(new ChNodeFEAxyz(ChVector<>(0.2, 0.1, 0  )+offset));
 
-    my_mesh->AddNode(mnode1);
-    my_mesh->AddNode(mnode2);
-    my_mesh->AddNode(mnode3);
-    my_mesh->AddNode(mnode4);
+        my_mesh->AddNode(mnode1);
+        my_mesh->AddNode(mnode2);
+        my_mesh->AddNode(mnode3);
+        my_mesh->AddNode(mnode4);
 
-    ChSharedPtr<ChElementTetra_4> melement1(new ChElementTetra_4);
-    melement1->SetNodes(mnode1,
-                        mnode2, 
-                        mnode3, 
-                        mnode4);
-    melement1->SetMaterial(mmaterial);
+        ChSharedPtr<ChElementTetra_4> melement1(new ChElementTetra_4);
+        melement1->SetNodes(mnode1,
+                            mnode2, 
+                            mnode3, 
+                            mnode4);
+        melement1->SetMaterial(mmaterial);
 
-    my_mesh->AddElement(melement1);
+        my_mesh->AddElement(melement1);
+    }
+
+    // Create the contact surface(s). 
+    // In this case it is a ChContactSurfaceGeneric, that allows mesh-mesh collsions.
+
+    ChSharedPtr<ChContactSurfaceGeneric> mcontactsurf (new ChContactSurfaceGeneric);
+    my_mesh->AddContactSurface(mcontactsurf);
+
+    mcontactsurf->AddFacesFromBoundary(0.004); // do this after my_mesh->AddContactSurface
+
+    mcontactsurf->SetMaterialSurface(mysurfmaterial); // use the DEM penalty contacts
+
+
+    // This is necessary in order to precompute the
+    // stiffness matrices for all inserted elements in mesh
+    my_mesh->SetupInitial();
+
+    // Remember to add the mesh to the system!
+    my_system.Add(my_mesh);
+
+
+    //
+    // Example 2: beams, with collisions
+    // 
+
+    // Create a mesh. We will use it for beams only. 
+
+    ChSharedPtr<ChMesh> my_mesh_beams(new ChMesh);
 
     // 2) an ANCF cable:
 
@@ -127,35 +206,33 @@ int main(int argc, char* argv[]) {
 
 	ChBuilderBeamANCF builder;
 
-	builder.BuildBeam(	my_mesh,		// the mesh where to put the created nodes and elements 
+	builder.BuildBeam(	my_mesh_beams,	// the mesh where to put the created nodes and elements 
 						msection_cable2,// the ChBeamSectionCable to use for the ChElementBeamANCF elements
 						10,				// the number of ChElementBeamANCF to create
 						ChVector<>(0, 0.1, -0.1),		// the 'A' point in space (beginning of beam)
 						ChVector<>(0.5, 0.13, -0.1));	// the 'B' point in space (end of beam)
 
-    // 3) the contact surface
-
     // Create the contact surface(s). 
     // In this case it is a ChContactSurfaceNodeCloud, so just pass 
     // all nodes to it.
 
-    ChSharedPtr<ChContactSurfaceNodeCloud> mcontactsurf (new ChContactSurfaceNodeCloud);
+    ChSharedPtr<ChContactSurfaceNodeCloud> mcontactcloud (new ChContactSurfaceNodeCloud);
+    my_mesh_beams->AddContactSurface(mcontactcloud);
 
-    for (unsigned int i = 0; i< my_mesh->GetNnodes(); ++i)
-        mcontactsurf->AddNode( my_mesh->GetNode(i).DynamicCastTo<ChNodeFEAxyz>() );
+    mcontactcloud->AddAllNodes(0.025); // use larger point size to match beam section radius
 
-    mcontactsurf->SetMaterialSurface(mysurfmaterial);
+    mcontactcloud->SetMaterialSurface(mysurfmaterial);
 
-    my_mesh->AddContactSurface(mcontactsurf);
-
-
-
+    
     // This is necessary in order to precompute the
     // stiffness matrices for all inserted elements in mesh
-    my_mesh->SetupInitial();
+    my_mesh_beams->SetupInitial();
 
     // Remember to add the mesh to the system!
-    my_system.Add(my_mesh);
+    my_system.Add(my_mesh_beams);
+
+
+    
 
 
 
@@ -177,11 +254,23 @@ int main(int argc, char* argv[]) {
     mvisualizemesh->SetSmoothFaces(true);
     my_mesh->AddAsset(mvisualizemesh); 
 
-    ChSharedPtr<ChVisualizationFEAmesh> mvisualizemeshC(new ChVisualizationFEAmesh(*(my_mesh.get_ptr())));
-    mvisualizemeshC->SetFEMglyphType(ChVisualizationFEAmesh::E_GLYPH_NODE_DOT_POS);
-    mvisualizemeshC->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_NONE);
-    mvisualizemeshC->SetSymbolsThickness(0.006);
-    my_mesh->AddAsset(mvisualizemeshC);
+    ChSharedPtr<ChVisualizationFEAmesh> mvisualizemeshcoll(new ChVisualizationFEAmesh(*(my_mesh.get_ptr())));
+    mvisualizemeshcoll->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_CONTACTSURFACES);
+    mvisualizemeshcoll->SetWireframe(true);
+    mvisualizemeshcoll->SetDefaultMeshColor(ChColor(1,0.5,0));
+    my_mesh->AddAsset(mvisualizemeshcoll); 
+
+    ChSharedPtr<ChVisualizationFEAmesh> mvisualizemeshbeam(new ChVisualizationFEAmesh(*(my_mesh_beams.get_ptr())));
+    mvisualizemeshbeam->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_NODE_SPEED_NORM);
+    mvisualizemeshbeam->SetColorscaleMinMax(0.0, 5.50);
+    mvisualizemeshbeam->SetSmoothFaces(true);
+    my_mesh->AddAsset(mvisualizemeshbeam);
+
+    ChSharedPtr<ChVisualizationFEAmesh> mvisualizemeshbeamnodes(new ChVisualizationFEAmesh(*(my_mesh_beams.get_ptr())));
+    mvisualizemeshbeamnodes->SetFEMglyphType(ChVisualizationFEAmesh::E_GLYPH_NODE_DOT_POS);
+    mvisualizemeshbeamnodes->SetFEMdataType(ChVisualizationFEAmesh::E_PLOT_NONE);
+    mvisualizemeshbeamnodes->SetSymbolsThickness(0.008);
+    my_mesh->AddAsset(mvisualizemeshbeamnodes);
  
  
 
