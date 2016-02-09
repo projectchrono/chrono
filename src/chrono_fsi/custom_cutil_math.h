a9c3beee96b2ab83b8c66a815c3221711a5f64f4
armanpazouki <apazouki@gmail.com>
2016-02-09 10:41:17 -0600

    modified custom_cutil so that it does not rely on cuda anymore

--- src/chrono_fsi/custom_cutil_math.h
+++ src/chrono_fsi/custom_cutil_math.h
@@ -14,10 +14,11 @@
 #ifndef CUSTOM_CUTIL_MATH_H
 #define CUSTOM_CUTIL_MATH_H
 ////////Define Real, either float or double
-#include <cuda_runtime.h>
+// #include <cuda_runtime.h>
 
 namespace chrono{
 namespace fsi {
+
 typedef unsigned int uint;
 typedef unsigned short ushort;
 
@@ -32,32 +33,140 @@ typedef float Real;
 #define INCLUDECUTILMATHSTUFF 1
 #if INCLUDECUTILMATHSTUFF
 #ifndef __CUDACC__
-#include <math.h>
+#include <cmath>
+#endif
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // host implementations of CUDA functions
 ////////////////////////////////////////////////////////////////////////////////
 
-inline float fminf(float a, float b) {
+inline __host__  __device__ float fminf(float a, float b) {
 	return a < b ? a : b;
 }
 
-inline float fmaxf(float a, float b) {
+inline __host__  __device__ float fmaxf(float a, float b) {
 	return a > b ? a : b;
 }
 
-inline int max(int a, int b) {
+inline __host__  __device__ int max(int a, int b) {
 	return a > b ? a : b;
 }
 
-inline int min(int a, int b) {
+inline __host__  __device__ int min(int a, int b) {
 	return a < b ? a : b;
 }
 
-inline float rsqrtf(float x) {
+inline __host__  __device__ float rsqrtf(float x) {
 	return 1.0f / sqrtf(x);
 }
-#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// definition of basic cuda types
+////////////////////////////////////////////////////////////////////////////////
+// __cuda_builtin_vector_align8(int2, int x; int y;);
+// __cuda_builtin_vector_align8(uint2, unsigned int x; unsigned int y;);
+
+// struct __device_builtin__ int3
+// {
+//     int x, y, z;
+// };
+
+// struct __device_builtin__ uint3
+// {
+//     unsigned int x, y, z;
+// };
+
+// struct __device_builtin__ __builtin_align__(16) int4
+// {
+//     int x, y, z, w;
+// };
+
+// struct __device_builtin__ __builtin_align__(16) uint4
+// {
+//     unsigned int x, y, z, w;
+// };
+
+
+// struct __device_builtin__ float3
+// {
+//     float x, y, z;
+// };
+
+// struct __device_builtin__ __builtin_align__(16) float4
+// {
+//     float x, y, z, w;
+// };
+
+////////////////////////////////////////////////////////////////////////////////
+// implementations of basic cuda types
+////////////////////////////////////////////////////////////////////////////////
+
+#if defined(__CUDACC_RTC__)
+#define __VECTOR_FUNCTIONS_DECL__ __host__ __device__
+#else /* !__CUDACC_RTC__ */
+#define __VECTOR_FUNCTIONS_DECL__ static __inline__ __host__ __device__
+#endif /* __CUDACC_RTC__ */
+
+__VECTOR_FUNCTIONS_DECL__ uint2 make_uint2(unsigned int x, unsigned int y)
+{
+  uint2 t; t.x = x; t.y = y; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ uint3 make_uint3(unsigned int x, unsigned int y, unsigned int z)
+{
+  uint3 t; t.x = x; t.y = y; t.z = z; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ uint4 make_uint4(unsigned int x, unsigned int y, unsigned int z, unsigned int w)
+{
+  uint4 t; t.x = x; t.y = y; t.z = z; t.w = w; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ int2 make_int2(int x, int y)
+{
+  int2 t; t.x = x; t.y = y; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ int3 make_int3(int x, int y, int z)
+{
+  int3 t; t.x = x; t.y = y; t.z = z; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ int4 make_int4(int x, int y, int z, int w)
+{
+  int4 t; t.x = x; t.y = y; t.z = z; t.w = w; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ float2 make_float2(float x, float y)
+{
+  float2 t; t.x = x; t.y = y; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ float3 make_float3(float x, float y, float z)
+{
+  float3 t; t.x = x; t.y = y; t.z = z; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ float4 make_float4(float x, float y, float z, float w)
+{
+  float4 t; t.x = x; t.y = y; t.z = z; t.w = w; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ double2 make_double2(double x, double y)
+{
+  double2 t; t.x = x; t.y = y; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ double3 make_double3(double x, double y, double z)
+{
+  double3 t; t.x = x; t.y = y; t.z = z; return t;
+}
+
+__VECTOR_FUNCTIONS_DECL__ double4 make_double4(double x, double y, double z, double w)
+{
+  double4 t; t.x = x; t.y = y; t.z = z; t.w = w; return t;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // constructors
@@ -1084,73 +1193,27 @@ inline __host__  __device__ float4 normalize(float4 v) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// floor
-////////////////////////////////////////////////////////////////////////////////
-
-inline __host__  __device__ float2 floorf(float2 v) {
-	return make_float2(floorf(v.x), floorf(v.y));
-}
-inline __host__  __device__ float3 floorf(float3 v) {
-	return make_float3(floorf(v.x), floorf(v.y), floorf(v.z));
-}
-inline __host__  __device__ float4 floorf(float4 v) {
-	return make_float4(floorf(v.x), floorf(v.y), floorf(v.z), floorf(v.w));
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// frac - returns the fractional portion of a scalar or each vector component
-////////////////////////////////////////////////////////////////////////////////
-
-inline __host__ __device__ float fracf(float v) {
-	return v - floorf(v);
-}
-inline __host__  __device__ float2 fracf(float2 v) {
-	return make_float2(fracf(v.x), fracf(v.y));
-}
-inline __host__  __device__ float3 fracf(float3 v) {
-	return make_float3(fracf(v.x), fracf(v.y), fracf(v.z));
-}
-inline __host__  __device__ float4 fracf(float4 v) {
-	return make_float4(fracf(v.x), fracf(v.y), fracf(v.z), fracf(v.w));
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// fmod
-////////////////////////////////////////////////////////////////////////////////
-
-inline __host__  __device__ float2 fmodf(float2 a, float2 b) {
-	return make_float2(fmodf(a.x, b.x), fmodf(a.y, b.y));
-}
-inline __host__  __device__ float3 fmodf(float3 a, float3 b) {
-	return make_float3(fmodf(a.x, b.x), fmodf(a.y, b.y), fmodf(a.z, b.z));
-}
-inline __host__  __device__ float4 fmodf(float4 a, float4 b) {
-	return make_float4(fmodf(a.x, b.x), fmodf(a.y, b.y), fmodf(a.z, b.z),
-			fmodf(a.w, b.w));
-}
-
-////////////////////////////////////////////////////////////////////////////////
 // absolute value
 ////////////////////////////////////////////////////////////////////////////////
 
 inline __host__  __device__ float2 fabs(float2 v) {
-	return make_float2(fabs(v.x), fabs(v.y));
+	return make_float2(std::fabs(v.x), std::fabs(v.y));
 }
 inline __host__  __device__ float3 fabs(float3 v) {
-	return make_float3(fabs(v.x), fabs(v.y), fabs(v.z));
+	return make_float3(std::fabs(v.x), std::fabs(v.y), std::fabs(v.z));
 }
 inline __host__  __device__ float4 fabs(float4 v) {
-	return make_float4(fabs(v.x), fabs(v.y), fabs(v.z), fabs(v.w));
+	return make_float4(std::fabs(v.x), std::fabs(v.y), std::fabs(v.z), std::fabs(v.w));
 }
 
 inline __host__  __device__ int2 abs(int2 v) {
-	return make_int2(abs(v.x), abs(v.y));
+	return make_int2(std::abs(v.x), std::abs(v.y));
 }
 inline __host__  __device__ int3 abs(int3 v) {
-	return make_int3(abs(v.x), abs(v.y), abs(v.z));
+	return make_int3(std::abs(v.x), std::abs(v.y), std::abs(v.z));
 }
 inline __host__  __device__ int4 abs(int4 v) {
-	return make_int4(abs(v.x), abs(v.y), abs(v.z), abs(v.w));
+	return make_int4(std::abs(v.x), std::abs(v.y), std::abs(v.z), std::abs(v.w));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1671,51 +1734,15 @@ __host__ __device__ inline Real4 normalize(Real4 v) {
 	return v * invLen;
 }
 
-__host__ __device__ inline Real2 floor(Real2 v) {
-	return make_Real2(floor(v.x), floor(v.y));
-}
-__host__ __device__ inline Real3 floor(Real3 v) {
-	return make_Real3(floor(v.x), floor(v.y), floor(v.z));
-}
-__host__ __device__ inline Real4 floor(Real4 v) {
-	return make_Real4(floor(v.x), floor(v.y), floor(v.z), floor(v.w));
-}
-
-__host__ __device__ inline Real fracr(Real v) {
-	return v - floor(v);
-}
-__host__ __device__ inline Real2 fracr(Real2 v) {
-	return make_Real2(fracr(v.x), fracr(v.y));
-}
-__host__ __device__ inline Real3 fracr(Real3 v) {
-	return make_Real3(fracr(v.x), fracr(v.y), fracr(v.z));
-}
-__host__ __device__ inline Real4 fracr(Real4 v) {
-	return make_Real4(fracr(v.x), fracr(v.y), fracr(v.z), fracr(v.w));
-}
-
-__host__ __device__ inline Real rmodr(Real a, Real b) {
-	return fmod(a, b);
-}
-__host__ __device__ inline Real2 rmodr(Real2 a, Real2 b) {
-	return make_Real2(rmodr(a.x, b.x), rmodr(a.y, b.y));
-}
-__host__ __device__ inline Real3 rmodr(Real3 a, Real3 b) {
-	return make_Real3(rmodr(a.x, b.x), rmodr(a.y, b.y), rmodr(a.z, b.z));
-}
-__host__ __device__ inline Real4 rmodr(Real4 a, Real4 b) {
-	return make_Real4(rmodr(a.x, b.x), rmodr(a.y, b.y), rmodr(a.z, b.z),
-			rmodr(a.w, b.w));
-}
 
 __host__ __device__ inline Real2 fabs(Real2 v) {
-	return make_Real2(fabs(v.x), fabs(v.y));
+	return make_Real2(std::fabs(v.x), std::fabs(v.y));
 }
 __host__ __device__ inline Real3 fabs(Real3 v) {
-	return make_Real3(fabs(v.x), fabs(v.y), fabs(v.z));
+	return make_Real3(std::fabs(v.x), std::fabs(v.y), std::fabs(v.z));
 }
 __host__ __device__ inline Real4 fabs(Real4 v) {
-	return make_Real4(fabs(v.x), fabs(v.y), fabs(v.z), fabs(v.w));
+	return make_Real4(std::fabs(v.x), std::fabs(v.y), std::fabs(v.z), std::fabs(v.w));
 }
 
 __host__ __device__ inline Real3 reflect(Real3 i, Real3 n) {
