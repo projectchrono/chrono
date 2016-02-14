59174e5277ce279bb59dbbfec93a4c02981cabd2
Radu Serban <serban@wisc.edu>
2016-02-14 12:59:10 +0100

    Tracked vehicle: Convert test_profile to use std::shared_ptr

--- src/tests/vehicle/sprocket/test_profile.cpp
+++ src/tests/vehicle/sprocket/test_profile.cpp
@@ -21,9 +21,8 @@ using namespace irr::io;
 using namespace irr::gui;
 
 // -----------------------------------------------------------------------------
-ChSharedPtr<geometry::ChLinePath> CreateProfile(int num_teeth, double R_T, double R_C, double R)
-{
-    ChSharedPtr<geometry::ChLinePath> profile(new geometry::ChLinePath);
+std::shared_ptr<geometry::ChLinePath> CreateProfile(int num_teeth, double R_T, double R_C, double R) {
+    auto profile = std::make_shared<geometry::ChLinePath>();
 
     double beta = CH_C_2PI / num_teeth;
     double sbeta = std::sin(beta / 2);
@@ -78,7 +77,7 @@ int main(int argc, char* argv[]) {
     // Create the ground body
     // ----------------------
 
-    ChSharedPtr<ChBody> ground(new ChBody());
+    auto ground = std::make_shared<ChBody>();
     ground->SetIdentifier(-1);
     ground->SetBodyFixed(true);
     application.GetSystem()->AddBody(ground);
@@ -89,7 +88,7 @@ int main(int argc, char* argv[]) {
 
     ChVector<> gear_loc(0, 0, 0);
 
-    ChSharedPtr<ChBody> gear(new ChBody());
+    auto gear = std::make_shared<ChBody>();
     gear->SetIdentifier(0);
     gear->SetPos(gear_loc);
     //gear->SetWvel_loc(ChVector<>(0, 0, 0.1));
@@ -101,37 +100,37 @@ int main(int argc, char* argv[]) {
     double R = 0.089;
     double separation = 0.25;
 
-    ChSharedPtr<ChLinePath> gear_profile = CreateProfile(n_teeth, R_T, R_C, R);
+    std::shared_ptr<ChLinePath> gear_profile = CreateProfile(n_teeth, R_T, R_C, R);
 
     // Add the collision shape to gear
     gear->GetCollisionModel()->SetSafeMargin(0.02);
     gear->SetCollide(true);
     gear->GetCollisionModel()->ClearModel();
-    gear->GetCollisionModel()->Add2Dpath(*gear_profile.get_ptr(), ChVector<>(0, 0, separation / 2));
-    gear->GetCollisionModel()->Add2Dpath(*gear_profile.get_ptr(), ChVector<>(0, 0, -separation / 2));
+    gear->GetCollisionModel()->Add2Dpath(*gear_profile.get(), ChVector<>(0, 0, separation / 2));
+    gear->GetCollisionModel()->Add2Dpath(*gear_profile.get(), ChVector<>(0, 0, -separation / 2));
     gear->GetCollisionModel()->BuildModel();
 
     // Add ChLineShape visualization asset to gear
-    ChSharedPtr<ChLineShape> gear_profile_plus(new ChLineShape);
+    auto gear_profile_plus = std::make_shared<ChLineShape>();
     gear_profile_plus->SetLineGeometry(gear_profile);
     gear_profile_plus->Pos = ChVector<>(0, 0, separation / 2);
     gear_profile_plus->SetColor(ChColor(1, 0, 0));
     gear->AddAsset(gear_profile_plus);
 
-    ChSharedPtr<ChLineShape> gear_profile_minus(new ChLineShape);
+    auto gear_profile_minus = std::make_shared<ChLineShape>();
     gear_profile_minus->SetLineGeometry(gear_profile);
     gear_profile_minus->SetColor(ChColor(0, 1, 0));
     gear_profile_minus->Pos = ChVector<>(0, 0, -separation / 2);
     gear->AddAsset(gear_profile_minus);
 
-    ChSharedPtr<ChCylinderShape> gear_axle(new ChCylinderShape);
+    auto gear_axle = std::make_shared<ChCylinderShape>();
     gear_axle->GetCylinderGeometry().p1 = ChVector<>(0, 0, separation / 2);
     gear_axle->GetCylinderGeometry().p2 = ChVector<>(0, 0, -separation / 2);
     gear_axle->GetCylinderGeometry().rad = 0.1 * R;
     gear->AddAsset(gear_axle);
 
     // Revolute constraint (gear-ground)
-    ChSharedPtr<ChLinkLockRevolute> revolute(new ChLinkLockRevolute);
+    auto revolute = std::make_shared<ChLinkLockRevolute>();
     revolute->Initialize(gear, ground, ChCoordsys<>(gear_loc));
     application.GetSystem()->Add(revolute);
 
@@ -143,13 +142,13 @@ int main(int argc, char* argv[]) {
     double pin_radius = 0.3 * R;
     ChVector<> pin_loc = gear_loc + ChVector<>(0, R_T, 0);
 
-    ChSharedPtr<ChBody> pin(new ChBody());
+    auto pin = std::make_shared<ChBody>();
     pin->SetIdentifier(1);
     pin->SetPos(pin_loc);
     //pin->SetBodyFixed(true);
     application.GetSystem()->AddBody(pin);
 
-    ChSharedPtr<ChLinePath> pin_profile(new ChLinePath);
+    auto pin_profile = std::make_shared<ChLinePath>();
     ChLineArc pin_circle(ChCoordsys<>(), pin_radius, CH_C_2PI, 0, false);
     pin_profile->AddSubLine(pin_circle);
 
@@ -157,18 +156,18 @@ int main(int argc, char* argv[]) {
     pin->GetCollisionModel()->SetSafeMargin(0.02);
     pin->SetCollide(true);
     pin->GetCollisionModel()->ClearModel();
-    pin->GetCollisionModel()->Add2Dpath(*pin_profile.get_ptr(), ChVector<>(0, 0, separation / 2));
-    pin->GetCollisionModel()->Add2Dpath(*pin_profile.get_ptr(), ChVector<>(0, 0, -separation / 2));
+    pin->GetCollisionModel()->Add2Dpath(*pin_profile.get(), ChVector<>(0, 0, separation / 2));
+    pin->GetCollisionModel()->Add2Dpath(*pin_profile.get(), ChVector<>(0, 0, -separation / 2));
     //pin->GetCollisionModel()->AddCylinder(pin_radius, pin_radius, pin_hlen);
     pin->GetCollisionModel()->BuildModel();
 
     // Add pin visualization
-    ChSharedPtr<ChCylinderShape> pin_cyl(new ChCylinderShape);
+    auto pin_cyl = std::make_shared<ChCylinderShape>();
     pin_cyl->GetCylinderGeometry().p1 = ChVector<>(0, 0, pin_hlen);
     pin_cyl->GetCylinderGeometry().p2 = ChVector<>(0, 0, -pin_hlen);
     pin_cyl->GetCylinderGeometry().rad = pin_radius;
     pin->AddAsset(pin_cyl);
-    ChSharedPtr<ChColorAsset> pin_col(new ChColorAsset);
+    auto pin_col = std::make_shared<ChColorAsset>();
     pin_col->SetColor(ChColor(0.2f, 0.5f, 0.8f));
     pin->AddAsset(pin_col);
 
