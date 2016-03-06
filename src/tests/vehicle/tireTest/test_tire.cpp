092a17dd534bad4868db2f50381673dab15d7d4b
Radu Serban <serban@wisc.edu>
2016-03-06 18:53:16 +0100

    Add example concrete ANCF tire (toroidal, single-layer).  Update test_tire.

--- src/tests/vehicle/tireTest/test_tire.cpp
+++ src/tests/vehicle/tireTest/test_tire.cpp
@@ -13,25 +13,60 @@
 // =============================================================================
 //
 // Quarter-vehicle tire test rig.
+// The rig mechanism consists of a "chassis" body constrained to only move in a
+// vertical plane and a wheel body connected to the chassis through a revolute
+// joint.
+//
+// One of the following types of tires can be attached to the wheel body:
+// RIGID, FIALA, LUGRE, or ANCF (toroidal).
+// - the RIGID, LUGRE, and FIALA tires use JSON specification files
+// - the ANCF tire uses concrete custom implementation: ANCFToroidal.{h,cpp}
+//
+// Either DEM-P or DEM-C contact models can be specified. The integrator can be
+// set as either Euler semi-implicit or HHT.  The solver can be one of: SOR,
+// MINRES, or MKL.
+//
+// Notes:
+// - the ANCF tire is available only if the Chrono::FEA module is enabled
+//   (otherwise selection reverts to RIGID tire)
+// - selecting the ANCF tire forces the integrator to HHT and the solver to MKL
+// - the MKL solver is available only if the Chrono::MKL module is enabled
+//   (otherwise selection reverts to MINRES)
+//
+// The coordinate frame respects the ISO standard adopted in Chrono::Vehicle:
+// right-handed frame with X pointing towards the front, Y to the left, and Z up
 //
 // =============================================================================
+////#include <float.h>
+////unsigned int fp_control_state = _controlfp(_EM_INEXACT, _MCW_EM);
 
 #include <cmath>
-#include <vector>
-#include <valarray>
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
+#include <valarray>
+#include <vector>
 
-#include "chrono_irrlicht/ChIrrApp.h"
+#include "chrono/lcp/ChLcpIterativeMINRES.h"
 #include "chrono/utils/ChUtilsCreators.h"
 #include "chrono/utils/ChUtilsInputOutput.h"
 
+#include "chrono_irrlicht/ChIrrApp.h"
+
+#include "chrono_vehicle/ChConfigVehicle.h"
 #include "chrono_vehicle/ChVehicleModelData.h"
-#include "chrono_vehicle/wheeled_vehicle/tire/RigidTire.h"
-#include "chrono_vehicle/wheeled_vehicle/tire/LugreTire.h"
-#include "chrono_vehicle/wheeled_vehicle/tire/FialaTire.h"
 #include "chrono_vehicle/terrain/RigidTerrain.h"
+#include "chrono_vehicle/wheeled_vehicle/tire/FialaTire.h"
+#include "chrono_vehicle/wheeled_vehicle/tire/LugreTire.h"
+#include "chrono_vehicle/wheeled_vehicle/tire/RigidTire.h"
+
+#ifdef CHRONO_FEA
+#include "ANCFToroidalTire.h"
+#endif
+
+#ifdef CHRONO_MKL
+#include "chrono_mkl/ChLcpMklSolver.h"
+#endif
 
 using namespace chrono;
 using namespace chrono::vehicle;
@@ -46,9 +81,6 @@ ChMaterialSurfaceBase::ContactMethod contact_method = ChMaterialSurfaceBase::DVI
 
 // Type of tire model
 TireModelType tire_model = RIGID;
-
-// Output file name
-std::string out_file("results.out");
 
 // JSON file names for tire models
 std::string rigidtire_file("generic/tire/RigidTire.json");
@@ -69,27 +101,33 @@ double tire_offset = 0.1;
 double terrain_length = 100.0;  // size in X direction
 double terrain_width = 2.0;     // size in Y direction
 
-// Simulation step size and simulation length
+// Solver settings
+enum SolverType { SOR, MINRES, MKL };
+SolverType solver_type = SOR;
+
+enum IntegratorType { EULER, HHT };
+IntegratorType integrator_type = EULER;
+
 double step_size = 1e-3;  // integration step size
-int num_steps = 5000;     // number of steps for data colection
 
 // =============================================================================
 // Main driver program
 
 int main(int argc, char* argv[]) {
+#ifndef CHRONO_FEA
+    if (tire_model == ANCF)
+        tire_model = RIGID;
+#endif
+
+    if (tire_model == ANCF)
+        contact_method = ChMaterialSurfaceBase::DEM;
+
     // Create the mechanical system
     // ----------------------------
 
     ChSystem* system = (contact_method == ChMaterialSurfaceBase::DVI) ? new ChSystem : new ChSystemDEM;
 
     system->Set_G_acc(ChVector<>(0.0, 0.0, -9.8));
-
-    system->SetIntegrationType(ChSystem::INT_EULER_IMPLICIT_LINEARIZED);
-    system->SetIterLCPmaxItersSpeed(100);
-    system->SetIterLCPmaxItersStab(100);
-    system->SetLcpSolverType(ChSystem::LCP_ITERATIVE_SOR);
-    system->SetTol(1e-10);
-    system->SetTolForce(1e-8);
 
     // Create the quarter-vehicle chassis
     // ----------------------------------
@@ -121,6 +159,7 @@ int main(int argc, char* argv[]) {
     // ---------------
 
     std::shared_ptr<ChTire> tire;
+    double wheel_radius;
     double tire_radius;
     double tire_width;
 
@@ -129,6 +168,7 @@ int main(int argc, char* argv[]) {
             auto tire_rigid = std::make_shared<RigidTire>(vehicle::GetDataFile(rigidtire_file));
             tire_rigid->Initialize(wheel);
             tire_radius = tire_rigid->getRadius();
+            wheel_radius = tire_radius;
             tire_width = tire_rigid->getWidth();
             tire = tire_rigid;
             break;
@@ -137,6 +177,7 @@ int main(int argc, char* argv[]) {
             auto tire_lugre = std::make_shared<LugreTire>(vehicle::GetDataFile(lugretire_file));
             tire_lugre->Initialize(wheel);
             tire_radius = tire_lugre->getRadius();
+            wheel_radius = tire_radius;
             int num_discs = tire_lugre->getNumDiscs();
             tire_width = std::abs(tire_lugre->getDiscLocations()[0] - tire_lugre->getDiscLocations()[num_discs - 1]);
             tire = tire_lugre;
@@ -146,24 +187,73 @@ int main(int argc, char* argv[]) {
             auto tire_fiala = std::make_shared<FialaTire>(vehicle::GetDataFile(fialatire_file));
             tire_fiala->Initialize();
             tire_radius = tire_fiala->GetUnloadedRadius();
+            wheel_radius = tire_radius;
             tire_width = tire_fiala->GetWidth();
             tire = tire_fiala;
             break;
         }
+        case ANCF: {
+#ifdef CHRONO_FEA
+            auto tire_ancf = std::make_shared<ANCFToroidalTire>("ANCF_Tire");
+
+            tire_ancf->EnablePressure(true);
+            tire_ancf->EnableContact(false);
+            tire_ancf->EnableRimConnection(true);
+
+            tire_ancf->Initialize(wheel, LEFT);
+            tire_radius = tire_ancf->GetTireRadius();
+            wheel_radius = tire_ancf->GetRimRadius();
+            tire_width = tire_ancf->GetWidth();
+            tire = tire_ancf;
+#endif
+            break;
+        }
+    }
+
+    // Add chassis visualization
+    // -------------------------
+
+    {
+        auto boxH = std::make_shared<ChBoxShape>();
+        boxH->GetBoxGeometry().SetLengths(ChVector<>(2, 0.02, 0.02));
+        chassis->AddAsset(boxH);
+        auto boxV = std::make_shared<ChBoxShape>();
+        boxV->GetBoxGeometry().SetLengths(ChVector<>(0.02, 0.02, 2));
+        chassis->AddAsset(boxV);
+        auto cyl = std::make_shared<ChCylinderShape>();
+        cyl->GetCylinderGeometry().rad = 0.05;
+        cyl->GetCylinderGeometry().p1 = ChVector<>(0, 0.55 * tire_width, 0);
+        cyl->GetCylinderGeometry().p1 = ChVector<>(0, -0.55 * tire_width, 0);
+        chassis->AddAsset(cyl);
+        auto color = std::make_shared<ChColorAsset>(0.4f, 0.5f, 0.6f);
+        chassis->AddAsset(color);
     }
 
     // Add wheel visualization
     // -----------------------
 
-    auto wheel_cyl = std::make_shared<ChCylinderShape>();
-    wheel_cyl->GetCylinderGeometry().rad = tire_radius;
-    wheel_cyl->GetCylinderGeometry().p1 = ChVector<>(0, tire_width / 2, 0);
-    wheel_cyl->GetCylinderGeometry().p2 = ChVector<>(0, -tire_width / 2, 0);
-    wheel->AddAsset(wheel_cyl);
+    if (tire_model == ANCF) {
+        geometry::ChTriangleMeshConnected trimesh;
+        trimesh.LoadWavefrontMesh(GetChronoDataFile("fea/tractor_wheel_rim.obj"), false, false);
+        trimesh.Transform(ChVector<>(0, 0, 0), ChMatrix33<>(CH_C_PI_2, ChVector<>(0, 0, 1)));
+        auto trimesh_shape = std::make_shared<ChTriangleMeshShape>();
+        trimesh_shape->SetMesh(trimesh);
+        wheel->AddAsset(trimesh_shape);
 
-    auto tex = std::make_shared<ChTexture>();
-    tex->SetTextureFilename(GetChronoDataFile("bluwhite.png"));
-    wheel->AddAsset(tex);
+        auto color = std::make_shared<ChColorAsset>(0.95f, 0.82f, 0.38f);
+        wheel->AddAsset(color);
+    }
+    else {
+        auto wheel_cyl = std::make_shared<ChCylinderShape>();
+        wheel_cyl->GetCylinderGeometry().rad = wheel_radius;
+        wheel_cyl->GetCylinderGeometry().p1 = ChVector<>(0, tire_width / 2, 0);
+        wheel_cyl->GetCylinderGeometry().p2 = ChVector<>(0, -tire_width / 2, 0);
+        wheel->AddAsset(wheel_cyl);
+
+        auto tex = std::make_shared<ChTexture>();
+        tex->SetTextureFilename(GetChronoDataFile("bluwhite.png"));
+        wheel->AddAsset(tex);
+    }
 
     // Create the terrain
     // ------------------
@@ -182,7 +272,7 @@ int main(int argc, char* argv[]) {
     system->AddLink(plane_plane);
     plane_plane->SetName("plane_plane");
     plane_plane->Initialize(terrain->GetGroundBody(), chassis,
-                            ChCoordsys<>(ChVector<>(0, 0, 0), Q_from_AngX(CH_C_PI_2)));
+        ChCoordsys<>(ChVector<>(0, 0, 0), Q_from_AngX(CH_C_PI_2)));
 
     // Connect wheel to chassis through a revolute joint.
     // The axis of rotation is along the y global axis.
@@ -191,29 +281,102 @@ int main(int argc, char* argv[]) {
     revolute->SetName("revolute");
     revolute->Initialize(chassis, wheel, ChCoordsys<>(ChVector<>(0, 0, 0), Q_from_AngX(CH_C_PI_2)));
 
+    // Complete system setup
+    // ---------------------
+
+    system->SetupInitial();
+
+    // Solver and integrator settings
+    // ------------------------------
+
+    if (tire_model == ANCF) {
+        solver_type = MKL;
+        integrator_type = HHT;
+    }
+
+    if (solver_type == MKL) {
+#ifndef CHRONO_MKL
+        solver_type = MINRES;
+#endif
+    }
+
+    switch (solver_type) {
+        case SOR: {
+            GetLog() << "Using SOR solver\n";
+            system->SetLcpSolverType(ChSystem::LCP_ITERATIVE_SOR);
+            system->SetIterLCPmaxItersSpeed(100);
+            system->SetIterLCPmaxItersStab(100);
+            system->SetTol(1e-10);
+            system->SetTolForce(1e-8);
+            break;
+        }
+        case MINRES: {
+            GetLog() << "Using MINRES solver\n";
+            system->SetLcpSolverType(ChSystem::LCP_ITERATIVE_MINRES);
+            ChLcpIterativeMINRES* minres_solver = (ChLcpIterativeMINRES*)system->GetLcpSolverSpeed();
+            ////minres_solver->SetDiagonalPreconditioning(true);
+            system->SetIterLCPwarmStarting(true);
+            system->SetIterLCPmaxItersSpeed(500);
+            system->SetTolForce(1e-5);
+            break;
+        }
+        case MKL: {
+#ifdef CHRONO_MKL
+            GetLog() << "Using MKL solver\n";
+            ChLcpMklSolver* mkl_solver_stab = new ChLcpMklSolver;
+            ChLcpMklSolver* mkl_solver_speed = new ChLcpMklSolver;
+            system->ChangeLcpSolverStab(mkl_solver_stab);
+            system->ChangeLcpSolverSpeed(mkl_solver_speed);
+            mkl_solver_speed->SetSparsityPatternLock(true);
+            mkl_solver_stab->SetSparsityPatternLock(true);
+#endif
+            break;
+        }
+    }
+
+    // Set up integrator
+    switch (integrator_type) {
+        case EULER:
+            GetLog() << "Using EULER_IMPLICIT_LINEARIZED integrator\n";
+            system->SetIntegrationType(ChSystem::INT_EULER_IMPLICIT_LINEARIZED);
+            break;
+        case HHT: {
+            GetLog() << "Using HHT integrator\n";
+            system->SetIntegrationType(ChSystem::INT_HHT);
+            auto integrator = std::static_pointer_cast<ChTimestepperHHT>(system->GetTimestepper());
+            integrator->SetAlpha(-0.2);
+            integrator->SetMaxiters(20);
+            integrator->SetAbsTolerances(5e-05, 5e-03);
+            integrator->SetMode(ChTimestepperHHT::POSITION);
+            integrator->SetScaling(true);
+            integrator->SetVerbose(true);
+            break;
+        }
+    }
+
     // Create the Irrlicht app
     // -----------------------
-    ChIrrApp* app = new ChIrrApp(system, L"Tire Test Rig", core::dimension2d<u32>(800, 600), false, true);
-    app->AddTypicalLogo();
-    app->AddTypicalSky();
-    app->AddTypicalLights();
-    app->AddTypicalCamera(core::vector3df(0, 1, 1), core::vector3df(0, 0, 0));
+    ChIrrApp app(system, L"Tire Test Rig", core::dimension2d<u32>(800, 600), false, true);
+    app.AddTypicalLogo();
+    app.AddTypicalSky();
+    app.AddTypicalLights(irr::core::vector3df(-130.f, -130.f, 50.f), irr::core::vector3df(30.f, 50.f, 100.f), 250, 130);
+    app.AddTypicalCamera(core::vector3df(0, -1, 0.2f), core::vector3df(0, 0, 0));
 
-    app->AssetBindAll();
-    app->AssetUpdateAll();
+    app.AssetBindAll();
+    app.AssetUpdateAll();
 
     // Perform the simulation
     // ----------------------
     WheelState wheel_state;
     TireForce tire_force;
 
-    app->SetTimestep(step_size);
+    app.SetTimestep(step_size);
 
-    while (app->GetDevice()->run()) {
+    while (app.GetDevice()->run()) {
         // Render scene
-        app->BeginScene();
-        app->DrawAll();
-        app->EndScene();
+        app.BeginScene();
+        app.DrawAll();
+        app.EndScene();
 
         // Extract wheel state
         wheel_state.pos = wheel->GetPos();           // global position
