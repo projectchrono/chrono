3186eec5bb2d92f51ae27a1ef8982aec85e96d85
Radu Serban <serban@wisc.edu>
2016-06-19 17:14:53 +0200

    Modify utest_CH_AVX to be a proper unit test:
    - remove all timing (not the goal of a unit test)
    - check multiple combinations of matrix dimensions
    - return appropriate value from main() function

--- src/unit_tests/core/utest_CH_AVX.cpp
+++ src/unit_tests/core/utest_CH_AVX.cpp
@@ -1,25 +1,26 @@
-//
+// =============================================================================
 // PROJECT CHRONO - http://projectchrono.org
 //
-// Copyright (c) 2010 Alessandro Tasora
-// All rights reserved.
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
 //
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file at the top level of the distribution
-// and at http://projectchrono.org/license-chrono.txt.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
 // =============================================================================
 // Authors: Milad Rakhsha, Radu Serban
 // =============================================================================
-// unit test for MatrMultiplyAVX and MatrMultiplyTAVX.
+//
+// Unit test for MatrMultiplyAVX and MatrMultiplyTAVX.
+//
 // =============================================================================
-#include "core/ChTransform.h"
-#include "core/ChMatrix.h"
-#include "core/ChLog.h"
-#include "core/ChVector.h"
-#include "core/ChQuadrature.h"
-#include "core/ChException.h"
-#include "core/ChTimer.h"
+
+#include "chrono/core/ChMatrixDynamic.h"
+#include "chrono/core/ChLog.h"
+
 using namespace chrono;
+
 void FillRand(ChMatrix<double>& matra) {
     int A_Nrow = matra.GetRows();
     int A_NCol = matra.GetColumns();
@@ -31,146 +32,125 @@ void FillRand(ChMatrix<double>& matra) {
     }
 }
 
+// Check multiplication A*B of random matrices A (MxN) and B (NxK)
+bool CheckMatMult(int M, int N, int K, double tolerance) {
+    GetLog() << "(" << M << "x" << N << ") * (" << N << "x" << K << ")   ... ";
+
+    ChMatrixDynamic<double> A(M, N);
+    ChMatrixDynamic<double> B(N, K);
+    A.FillRandom(10, -10);
+    B.FillRandom(10, -10);
+
+    ChMatrixDynamic<double> ref(M, K);
+    ref.MatrMultiply(A, B);
+
+    ChMatrixDynamic<double> avx(M, K);
+    avx.MatrMultiplyAVX(A, B);
+
+    if (avx.Equals(ref, tolerance)) {
+        GetLog() << "OK\n";
+        return true;
+    }
+
+    GetLog() << "FAILED\n";
+    GetLog() << "\n(A*B)_ref";
+    ref.StreamOUT(GetLog());
+    GetLog() << "\n(A*B)_avx";
+    avx.StreamOUT(GetLog());
+    GetLog() << "\n(A*B)_avx - (A*B)_ref";
+    (avx - ref).StreamOUT(GetLog());
+
+    return false;
+}
+
+// Check multiplication A*B' of random matrices A (MxN) and B (KxN)
+bool CheckMatMultT(int M, int N, int K, double tolerance) {
+    GetLog() << "(" << M << "x" << N << ") * (" << K << "x" << N << ")^T   ... ";
+
+    ChMatrixDynamic<double> A(M, N);
+    ChMatrixDynamic<double> B(K, N);
+    A.FillRandom(10, -10);
+    B.FillRandom(10, -10);
+
+    ChMatrixDynamic<double> ref(M, K);
+    ref.MatrMultiplyT(A, B);
+
+    ChMatrixDynamic<double> avx(M, K);
+    avx.MatrMultiplyTAVX(A, B);
+
+    if (avx.Equals(ref, tolerance)) {
+        GetLog() << "OK\n";
+        return true;
+    }
+
+    GetLog() << "FAILED\n";
+    GetLog() << "\n(A*B')_ref";
+    ref.StreamOUT(GetLog());
+    GetLog() << "\n(A*B')_avx";
+    avx.StreamOUT(GetLog());
+    GetLog() << "\n(A*B')_avx - (A*B')_ref";
+    (avx - ref).StreamOUT(GetLog());
+
+    return false;
+}
+
 int main(int argc, char* argv[]) {
-    // If you want to pollute the cache line to see if this affects the speed up or not
-    bool polluteCache = false;
-    // If you want to print the difference between AVX implementation and non-AVX implementation
-    // This should print zeros since you are printing results of (AVX - nonAVX) implementation.
+    // Print differences between standard and AVX-based multiplicaitons
     bool printMul = true;
-    bool MatrMultiply = true;
-    bool MatrMultiplyT = true;
-    // This is the number of repetition of the operation. Remember that timing only one operation is not accurate
-    int ITERATION = 10000;
 
-    ChTimer<double> timer;
+    // Tolerance for comparing matrices
+    double tolerance = 1e-12;
+
+    // Result of unit tests
+    bool passed = true;
+
+    // Initialize seed for rand()
     srand(time(NULL));
-    if (MatrMultiply) {
-        int A_row = 24;
-        int A_col = 6;
-        int B_row = A_col;
-        int B_col = 24;
-        ChMatrixDynamic<double> A(A_row, A_col);
-        ChMatrixDynamic<double> B(B_row, B_col);
-        ChMatrixDynamic<double> AmulB(A_row, B_col);
-        ChMatrixDynamic<double> AmulB_ref(A_row, B_col);
 
-        if (polluteCache) {
-            GetLog() << "\nCache is polluted by changing the element of matrices.\n";
-            GetLog() << "No report for the correctness of the operation...\n";
-            GetLog() << "==================================================\n\n";
-            GetLog() << "Testing the FillRand()... \n";
-            GetLog() << "The first call to FillRand(A): ";
-            FillRand(A);
-            (A).StreamOUT(GetLog());  // Print a matrix to cout (ex. the console, if open)
-            GetLog() << "The second call to FillRand(A): ";
-            FillRand(A);
-            (A).StreamOUT(GetLog());  // Print a matrix to cout (ex. the console, if open)
-        }
-        GetLog() << "-----------------MatrMultiply---------------------- \n";
-        timer.reset();
-        timer.start();
-        for (int j = 0; j < ITERATION; j++) {
-            if (polluteCache) {
-                timer.stop();
-                FillRand(A);
-                FillRand(B);
-                timer.start();
-            }
-            AmulB.MatrMultiply(A, B);
-        }
-        timer.stop();
+    GetLog() << "\n-----------------MatrMultiply---------------------- \n";
 
-        double tempTime = timer();
-        GetLog() << "The MatrMultiply results in " << timer() << " (s), ";
-        timer.reset();
-        timer.start();
-        for (int j = 0; j < ITERATION; j++) {
-            if (polluteCache) {
-                timer.stop();
-                FillRand(A);
-                FillRand(B);
-                timer.start();
-            }
-            AmulB.MatrMultiplyAVX(A, B);
-        }
-        timer.stop();
-        double AVXTime = timer();
-        GetLog() << "The AVX results in " << timer() << " (s) \n";
-        GetLog() << "Speed up =  " << tempTime / AVXTime << "x \n";
+    passed &= CheckMatMult(20, 8, 24, tolerance);
+    passed &= CheckMatMult(21, 8, 24, tolerance);
+    passed &= CheckMatMult(22, 8, 24, tolerance);
+    passed &= CheckMatMult(23, 8, 24, tolerance);
 
-        // Let's not compare the results if polluteCache=true; since A and B elements have been changed...
-        if (printMul && !polluteCache) {
-            GetLog() << "--------------------------------------- \n";
-            GetLog() << "AVX-Ref result is : ";
-            (AmulB - AmulB_ref).StreamOUT(GetLog());  // Print a matrix to cout (ex. the console, if open)
-            GetLog() << "--------------------------------------- \n";
-        }
-        // if polluteCache=true let's assume the AVX works Okay
-        if (AmulB_ref == AmulB && !polluteCache) {
-            GetLog() << "MatrMultiplyAVX is Ok ... \n\n";
-        } else if (!polluteCache) {
-            GetLog() << "MatrMultiplyTAVX is not Ok! \n\n";
-        }
-    }
+    passed &= CheckMatMult(20, 9, 24, tolerance);
+    passed &= CheckMatMult(21, 9, 24, tolerance);
+    passed &= CheckMatMult(22, 9, 24, tolerance);
+    passed &= CheckMatMult(23, 9, 24, tolerance);
 
-    if (MatrMultiplyT) {
-        int A_row = 5;
-        int A_col = 20;
-        int B_row = 5;
-        int B_col = A_col;
+    passed &= CheckMatMult(20, 10, 24, tolerance);
+    passed &= CheckMatMult(21, 10, 24, tolerance);
+    passed &= CheckMatMult(22, 10, 24, tolerance);
+    passed &= CheckMatMult(23, 10, 24, tolerance);
 
-        ChMatrixDynamic<double> A(A_row, A_col);
-        ChMatrixDynamic<double> B(B_row, B_col);
-        ChMatrixDynamic<double> AmulB(A_row, B_row);
-        ChMatrixDynamic<double> AmulB_ref(A_row, B_row);
+    passed &= CheckMatMult(20, 11, 24, tolerance);
+    passed &= CheckMatMult(21, 11, 24, tolerance);
+    passed &= CheckMatMult(22, 11, 24, tolerance);
+    passed &= CheckMatMult(23, 11, 24, tolerance);
 
-        A.FillRandom(10, -10);
-        B.FillRandom(10, -10);
-        GetLog() << "-----------------MatrMultiplyT---------------------- \n";
-        timer.reset();
-        timer.start();
-        for (int j = 0; j < ITERATION; j++) {
-            if (polluteCache) {
-                timer.stop();
-                FillRand(A);
-                FillRand(B);
-                timer.start();
-            }
-            AmulB_ref.MatrMultiplyT(A, B);
-        }
-        timer.stop();
+    GetLog() << "\n-----------------MatrMultiplyT---------------------- \n";
 
-        double tempTime = timer();
-        GetLog() << "The MatrMultiplyT results in " << timer() << " (s), ";
-        timer.reset();
-        timer.start();
-        for (int j = 0; j < ITERATION; j++) {
-            if (polluteCache) {
-                timer.stop();
-                FillRand(A);
-                FillRand(B);
-                timer.start();
-            }
-            AmulB.MatrMultiplyTAVX(A, B);
-        }
-        timer.stop();
-        double AVXTime = timer();
-        GetLog() << "The AVX results in " << timer() << " (s) \n";
-        GetLog() << "Speed up =  " << tempTime / AVXTime << "x \n";
-        // Let's not compare the results if polluteCache=true; since A and B elements have been changed...
-        if (printMul && !polluteCache) {
-            GetLog() << "--------------------------------------- \n";
-            GetLog() << "AVX-Ref result is : ";
-            (AmulB - AmulB_ref).StreamOUT(GetLog());
-            GetLog() << "--------------------------------------- \n";
-        }
-        // if polluteCache=true let's assume the AVX works Okay
-        if ((AmulB_ref == AmulB && !polluteCache)) {
-            GetLog() << "MatrMultiplyTAVX is Ok ... \n\n";
-        } else if (!polluteCache) {
-            GetLog() << "MatrMultiplyTAVX is not Ok!? \n\n";
-        }
-    }
+    passed &= CheckMatMultT(20, 8, 24, tolerance);
+    passed &= CheckMatMultT(21, 8, 24, tolerance);
+    passed &= CheckMatMultT(22, 8, 24, tolerance);
+    passed &= CheckMatMultT(23, 8, 24, tolerance);
 
-    return 0;
+    passed &= CheckMatMultT(20, 9, 24, tolerance);
+    passed &= CheckMatMultT(21, 9, 24, tolerance);
+    passed &= CheckMatMultT(22, 9, 24, tolerance);
+    passed &= CheckMatMultT(23, 9, 24, tolerance);
+
+    passed &= CheckMatMultT(20, 10, 24, tolerance);
+    passed &= CheckMatMultT(21, 10, 24, tolerance);
+    passed &= CheckMatMultT(22, 10, 24, tolerance);
+    passed &= CheckMatMultT(23, 10, 24, tolerance);
+
+    passed &= CheckMatMultT(20, 11, 24, tolerance);
+    passed &= CheckMatMultT(21, 11, 24, tolerance);
+    passed &= CheckMatMultT(22, 11, 24, tolerance);
+    passed &= CheckMatMultT(23, 11, 24, tolerance);
+
+    // Return 0 if all tests passed.
+    return !passed;
 }
