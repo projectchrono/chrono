57f871b727a0f8f01bdd0424899e2f3db7d49679
Conlain Kelly <conlain.k@gmail.com>
2016-06-23 19:08:30 -0500

    Testing broken javascript on doxygen searching

--- website/js/search.js
+++ website/js/search.js
@@ -90,6 +90,7 @@ jQuery(function() {
     })
   }
   function docshow(result) {
+    console.log("result is: " + json.stringify(result, null, 4));
     var hits = result["items"];
     for (var i = 0; i < hits.length; i++) {
       var hit = hits[i];
