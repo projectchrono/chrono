8a053c65b9ad9c69b6a21959ebfcd2186293c3a4
Alessandro Tasora <tasora@ied.unipr.it>
2016-06-29 21:56:18 +0200

    Make public some shell methods

--- src/chrono_fea/ChElementShellANCF.h
+++ src/chrono_fea/ChElementShellANCF.h
@@ -234,6 +234,7 @@ class ChApiFea ChElementShellANCF : public ChElementShell, public ChLoadableUV, 
     static const double m_toleranceEAS;   ///< tolerance for nonlinear EAS solver (on residual)
     static const int m_maxIterationsEAS;  ///< maximum number of nonlinear EAS iterations
 
+public:
     // Interface to ChElementBase base class
     // -------------------------------------
 
