7a2f95b76dcdc4557f8fd5b8662c4b7fa54b5db9
Radu Serban <serban@wisc.edu>
2016-07-10 09:04:25 +0200

    Add missing implementation of pure virtual method in ChSolverMatlab

--- src/chrono_matlab/ChSolverMatlab.h
+++ src/chrono_matlab/ChSolverMatlab.h
@@ -1,13 +1,16 @@
-//
+// =============================================================================
 // PROJECT CHRONO - http://projectchrono.org
 //
-// Copyright (c) 2010 Alessandro Tasora
-// All rights reserved.
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
 //
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file at the top level of the distribution
-// and at http://projectchrono.org/license-chrono.txt.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
 //
+// =============================================================================
+// Authors: Alessandro Tasora, Radu Serban
+// =============================================================================
 
 #ifndef CHSOLVERMATLAB_H
 #define CHSOLVERMATLAB_H
@@ -36,36 +39,22 @@ class ChApiMatlab ChSolverMatlab : public ChSolver {
     virtual ~ChSolverMatlab() {}
 
     /// Set the Matlab engine.
-    void SetEngine(ChMatlabEngine* me) {mengine = me;}
+    void SetEngine(ChMatlabEngine* me) { mengine = me; }
+
+    /// Indicate whether or not the Solve() phase requires an up-to-date problem matrix.
+    /// As typical of direct solvers, the Pardiso solver only requires the matrix for its Setup() phase.
+    virtual bool SolveRequiresMatrix() const override { return false; }
 
     /// Solve using the Matlab default direct solver (as in x=A\b)
-    virtual double Solve(ChSystemDescriptor& sysd);  ///< system description with constraints and variables
+    virtual double Solve(ChSystemDescriptor& sysd) override;
 
-
-    //
-    // SERIALIZATION
-    //
-
-    virtual void ArchiveOUT(ChArchiveOut& marchive) {
-        // version number
-        marchive.VersionWrite(1);
-        // serialize parent class
-        ChSolver::ArchiveOUT(marchive);
-        // serialize all member data:
-        marchive << CHNVP(mengine);
-    }
+    /// Method to allow serialization of transient data to archives.
+    virtual void ArchiveOUT(ChArchiveOut& marchive) override;
 
     /// Method to allow de serialization of transient data from archives.
-    virtual void ArchiveIN(ChArchiveIn& marchive) {
-        // version number
-        int version = marchive.VersionRead();
-        // deserialize parent class
-        ChSolver::ArchiveIN(marchive);
-        // stream in all member data:
-        marchive >> CHNVP(mengine);
-    }
+    virtual void ArchiveIN(ChArchiveIn& marchive) override;
 };
 
-}  // END_OF_NAMESPACE____
+}  // end namespace chrono
 
 #endif
