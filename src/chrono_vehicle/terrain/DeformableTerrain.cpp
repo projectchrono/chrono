ae1e2c58e8534f2d8213cd62a4636f2c02bfa3f8
Daniel Melanz <daniel.melanz@gmail.com>
2016-08-01 15:02:00 -0500

    Added getter for soil mesh (needed for rendering).

--- src/chrono_vehicle/terrain/DeformableTerrain.cpp
+++ src/chrono_vehicle/terrain/DeformableTerrain.cpp
@@ -77,6 +77,9 @@ void DeformableTerrain::SetPlane(ChCoordsys<> mplane) { m_ground->plane = mplane
 // Get the plane reference.
 const ChCoordsys<>& DeformableTerrain::GetPlane() const { return m_ground->plane; }
 
+// Get the trimesh that defines the ground shape.
+const std::shared_ptr<ChTriangleMeshShape> DeformableTerrain::GetMesh() const { return m_ground->m_trimesh_shape; }
+
 // Enable bulldozing effect.
 void DeformableTerrain::SetBulldozingFlow(bool mb) {
     m_ground->do_bulldozing = mb;
