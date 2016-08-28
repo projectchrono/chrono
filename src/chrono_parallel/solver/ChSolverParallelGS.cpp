7b7699848fe6f26500984c0160f89d410c341471
Hammad Mazhar <hammad.maz@gmail.com>
2016-08-27 21:06:50 -0700

    gauss seidel solver working

--- src/chrono_parallel/solver/ChSolverParallelGS.cpp
+++ src/chrono_parallel/solver/ChSolverParallelGS.cpp
@@ -14,20 +14,40 @@ uint ChSolverParallelGS::Solve(ChShurProduct& ShurProduct,
 
 	ml = gamma;
 	Project(ml.data());
+
+	uint num_contacts = data_manager->num_constraints;
 	CompressedMatrix<real> Nshur = data_manager->host_data.D_T * data_manager->host_data.M_invD;
+	DynamicVector<real> D;
+	D.resize(num_contacts, false);
+
+	for (size_t i = 0; i < num_contacts; ++i) {
+		Nshur(i, i) += data_manager->host_data.E[i];
+		D[i] = 1.0 / (Nshur(i, i));
+	}
+
+
 	for (current_iteration = 0; current_iteration < max_iter; current_iteration++) {
 		real omega = 1.0;
 
 		for (int i = 0; i < data_manager->num_constraints; ++i) {
 			ml[i] = ml[i] -
-				omega * 1.0 / Nshur(i, i) * ((row(Nshur, i * 1 + 0), blaze::subvector(ml, 0, 1 * data_manager->num_constraints)) +
-					data_manager->host_data.E[i] * ml[i] - r[i]);
-			
+				omega * D[i] * ((row(Nshur, i * 1 + 0), blaze::subvector(ml, 0, 1 * data_manager->num_constraints)) - r[i]);
 		}
 		Project(ml.data());
 		gamma = ml;
-		residual = 0;         // Res4Blaze(ml, mb);
-		objective_value = 0;  // GetObjective(ml, mb);
+		real gdiff = 1.0 / pow(data_manager->num_constraints, 2.0);
+		ShurProduct(gamma, ml_old);
+
+		objective_value = (ml, (0.5*ml_old - r));
+
+		ml_old = ml_old - r;
+		ml_old = gamma - gdiff * (ml_old);
+		Project(ml_old.data());
+		ml_old = (1.0 / gdiff) * (gamma - ml_old);
+
+		residual = Sqrt((double)(ml_old, ml_old));
+
+		AtIterationEnd(residual, objective_value);
 	}
 
 	return current_iteration;
