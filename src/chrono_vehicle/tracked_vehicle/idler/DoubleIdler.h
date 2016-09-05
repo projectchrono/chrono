6bb12d1fe9c4d9f907677e73987f444886494ca7
Radu Serban <serban@wisc.edu>
2016-09-05 10:39:36 +0200

    Add concrete classes for defining tracked vehicle templates for single and double idler subsystems from JSON specification files

---
+++ src/chrono_vehicle/tracked_vehicle/idler/DoubleIdler.h
@@ -0,0 +1,86 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Double idler model constructed with data from file (JSON format).
+//
+// =============================================================================
+
+#ifndef DOUBLE_IDLER_H
+#define DOUBLE_IDLER_H
+
+#include "chrono_vehicle/ChApiVehicle.h"
+#include "chrono_vehicle/tracked_vehicle/idler/ChDoubleIdler.h"
+
+#include "chrono_thirdparty/rapidjson/document.h"
+
+namespace chrono {
+namespace vehicle {
+
+/// @addtogroup vehicle_tracked_brake
+/// @{
+
+/// Double idler model constructed with data from file (JSON format).
+class CH_VEHICLE_API DoubleIdler : public ChDoubleIdler {
+  public:
+    DoubleIdler(const std::string& filename);
+    DoubleIdler(const rapidjson::Document& d);
+    ~DoubleIdler() {}
+
+    virtual double GetWheelRadius() const override { return m_wheel_radius; }
+    virtual double GetWheelWidth() const override { return m_wheel_width; }
+    virtual double GetWheelGap() const override { return m_wheel_gap; }
+
+    virtual double GetWheelMass() const override { return m_wheel_mass; }
+    virtual const ChVector<>& GetWheelInertia() override { return m_wheel_inertia; }
+
+    virtual double GetCarrierMass() const override { return m_carrier_mass; }
+    virtual const ChVector<>& GetCarrierInertia() override { return m_carrier_inertia; }
+    virtual double GetCarrierVisRadius() const { return m_carrier_vis_radius; }
+
+    virtual double GetPrismaticPitchAngle() const override { return m_pitch_angle; }
+
+    virtual ChSpringForceCallback* GetTensionerForceCallback() const override { return m_tensionerForceCB; }
+
+    virtual void AddWheelVisualization() override;
+
+  private:
+    void Create(const rapidjson::Document& d);
+
+    double m_wheel_radius;
+    double m_wheel_width;
+    double m_wheel_gap;
+
+    double m_wheel_mass;
+    ChVector<> m_wheel_inertia;
+
+    double m_carrier_mass;
+    ChVector<> m_carrier_inertia;
+
+    double m_carrier_vis_radius;
+
+    double m_pitch_angle;
+
+    ChSpringForceCallback* m_tensionerForceCB;
+
+    VisualizationType m_vis_type;
+    std::string m_meshName;
+    std::string m_meshFile;
+};
+
+/// @} vehicle_tracked_brake
+
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
---
+++ src/chrono_vehicle/tracked_vehicle/idler/SingleIdler.h
@@ -0,0 +1,84 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Single idler model constructed with data from file (JSON format).
+//
+// =============================================================================
+
+#ifndef SINGLE_IDLER_H
+#define SINGLE_IDLER_H
+
+#include "chrono_vehicle/ChApiVehicle.h"
+#include "chrono_vehicle/tracked_vehicle/idler/ChSingleIdler.h"
+
+#include "chrono_thirdparty/rapidjson/document.h"
+
+namespace chrono {
+namespace vehicle {
+
+/// @addtogroup vehicle_tracked_brake
+/// @{
+
+/// Single idler model constructed with data from file (JSON format).
+class CH_VEHICLE_API SingleIdler : public ChSingleIdler {
+  public:
+    SingleIdler(const std::string& filename);
+    SingleIdler(const rapidjson::Document& d);
+    ~SingleIdler() {}
+
+    virtual double GetWheelRadius() const override { return m_wheel_radius; }
+    virtual double GetWheelWidth() const override { return m_wheel_width; }
+
+    virtual double GetWheelMass() const override { return m_wheel_mass; }
+    virtual const ChVector<>& GetWheelInertia() override { return m_wheel_inertia; }
+
+    virtual double GetCarrierMass() const override { return m_carrier_mass; }
+    virtual const ChVector<>& GetCarrierInertia() override { return m_carrier_inertia; }
+    virtual double GetCarrierVisRadius() const { return m_carrier_vis_radius; }
+
+    virtual double GetPrismaticPitchAngle() const override { return m_pitch_angle; }
+
+    virtual ChSpringForceCallback* GetTensionerForceCallback() const override { return m_tensionerForceCB; }
+
+    virtual void AddWheelVisualization() override;
+
+  private:
+    void Create(const rapidjson::Document& d);
+
+    double m_wheel_radius;
+    double m_wheel_width;
+
+    double m_wheel_mass;
+    ChVector<> m_wheel_inertia;
+
+    double m_carrier_mass;
+    ChVector<> m_carrier_inertia;
+
+    double m_carrier_vis_radius;
+
+    double m_pitch_angle;
+
+    ChSpringForceCallback* m_tensionerForceCB;
+
+    VisualizationType m_vis_type;
+    std::string m_meshName;
+    std::string m_meshFile;
+};
+
+/// @} vehicle_tracked_brake
+
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
