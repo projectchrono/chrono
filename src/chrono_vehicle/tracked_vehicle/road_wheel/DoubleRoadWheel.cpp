38c0a476a442579b0c70070347e21f51319c8bc3
Radu Serban <serban@wisc.edu>
2016-09-05 11:00:27 +0200

    Add concrete classes for defining tracked vehicle templates for single and double road-wheel subsystems from JSON specification files

---
+++ src/chrono_vehicle/tracked_vehicle/road_wheel/DoubleRoadWheel.cpp
@@ -0,0 +1,131 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Double road-wheel model constructed with data from file (JSON format).
+//
+// =============================================================================
+
+#include "chrono/assets/ChTriangleMeshShape.h"
+#include "chrono_vehicle/tracked_vehicle/road_wheel/DoubleRoadWheel.h"
+
+#include "chrono_thirdparty/rapidjson/filereadstream.h"
+
+using namespace rapidjson;
+
+namespace chrono {
+namespace vehicle {
+
+// -----------------------------------------------------------------------------
+// This utility function returns a ChVector from the specified JSON array
+// -----------------------------------------------------------------------------
+static ChVector<> loadVector(const Value& a) {
+    assert(a.IsArray());
+    assert(a.Size() == 3);
+
+    return ChVector<>(a[0u].GetDouble(), a[1u].GetDouble(), a[2u].GetDouble());
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+DoubleRoadWheel::DoubleRoadWheel(const std::string& filename) : ChDoubleRoadWheel(""), m_vis_type(NONE) {
+    FILE* fp = fopen(filename.c_str(), "r");
+
+    char readBuffer[65536];
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+    fclose(fp);
+
+    Document d;
+    d.ParseStream(is);
+
+    Create(d);
+
+    GetLog() << "Loaded JSON: " << filename.c_str() << "\n";
+}
+
+DoubleRoadWheel::DoubleRoadWheel(const rapidjson::Document& d) : ChDoubleRoadWheel(""), m_vis_type(NONE) {
+    Create(d);
+}
+
+void DoubleRoadWheel::Create(const rapidjson::Document& d) {
+    // Read top-level data
+    assert(d.HasMember("Type"));
+    assert(d.HasMember("Template"));
+    assert(d.HasMember("Name"));
+
+    SetName(d["Name"].GetString());
+
+    // Read wheel geometry and mass properties
+    assert(d.HasMember("Wheel"));
+    m_wheel_radius = d["Wheel"]["Radius"].GetDouble();
+    m_wheel_width = d["Wheel"]["Width"].GetDouble();
+    m_wheel_gap = d["Wheel"]["Gap"].GetDouble();
+    m_wheel_mass = d["Wheel"]["Mass"].GetDouble();
+    m_wheel_inertia = loadVector(d["Wheel"]["Inertia"]);
+
+    // Read contact material data
+    assert(d.HasMember("Contact Material"));
+
+    float mu = d["Contact Material"]["Coefficient of Friction"].GetDouble();
+    float cr = d["Contact Material"]["Coefficient of Restitution"].GetDouble();
+
+    SetContactFrictionCoefficient(mu);
+    SetContactRestitutionCoefficient(cr);
+
+    if (d["Contact Material"].HasMember("Properties")) {
+        float ym = d["Contact Material"]["Properties"]["Young Modulus"].GetDouble();
+        float pr = d["Contact Material"]["Properties"]["Poisson Ratio"].GetDouble();
+        SetContactMaterialProperties(ym, pr);
+    }
+    if (d["Contact Material"].HasMember("Coefficients")) {
+        float kn = d["Contact Material"]["Coefficients"]["Normal Stiffness"].GetDouble();
+        float gn = d["Contact Material"]["Coefficients"]["Normal Damping"].GetDouble();
+        float kt = d["Contact Material"]["Coefficients"]["Tangential Stiffness"].GetDouble();
+        float gt = d["Contact Material"]["Coefficients"]["Tangential Damping"].GetDouble();
+        SetContactMaterialCoefficients(kn, gn, kt, gt);
+    }
+
+    // Read wheel visualization
+    if (d.HasMember("Visualization")) {
+        if (d["Visualization"].HasMember("Mesh Filename")) {
+            m_meshFile = d["Visualization"]["Mesh Filename"].GetString();
+            m_meshName = d["Visualization"]["Mesh Name"].GetString();
+            m_vis_type = MESH;
+        } else {
+            m_vis_type = PRIMITIVES;
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void DoubleRoadWheel::AddWheelVisualization() {
+    switch (m_vis_type) {
+        case PRIMITIVES:
+            ChDoubleRoadWheel::AddWheelVisualization();
+            break;
+        case MESH: {
+            geometry::ChTriangleMeshConnected trimesh;
+            trimesh.LoadWavefrontMesh(m_meshFile, false, false);
+            auto trimesh_shape = std::make_shared<ChTriangleMeshShape>();
+            trimesh_shape->SetMesh(trimesh);
+            trimesh_shape->SetName(m_meshName);
+            m_wheel->AddAsset(trimesh_shape);
+            break;
+        }
+    }
+}
+
+}  // end namespace vehicle
+}  // end namespace chrono
