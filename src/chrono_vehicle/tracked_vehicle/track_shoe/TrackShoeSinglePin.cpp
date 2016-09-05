c3e7a1b68b9bdc158e189bc69f30ebadce0f974f
Radu Serban <serban@wisc.edu>
2016-09-05 19:10:13 +0200

    Add concrete class for a tracked vehicle single-pin track shoe subsystem from JSON specification file

---
+++ src/chrono_vehicle/tracked_vehicle/track_shoe/TrackShoeSinglePin.cpp
@@ -0,0 +1,144 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Single-pin track shoe constructed with data from file (JSON format).
+//
+// =============================================================================
+
+#include "chrono/assets/ChTriangleMeshShape.h"
+#include "chrono_vehicle/tracked_vehicle/track_shoe/TrackShoeSinglePin.h"
+
+#include "chrono_thirdparty/rapidjson/filereadstream.h"
+
+using namespace rapidjson;
+
+namespace chrono {
+namespace vehicle {
+
+// -----------------------------------------------------------------------------
+// This utility function returns a ChVector from the specified JSON array
+// -----------------------------------------------------------------------------
+static ChVector<> loadVector(const Value& a) {
+    assert(a.IsArray());
+    assert(a.Size() == 3);
+
+    return ChVector<>(a[0u].GetDouble(), a[1u].GetDouble(), a[2u].GetDouble());
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+TrackShoeSinglePin::TrackShoeSinglePin(const std::string& filename) : ChTrackShoeSinglePin(""), m_vis_type(NONE) {
+    FILE* fp = fopen(filename.c_str(), "r");
+
+    char readBuffer[65536];
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+    fclose(fp);
+
+    Document d;
+    d.ParseStream(is);
+
+    Create(d);
+
+    GetLog() << "Loaded JSON: " << filename.c_str() << "\n";
+}
+
+TrackShoeSinglePin::TrackShoeSinglePin(const rapidjson::Document& d) : ChTrackShoeSinglePin(""), m_vis_type(NONE) {
+    Create(d);
+}
+
+void TrackShoeSinglePin::Create(const rapidjson::Document& d) {
+    // Read top-level data
+    assert(d.HasMember("Type"));
+    assert(d.HasMember("Template"));
+    assert(d.HasMember("Name"));
+
+    SetName(d["Name"].GetString());
+
+    // Read wheel geometry and mass properties
+    assert(d.HasMember("Shoe"));
+    m_shoe_height = d["Shoe"]["Height"].GetDouble();
+    m_shoe_pitch = d["Shoe"]["Pitch"].GetDouble();
+    m_shoe_mass = d["Shoe"]["Mass"].GetDouble();
+    m_shoe_inertia = loadVector(d["Shoe"]["Inertia"]);
+
+    // Read contact geometry data
+    assert(d.HasMember("Contact Geometry"));
+    assert(d["Contact Geometry"].HasMember("Shoe"));
+    assert(d["Contact Geometry"].HasMember("Cylinder"));
+
+    m_pad_box_dims = loadVector(d["Contact Geometry"]["Shoe"]["Pad dimensions"]);
+    m_pad_box_loc = loadVector(d["Contact Geometry"]["Shoe"]["Pad location"]);
+    m_guide_box_dims = loadVector(d["Contact Geometry"]["Shoe"]["Guide Dimensions"]);
+    m_guide_box_loc = loadVector(d["Contact Geometry"]["Shoe"]["Guide location"]);
+
+    m_cyl_radius = d["Contact Geometry"]["Cylinder"]["Radius"].GetDouble();
+    m_front_cyl_loc = d["Contact Geometry"]["Cylinder"]["Front Offset"].GetDouble();
+    m_rear_cyl_loc = d["Contact Geometry"]["Cylinder"]["Rear Offset"].GetDouble();
+
+    // Read contact material data
+    assert(d.HasMember("Contact Material"));
+
+    float mu = d["Contact Material"]["Coefficient of Friction"].GetDouble();
+    float cr = d["Contact Material"]["Coefficient of Restitution"].GetDouble();
+
+    SetContactFrictionCoefficient(mu);
+    SetContactRestitutionCoefficient(cr);
+
+    if (d["Contact Material"].HasMember("Properties")) {
+        float ym = d["Contact Material"]["Properties"]["Young Modulus"].GetDouble();
+        float pr = d["Contact Material"]["Properties"]["Poisson Ratio"].GetDouble();
+        SetContactMaterialProperties(ym, pr);
+    }
+    if (d["Contact Material"].HasMember("Coefficients")) {
+        float kn = d["Contact Material"]["Coefficients"]["Normal Stiffness"].GetDouble();
+        float gn = d["Contact Material"]["Coefficients"]["Normal Damping"].GetDouble();
+        float kt = d["Contact Material"]["Coefficients"]["Tangential Stiffness"].GetDouble();
+        float gt = d["Contact Material"]["Coefficients"]["Tangential Damping"].GetDouble();
+        SetContactMaterialCoefficients(kn, gn, kt, gt);
+    }
+
+    // Read wheel visualization
+    if (d.HasMember("Visualization")) {
+        if (d["Visualization"].HasMember("Mesh Filename")) {
+            m_meshFile = d["Visualization"]["Mesh Filename"].GetString();
+            m_meshName = d["Visualization"]["Mesh Name"].GetString();
+            m_vis_type = MESH;
+        } else {
+            m_vis_type = PRIMITIVES;
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void TrackShoeSinglePin::AddShoeVisualization() {
+    switch (m_vis_type) {
+        case PRIMITIVES:
+            ChTrackShoeSinglePin::AddShoeVisualization();
+            break;
+        case MESH: {
+            geometry::ChTriangleMeshConnected trimesh;
+            trimesh.LoadWavefrontMesh(m_meshFile, false, false);
+            auto trimesh_shape = std::make_shared<ChTriangleMeshShape>();
+            trimesh_shape->SetMesh(trimesh);
+            trimesh_shape->SetName(m_meshName);
+            m_shoe->AddAsset(trimesh_shape);
+            break;
+        }
+    }
+}
+
+}  // end namespace vehicle
+}  // end namespace chrono
