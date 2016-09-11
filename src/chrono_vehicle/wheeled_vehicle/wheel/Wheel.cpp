95f7ac4cd4027c057d86f5c6b5da11e6cc47dead
Radu Serban <serban@wisc.edu>
2016-09-11 22:50:24 +0200

    Include required <algorithm> header

--- src/chrono_vehicle/wheeled_vehicle/wheel/Wheel.cpp
+++ src/chrono_vehicle/wheeled_vehicle/wheel/Wheel.cpp
@@ -16,6 +16,8 @@
 //
 // =============================================================================
 
+#include <algorithm>
+
 #include "chrono_vehicle/wheeled_vehicle/wheel/Wheel.h"
 #include "chrono_vehicle/ChVehicleModelData.h"
 
