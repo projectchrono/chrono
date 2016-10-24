ee8ba8deec7e43d62a26afc89288b9e6cd0a2e23
Radu Serban <serban@wisc.edu>
2016-10-24 09:15:08 +0200

    Remove redundant constraints in double-pin track assembly.
    To ensure that the (open) chain of track shoes is not over-constrained, use spherical joints between connector bodies of a track shoe and the shoe body of the neighbor.
    To ensure that the (closed) chain of track shoes is not over-constrained, replace one set of spherical joints with a point-line joints.
    To accommodate an arbitrary number of track shoes, we special-case the connection between the first two track shoes.

--- src/chrono_vehicle/tracked_vehicle/track_shoe/ChTrackShoeDoublePin.cpp
+++ src/chrono_vehicle/tracked_vehicle/track_shoe/ChTrackShoeDoublePin.cpp
@@ -275,37 +275,59 @@ void ChTrackShoeDoublePin::AddConnectorVisualization(std::shared_ptr<ChBody> con
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 void ChTrackShoeDoublePin::Connect(std::shared_ptr<ChTrackShoe> next) {
+    ChSystem* system = m_shoe->GetSystem();
+
     // Create and initialize the revolute joints between shoe body and connector bodies.
     ChVector<> loc_L = m_shoe->TransformPointLocalToParent(ChVector<>(GetShoeLength() / 2, +GetShoeWidth() / 2, 0));
     ChVector<> loc_R = m_shoe->TransformPointLocalToParent(ChVector<>(GetShoeLength() / 2, -GetShoeWidth() / 2, 0));
     ChQuaternion<> rot = m_shoe->GetRot() * Q_from_AngX(CH_C_PI_2);
 
-    m_rev_this_L = std::make_shared<ChLinkLockRevolute>();
-    m_rev_this_L->SetNameString(m_name + "_rev_this_L");
-    m_rev_this_L->Initialize(m_shoe, m_connector_L, ChCoordsys<>(loc_L, rot));
-    m_shoe->GetSystem()->AddLink(m_rev_this_L);
+    auto rev_L = std::make_shared<ChLinkLockRevolute>();
+    rev_L->SetNameString(m_name + "_rev_this_L");
+    rev_L->Initialize(m_shoe, m_connector_L, ChCoordsys<>(loc_L, rot));
+    system->AddLink(rev_L);
 
-    m_rev_this_R = std::make_shared<ChLinkLockRevolute>();
-    m_rev_this_R->SetNameString(m_name + "_rev_this_R");
-    m_rev_this_R->Initialize(m_shoe, m_connector_R, ChCoordsys<>(loc_R, rot));
-    m_shoe->GetSystem()->AddLink(m_rev_this_R);
+    auto rev_R = std::make_shared<ChLinkLockRevolute>();
+    rev_R->SetNameString(m_name + "_rev_this_R");
+    rev_R->Initialize(m_shoe, m_connector_R, ChCoordsys<>(loc_R, rot));
+    system->AddLink(rev_R);
 
-    // Create and initialize the revolute joints between connector bodies and next shoe body.
-    loc_L = m_connector_L->TransformPointLocalToParent(ChVector<>(GetConnectorLength() / 2, 0, 0));
-    rot = m_connector_L->GetRot() * Q_from_AngX(CH_C_PI_2);
+    // Create connections between these connector bodies and the next shoe body
+    if (m_index == 0) {
+        // Create and initialize a point-line joint for left connector (sliding along X axis)
+        loc_L = m_connector_L->TransformPointLocalToParent(ChVector<>(GetConnectorLength() / 2, 0, 0));
+        rot = m_connector_L->GetRot() * Q_from_AngZ(CH_C_PI_2);
 
-    m_rev_next_L = std::make_shared<ChLinkLockRevolute>();
-    m_rev_next_L->SetNameString(m_name + "_rev_next_L");
-    m_rev_next_L->Initialize(next->GetShoeBody(), m_connector_L, ChCoordsys<>(loc_L, rot));
-    m_shoe->GetSystem()->AddLink(m_rev_next_L);
+        auto pointline_L = std::make_shared<ChLinkLockPointLine>();
+        pointline_L->SetNameString(m_name + "_pointline_next_L");
+        pointline_L->Initialize(next->GetShoeBody(), m_connector_L, ChCoordsys<>(loc_L, rot));
+        system->AddLink(pointline_L);
 
-    loc_R = m_connector_R->TransformPointLocalToParent(ChVector<>(GetConnectorLength() / 2, 0, 0));
-    rot = m_connector_R->GetRot() * Q_from_AngX(CH_C_PI_2);
+        // Create and initialize a point-line joint for left connector (sliding along X axis)
+        loc_R = m_connector_R->TransformPointLocalToParent(ChVector<>(GetConnectorLength() / 2, 0, 0));
+        rot = m_connector_R->GetRot() * Q_from_AngZ(CH_C_PI_2);
 
-    m_rev_next_R = std::make_shared<ChLinkLockRevolute>();
-    m_rev_next_R->SetNameString(m_name + "_rev_next_R");
-    m_rev_next_R->Initialize(next->GetShoeBody(), m_connector_R, ChCoordsys<>(loc_R, rot));
-    m_shoe->GetSystem()->AddLink(m_rev_next_R);
+        auto pointline_R = std::make_shared<ChLinkLockPointLine>();
+        pointline_R->SetNameString(m_name + "_pointline_next_R");
+        pointline_R->Initialize(next->GetShoeBody(), m_connector_R, ChCoordsys<>(loc_R, rot));
+        system->AddLink(pointline_R);
+    } else {
+        // Create and initialize a spherical joint for left connector
+        loc_L = m_connector_L->TransformPointLocalToParent(ChVector<>(GetConnectorLength() / 2, 0, 0));
+
+        auto sph_L = std::make_shared<ChLinkLockSpherical>();
+        sph_L->SetNameString(m_name + "_sph_next_L");
+        sph_L->Initialize(next->GetShoeBody(), m_connector_L, ChCoordsys<>(loc_L));
+        system->AddLink(sph_L);
+
+        // Create and initialize a spherical joint for left connector
+        loc_R = m_connector_R->TransformPointLocalToParent(ChVector<>(GetConnectorLength() / 2, 0, 0));
+
+        auto sph_R = std::make_shared<ChLinkLockSpherical>();
+        sph_R->SetNameString(m_name + "_sph_next_R");
+        sph_R->Initialize(next->GetShoeBody(), m_connector_R, ChCoordsys<>(loc_R));
+        system->AddLink(sph_R);
+    }
 }
 
 }  // end namespace vehicle
