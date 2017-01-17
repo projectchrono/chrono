7552b1dd5f2900c7b61fe787bfb7f904e430db32
Conlain Kelly <conlain.k@gmail.com>
2017-01-17 16:44:45 -0600

    Added a tolerance in lambda unit test to avoid failures due to tiny numerical differences (~1e-17) in sin/cos autodiff. Due to compiler behavior this test would otherwise fail on OSX machines. Also added more verbose output in case failure occurs.

--- src/unit_tests/core/utest_CH_ChFunction_Lambda.cpp
+++ src/unit_tests/core/utest_CH_ChFunction_Lambda.cpp
@@ -22,34 +22,39 @@
 using namespace chrono;
 
 int main() {
-
-    bool passed = true;
+    bool passed = true, temp = true;
 
     // y = x*x @ x=3.0. --> y=9, dy/dx=6, d^2y/dx^2 = 2
-    auto F1 = make_ChFunction_Lambda([](auto x) { return x*x; });
-    passed = passed
-             && F1.Get_y(3.0) == 9.0
-             && F1.Get_y_dx(3.0) == 6.0
-             && F1.Get_y_dxdx(3.0) == 2.0;
+    auto F1 = make_ChFunction_Lambda([](auto x) { return x * x; });
+    temp = temp && F1.Get_y(3.0) == 9.0 && F1.Get_y_dx(3.0) == 6.0 && F1.Get_y_dxdx(3.0) == 2.0;
 
+    if (!temp)
+        std::cerr << "ChFunction_Lambda Error: Test 1 not passed\n";
+    passed &= temp;
+    temp = true;
 
     // y = exp(2*x)
-    auto F2 = make_ChFunction_Lambda([](auto x){ return exp(2*x); });
-    passed = passed
-            && F2.Get_y(2.0) == exp(4.0)
-            && F2.Get_y_dx(2.0) == 2.0*exp(4.0)
-            && F2.Get_y_dxdx(2.0) == 4.0*exp(4.0);
+    auto F2 = make_ChFunction_Lambda([](auto x) { return exp(2 * x); });
+    temp = temp && F2.Get_y(2.0) == std::exp(4.0) && F2.Get_y_dx(2.0) == 2.0 * std::exp(4.0) &&
+           F2.Get_y_dxdx(2.0) == 4.0 * std::exp(4.0);
 
+    if (!temp)
+        std::cerr << "ChFunction_Lambda Error: Test 2 not passed\n";
+    passed &= temp;
+    temp = true;
+
+    // Allows for minute differences due to numerical errors
+    double F3_tolerance = 1e-16;
 
     // y = cos(x), via shared pointer
-    auto Fp3 = make_shared_ChFunction_Lambda([](auto x){ return cos(x); });
-    passed = passed
-             && Fp3->Get_y(5.0) == cos(5.0)
-             && Fp3->Get_y_dx(5.0) == -sin(5.0)
-             && Fp3->Get_y_dxdx(5.0) == -cos(5.0);
+    auto Fp3 = make_shared_ChFunction_Lambda([](auto x) { return cos(x); });
+    temp = temp && std::abs(Fp3->Get_y(5.0) - std::cos(5.0)) < F3_tolerance &&
+           std::abs(Fp3->Get_y_dx(5.0) - (-std::sin(5.0))) < F3_tolerance &&
+           std::abs(Fp3->Get_y_dxdx(5.0) - (-std::cos(5.0))) < F3_tolerance;
 
-    if(!passed)
-        std::cerr << "ChFunction_Lambda Error: All tests not passed\n";
+    if (!temp)
+        std::cerr << "ChFunction_Lambda Error: Test 3 not passed\n";
+    passed &= temp;
 
     return !passed;
 }
