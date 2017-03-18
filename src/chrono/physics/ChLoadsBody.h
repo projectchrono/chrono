d0eac42ae1e1d076c808165715af30cbfbf55424
Alessandro Tasora <tasora@ied.unipr.it>
2017-03-18 21:29:36 +0100

    New Load type: ChLoadBodyBody , as a base class for all loads that act between a pair of bodies. Uses automatic jacobian differentiation.

--- src/chrono/physics/ChLoadsBody.h
+++ src/chrono/physics/ChLoadsBody.h
@@ -168,57 +168,119 @@ public:
 
 //------------------------------------------------------------------------------------------------
 
-/*
-/// BUSHING BETWEEN RIGID BODIES
-/// Load for a spherical flexible bushing connection between two bodies.
 
-class ChLoadBodyBushing : public ChLoadCustomMultiple {
-private:
-    double radial_stiffness;
-    double radial_damping;
-    ChVector<> loc_application_A;
-    ChVector<> loc_application_B;
+/// Bushing base class.
+/// Base class for wrench loads (a force + a torque) acting between two bodies.
+/// See children classes for concrete implementations.
+
+class ChLoadBodyBody : public ChLoadCustomMultiple {
+protected:
+    ChFrame<>  loc_application_A;
+    ChFrame<>  loc_application_B;
+    ChVector<> locB_force;  // store computed values here 
+    ChVector<> locB_torque; // store computed values here 
 public:
-    ChLoadBodyBushing(std::shared_ptr<ChBody> mbodyA,       ///< object A
-                      std::shared_ptr<ChBody> mbodyB,       ///< object B
-                      const ChVector<> abs_application, ///< create the bushing here, in abs. coordinates
-                      const double mstiffness,          ///< radial stiffness
-                      const double mdamping             ///< radial damping
-                    ) 
-         : ChLoadCustomMultiple(mbodyA,mbodyB), 
-           radial_stiffness(mstiffness),
-           radial_damping(mdamping) {         
+    ChLoadBodyBody( std::shared_ptr<ChBody> mbodyA,   ///< object A
+                    std::shared_ptr<ChBody> mbodyB,   ///< object B
+                    const ChFrame<> abs_application   ///< create the bushing here, in abs. coordinates.
+                        ) 
+         : ChLoadCustomMultiple(mbodyA,mbodyB)  {         
             
-            loc_application_A = mbodyA->TransformPointParentToLocal(abs_application);
-            loc_application_B = mbodyB->TransformPointParentToLocal(abs_application);
+            mbodyA->ChFrame::TransformParentToLocal(abs_application,loc_application_A);
+            mbodyB->ChFrame::TransformParentToLocal(abs_application,loc_application_B);
         }
 
-        /// Compute Q, the generalized load. 
+        /// Compute the force between the two bodies, in local reference loc_application_B,
+        /// given rel_AB, i.e. the position and speed of loc_application_A respect to loc_application_B.
+        /// Force is assumed applied to body B, and its opposite to A.
+        /// Inherited classes MUST IMPLEMENT THIS.
+    virtual void ComputeBushingForceTorque(const ChFrameMoving<>& rel_AB, 
+                                            ChVector<>& loc_force,
+                                            ChVector<>& loc_torque) 
+                                            = 0;
+
+        // Optional: inherited classes could implement this to avoid the
+        // default numerical computation of jacobians:
+        //   virtual void ComputeJacobian(...) // see ChLoad
+
+        /// For diagnosis purposes, this can return the actual last computed value of 
+        /// the applied force, expressed in coordinate system of loc_application_B, assumed applied to body B
+    ChVector<> GetBushingForce() {return locB_force;}
+        /// For diagnosis purposes, this can return the actual last computed value of 
+        /// the applied torque, expressed in coordinate system of loc_application_B, assumed applied to body B
+    ChVector<> GetBushingTorque() {return locB_torque;}
+
+        /// Set the application frame of bushing on bodyA
+    void SetApplicationFrameA(const ChFrame<>& mpA) {this->loc_application_A = mpA;}
+    ChFrame<> GetApplicationFrameA() const {return this->loc_application_A;}
+
+        /// Set the application frame of bushing on bodyB
+    void SetApplicationFrameB(const ChFrame<>& mpB) {this->loc_application_B = mpB;}
+    ChFrame<> GetApplicationFrameB() const {return this->loc_application_B;}
+
+
+        /// Compute Q, the generalized load. It calls ComputeBushingForceTorque, so in
+        /// children classes you do not need to implement it.
         /// Called automatically at each Update().
     virtual void ComputeQ(ChState*      state_x, ///< state position to evaluate Q
                           ChStateDelta* state_w  ///< state speed to evaluate Q
                           )  {
         auto mbodyA = std::dynamic_pointer_cast<ChBody>(this->loadables[0]);
         auto mbodyB = std::dynamic_pointer_cast<ChBody>(this->loadables[1]);
-    
-        ChCoordsys<> bodycoordA, bodycoordB;
+
+        ChFrameMoving<> bodycoordA, bodycoordB;
         if(state_x) {
-            bodycoordA = state_x->ClipCoordsys(0,0); // the numerical jacobian algo might change state_x
-            bodycoordB = state_x->ClipCoordsys(7,0); // the numerical jacobian algo might change state_x
+            // the numerical jacobian algo might change state_x
+            bodycoordA.SetCoord(state_x->ClipCoordsys(0,0)); 
+            bodycoordB.SetCoord(state_x->ClipCoordsys(7,0));
         }
         else  {
-            bodycoordA = mbodyA->coord;
-            bodycoordB = mbodyB->coord;
+            bodycoordA.SetCoord(mbodyA->coord);
+            bodycoordB.SetCoord(mbodyB->coord);
         }
-        ChVector<> abs_d = bodycoordB.pos -  bodycoordA.pos;
-        ChVector<> abs_force = abs_d * this->radial_stiffness;
+        if(state_w) {
+             // the numerical jacobian algo might change state_w
+            bodycoordA.SetPos_dt  (state_w->ClipVector(0,0));
+            bodycoordA.SetWvel_loc(state_w->ClipVector(3,0));
+            bodycoordB.SetPos_dt  (state_w->ClipVector(6,0));
+            bodycoordB.SetWvel_loc(state_w->ClipVector(9,0));
+        }
+        else  {
+            bodycoordA.SetCoord_dt(mbodyA->GetCoord_dt());
+            bodycoordB.SetCoord_dt(mbodyB->GetCoord_dt());
+        }
+        
+        ChFrameMoving<> frame_Aw = ChFrameMoving<>(loc_application_A) >> bodycoordA;
+        ChFrameMoving<> frame_Bw = ChFrameMoving<>(loc_application_B) >> bodycoordB;
+        ChFrameMoving<> rel_AB = frame_Aw >> frame_Bw.GetInverse();
+        
+        // ----COMPUTE THE BUSHING FORCE
+
+        this->ComputeBushingForceTorque(rel_AB, locB_force, locB_torque);
+
+        // ----
+        
+        ChVector<> abs_force  = frame_Bw.TransformDirectionLocalToParent(locB_force);
+        ChVector<> abs_torque = frame_Bw.TransformDirectionLocalToParent(locB_torque);
 
         // Compute Q 
 
-        ChVector<> abs_applicationA = bodycoordA.TransformPointLocalToParent(loc_application_A);
-        ChVector<> loc_torque = bodycoordA.rot.RotateBack(  ((abs_applicationA-bodycoordA.pos) %  abs_force) );
-        this->load_Q.PasteVector(abs_force, 0,0);
-        this->load_Q.PasteVector(loc_torque,3,0);
+        ChVector<> loc_ftorque = bodycoordA.GetRot().RotateBack(  ((frame_Aw.GetPos()-bodycoordA.GetPos()) %  -abs_force) );
+        ChVector<> loc_torque  = bodycoordA.GetRot().RotateBack(-abs_torque);
+        this->load_Q.PasteVector(-abs_force, 0,0);
+        this->load_Q.PasteVector(loc_ftorque+loc_torque,3,0);
+
+                   loc_ftorque = bodycoordB.GetRot().RotateBack(  ((frame_Bw.GetPos()-bodycoordB.GetPos()) %  abs_force) );
+                   loc_torque  = bodycoordB.GetRot().RotateBack( abs_torque);
+        this->load_Q.PasteVector( abs_force, 6,0);
+        this->load_Q.PasteVector( loc_ftorque+loc_torque,9,0);
+        
+    }
+
+
+};
+
+
 
         ChVector<> abs_applicationB = bodycoordB.TransformPointLocalToParent(loc_application_B);
                    loc_torque = bodycoordB.rot.RotateBack(  ((abs_applicationB-bodycoordB.pos) % -abs_force) );
