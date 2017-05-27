ddf44a07d255ceace376122246fa187cb2762343
Radu Serban <serban@wisc.edu>
2017-05-27 16:21:42 +0200

    Add new type of wheeled vehicle suspension (rigid axle pinned to chassis)

--- src/chrono_models/vehicle/generic/Generic_RigidPinnedAxle.cpp
+++ src/chrono_models/vehicle/generic/Generic_RigidPinnedAxle.cpp
@@ -12,7 +12,7 @@
 // Authors: Radu Serban
 // =============================================================================
 //
-// Generic concrete rigid suspension subsystem.
+// Generic concrete rigid-pinned-axle suspension subsystem.
 //
 // This concrete suspension subsystem is defined with respect to a right-handed
 // frame with X pointing towards the front, Y to the left, and Z up (as imposed
@@ -25,7 +25,7 @@
 #include <vector>
 #include <algorithm>
 
-#include "chrono_models/vehicle/generic/Generic_RigidSuspension.h"
+#include "chrono_models/vehicle/generic/Generic_RigidPinnedAxle.h"
 #include "chrono/core/ChCubicSpline.h"
 
 namespace chrono {
@@ -36,14 +36,20 @@ namespace generic {
 // Static variables -- all in SI units
 // -----------------------------------------------------------------------------
 
-const double Generic_RigidSuspension::m_spindleMass = 1.103;
+const double Generic_RigidPinnedAxle::m_spindleMass = 1.103;
+const double Generic_RigidPinnedAxle::m_axleTubeMass = 10;
 
-const double Generic_RigidSuspension::m_spindleRadius = 0.15;
-const double Generic_RigidSuspension::m_spindleWidth = 0.06;
+const double Generic_RigidPinnedAxle::m_spindleRadius = 0.15;
+const double Generic_RigidPinnedAxle::m_spindleWidth = 0.06;
+const double Generic_RigidPinnedAxle::m_axleTubeRadius = 0.05;
 
-const ChVector<> Generic_RigidSuspension::m_spindleInertia(0.000478, 0.000496, 0.000478);
+const ChVector<> Generic_RigidPinnedAxle::m_spindleInertia(0.000478, 0.000496, 0.000478);
+const ChVector<> Generic_RigidPinnedAxle::m_axleTubeInertia(0.006, 0.003, 0.006);
 
-const double Generic_RigidSuspension::m_axleInertia = 0.4;
+const ChVector<> Generic_RigidPinnedAxle::m_axleTubeCOM(0, 0, 0);
+const ChVector<> Generic_RigidPinnedAxle::m_axlePinLoc(0, 0, 0.15);
+
+const double Generic_RigidPinnedAxle::m_axleInertia = 0.4;
 
 // -----------------------------------------------------------------------------
 // Implementation of the getLocation() virtual method.
@@ -54,7 +60,7 @@ const double Generic_RigidSuspension::m_axleInertia = 0.4;
 // function are for the left half of the suspension only.
 // -----------------------------------------------------------------------------
 
-const ChVector<> Generic_RigidSuspension::getLocation(PointId which) {
+const ChVector<> Generic_RigidPinnedAxle::getLocation(PointId which) {
     switch (which) {
         case SPINDLE:
             return ChVector<>(0, 1.100, 0);  // location of spindle center of mass
