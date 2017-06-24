930606bde27a1c25bec371f7b73fb88f112d47c6
Radu Serban <serban@wisc.edu>
2017-06-24 09:37:10 +0200

    Fix comment in standard file header

--- src/demos/opengl/demo_GL_cohesion.cpp
+++ src/demos/opengl/demo_GL_cohesion.cpp
@@ -5,7 +5,7 @@
 // PROJECT CHRONO - http://projectchrono.org
 //
 // Copyright (c) 2014 projectchrono.org
-// All right reserved.
+// All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license that can be found
 // in the LICENSE file at the top level of the distribution and at
