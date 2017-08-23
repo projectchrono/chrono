9a0d5e11b266ae50356d950b4641f0af11789ba0
Alessandro Tasora <tasora@ied.unipr.it>
2017-08-23 22:38:54 +0200

    Base class for triparametric volumes

---
+++ src/chrono/geometry/ChVolume.h
@@ -0,0 +1,87 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Alessandro Tasora
+// =============================================================================
+
+#ifndef CHC_VOLUME_H
+#define CHC_VOLUME_H
+
+#include <cmath>
+
+#include "chrono/geometry/ChGeometry.h"
+#include "chrono/core/ChFilePS.h"
+
+namespace chrono {
+namespace geometry {
+
+/// Base class for all geometric objects representing tri-parametric surfaces in 3D space.
+/// This is the base for all U,V,W-parametric object, implementing Evaluate() 
+/// that returns a point as a function of three U,V,W parameters. 
+
+class ChApi ChVolume : public ChGeometry {
+
+  protected:
+
+  public:
+    ChVolume()  {}
+    ChVolume(const ChVolume& source) {};
+    virtual ~ChVolume() {}
+
+    /// "Virtual" copy constructor (covariant return type).
+    //virtual ChVolume* Clone() const override { };
+
+        /// Evaluates a point in the volume, given parametric coordinates U,V,W.
+        /// Parameters U V W always work in 0..1 range.
+        /// Computed value goes into the 'pos' reference.
+        /// It must be implemented by inherited classes.
+    virtual void Evaluate(ChVector<>& pos, const double parU, const double parV, const double parW) const = 0;
+
+        /// Tell if the volume is closed (periodic) in parametric coordinate 
+    virtual bool Get_closed_U() const { return false; }
+
+        /// Tell if the volume is closed (periodic) in parametric coordinate 
+    virtual bool Get_closed_V() const { return false; }
+
+        /// Tell if the volume is closed (periodic) in parametric coordinate 
+    virtual bool Get_closed_W() const { return false; }
+
+    /// This is a volume
+    virtual int GetManifoldDimension() const override { return 3; }
+
+
+    virtual void ArchiveOUT(ChArchiveOut& marchive) override {
+        // version number
+        marchive.VersionWrite<ChVolume>();
+        // serialize parent class
+        ChGeometry::ArchiveOUT(marchive);
+        // serialize all member data:
+        //marchive << CHNVP(closed);
+    }
+
+    /// Method to allow de serialization of transient data from archives.
+    virtual void ArchiveIN(ChArchiveIn& marchive) override {
+        // version number
+        int version = marchive.VersionRead<ChVolume>();
+        // deserialize parent class
+        ChGeometry::ArchiveIN(marchive);
+        // stream in all member data:
+        //marchive >> CHNVP(closed);
+    }
+};
+
+}  // end namespace geometry
+
+CH_CLASS_VERSION(geometry::ChVolume,0)
+
+}  // end namespace chrono
+
+#endif
