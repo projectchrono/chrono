9d05fee44dc3ac6d28f4ac64dc8973c994c501ce
Conlain Kelly <conlain.k@gmail.com>
2018-01-29 13:29:32 -0600

    Fix cmake build, add meat to poiseuilleCyl demo

--- src/chrono_granular/physics/ChGranular.h
+++ src/chrono_granular/physics/ChGranular.h
@@ -11,97 +11,97 @@
 // =============================================================================
 // Authors: Dan Negrut
 // =============================================================================
-
+#include <cstddef>
 #include "../ChApiGranular.h"
 
 #pragma once
 
 /**
-* Discrete Elment info 
-*
-* Observations:
-*   - The units are not specified; they are user units. Additionally, internally, Chrono::Grnular adimensiolizes evertyhing using element characteristic size, etc.
-*
-*/
+ * Discrete Elment info
+ *
+ * Observations:
+ *   - The units are not specified; they are user units. Additionally, internally, Chrono::Grnular adimensiolizes
+ * evertyhing using element characteristic size, etc.
+ *
+ */
 namespace chrono {
 
-    enum GRN_TIME_STEPPING { AUTO, USER_SET };
+enum GRN_TIME_STEPPING { AUTO, USER_SET };
 
-    class CH_GRANULAR_API ChGRN_DE_Container {
-    protected:
-        size_t nDEs;                  ///< Number of discrete elements
-        float* pGRN_xyz_DE;
-        float* pGRN_xyzDOT_DE;
+class CH_GRANULAR_API ChGRN_DE_Container {
+  protected:
+    size_t nDEs;  ///< Number of discrete elements
+    float* pGRN_xyz_DE;
+    float* pGRN_xyzDOT_DE;
 
-        float slide_mu_kin;
-        float slide_mu_dyn;
-        GRN_TIME_STEPPING time_stepping;
+    float slide_mu_kin;
+    float slide_mu_dyn;
+    GRN_TIME_STEPPING time_stepping;
 
-    public:
+  public:
+    ChGRN_DE_Container()
+        : time_stepping(GRN_TIME_STEPPING::AUTO), nDEs(0), pGRN_xyz_DE(nullptr), pGRN_xyzDOT_DE(nullptr) {}
+    ~ChGRN_DE_Container() {
+        if (pGRN_xyz_DE != nullptr)
+            delete[] pGRN_xyz_DE;
+        if (pGRN_xyzDOT_DE != nullptr)
+            delete[] pGRN_xyzDOT_DE;
+    }
 
-        ChGRN_DE_Container() : time_stepping(GRN_TIME_STEPPING::AUTO), nDEs(0), pGRN_xyz_DE(nullptr), pGRN_xyzDOT_DE(nullptr) {}
-        ~ChGRN_DE_Container() {
-            if (pGRN_xyz_DE != nullptr)
-                delete[] pGRN_xyz_DE;
-            if (pGRN_xyzDOT_DE != nullptr)
-                delete[] pGRN_xyzDOT_DE;
-        }
+    void setup(const size_t& nElems) {
+        nDEs = nElems;
+        pGRN_xyz_DE = new float[nDEs * 3 * sizeof(float)];
+        pGRN_xyzDOT_DE = new float[nDEs * 3 * sizeof(float)];
+    }
 
-        void setup(const size_t& nElems) {
-            nDEs = nElems;
-            pGRN_xyz_DE = new float[nDEs * 3 * sizeof(float)];
-            pGRN_xyzDOT_DE = new float[nDEs * 3 * sizeof(float)];
-        }
+    inline size_t elementCount() const { return nDEs; }
+    inline float* pXYZsphereLocation() const { return pGRN_xyz_DE; }
+    inline float* pXYZsphereVelocity() const { return pGRN_xyzDOT_DE; }
+};
 
-        inline size_t elementCount() const { return nDEs; }
-        inline float* pXYZsphereLocation() const { return pGRN_xyz_DE; }
-        inline float* pXYZsphereVelocity() const { return pGRN_xyzDOT_DE; }
+/**
+ * ChGRN_DE_MONODISP_SPH_IN_BOX: Mono-disperse setup, one radius for all spheres
+ */
+class CH_GRANULAR_API ChGRN_DE_MONODISP_SPH_IN_BOX_SMC : public ChGRN_DE_Container {
+  protected:
+    //!< Reference Frame of the box
+    float sphere_radius;
+    //!< XYZ location of the center of the box
+    //!< Euler params for orientation of the box
+    float modulusYoung_SPH2SPH;
+    float modulusYoung_SPH2WALL;
 
-    };
+    unsigned int PRECISION_FACTOR_SPACE;  //!< Everthing is measured as multiples of
+                                          //!< sphere_radius/PRECISION_FACTOR_SPACE. Ex.: the radius of the sphere is
+                                          //!< 1000 units
+    unsigned int PRECISION_FACTOR_TIME;   //!< Any time quanity is measured as a multiple of
 
+  public:
+    ChGRN_DE_MONODISP_SPH_IN_BOX_SMC(float radiusSPH = 1.f, unsigned int countSPHs = 0) : ChGRN_DE_Container() {
+        this->setup(countSPHs);
+    }
 
-    /**
-    * ChGRN_DE_MONODISP_SPH_IN_BOX: Mono-disperse setup, one radius for all spheres
-    */
-    class CH_GRANULAR_API ChGRN_DE_MONODISP_SPH_IN_BOX_SMC: public ChGRN_DE_Container {
-    protected:
-        //!< Reference Frame of the box
-        float sphere_radius;
-        //!< XYZ location of the center of the box
-        //!< Euler params for orientation of the box
-        float modulusYoung_SPH2SPH;
-        float modulusYoung_SPH2WALL;
+    ~ChGRN_DE_MONODISP_SPH_IN_BOX_SMC() {}
 
-        unsigned int PRECISION_FACTOR_SPACE; //!< Everthing is measured as multiples of sphere_radius/PRECISION_FACTOR_SPACE. Ex.: the radius of the sphere is 1000 units
-        unsigned int PRECISION_FACTOR_TIME;  //!< Any time quanity is measured as a multiple of 
+    virtual void settle(float t_end) = 0;
+    void setBOXdims(float xDIM, float yDIM, float zDIM) {}  /// Set unimplemented for now
+    inline void YoungModulus_SPH2SPH(float someValue) { modulusYoung_SPH2SPH = someValue; }
+    inline void YoungModulus_SPH2WALL(float someValue) { modulusYoung_SPH2WALL = someValue; }
+};
 
-    public:
-        ChGRN_DE_MONODISP_SPH_IN_BOX_SMC(float radiusSPH = 1.f, unsigned int countSPHs = 0) : ChGRN_DE_Container(){
-            this->setup(countSPHs);
-        }
+/**
+ * ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC: Mono-disperse setup, one radius for all spheres. There is no friction,
+ * which means that there is no need to keep data that stores history for contacts
+ */
+class CH_GRANULAR_API ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC : public ChGRN_DE_MONODISP_SPH_IN_BOX_SMC {
+  protected:
+  public:
+    ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC(float radiusSPH = 1.f, unsigned int countSPHs = 0)
+        : ChGRN_DE_MONODISP_SPH_IN_BOX_SMC(radiusSPH, countSPHs) {}
 
-        ~ChGRN_DE_MONODISP_SPH_IN_BOX_SMC(){}
+    ~ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC() {}
 
-        virtual void settle(float t_end) = 0;
-        void setBOXdims(float xDIM, float yDIM, float zDIM);
-        inline void YoungModulus_SPH2SPH (float someValue) { modulusYoung_SPH2SPH  = someValue; }
-        inline void YoungModulus_SPH2WALL(float someValue) { modulusYoung_SPH2WALL = someValue; }
+    virtual void settle(float t_end);
+};
 
-    };
-
-    /**
-    * ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC: Mono-disperse setup, one radius for all spheres. There is no friction,
-    * which means that there is no need to keep data that stores history for contacts
-    */
-    class CH_GRANULAR_API ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC : public ChGRN_DE_MONODISP_SPH_IN_BOX_SMC {
-    protected:
-
-    public:
-        ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC(float radiusSPH = 1.f, unsigned int countSPHs = 0) : ChGRN_DE_MONODISP_SPH_IN_BOX_SMC(radiusSPH, countSPHs){}
-
-        ~ChGRN_DE_MONODISP_SPH_IN_BOX_NOFRIC_SMC(){}
-
-        virtual void settle(float t_end);
-    };
-
-}
+}  // namespace chrono
--- src/demos/granular/demo_GRAN_PoiseuilleCylSMC.cpp
+++ src/demos/granular/demo_GRAN_PoiseuilleCylSMC.cpp
@@ -24,8 +24,9 @@
 // generated for post-processing with POV-Ray.
 // =============================================================================
 
-#include<iostream>
+#include <iostream>
 #include "chrono/core/ChTimer.h"
+#include "chrono/utils/ChUtilsGenerators.h"
 #include "chrono_granular/physics/ChSettingsGranularSMC.h"
 
 #ifdef CHRONO_OPENGL
@@ -39,20 +40,57 @@ double time_step = 1e-5;
 double time_end = 6.;
 std::string output_prefix = "poiseuilleCyl";
 
-
 // Cylinder radius
 float r1 = 1;
 
-// Material properties 
+// Material properties
 float Y = 2e5f;
 float wallY = 1e7f;
 
-double ball_radius = 0.25;
-
+double ball_radius = 0.1;
 
 // -----------------------------------------------------------------------------
 // FOR ALL PURPOSES, THIS IS A STUB.
 // -----------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
+    ChSystemSMC my_system;
+    // Create the falling balls
+    float ball_epsilon = .005;  // Margine between balls to ensure no overlap / DEM-splosion
+    // Add epsilon
+    utils::HCPSampler<float> sampler((2 + ball_epsilon) * ball_radius);
+
+    // 2-wide box centered at origin
+    ChVector<float> parCenter(0, 0, 0);
+    ChVector<float> hdims{1, 1, 1};
+    // This is a disgustingly-aliased std::vector<ChVector<float>>
+    std::vector<ChVector<float>> points = sampler.SampleBox(parCenter, hdims);  // Vector of points
+    for (unsigned int i = 0; i < points.size(); i++) {
+        // safe to use index operator since we know we'll be in bounds
+        // printf("Point %d is at (%f, %f, %f)\n", i, points[i].x(), points[i].y(), points[i].z());
+        std::shared_ptr<ChBody> ball(my_system.NewBody());
+        ball->SetPos(points[i]);
+
+        // ball->SetBodyFixed(true);
+        ball->GetCollisionModel()->ClearModel();
+        utils::AddSphereGeometry(ball.get(), ball_radius);
+        ball->GetCollisionModel()->BuildModel();
+        my_system.AddBody(ball);
+    }
+    my_system.Set_G_acc({0, 0, -9.8});
+
+#if defined(CHRONO_OPENGL)
+    opengl::ChOpenGLWindow& gl_window = opengl::ChOpenGLWindow::getInstance();
+    gl_window.Initialize(1280, 720, "ballsNSC", &my_system);
+    gl_window.SetCamera(ChVector<>(0, -6, 0), ChVector<>(0, 0, 0), ChVector<>(0, 0, 1));
+    gl_window.SetRenderMode(opengl::WIREFRAME);
+    while (true) {
+        if (gl_window.Active()) {
+            gl_window.DoStepDynamics(time_step);
+            gl_window.Render();
+        } else {
+            break;
+        }
+    }
+#endif
     return 0;
 }
