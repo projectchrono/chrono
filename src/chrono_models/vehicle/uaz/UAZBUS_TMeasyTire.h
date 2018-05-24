158f2c38ddf1dbd3f43967daf146f925af6b7286
Radu Serban <serban@wisc.edu>
2018-05-24 12:18:17 +0200

    Add vehicle model UAZBUS (full van) and associated demo
    (contributed by Rainer Gericke)

---
+++ src/chrono_models/vehicle/uaz/UAZBUS_TMeasyTire.h
@@ -0,0 +1,96 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Rainer Gericke
+// =============================================================================
+//
+// UAZBUS TMeasy tire subsystem
+//
+// Updated: 2018-02-24
+// =============================================================================
+
+#ifndef UAZBUS_TMEASY_TIRE_H
+#define UAZBUS_TMEASY_TIRE_H
+
+#include "chrono/assets/ChTriangleMeshShape.h"
+
+#include "chrono_vehicle/wheeled_vehicle/tire/ChTMeasyTire.h"
+
+#include "chrono_models/ChApiModels.h"
+
+namespace chrono {
+namespace vehicle {
+namespace uaz {
+
+/// @addtogroup vehicle_models_uaz
+/// @{
+
+/// TMeasy tire model for the UAZBUS (front)
+class CH_MODELS_API UAZBUS_TMeasyTireFront : public ChTMeasyTire {
+  public:
+    UAZBUS_TMeasyTireFront(const std::string& name);
+    ~UAZBUS_TMeasyTireFront() {}
+
+    virtual double GetVisualizationWidth() const override { return m_width; }
+
+    virtual void SetTMeasyParams() override;
+    virtual double GetMass() const override { return m_mass; }
+    virtual ChVector<> GetInertia() const override { return m_inertia; }
+
+    virtual void AddVisualizationAssets(VisualizationType vis) override;
+    virtual void RemoveVisualizationAssets() override final;
+
+    void GenerateCharacteristicPlots(const std::string& dirname);
+
+  private:
+    static const std::string m_meshName;
+    static const std::string m_meshFile;
+    static const double m_mass;
+    static const ChVector<> m_inertia;
+
+    std::shared_ptr<ChTriangleMeshShape> m_trimesh_shape;
+    ChFunction_Recorder m_stiffnessMap;
+};
+
+/// TMeasy tire model for the UAZBUS (front)
+class CH_MODELS_API UAZBUS_TMeasyTireRear : public ChTMeasyTire {
+  public:
+    UAZBUS_TMeasyTireRear(const std::string& name);
+    ~UAZBUS_TMeasyTireRear() {}
+
+    virtual double GetVisualizationWidth() const override { return m_width; }
+
+    virtual void SetTMeasyParams() override;
+    virtual double GetMass() const override { return m_mass; }
+    virtual ChVector<> GetInertia() const override { return m_inertia; }
+
+    virtual void AddVisualizationAssets(VisualizationType vis) override;
+    virtual void RemoveVisualizationAssets() override final;
+
+    void GenerateCharacteristicPlots(const std::string& dirname);
+
+  private:
+    static const std::string m_meshName;
+    static const std::string m_meshFile;
+    static const double m_mass;
+    static const ChVector<> m_inertia;
+
+    std::shared_ptr<ChTriangleMeshShape> m_trimesh_shape;
+    ChFunction_Recorder m_stiffnessMap;
+};
+
+/// @} vehicle_models_uaz
+
+}  // end namespace uaz
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
---
+++ src/chrono_models/vehicle/uaz/UAZBUS_Wheel.cpp
@@ -0,0 +1,82 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Justin Madsen
+// =============================================================================
+//
+// UAZBUS wheel subsystem
+//
+// =============================================================================
+
+#include <algorithm>
+
+#include "chrono_vehicle/ChVehicleModelData.h"
+
+#include "chrono_models/vehicle/uaz/UAZBUS_Wheel.h"
+
+namespace chrono {
+namespace vehicle {
+namespace uaz {
+
+// -----------------------------------------------------------------------------
+// Static variables
+// -----------------------------------------------------------------------------
+
+const double UAZBUS_Wheel::m_mass = 12.0;
+const ChVector<> UAZBUS_Wheel::m_inertia(0.240642, 0.410903, 0.240642);
+
+const double UAZBUS_Wheel::m_radius = 0.2032;
+const double UAZBUS_Wheel::m_width = 0.1524;
+
+const std::string UAZBUS_WheelLeft::m_meshName = "wheel_L_POV_geom";
+const std::string UAZBUS_WheelLeft::m_meshFile = "uaz/wheel_L.obj";
+
+const std::string UAZBUS_WheelRight::m_meshName = "wheel_R_POV_geom";
+const std::string UAZBUS_WheelRight::m_meshFile = "uaz/wheel_R.obj";
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+UAZBUS_Wheel::UAZBUS_Wheel(const std::string& name) : ChWheel(name) {}
+
+UAZBUS_WheelLeft::UAZBUS_WheelLeft(const std::string& name) : UAZBUS_Wheel(name) {}
+
+UAZBUS_WheelRight::UAZBUS_WheelRight(const std::string& name) : UAZBUS_Wheel(name) {}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+void UAZBUS_Wheel::AddVisualizationAssets(VisualizationType vis) {
+    if (vis == VisualizationType::MESH) {
+        geometry::ChTriangleMeshConnected trimesh;
+        trimesh.LoadWavefrontMesh(GetMeshFile(), false, false);
+        m_trimesh_shape = std::make_shared<ChTriangleMeshShape>();
+        m_trimesh_shape->SetMesh(trimesh);
+        m_trimesh_shape->SetName(GetMeshName());
+        m_trimesh_shape->SetStatic(true);
+        m_spindle->AddAsset(m_trimesh_shape);
+    } else {
+        ChWheel::AddVisualizationAssets(vis);
+    }
+}
+
+void UAZBUS_Wheel::RemoveVisualizationAssets() {
+    ChWheel::RemoveVisualizationAssets();
+
+    // Make sure we only remove the assets added by UAZBUS_Wheel::AddVisualizationAssets.
+    // This is important for the ChWheel object because a tire may add its own assets
+    // to the same body (the spindle).
+    auto it = std::find(m_spindle->GetAssets().begin(), m_spindle->GetAssets().end(), m_trimesh_shape);
+    if (it != m_spindle->GetAssets().end())
+        m_spindle->GetAssets().erase(it);
+}
+
+}  // end namespace uaz
+}  // end namespace vehicle
+}  // end namespace chrono
