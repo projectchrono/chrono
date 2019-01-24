dbbd274479558071574e0014b4b4255fb3b4edac
Feller Christian <cfeller@danfoss.com>
2019-01-24 17:03:33 +0100

    Modified contact point calculation to improve handling of (constant) terrain slopes.

--- src/chrono_vehicle/wheeled_vehicle/ChTire.cpp
+++ src/chrono_vehicle/wheeled_vehicle/ChTire.cpp
@@ -96,6 +96,10 @@ void ChTire::CalculateKinematics(double time, const WheelState& state, const ChT
 // This function returns false if no contact occurs. Otherwise, it sets the
 // contact points on the disc (ptD) and on the terrain (ptT), the normal contact
 // direction, and the resulting penetration depth (a positive value).
+//
+// NOTE: uses terrain normal at disc center for approximative calculation. 
+// Hence only valid for terrains with constant slope. A completely accurate 
+// solution would require an iterative calculation of the contact point.
 // -----------------------------------------------------------------------------
 bool ChTire::disc_terrain_contact(const ChTerrain& terrain,
                                   const ChVector<>& disc_center,
@@ -111,7 +115,8 @@ bool ChTire::disc_terrain_contact(const ChTerrain& terrain,
 
     // Find the lowest point on the disc. There is no contact if the disc is
     // (almost) horizontal.
-    ChVector<> dir1 = Vcross(disc_normal, ChVector<>(0, 0, 1));
+	ChVector<> nhelp = terrain.GetNormal(disc_center.x(), disc_center.y());
+    ChVector<> dir1 = Vcross(disc_normal, nhelp); 
     double sinTilt2 = dir1.Length2();
 
     if (sinTilt2 < 1e-3)
