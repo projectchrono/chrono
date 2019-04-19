b957c2cb584c7bd3f77039cca1ec455b4ee2a3b0
nicmolsen <nicholas.olsen@wisc.edu>
2019-04-19 10:36:46 -0500

    Improves repose angle estimate

--- scripts/repose_angle.py
+++ scripts/repose_angle.py
@@ -3,6 +3,7 @@
 import sys
 import pandas as pd
 import numpy as np
+# import matplotlib.pyplot as plt
 
 argv = sys.argv
 if len(argv) != 3:
@@ -18,20 +19,43 @@ data = pd.read_csv(infile)
 pos = np.array(data[['x','y','z']])
 
 max_i = np.argmax(pos[:,2])
-center = pos[max_i,:]
-
+top_point = pos[max_i,:]
+pos = pos[pos[:,0].argsort()]
 r_z = np.zeros((pos.shape[0],2))
+r_z[:,0] = np.sqrt((pos[:,0]-top_point[0])**2 + (pos[:,1]-top_point[1])**2)
 r_z[:,1] = pos[:,2]
-r_z[:,0] = np.sqrt((pos[:,0] - center[0])**2 + (pos[:,1] - center[1])**2)
-r_z = r_z[r_z[:,0].argsort()]
-# print(r_z)
 
-lo, hi = np.percentile(r_z[:,0], [95,100])
-third_quart = r_z[np.where(np.logical_and(lo <= r_z[:,0], r_z[:,0] <= hi)),:][0]
-mid_point_i = np.argmax(third_quart[:,1])
-mid_point = third_quart[mid_point_i,:]
+part_size = 1.0
+rmax = np.max(r_z[:,0])
+top_layer = []
+# Divide r into partitions
+# Get highest point in each partition
+# => Gives profile of top surface of mound
+# Decide where mounding starts and measure angle to top point.
+# IDEA: find highest point, then chose the point (on the top surface) that maximizes
+# the slope between it and the highest point. DANGER: don't let it choose a point
+# right next to the highest point.
+for r in np.arange(0, rmax, part_size):
+    lo = r
+    hi = r + part_size
+    section = r_z[np.where(np.logical_and(r_z[:,0] > lo, r_z[:,0] <= hi))]
+    if section.shape[0] == 0:
+        continue
+    top_layer.append(section[np.argmax(section[:,1]), :])
 
-slope = (center[2] - mid_point[1]) / mid_point[0]
-theta = np.degrees(np.arctan(slope))
+# top_layer = np.array(top_layer)
+# plt.figure()
+# plt.scatter(top_layer[:,0], top_layer[:,1])
+# plt.xlabel('r')
+# plt.ylabel('z')
+# plt.axis('equal')
+# plt.show()
 
+max_slope = 0.0
+for point in top_layer:
+    slope = (top_point[2] - point[1]) / point[0]
+    if slope > max_slope:
+        max_slope = slope
+
+theta = np.degrees(np.arctan(max_slope))
 print(",".join([t[0], t[1], t[2], str(theta)]))
