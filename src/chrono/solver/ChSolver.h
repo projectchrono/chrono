170ce33c696010e78bba47ceacc6986f9cf56b44
Radu Serban <serban@wisc.edu>
2019-12-15 17:19:09 +0100

    Implement two new sparse direct solvers: SparseLU and SparseQR.
    - these solvers use the corresponding built-in Eigen solvers, so they provide direct sparse solvers without the need of a third-party library (MKL or Mumps).
    - currently, a matrix copy is performed (to change storage order from row-major to column-major, as expected by the SparseLU and SparseQR solvers).

--- src/chrono/solver/ChSolver.h
+++ src/chrono/solver/ChSolver.h
@@ -42,8 +42,10 @@ class ChApi ChSolver {
         BARZILAIBORWEIN,  ///< Barzilai-Borwein
         APGD,             ///< Accelerated Projected Gradient Descent
         // Direct linear solvers
-        PARDISO,  ///< Pardiso (super-nodal sparse direct solver)
-        MUMPS,    ///< Mumps (MUltifrontal Massively Parallel sparse direct Solver)
+        SPARSE_LU,  ///< Sparse supernodal LU factorization
+        SPARSE_QR,  ///< Sparse left-looking rank-revealing QR factorization
+        PARDISO,    ///< Pardiso (super-nodal sparse direct solver)
+        MUMPS,      ///< Mumps (MUltifrontal Massively Parallel sparse direct Solver)
         // Iterative linear solvers
         GMRES,     ///< Generalized Minimal RESidual Algorithm
         MINRES,    ///< MINimum RESidual method
