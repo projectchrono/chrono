c0c0699f3cd98a41efcbc0988ff6d299c85c12d4
Cecily SUNDAY <c.sunday@ad.isae-supaero.fr>
2020-01-07 18:00:15 +0100

    Remove code lines that result in an incorrect coefficient of restitution

--- src/chrono_parallel/solver/ChIterativeSolverParallelSMC.cpp
+++ src/chrono_parallel/solver/ChIterativeSolverParallelSMC.cpp
@@ -307,8 +307,6 @@ void function_CalcContactForces(
 
             {
                 real forceN_mag = kn * delta_n - gn * relvel_n_mag;
-                if (forceN_mag < 0)
-                    forceN_mag = 0;
                 real forceT_mag = mu_eff * Tanh(5.0 * relvel_t_mag) * forceN_mag;
                 
 				// Accumulate normal and tangential forces
@@ -350,18 +348,6 @@ void function_CalcContactForces(
     real forceN_mag = kn * delta_n - gn * relvel_n_mag;
     real3 forceT_stiff = kt * delta_t;
     real3 forceT_damp = gt * relvel_t;
-
-    // If the resulting normal force is negative, then the two shapes are
-    // moving away from each other so fast that no contact force is generated.
-    if (forceN_mag < 0) {
-        forceN_mag = 0;
-        forceT_stiff.x = 0;
-        forceT_stiff.y = 0;
-        forceT_stiff.z = 0;
-        forceT_damp.x = 0;
-        forceT_damp.y = 0;
-        forceT_damp.z = 0;
-    }
 
     // Apply Coulomb friction law.
     // We must enforce force_T_mag <= mu_eff * |forceN_mag|.
