9bec87efc49984bd2d42476b205a5bf7574a46bb
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-22 11:26:51 +0100

    Added pychrono conda  python 3.8 deployment on gitlab-ci

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -11,9 +11,12 @@ call conda install jinja2 --yes
 call conda install ninja --yes
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 Rem CMAKE config output is redirected to a file otherwise it gets truncated due to depth
+call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
+call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\win-64\pychrono*.bz2 --force --label develop  >> "%LOG_DIR%"\condauploadlog.txt 2>&1
+call conda build purge  timeout /t 240
 call conda build .\contrib\packaging-python\conda --python=3.7 --no-remove-work-dir --dirty
 call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\win-64\pychrono*.bz2 --force --label develop  >> "%LOG_DIR%"\condauploadlog.txt 2>&1
 call conda build purge  timeout /t 240
-call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
+call conda build .\contrib\packaging-python\conda --python=3.8 --no-remove-work-dir --dirty
 call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\win-64\pychrono*.bz2 --force --label develop  >> "%LOG_DIR%"\condauploadlog.txt 2>&1
 echo End Reached
