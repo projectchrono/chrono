ec7d9a48667239d424de0e9a44989c2a0c867ec6
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-23 12:41:24 +0100

    Manually add libopenmp

--- contrib/packaging-python/conda/bld.bat
+++ contrib/packaging-python/conda/bld.bat
@@ -37,6 +37,7 @@ cmake -G "Ninja" ^
  -DENABLE_MODULE_MKL=ON ^
  -DMKL_INCLUDE_DIR="%CONDA_INSTALL_LOCN%"\Library\include ^
  -DMKL_RT_LIBRARY="%CONDA_INSTALL_LOCN%"\Library\lib\mkl_rt.lib ^
+ -DIOMP5_LIBRARY="%CONDA_INSTALL_LOCN%"\Library\lib\libiomp5md.lib ^
  -DPYCHRONO_DATA_PATH="..\..\..\..\Library\data" ^
  .. >> "%LOG_DIR%"\cmakeconfiglog.txt 2>&1
 if errorlevel 1 exit 1
