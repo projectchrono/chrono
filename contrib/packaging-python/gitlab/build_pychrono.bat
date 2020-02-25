eb614fc23b7ca5bf1d7c597760e62439c920d330
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-25 16:15:18 +0100

    Use purge-all to get rid of old packages

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -2,7 +2,6 @@
 echo Process begins
 call "%CONDA_INSTALL_LOCN%"\Scripts\activate.bat
 call conda install --yes conda-build anaconda-client
-call conda build purge  timeout /t 240
 call conda install --yes -c intel mkl-devel
 call conda install -c conda-forge swig=4.0 --yes
 call conda install mkl --yes
@@ -11,9 +10,10 @@ call conda install jinja2 --yes
 call conda install ninja --yes
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 Rem CMAKE config output is redirected to a file otherwise it gets truncated due to depth
+call conda build purge-all timeout /t 240
 call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
 call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\win-64\pychrono*.bz2 --force --label develop  >> "%LOG_DIR%"\condauploadlog.txt 2>&1
-call conda build purge  timeout /t 240
+call conda build purge-all  timeout /t 240
 call conda build .\contrib\packaging-python\conda --python=3.7 --no-remove-work-dir --dirty
 call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\win-64\pychrono*.bz2 --force --label develop  >> "%LOG_DIR%"\condauploadlog.txt 2>&1
 Rem call conda build purge  timeout /t 240
