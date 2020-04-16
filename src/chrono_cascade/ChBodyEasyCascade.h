98c1d4b2f352ec578e5d1176f9cf2f8f75687d97
tasora <tasora@ied.unipr.it>
2020-04-16 12:52:03 +0200

    New feature: each ChBodyEasyCascade can have multiple 2D profiles, using AddProfile, so you can create, say, stacked cams. it is enough that the defining ChLinePath have different Z values. They still must be parallel to XY of course.

--- src/chrono_cascade/ChBodyEasyCascade.h
+++ src/chrono_cascade/ChBodyEasyCascade.h
@@ -24,6 +24,7 @@
 
 #include <TopExp_Explorer.hxx>
 #include <TopoDS_Wire.hxx>
+#include <TopoDS_Builder.hxx>
 #include <BRepBuilderAPI_MakeWire.hxx>
 #include <BRepBuilderAPI_MakeEdge.hxx>
 #include <BRepBuilderAPI_MakeFace.hxx>
@@ -168,17 +169,20 @@ class ChBodyEasyCascade : public ChBodyAuxRef {
 
 /// Easy-to-use class for quick creation of flat "2D" rigid bodies given a 2D 'wire' profile and
 /// a thickness. These bodies are meant for 2D problems with optimized contact between 2D profiles.
+/// Profiles must be defined in the REF local reference of the body and must be parallel to XY plane, 
+/// then the thickness is extruded in the positive Y direction.
 /// Compared to the base ChBodyAuxRef class, this class also does
 /// automatically, at object creation, the following tasks that
 /// you would normally do by hand if using ChBodyAuxRef:
 /// - mass and moment of inertia is automatically set, according to the wire profile and 
-///   thickness of the shape. The profile can contain holes. 
+///   thickness of the shape. The profile can contain holes.
 /// - the COG (center of mass) of the body is automatically moved at the barycenter, 
 ///   the REF (reference) is automatically moved where the Cascade TopoDS_Face has the reference (if passing a face, otherwise in 0,0,0)
 /// - a visualization shape is created and added, if visualization asset is desired
 /// - a collision shape is created and added, if collision is desired, and such collision shape is
-///   made with 2D arcs and 2D lines in the profile, optimized for the 2D-2D contact case.  
-
+///   made with 2D arcs and 2D lines in the profile, optimized for the 2D-2D contact case. 
+///   Note that the collision shape is a 2D profile placed at half thickness of the extruded profile, 
+///   here is the slice where contacts will happen. 
 
 class ChBodyEasyCascadeProfile : public ChBodyAuxRef {
 public:
@@ -191,33 +195,178 @@ public:
 		std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> mholes, ///< profiles of holes, in XY plane, if any. Each must be closed paths. Must not intersecate.
 		double mthickness,				///< thickness in Z direction
 		double mdensity,				///< density
-		const ChCascadeTriangulate& visualization, ///< pass ChCascadeTriangulateTolerances if need visualization, pass a ChCascadeTriangulateNone if no visualization needed.
-		bool collide = false,			///< if true, add a 2D collision shape that uses the outer profile of the face - only arcs and linear edges are considered. 
+		const ChCascadeTriangulate& mvisualization, ///< pass ChCascadeTriangulateTolerances if need visualization, pass a ChCascadeTriangulateNone if no visualization needed.
+		bool mcollide = false,			///< if true, add a 2D collision shape that uses the outer profile of the face - only arcs and linear edges are considered. 
 		std::shared_ptr<ChMaterialSurface> mat = nullptr  ///< surface contact material if colliding
 	)
 	{
-		this->wires = mwires;
-		this->holes = mholes;
-		this->thickness = mthickness;
-		this->density = mdensity;
+		this->AddProfile(mwires, mholes, mthickness, mdensity, mvisualization, mcollide, mat);
+	}
 
-		this->Init(visualization, collide, mat);
-	 }
+	/// If multiple profiles on different Z dephts are needed, after the ChBodyEasyCascadeProfile constructor is executed with the first profile,
+	/// you can use this function to add further profiles.
+	/// Note that the additional profiles should be at different Z depths, and not intersecting along Z distance, because no boolean 'join' operation is done and
+	/// in case they overlap by some amount, the computation of inertia and mass would be overestimated (i.e. each extruded profile is considered separately).
+	void AddProfile(
+		std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> mwires, ///< profile of face, in XY plane. Usually just one, but might be N if disconnected like leopard dots. Each must be closed.
+		std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> mholes, ///< profiles of holes, in XY plane, if any. Each must be closed paths. Must not intersecate.
+		double mthickness,				///< thickness in Z direction
+		double mdensity,				///< density
+		const ChCascadeTriangulate& visualization, ///< pass ChCascadeTriangulateTolerances if need visualization, pass a ChCascadeTriangulateNone if no visualization needed.
+		bool mcollide = false,			///< if true, add a 2D collision shape that uses the outer profile of the face - only arcs and linear edges are considered. 
+		std::shared_ptr<ChMaterialSurface> mat = nullptr  ///< surface contact material if colliding
+	) {
+		ChCascadeExtrusionFace mface;
+		mface.wires = mwires;
+		mface.holes = mholes;
+		mface.thickness = mthickness;
+		mface.density = mdensity;
+		mface.collide = mcollide;
+		mface.material = mat;
+		this->faces.push_back(mface);
+		this->UpdateCollisionAndVisualizationShapes(visualization);
+	}
 
+	void ClearProfiles() {
+		this->faces.clear();
+		this->UpdateCollisionAndVisualizationShapes(ChCascadeTriangulateNone());
+	}
+
+	  /// This function 1) generates the visualizer asset shapes for the part and 2) adds the proper collision shapes 
+	  /// as 2D profiles optimized for 2D-2D contact. 
+	  /// This is already called automatically when construction the ChBodyEasyCascadeProfile and each time you 
+	  /// call AddProfile(), but you might want to call this by hand if you ever change the profile coordinates, arc 
+	  /// radii etc. after you created the shape and you want to rebuild visualization and collision shapes.
+	  void UpdateCollisionAndVisualizationShapes(const ChCascadeTriangulate& visualization ///< pass ChCascadeTriangulateTolerances if need visualization, pass a ChCascadeTriangulateNone if no visualization needed.
+	  ) {
+
+		  TopoDS_Compound mcompound;
+		  TopoDS_Builder builder;
+		  builder.MakeCompound(mcompound);
+
+
+		  for (auto& chface : this->faces) {
+			  BRepBuilderAPI_MakeFace facebuilder(FromChronoPathToCascadeWire(chface.wires[0]));
+
+			  bool first = true;
+			  for (auto profile : chface.wires) {
+				  if (first) {
+					  first = false;
+					  continue;  // first already added in BRepBuilderAPI_MakeFace constructor
+				  }
+				  auto cascade_wire = FromChronoPathToCascadeWire(profile);
+				  facebuilder.Add(cascade_wire);
+			  }
+			  for (auto profilehole : chface.holes) {
+				  auto cascade_wire_hole = FromChronoPathToCascadeWire(profilehole);
+				  cascade_wire_hole.Reverse();
+				  facebuilder.Add(cascade_wire_hole);
+			  }
+
+			  gp_Pnt starting_point(0., 0., 0.);
+			  gp_Pnt end_point(0., 0., chface.thickness);
+			  gp_Vec vec(starting_point, end_point);
+
+			  auto prism = BRepPrimAPI_MakePrism(facebuilder.Face(), vec).Shape();
+
+			  // Add a visualization asset if needed
+			  if (const ChCascadeTriangulateTolerances * tolerances =
+				  dynamic_cast<const ChCascadeTriangulateTolerances*>(&visualization)) {
+				  auto trimesh = chrono_types::make_shared<geometry::ChTriangleMeshConnected>();
+				  ChCascadeMeshTools::fillTriangleMeshFromCascade(*trimesh, prism, *tolerances);
+
+				  auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
+				  trimesh_shape->SetMesh(trimesh);
+				  this->AddAsset(trimesh_shape);
+			  }
+
+			  builder.Add(mcompound, prism);
+		  }
+
+		  chrono::ChFrame<>* user_ref_to_abs = 0;  // as parameter?
+		  chrono::ChFrame<> frame_ref_to_abs;
+
+		  if (!user_ref_to_abs) {
+			  TopLoc_Location loc_shape_to_abs = mcompound.Location();
+			  chrono::cascade::ChCascadeDoc::FromCascadeToChrono(loc_shape_to_abs, frame_ref_to_abs);
+		  }
+		  else {
+			  frame_ref_to_abs = *user_ref_to_abs;
+		  }
+
+		  // Reset shape location to local ref csys (identity).
+		  this->topods_shape = mcompound;
+		  this->topods_shape.Location(TopLoc_Location());
+
+		  // compute mass properties and COG reference
+		  chrono::ChVector<> mcog;
+		  chrono::ChVector<> minertiaXX;
+		  chrono::ChVector<> minertiaXY;
+		  double mvol;
+		  double mmass;
+		  chrono::cascade::ChCascadeDoc::GetVolumeProperties(topods_shape, density, mcog, minertiaXX, minertiaXY, mvol,
+			  mmass);
+
+		  // Set mass and COG and REF references
+		  this->SetDensity((float)density);
+		  this->SetMass(mmass);
+		  this->SetInertiaXX(minertiaXX);
+		  this->SetInertiaXY(minertiaXY);
+		  //this->SetFrame_REF_to_abs(frame_ref_to_abs);
+
+		  chrono::ChFrame<> frame_cog_to_ref;
+		  frame_cog_to_ref.SetPos(mcog);
+		  frame_cog_to_ref.SetRot(chrono::QUNIT);
+		  this->SetFrame_COG_to_REF(frame_cog_to_ref);
+
+
+
+		  // Add a collision shape if needed
+
+		  GetCollisionModel()->ClearModel();
+		  bool somefacecollide = false;
+
+		  for (auto& chface : this->faces) {
+			  if (chface.collide) {
+				  assert(chface.material);
+				  for (auto mpath : chface.wires) {
+					  ChVector<> pathposz; mpath->Evaluate(pathposz, 0.0); // for offset along Z
+					  GetCollisionModel()->Add2Dpath(chface.material, mpath, ChVector<>(0,0,pathposz.z() + chface.thickness*0.5), ChMatrix33<>(1), chface.thickness*0.99);
+				  }
+				  for (auto mhole : chface.holes) {
+					  ChVector<> pathposz; mhole->Evaluate(pathposz, 0.0); // for offset along Z
+					  GetCollisionModel()->Add2Dpath(chface.material, mhole, ChVector<>(0,0,pathposz.z() + chface.thickness*0.5), ChMatrix33<>(1), chface.thickness*0.99);
+				  }
+				  somefacecollide = true;
+			  }
+		  }
+		  GetCollisionModel()->BuildModel();
+		  if (somefacecollide)
+			  SetCollide(true);
+		  else
+			  SetCollide(false);
+	  
+      }
 
   private:
 
-	  std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> wires;
-	  std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> holes;
-	  double thickness;
-	  double density;
+	  class ChCascadeExtrusionFace {
+	  public:
+		  std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> wires;
+		  std::vector<std::shared_ptr<::chrono::geometry::ChLinePath>> holes;
+		  double thickness;
+		  double density;
+		  bool collide;
+		  std::shared_ptr<ChMaterialSurface> material;
+	  };
+	  std::vector<ChCascadeExtrusionFace> faces;
 
 	  const TopoDS_Wire FromChronoPathToCascadeWire(std::shared_ptr<::chrono::geometry::ChLinePath> profile) {
 		  BRepBuilderAPI_MakeWire mwirebuilder;
 		  for (size_t i = 0; i < profile->GetSubLinesCount(); ++i) {
 			  if (auto msegment = std::dynamic_pointer_cast<::chrono::geometry::ChLineSegment>(profile->GetSubLineN(i))) {
-				  if ((msegment->pA.z() != 0) || (msegment->pB.z() != 0))
-					  throw ChException("Error! ChBodyEasyCascadeProfile: sub segment of ChLinePath has non-zero z coordinate!");
+				  if (msegment->pA.z() != msegment->pB.z())
+				    throw ChException("Error! ChBodyEasyCascadeProfile: sub segment of ChLinePath not parallel to XY plane!");
 
 				  gp_Pnt aPntA(msegment->pA.x(), msegment->pA.y(), msegment->pA.z());
 				  gp_Pnt aPntB(msegment->pB.x(), msegment->pB.y(), msegment->pB.z());
@@ -229,8 +378,8 @@ public:
 
 			  }
 			  else if (auto marc = std::dynamic_pointer_cast<::chrono::geometry::ChLineArc>(profile->GetSubLineN(i))) {
-				  if ((marc->origin.pos.z() != 0))
-					  throw ChException("Error! ChBodyEasyCascadeProfile: a sub arc of ChLinePath has center with non-zero z coordinate!");
+				  if ((marc->origin.rot.e1() != 0) ||  (marc->origin.rot.e2() != 0))
+				    throw ChException("Error! ChBodyEasyCascadeProfile: a sub arc of ChLinePath not parallel to XY plane!");
 
 				  gp_Circ aCirc(gp_Ax2(gp_Pnt(marc->origin.pos.x(), marc->origin.pos.y(), marc->origin.pos.z()), gp_Dir(0., 0., 1.)), marc->radius);
 
@@ -262,99 +411,10 @@ public:
 	  }
 
 	 
-	 void Init(		  const ChCascadeTriangulate& visualization, ///< pass ChCascadeTriangulateTolerances if need visualization, pass a ChCascadeTriangulateNone if no visualization needed.
-                      bool collide = false,  ///< if true, add a collision shape that uses the triangulation of shape
-                      std::shared_ptr<ChMaterialSurface> mat = nullptr  ///< surface contact material
-    ) {
-
-		BRepBuilderAPI_MakeFace facebuilder(FromChronoPathToCascadeWire(wires[0]));
-
-		bool first = true;
-		for (auto profile  : wires) {
-			if (first) {
-				first = false; continue; // first already added in BRepBuilderAPI_MakeFace constructor
-			}
-			auto cascade_wire = FromChronoPathToCascadeWire(profile);
-			facebuilder.Add(cascade_wire);
-		}
-		for (auto profilehole : holes) {
-			auto cascade_wire_hole = FromChronoPathToCascadeWire(profilehole);
-			cascade_wire_hole.Reverse();
-			facebuilder.Add(cascade_wire_hole);
-		}
-
-		gp_Pnt starting_point(0., 0., 0.);
-		gp_Pnt end_point(0., 0., thickness);
-		gp_Vec vec(starting_point, end_point);
-
-		auto prism = BRepPrimAPI_MakePrism(facebuilder.Face(), vec).Shape();
-
-
-        chrono::ChFrame<>* user_ref_to_abs = 0;  // as parameter?
-        chrono::ChFrame<> frame_ref_to_abs;
-
-        if (!user_ref_to_abs) {
-            TopLoc_Location loc_shape_to_abs = prism.Location();
-            chrono::cascade::ChCascadeDoc::FromCascadeToChrono(loc_shape_to_abs, frame_ref_to_abs);
-        } else {
-            frame_ref_to_abs = *user_ref_to_abs;
-        }
-
-        // Reset shape location to local ref csys (identity).
-        this->topods_shape = prism;
-        this->topods_shape.Location(TopLoc_Location());
-
-        // compute mass properties and COG reference
-        chrono::ChVector<> mcog;
-        chrono::ChVector<> minertiaXX;
-        chrono::ChVector<> minertiaXY;
-        double mvol;
-        double mmass;
-        chrono::cascade::ChCascadeDoc::GetVolumeProperties(topods_shape, density, mcog, minertiaXX, minertiaXY, mvol,
-                                                           mmass);
-
-        // Set mass and COG and REF references
-        this->SetDensity((float)density);
-        this->SetMass(mmass);
-        this->SetInertiaXX(minertiaXX);
-        this->SetInertiaXY(minertiaXY);
-        this->SetFrame_REF_to_abs(frame_ref_to_abs);
-
-        chrono::ChFrame<> frame_cog_to_ref;
-        frame_cog_to_ref.SetPos(mcog);
-        frame_cog_to_ref.SetRot(chrono::QUNIT);
-        this->SetFrame_COG_to_REF(frame_cog_to_ref);
-
-
-        // Add a visualization asset if needed
-		if (const ChCascadeTriangulateTolerances * tolerances = dynamic_cast<const ChCascadeTriangulateTolerances*>(&visualization))
-		{
-			auto trimesh = chrono_types::make_shared<geometry::ChTriangleMeshConnected>();
-			ChCascadeMeshTools::fillTriangleMeshFromCascade(*trimesh, topods_shape, *tolerances);
-
-			auto trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
-			trimesh_shape->SetMesh(trimesh);
-			this->AddAsset(trimesh_shape);
-		}
-
-		if (collide){
-            // Add a collision shape if needed
-             assert(mat);
-             GetCollisionModel()->ClearModel();
-			 for (auto mpath : this->wires) {
-				 GetCollisionModel()->Add2Dpath(mat, mpath, VNULL, ChMatrix33<>(1), this->thickness); 
-			 }
-			 for (auto mhole : this->holes) {
-				 GetCollisionModel()->Add2Dpath(mat, mhole, VNULL, ChMatrix33<>(1), this->thickness); 
-			 }
-             GetCollisionModel()->BuildModel();
-             SetCollide(true);
-		}
-    }
 
   public:
-    // Store the Cascade shape here, with null transformation relative to the REF
-    TopoDS_Shape topods_shape;
+      // Store the Cascade shape here, with null transformation relative to the REF
+      TopoDS_Shape topods_shape;
 
 };
 
