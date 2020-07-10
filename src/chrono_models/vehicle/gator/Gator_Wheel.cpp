c7fbf09a6d9555d1c5baf6a1331fad875565cbc6
Radu Serban <serban@wisc.edu>
2020-07-10 14:15:18 +0200

    Provide access to the Wavefront OBJ file for wheel visualization (if any) through ChWheel::GetMeshFilename

--- src/chrono_models/vehicle/gator/Gator_Wheel.cpp
+++ src/chrono_models/vehicle/gator/Gator_Wheel.cpp
@@ -36,41 +36,10 @@ const ChVector<> Gator_Wheel::m_inertia(0.100, 0.100, 0.100);
 const double Gator_Wheel::m_radius = 0.3365;
 const double Gator_Wheel::m_width = 0.205;
 
-const std::string Gator_Wheel::m_meshFile = "gator/gator_rim.obj";
-
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-Gator_Wheel::Gator_Wheel(const std::string& name) : ChWheel(name) {}
-
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-void Gator_Wheel::AddVisualizationAssets(VisualizationType vis) {
-    if (vis == VisualizationType::MESH) {
-        ChQuaternion<> rot = (m_side == VehicleSide::LEFT) ? Q_from_AngZ(0) : Q_from_AngZ(CH_C_PI);
-        auto trimesh = chrono_types::make_shared<geometry::ChTriangleMeshConnected>();
-        trimesh->LoadWavefrontMesh(GetDataFile(m_meshFile), false, false);
-        trimesh->Transform(ChVector<>(0, m_offset, 0), ChMatrix33<>(rot));
-        m_trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
-        m_trimesh_shape->Pos = ChVector<>(0, m_offset, 0);
-        m_trimesh_shape->Rot = ChMatrix33<>(rot);
-        m_trimesh_shape->SetMesh(trimesh);
-        m_trimesh_shape->SetStatic(true);
-        m_trimesh_shape->SetName(filesystem::path(m_meshFile).stem());
-        GetSpindle()->AddAsset(m_trimesh_shape);
-    } else {
-        ChWheel::AddVisualizationAssets(vis);
-    }
-}
-
-void Gator_Wheel::RemoveVisualizationAssets() {
-    ChWheel::RemoveVisualizationAssets();
-
-    // Make sure we only remove the assets added by Gator_Wheel::AddVisualizationAssets.
-    // This is important for the ChWheel object because a tire may add its own assets
-    // to the same body (the spindle).
-    auto it = std::find(GetSpindle()->GetAssets().begin(), GetSpindle()->GetAssets().end(), m_trimesh_shape);
-    if (it != GetSpindle()->GetAssets().end())
-        GetSpindle()->GetAssets().erase(it);
+Gator_Wheel::Gator_Wheel(const std::string& name) : ChWheel(name) {
+    m_vis_mesh_file = "gator/gator_rim.obj";
 }
 
 }  // end namespace gator
--- src/chrono_models/vehicle/mtv/FMTV_Wheel.cpp
+++ src/chrono_models/vehicle/mtv/FMTV_Wheel.cpp
@@ -34,41 +34,10 @@ const ChVector<> FMTV_Wheel::m_inertia(.6, .63, .6);
 const double FMTV_Wheel::m_radius = 0.254;
 const double FMTV_Wheel::m_width = 0.254;
 
-const std::string FMTV_Wheel::m_meshFile = "mtv/meshes/MTV_rim.obj";
-
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
-FMTV_Wheel::FMTV_Wheel(const std::string& name) : ChWheel(name) {}
-
-// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-void FMTV_Wheel::AddVisualizationAssets(VisualizationType vis) {
-    if (vis == VisualizationType::MESH) {
-        ChQuaternion<> rot = (m_side == VehicleSide::LEFT) ? Q_from_AngZ(0) : Q_from_AngZ(CH_C_PI);
-        auto trimesh = chrono_types::make_shared<geometry::ChTriangleMeshConnected>();
-        trimesh->LoadWavefrontMesh(GetDataFile(m_meshFile), false, false);
-        trimesh->Transform(ChVector<>(0, m_offset, 0), ChMatrix33<>(rot));
-        m_trimesh_shape = chrono_types::make_shared<ChTriangleMeshShape>();
-        m_trimesh_shape->Pos = ChVector<>(0, m_offset, 0);
-        m_trimesh_shape->Rot = ChMatrix33<>(rot);
-        m_trimesh_shape->SetMesh(trimesh);
-        m_trimesh_shape->SetStatic(true);
-        m_trimesh_shape->SetName(filesystem::path(m_meshFile).stem());
-        GetSpindle()->AddAsset(m_trimesh_shape);
-    } else {
-        ChWheel::AddVisualizationAssets(vis);
-    }
-}
-
-void FMTV_Wheel::RemoveVisualizationAssets() {
-    ChWheel::RemoveVisualizationAssets();
-
-    // Make sure we only remove the assets added by FMTV_Wheel::AddVisualizationAssets.
-    // This is important for the ChWheel object because a tire may add its own assets
-    // to the same body (the spindle).
-    auto it = std::find(GetSpindle()->GetAssets().begin(), GetSpindle()->GetAssets().end(), m_trimesh_shape);
-    if (it != GetSpindle()->GetAssets().end())
-        GetSpindle()->GetAssets().erase(it);
+FMTV_Wheel::FMTV_Wheel(const std::string& name) : ChWheel(name) {
+    m_vis_mesh_file = "mtv/meshes/MTV_rim.obj";
 }
 
 }  // namespace fmtv
