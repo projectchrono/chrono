4bbd85e026849e2a05c3b7b8533f0da4a19bed69
Radu Serban <serban@wisc.edu>
2020-08-10 11:12:00 +0200

    Fix double-pin track assembly algorithm for clockwise wrapping (idler in front of sprocket)

--- src/demos/vehicle/demo_TrackTestRig/demo_VEH_TrackTestRig.cpp
+++ src/demos/vehicle/demo_TrackTestRig/demo_VEH_TrackTestRig.cpp
@@ -133,13 +133,15 @@ int main(int argc, char* argv[]) {
 
     ////ChVector<> target_point = rig->GetPostPosition();
     ////ChVector<> target_point = rig->GetTrackAssembly()->GetIdler()->GetWheelBody()->GetPos();
-    ChVector<> target_point = rig->GetTrackAssembly()->GetSprocket()->GetGearBody()->GetPos();
+    ////ChVector<> target_point = rig->GetTrackAssembly()->GetSprocket()->GetGearBody()->GetPos();
+    ChVector<> target_point = 0.5 * (rig->GetTrackAssembly()->GetSprocket()->GetGearBody()->GetPos() +
+                                     rig->GetTrackAssembly()->GetIdler()->GetWheelBody()->GetPos());
 
     ChVehicleIrrApp app(rig, L"Suspension Test Rig");
     app.SetSkyBox();
     app.AddTypicalLights(irr::core::vector3df(30.f, -30.f, 100.f), irr::core::vector3df(30.f, 50.f, 100.f), 250, 130);
     app.SetChaseCamera(ChVector<>(0), 3.0, 0.0);
-    app.SetChaseCameraPosition(target_point + ChVector<>(-2, 3, 0));
+    app.SetChaseCameraPosition(target_point + ChVector<>(0, 3, 0));
     app.SetChaseCameraState(utils::ChChaseCamera::Free);
     app.SetChaseCameraAngle(-CH_C_PI_2);
     app.SetChaseCameraMultipliers(1e-4, 10);
