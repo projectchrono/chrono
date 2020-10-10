40dae072672babd6e57c0cf3be6b30fdee5a7742
Radu Serban <serban@wisc.edu>
2020-10-10 21:45:31 +0200

    Refactor semi-trailer truck demos to use the Kraz model

--- src/demos/vehicle/demo_Kraz/demo_VEH_Kraz_ClosedLoop.cpp
+++ src/demos/vehicle/demo_Kraz/demo_VEH_Kraz_ClosedLoop.cpp
@@ -32,13 +32,8 @@
 #include "chrono_vehicle/terrain/RigidTerrain.h"
 #include "chrono_vehicle/utils/ChVehiclePath.h"
 #include "chrono_vehicle/wheeled_vehicle/utils/ChWheeledVehicleIrrApp.h"
-#include "chrono_vehicle/wheeled_vehicle/driveline/ChShaftsDriveline4WD.h"
 
-#include "subsystems/SemiTractor_tire.h"
-#include "subsystems/SemiTractor_powertrain.h"
-#include "subsystems/SemiTractor_vehicle.h"
-#include "subsystems/SemiTrailer.h"
-#include "subsystems/SemiTrailer_tire.h"
+#include "chrono_models/vehicle/kraz/Kraz.h"
 
 #ifdef CHRONO_POSTPROCESS
 #include "chrono_postprocess/ChGnuPlot.h"
@@ -47,6 +42,7 @@
 using namespace chrono;
 using namespace chrono::irrlicht;
 using namespace chrono::vehicle;
+using namespace chrono::vehicle::kraz;
 
 // =============================================================================
 
@@ -72,73 +68,24 @@ ChVector<> initLoc(0, 0, 0.6);
 int main(int argc, char* argv[]) {
     GetLog() << "Copyright (c) 2017 projectchrono.org\nChrono version: " << CHRONO_VERSION << "\n\n";
 
-    // --------------
-    // Create systems
-    // --------------
+    // Create the semi-trailer truck
+    Kraz truck;
+    truck.SetContactMethod(ChContactMethod::NSC);
+    truck.SetChassisFixed(false);
+    truck.SetInitPosition(ChCoordsys<>(initLoc, QUNIT));
+    truck.SetTireStepSize(tire_step_size);
+    truck.SetInitFwdVel(0.0);
 
-    SemiTractor_vehicle vehicle(false, ChContactMethod::NSC);
-    vehicle.Initialize(ChCoordsys<>(initLoc, QUNIT));
-    vehicle.SetChassisVisualizationType(chassis_vis_type);
-    vehicle.SetSuspensionVisualizationType(VisualizationType::PRIMITIVES);
-    vehicle.SetSteeringVisualizationType(VisualizationType::PRIMITIVES);
-    vehicle.SetWheelVisualizationType(VisualizationType::MESH);
-    auto drvLine = std::static_pointer_cast<ChShaftsDriveline4WD>(vehicle.GetDriveline());
-    drvLine->LockCentralDifferential(0, false);
+    truck.Initialize();
 
-    SemiTrailer trailer(vehicle.GetSystem(), false);
-    trailer.Initialize(vehicle.GetChassis());
-    trailer.SetChassisVisualizationType(VisualizationType::PRIMITIVES);
-    trailer.SetSuspensionVisualizationType(VisualizationType::PRIMITIVES);
-    trailer.SetWheelVisualizationType(VisualizationType::MESH);
-
-    // Create and initialize the powertrain system
-    auto powertrain = chrono_types::make_shared<SemiTractor_powertrain>("Powertrain");
-    vehicle.InitializePowertrain(powertrain);
-
-    // Create the tractor tires
-    auto tire_FL = chrono_types::make_shared<SemiTractor_tire>("TractorTire_FL");
-    auto tire_FR = chrono_types::make_shared<SemiTractor_tire>("TractorTire_FR");
-
-    auto tire_RL1i = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RL1i");
-    auto tire_RR1i = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RR1i");
-    auto tire_RL1o = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RL1o");
-    auto tire_RR1o = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RR1o");
-
-    auto tire_RL2i = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RL2i");
-    auto tire_RR2i = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RR2i");
-    auto tire_RL2o = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RL2o");
-    auto tire_RR2o = chrono_types::make_shared<SemiTractor_tire>("TractorTire_RR2o");
-
-    vehicle.InitializeTire(tire_FL, vehicle.GetAxle(0)->m_wheels[0], tire_vis_type);
-    vehicle.InitializeTire(tire_FR, vehicle.GetAxle(0)->m_wheels[1], tire_vis_type);
-
-    vehicle.InitializeTire(tire_RL1i, vehicle.GetAxle(1)->m_wheels[0], tire_vis_type);
-    vehicle.InitializeTire(tire_RR1i, vehicle.GetAxle(1)->m_wheels[1], tire_vis_type);
-    vehicle.InitializeTire(tire_RL1o, vehicle.GetAxle(1)->m_wheels[2], tire_vis_type);
-    vehicle.InitializeTire(tire_RR1o, vehicle.GetAxle(1)->m_wheels[3], tire_vis_type);
-
-    vehicle.InitializeTire(tire_RL2i, vehicle.GetAxle(2)->m_wheels[0], tire_vis_type);
-    vehicle.InitializeTire(tire_RR2i, vehicle.GetAxle(2)->m_wheels[1], tire_vis_type);
-    vehicle.InitializeTire(tire_RL2o, vehicle.GetAxle(2)->m_wheels[2], tire_vis_type);
-    vehicle.InitializeTire(tire_RR2o, vehicle.GetAxle(2)->m_wheels[3], tire_vis_type);
-
-    auto tr_tire_FL = chrono_types::make_shared<SemiTrailer_tire>("FL");
-    auto tr_tire_FR = chrono_types::make_shared<SemiTrailer_tire>("FR");
-    auto tr_tire_ML = chrono_types::make_shared<SemiTrailer_tire>("ML");
-    auto tr_tire_MR = chrono_types::make_shared<SemiTrailer_tire>("MR");
-    auto tr_tire_RL = chrono_types::make_shared<SemiTrailer_tire>("RL");
-    auto tr_tire_RR = chrono_types::make_shared<SemiTrailer_tire>("RR");
-
-    // Create the tractor tires
-    trailer.InitializeTire(tr_tire_FL, trailer.GetAxle(0)->m_wheels[0], tire_vis_type);
-    trailer.InitializeTire(tr_tire_FR, trailer.GetAxle(0)->m_wheels[1], tire_vis_type);
-    trailer.InitializeTire(tr_tire_ML, trailer.GetAxle(1)->m_wheels[0], tire_vis_type);
-    trailer.InitializeTire(tr_tire_MR, trailer.GetAxle(1)->m_wheels[1], tire_vis_type);
-    trailer.InitializeTire(tr_tire_RL, trailer.GetAxle(2)->m_wheels[0], tire_vis_type);
-    trailer.InitializeTire(tr_tire_RR, trailer.GetAxle(2)->m_wheels[1], tire_vis_type);
+    truck.SetChassisVisualizationType(VisualizationType::MESH, VisualizationType::PRIMITIVES);
+    truck.SetSteeringVisualizationType(VisualizationType::PRIMITIVES);
+    truck.SetSuspensionVisualizationType(VisualizationType::PRIMITIVES, VisualizationType::PRIMITIVES);
+    truck.SetWheelVisualizationType(VisualizationType::MESH, VisualizationType::MESH);
+    truck.SetTireVisualizationType(VisualizationType::MESH, VisualizationType::MESH);
 
     // Create the terrain
-    RigidTerrain terrain(vehicle.GetSystem());
+    RigidTerrain terrain(truck.GetSystem());
     auto patch_mat = chrono_types::make_shared<ChMaterialSurfaceSMC>();
     patch_mat->SetFriction(0.9f);
     patch_mat->SetRestitution(0.01f);
@@ -153,18 +100,15 @@ int main(int argc, char* argv[]) {
     terrain.Initialize();
 
     // Create the vehicle Irrlicht interface
-    ChWheeledVehicleIrrApp app(&vehicle, L"Open Loop Road Train Follows Straight Line");
+    ChWheeledVehicleIrrApp app(&truck.GetTractor(), L"Semi-trailer truck :: Follows Straight Line");
     app.SetSkyBox();
     app.AddTypicalLights(irr::core::vector3df(30.f, -30.f, 100.f), irr::core::vector3df(30.f, 50.f, 100.f), 250, 130);
     app.SetChaseCamera(ChVector<>(0.0, 0.0, 1.75), 6.0, 0.5);
     app.SetTimestep(step_size);
 
-    // ----------------------------------------------
     // Create the straight path and the driver system
-    // ----------------------------------------------
-
     auto path = StraightLinePath(ChVector<>(-terrainLength / 2, 0, 0.5), ChVector<>(10 * terrainLength / 2, 0, 0.5), 1);
-    ChPathFollowerDriver driver(vehicle, path, "my_path", 1000.0);
+    ChPathFollowerDriver driver(truck.GetTractor(), path, "my_path", 1000.0);
     driver.GetSteeringController().SetLookAheadDistance(5.0);
     driver.GetSteeringController().SetGains(0.5, 0, 0);
     driver.GetSpeedController().SetGains(0.4, 0, 0);
@@ -196,9 +140,9 @@ int main(int argc, char* argv[]) {
     ChTimer<> timer;
     timer.start();
     while (app.GetDevice()->run()) {
-        time = vehicle.GetSystem()->GetChTime();
+        time = truck.GetSystem()->GetChTime();
 
-        double speed = speed_filter.Add(vehicle.GetVehicleSpeed());
+        double speed = speed_filter.Add(truck.GetTractor().GetVehicleSpeed());
         if (!done) {
             speed_recorder.AddPoint(time, speed);
             if (time > 6 && std::abs((speed - last_speed) / step_size) < 2e-4) {
@@ -235,15 +179,13 @@ int main(int argc, char* argv[]) {
         // Update modules (process inputs from other modules)
         driver.Synchronize(time);
         terrain.Synchronize(time);
-        trailer.Synchronize(time, driver_inputs.m_braking, terrain);
-        vehicle.Synchronize(time, driver_inputs, terrain);
+        truck.Synchronize(time, driver_inputs, terrain);
         app.Synchronize("Acceleration test", driver_inputs);
 
         // Advance simulation for one timestep for all modules
         driver.Advance(step_size);
         terrain.Advance(step_size);
-        trailer.Advance(step_size);
-        vehicle.Advance(step_size);
+        truck.Advance(step_size);
         app.Advance(step_size);
 
         // Increment frame number
