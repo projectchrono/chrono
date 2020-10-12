5b5b82ee92b25ebd27c33bd2f4e13a6a8ed62eec
Radu Serban <serban@wisc.edu>
2020-10-12 16:34:57 +0200

    Modify vehicle models to specify hitch location (for potentially attaching a trailer)

--- src/chrono_models/vehicle/sedan/Sedan_Chassis.cpp
+++ src/chrono_models/vehicle/sedan/Sedan_Chassis.cpp
@@ -34,6 +34,7 @@ const double Sedan_Chassis::m_mass = 1250;
 const ChVector<> Sedan_Chassis::m_inertiaXX(222.8, 944.1, 1053.5);
 const ChVector<> Sedan_Chassis::m_inertiaXY(0, 0, 0);
 const ChVector<> Sedan_Chassis::m_COM_loc(0, 0, 0.2);
+const ChVector<> Sedan_Chassis::m_connector_rear_loc(-2.5, 0, -0.1);
 const ChCoordsys<> Sedan_Chassis::m_driverCsys(ChVector<>(0.0, 0.5, 1.2), ChQuaternion<>(1, 0, 0, 0));
 
 // -----------------------------------------------------------------------------
