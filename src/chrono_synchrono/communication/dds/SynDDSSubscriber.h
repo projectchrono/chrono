cbb66e193575a08ff76feab5542c19683e43eea7
Jay Taves <jht99@cornell.edu>
2021-02-08 20:26:02 -0600

    Fix duplicated function from merge

--- src/chrono_synchrono/communication/dds/SynDDSSubscriber.h
+++ src/chrono_synchrono/communication/dds/SynDDSSubscriber.h
@@ -127,7 +127,6 @@ class SYN_API SynDDSSubscriber {
     ///
     bool IsSynchronous() { return m_is_synchronous; }
     eprosima::fastdds::dds::Subscriber* GetSubscriber() { return m_subscriber; }
-    eprosima::fastdds::dds::DataReader* GetDataReader() { return m_reader; }
 
   private:
     bool m_is_synchronous;  ///< Is the Subscriber set to a synchronous subscriber?
