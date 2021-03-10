085759f47e7222b3d9dec76702ee28d73d94bcce
Asher Elmquist <amelmquist@wisc.edu>
2021-03-10 08:43:01 -0600

    fix windows gps utils link error, remove duplicate random function in rt kernel

--- src/chrono_sensor/utils/ChGPSUtils.h
+++ src/chrono_sensor/utils/ChGPSUtils.h
@@ -22,6 +22,7 @@
 
 #include "chrono/core/ChVector.h"
 #include "chrono/core/ChMathematics.h"
+#include "chrono_sensor/ChApiSensor.h"
 
 namespace chrono {
 namespace sensor {
@@ -32,12 +33,12 @@ namespace sensor {
 /// Utility function for calculating Cartesian coordinates from GPS coordinates given the simulation's reference point
 /// @param coords The Cartisian coordinates to be modified. This ChVector is modified and used as output
 /// @param ref The simulation's reference location
-void Cartesian2GPS(ChVector<double>& coords, ChVector<double>& ref);
+CH_SENSOR_API void Cartesian2GPS(ChVector<double>& coords, ChVector<double>& ref);
 
 /// Utility function for calculating GPS coordinates from Cartesian coordinates given the simulation's reference point
 /// @param coords The GPS coordinates to be modified. This ChVector is modified and used as output
 /// @param ref The simulation's reference location
-void GPS2Cartesian(ChVector<double>& coords, ChVector<double>& ref);
+CH_SENSOR_API void GPS2Cartesian(ChVector<double>& coords, ChVector<double>& ref);
 
 }  // namespace sensor
 }  // namespace chrono
