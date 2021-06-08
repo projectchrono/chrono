a5d6fa87cec320a42229347268718dcf6c13e369
tasora <tasora@ied.unipr.it>
2021-06-08 12:05:35 +0200

    Optimization flags that emable/disable the computation of inertial  Ki and Ri, in sake of high cpu speed.

--- src/chrono/fea/ChBeamSectionCosserat.h
+++ src/chrono/fea/ChBeamSectionCosserat.h
@@ -929,6 +929,21 @@ public:
     virtual double GetMassPerUnitLength() = 0;
 
 	ChBeamSectionCosserat* section;
+
+    // Optimization flags
+
+    /// Flag that turns on/off the computation of the [Ri] 'gyroscopic' inertial damping matrix.
+    /// If false, Ri=0. Can be used for cpu speedup, profiling, tests. Default: true.
+    bool compute_inertia_damping_matrix = true;
+
+    /// Flag that turns on/off the computation of the [Ki] inertial stiffness matrix.
+    /// If false, Ki=0. Can be used for cpu speedup, profiling, tests. Default: true.
+    bool compute_inertia_stiffness_matrix = true;
+
+    /// Flag for computing the Ri and Ki matrices via numerical differentiation even if
+    /// an analytical expression is provided. Children calsses must take care of this. Default: false.
+    bool compute_Ri_Ki_by_num_diff = false;
+
 };
 
 
