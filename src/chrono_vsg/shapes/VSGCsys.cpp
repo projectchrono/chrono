7f47750bb8400038917422e6cb0d387b7ec4411a
Rainer Gericke <31243285+rainergericke@users.noreply.github.com>
2021-09-11 18:35:15 +0200

    Remove unused code from  Csys graphics

--- src/chrono_vsg/shapes/VSGCsys.cpp
+++ src/chrono_vsg/shapes/VSGCsys.cpp
@@ -8,18 +8,8 @@ VSGCsys::VSGCsys() {
 }
 
 void VSGCsys::genSubgraph(vsg::ref_ptr<vsg::Switch> parentgraph) {
-    // set up search paths to SPIRV shaders and textures
-    vsg::Paths searchPaths = vsg::getEnvPaths("VSG_FILE_PATH");
-
     vsg::ref_ptr<vsg::ShaderStage> vertexShader = lineShader_vert();
     vsg::ref_ptr<vsg::ShaderStage> fragmentShader = lineShader_frag();
-    // read texture image
-    vsg::Path textureFile("textures/lz.vsgb");
-    auto textureData = vsg::read_cast<vsg::Data>(vsg::findFile(textureFile, searchPaths));
-    if (!textureData) {
-        std::cout << "Could not read texture file : " << textureFile << std::endl;
-        return;
-    }
 
     // set up graphics pipeline
     vsg::DescriptorSetLayoutBindings descriptorBindings{
@@ -60,16 +50,9 @@ void VSGCsys::genSubgraph(vsg::ref_ptr<vsg::Switch> parentgraph) {
     auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{vertexShader, fragmentShader}, pipelineStates);
     auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
 
-    // create texture image and associated DescriptorSets and binding
-    auto texture = vsg::DescriptorImage::create(vsg::Sampler::create(), textureData, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
-    auto descriptorSet = vsg::DescriptorSet::create(descriptorSetLayout, vsg::Descriptors{texture});
-    auto bindDescriptorSet = vsg::BindDescriptorSet::create(VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, descriptorSet);
-
     // create StateGroup as the root of the scene/command graph to hold the GraphicsProgram, and binding of Descriptors to decorate the whole graph
     auto scenegraph = vsg::StateGroup::create();
     scenegraph->add(bindGraphicsPipeline);
-    scenegraph->add(bindDescriptorSet);
 
     // set up model transformation node
     auto transform = vsg::MatrixTransform::create(); // VK_SHADER_STAGE_VERTEX_BIT
@@ -96,24 +79,10 @@ void VSGCsys::genSubgraph(vsg::ref_ptr<vsg::Switch> parentgraph) {
                     {0.0f, 0.0f, 1.0f}
             }); // VK_FORMAT_R32G32B32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
 
-    auto texcoords = vsg::vec2Array::create(
-            {
-                    {0.0f, 0.0f},
-                    {1.0f, 1.0f},
-                    {0.0f, 0.0f},
-                    {1.0f, 1.0f},
-                    {0.0f, 0.0f},
-                    {1.0f, 1.0f}
-            }); // VK_FORMAT_R32G32_SFLOAT, VK_VERTEX_INPUT_RATE_VERTEX, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_SHARING_MODE_EXCLUSIVE
-
     // setup geometry
     auto drawCommands = vsg::Commands::create();
     drawCommands->addChild(vsg::BindVertexBuffers::create(0, vsg::DataList{vertices, colors}));
-    /*
-    drawCommands->addChild(vsg::BindIndexBuffer::create(indices));
-    drawCommands->addChild(vsg::DrawIndexed::create(12, 1, 0, 0, 0));
-    */
-    drawCommands->addChild(vsg::Draw::create(6, 1, 0, 0));
+    drawCommands->addChild(vsg::Draw::create(vertices->size(), 1, 0, 0));
 
     // add drawCommands to transform
     transform->addChild(drawCommands);
