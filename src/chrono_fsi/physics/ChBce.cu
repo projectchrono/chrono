165c925b59dc0c27d4f43e7de7a7ccd7bda177b9
huweibit <huweibit@gmail.com>
2021-11-15 12:41:50 -0600

    Fix BCE index issue when no rigid body is added into the FSI system

--- src/chrono_fsi/physics/ChBce.cu
+++ src/chrono_fsi/physics/ChBce.cu
@@ -779,6 +779,9 @@ void ChBce::CalcBceAcceleration(thrust::device_vector<Real3>& bceAcc,
 void ChBce::ModifyBceVelocity(std::shared_ptr<SphMarkerDataD> sphMarkersD, std::shared_ptr<FsiBodiesDataD> fsiBodiesD) {
     // modify BCE velocity and pressure
     int numRigidAndBoundaryMarkers = fsiGeneralData->referenceArray[2].y - fsiGeneralData->referenceArray[0].y;
+    if (numObjectsH->numRigidBodies == 0)
+        numRigidAndBoundaryMarkers = fsiGeneralData->referenceArray[1].y - fsiGeneralData->referenceArray[0].y;
+
     if ((numObjectsH->numBoundaryMarkers + numObjectsH->numRigid_SphMarkers) != numRigidAndBoundaryMarkers) {
         throw std::runtime_error(
             "Error! number of rigid and boundary markers are "
@@ -794,6 +797,9 @@ void ChBce::ModifyBceVelocity(std::shared_ptr<SphMarkerDataD> sphMarkersD, std::
     }
     int3 updatePortion = mI3(fsiGeneralData->referenceArray[0].y, fsiGeneralData->referenceArray[1].y,
                              fsiGeneralData->referenceArray[2].y);
+    if (numObjectsH->numRigidBodies == 0)
+        updatePortion.z = fsiGeneralData->referenceArray[1].y;
+        
     if (paramsH->bceType == BceVersion::ADAMI) {
         thrust::device_vector<Real3> bceAcc(numObjectsH->numRigid_SphMarkers);
         if (numObjectsH->numRigid_SphMarkers > 0) {
