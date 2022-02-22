ffc79ff7dde4ce851d8eb689ab5b5412a755a137
Aaron Young <aryoung5@wisc.edu>
2022-02-22 07:39:47 -0600

    Reverted build.sh

--- contrib/packaging-python/conda/build.sh
+++ contrib/packaging-python/conda/build.sh
@@ -1,8 +1,6 @@
 
 mkdir ./build
 cd ./build
-echo $CI_PROJECT_DIR
-export NP_INCL=$(python $CI_PROJECT_DIR/contrib/packaging-python/conda/setvarnumpy.py )
 # in py <= 3.7, headers are in $PREFIX/include/python3.xm/, while since python 3.8 they are in $PREFIX/include/python3.8/ go figure.
 if [ "$PY3K" == "1" ] && [ "$PY_VER" != "3.8" ] ; then
     MY_PY_VER="${PY_VER}m"
@@ -30,32 +28,28 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/${PY_LIB} \
  -DCMAKE_BUILD_TYPE=$CONFIGURATION \
  -DENABLE_MODULE_IRRLICHT=ON \
- -DENABLE_MODULE_POSTPROCESS=OFF \
+ -DENABLE_MODULE_POSTPROCESS=ON \
  -DENABLE_MODULE_VEHICLE=ON \
  -DENABLE_MODULE_PYTHON=ON \
- -DENABLE_MODULE_SENSOR=ON \
  -DBUILD_DEMOS=OFF \
  -DBUILD_TESTING=OFF \
  -DBUILD_BENCHMARKING=OFF \
  -DBUILD_GMOCK=OFF \
- -DENABLE_MODULE_CASCADE=OFF \
+ -DENABLE_MODULE_CASCADE=ON \
  -DCASCADE_INCLUDE_DIR=$HOME/miniconda3/include/opencascade \
  -DCASCADE_LIBDIR=$HOME/miniconda3/lib \
- -DENABLE_MODULE_PARDISO_MKL=OFF \
+ -DENABLE_MODULE_MKL=ON \
  -DMKL_INCLUDE_DIR=$HOME/miniconda3/include \
  -DMKL_RT_LIBRARY=$HOME/miniconda3/lib/libmkl_rt.so \
  -DEIGEN3_INCLUDE_DIR=/usr/include/eigen3 \
  -DPYCHRONO_DATA_PATH=../../../../../../share/chrono/data \
- -DOptiX_INSTALL_DIR=/opt/optix/7.2.0 \
- -DNUMPY_INCLUDE_DIR=$NP_INCL \
- -DUSE_CUDA_NVRTC=OFF \
- -DCUDA_ARCH_NAME=Manual \
- -DCUDA_ARCH_PTX=52 \
- -DCUDA_ARCH_BIN=5.2 \
  ./..
 # Build step
 # on linux travis, limit the number of concurrent jobs otherwise
 # gcc gets out of memory
-cmake --build . --config "$CONFIGURATION" -j8
+cmake --build . --config "$CONFIGURATION"
 
 cmake --build . --config "$CONFIGURATION" --target install
+
+
+
