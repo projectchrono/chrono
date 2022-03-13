d6ae2c0d57e06885c1a482defec3fb131aa2c3d1
Radu Serban <serban@wisc.edu>
2022-03-13 15:25:04 +0100

    Switch ChParserAdams to use new visualization shapes

--- src/chrono/utils/ChParserAdams.cpp
+++ src/chrono/utils/ChParserAdams.cpp
@@ -384,8 +384,7 @@ void ChParserAdams::Parse(ChSystem& sys, const std::string& filename) {
 
         // auto sphere = chrono_types::make_shared<ChSphereShape>();
         // sphere->GetSphereGeometry().rad = 0.05;
-        // sphere->Pos = newBody->GetFrame_REF_to_abs().GetPos();
-        // newBody->AddAsset(sphere);
+        // newBody->AddVisualShape(sphere, );
     }
     // Make any markers that don't exist yet
     for (auto marker_pair : markers_map) {
@@ -549,7 +548,7 @@ void ChParserAdams::Parse(ChSystem& sys, const std::string& filename) {
                         cylinder->GetCylinderGeometry().p1 = (parentBody->GetFrame_COG_to_REF() * (*cm_marker))
                                                                  .TransformPointLocalToParent(ChVector<>(0, 0, 0));
 
-                        parentBody->AddAsset(cylinder);
+                        parentBody->AddVisualShape(cylinder);
                     } else if (iter->second == std::string("RADIUS")) {
                         iter++;  // get radius
                         cylinder->GetCylinderGeometry().rad = std::stod(iter->second);
@@ -583,10 +582,10 @@ void ChParserAdams::Parse(ChSystem& sys, const std::string& filename) {
                         // Put ellipsoid at marker, orientation might be wrong
                         // adams says the orientation is marker-specific but I'm guessing Chrono says it's global or
                         // body-specific
-                        ellipsoid->GetEllipsoidGeometry().center =
+                        auto ell_pos =
                             (parentBody->GetFrame_COG_to_REF() * (*cm_marker))
                                 .TransformPointLocalToParent(ChVector<>(0, 0, 0));
-                        parentBody->AddAsset(ellipsoid);
+                        parentBody->AddVisualShape(ellipsoid, ChFrame<>(ell_pos, QUNIT));
                     } else if (iter->second == std::string("XSCALE") || iter->second == std::string("XS")) {
                         iter++;  // get length
                         double scale = std::stod(iter->second);
@@ -623,11 +622,11 @@ void ChParserAdams::Parse(ChSystem& sys, const std::string& filename) {
                         parentBody = dynamic_cast<ChBodyAuxRef*>(cm_marker->GetBody());
                         assert(parentBody != nullptr);
                         // Put box at marker
-                        box->GetBoxGeometry().Pos = (parentBody->GetFrame_COG_to_REF() * (*cm_marker))
+                        auto box_pos = (parentBody->GetFrame_COG_to_REF() * (*cm_marker))
                                                         .TransformPointLocalToParent(ChVector<>(0, 0, 0));
 
-                        box->GetBoxGeometry().Rot = (parentBody->GetFrame_COG_to_REF() * (*cm_marker)).Amatrix;
-                        parentBody->AddAsset(box);
+                        auto box_rot = (parentBody->GetFrame_COG_to_REF() * (*cm_marker)).Amatrix;
+                        parentBody->AddVisualShape(box, ChFrame<>(box_pos, box_rot));
                     } else if (iter->second == std::string("X")) {
                         iter++;  // get length
                         double scale = std::stod(iter->second);
