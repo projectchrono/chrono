8163c90b10a4d6bb89e4293c78e54d4a4286b61a
Radu Serban <serban@wisc.edu>
2022-04-09 21:23:06 +0200

    Update articulated vehicle demo to reflect Chrono::Vehicle changes

--- src/demos/vehicle/articulated_vehicle/subsystems/ACV_ChassisRear.cpp
+++ src/demos/vehicle/articulated_vehicle/subsystems/ACV_ChassisRear.cpp
@@ -24,25 +24,25 @@ using namespace chrono::vehicle;
 // -----------------------------------------------------------------------------
 // Static variables
 // -----------------------------------------------------------------------------
-const double ACV_ChassisRear::m_mass = 2000.0;
-const ChVector<> ACV_ChassisRear::m_inertiaXX(100, 400, 500);
-const ChVector<> ACV_ChassisRear::m_inertiaXY(0, 0, 0);
-const ChVector<> ACV_ChassisRear::m_COM_loc(0, 0, 0.4);
+const double ACV_ChassisRear::m_body_mass = 2000.0;
+const ChVector<> ACV_ChassisRear::m_body_inertiaXX(100, 400, 500);
+const ChVector<> ACV_ChassisRear::m_body_inertiaXY(0, 0, 0);
+const ChVector<> ACV_ChassisRear::m_body_COM_loc(0, 0, 0.4);
 const ChVector<> ACV_ChassisRear::m_connector_loc(1.0, 0, 0.1);
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 ACV_ChassisRear::ACV_ChassisRear(const std::string& name) : ChRigidChassisRear(name) {
-    m_inertia(0, 0) = m_inertiaXX.x();
-    m_inertia(1, 1) = m_inertiaXX.y();
-    m_inertia(2, 2) = m_inertiaXX.z();
+    m_body_inertia(0, 0) = m_body_inertiaXX.x();
+    m_body_inertia(1, 1) = m_body_inertiaXX.y();
+    m_body_inertia(2, 2) = m_body_inertiaXX.z();
 
-    m_inertia(0, 1) = m_inertiaXY.x();
-    m_inertia(0, 2) = m_inertiaXY.y();
-    m_inertia(1, 2) = m_inertiaXY.z();
-    m_inertia(1, 0) = m_inertiaXY.x();
-    m_inertia(2, 0) = m_inertiaXY.y();
-    m_inertia(2, 1) = m_inertiaXY.z();
+    m_body_inertia(0, 1) = m_body_inertiaXY.x();
+    m_body_inertia(0, 2) = m_body_inertiaXY.y();
+    m_body_inertia(1, 2) = m_body_inertiaXY.z();
+    m_body_inertia(1, 0) = m_body_inertiaXY.x();
+    m_body_inertia(2, 0) = m_body_inertiaXY.y();
+    m_body_inertia(2, 1) = m_body_inertiaXY.z();
 
     ChVehicleGeometry::BoxShape box(ChVector<>(0.1, 0, 0.1), ChQuaternion<>(1, 0, 0, 0), ChVector<>(1.2, 1.0, 0.2));
     ChVehicleGeometry::CylinderShape cyl_axle(ChVector<>(-0.5, 0, 0), QUNIT, 0.05, 2);
