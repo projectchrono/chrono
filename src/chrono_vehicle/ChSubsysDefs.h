6a6ef6c49ab15e29cb9edca3866cf5c1f9343acd
Radu Serban <serban@wisc.edu>
2022-06-08 12:22:45 +0200

    Switch visualization mesh specification for some vehicle models.
    These models let the visual system open and parse the specified Wavefront OBJ file

--- src/chrono_vehicle/ChSubsysDefs.h
+++ src/chrono_vehicle/ChSubsysDefs.h
@@ -725,11 +725,12 @@ class CH_VEHICLE_API ChVehicleGeometry {
         int m_matID;           ///< index in contact material list
     };
 
+    /// Line shape for visualization.
     struct LineShape {
         LineShape(const ChVector<>& pos, const ChQuaternion<>& rot, std::shared_ptr<geometry::ChLine> line)
             : m_pos(pos), m_rot(rot), m_line(line) {}
-        ChVector<> m_pos;                              ///< position relative to body
-        ChQuaternion<> m_rot;                          ///< orientation relative to body
+        ChVector<> m_pos;                          ///< position relative to body
+        ChQuaternion<> m_rot;                      ///< orientation relative to body
         std::shared_ptr<geometry::ChLine> m_line;  ///< line data
     };
 
@@ -769,6 +770,7 @@ class CH_VEHICLE_API ChVehicleGeometry {
     ChColor m_color_spheres;    ///< visualization color
     ChColor m_color_cylinders;  ///< visualization color
 
+    bool m_has_obj;               ///< true if the body uses visualization from an OBJ
     bool m_has_mesh;              ///< true if the body uses a visualization mesh
     std::string m_vis_mesh_file;  ///< name of Wavefront OBJ file with visualizaiton mesh
 
