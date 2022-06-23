eb8e3463799ad443377055a1ad3aa39ebb5869bd
Rainer Gericke <rainer.gericke@mac.com>
2022-06-23 10:20:50 +0200

    Update GetTriangleMeshShapeData.cpp
    Consider all defined triangles

--- src/chrono_vsg/shapes/GetTriangleMeshShapeData.cpp
+++ src/chrono_vsg/shapes/GetTriangleMeshShapeData.cpp
@@ -9,30 +9,45 @@ void GetTriangleMeshShapeData(std::shared_ptr<ChTriangleMeshShape> tms,
                               vsg::ref_ptr<vsg::vec2Array>& texcoords,
                               vsg::ref_ptr<vsg::ushortArray>& indices,
                               float& boundingSphereRadius) {
+    double sxmin = 1.0e99;
+    double symin = 1.0e99;
+    double szmin = 1.0e99;
+    double sxmax = -1.0e99;
+    double symax = -1.0e99;
+    double szmax = -1.0e99;
     auto mesh = tms->GetMesh();
     int numTriangles = mesh->getNumTriangles();
-    GetLog() << "Num Triangles = " << numTriangles << " (Really?)\n";
-    auto tri = mesh->getTriangle(0);
-    const float a = 0.1;
-    vertices = vsg::vec3Array::create(3);
-    vertices->set(0, vsg::vec3(tri.p1.x(), tri.p1.y(), tri.p1.z()));
-    vertices->set(1, vsg::vec3(tri.p2.x(), tri.p2.y(), tri.p2.z()));
-    vertices->set(2, vsg::vec3(tri.p3.x(), tri.p3.y(), tri.p3.z()));
-    auto trinorm = tri.GetNormal();
-    normals = vsg::vec3Array::create(3);
-    normals->set(0, vsg::vec3(trinorm.x(), trinorm.y(), trinorm.z()));
-    normals->set(1, vsg::vec3(trinorm.x(), trinorm.y(), trinorm.z()));
-    normals->set(2, vsg::vec3(trinorm.x(), trinorm.y(), trinorm.z()));
+    int iVert = 0;
+    int nVert = 3 * numTriangles;
+    vertices = vsg::vec3Array::create(nVert);
+    int kVert = 0;
+    normals = vsg::vec3Array::create(nVert);
+    // u,v may be unknown or not appliable, phantasy settings
+    texcoords = vsg::vec2Array::create(nVert);
+    for (int iTri = 0; iTri < numTriangles; iTri++) {
+        auto tri = mesh->getTriangle(iTri);
+        auto trinorm = tri.GetNormal();
+        vertices->set(kVert, vsg::vec3(tri.p1.x(), tri.p1.y(), tri.p1.z()));
+        texcoords->set(kVert, vsg::vec2(0, 0));
+        normals->set(kVert++, vsg::vec3(trinorm.x(), trinorm.y(), trinorm.z()));
+        vertices->set(kVert, vsg::vec3(tri.p2.x(), tri.p2.y(), tri.p2.z()));
+        texcoords->set(kVert, vsg::vec2(1, 0));
+        normals->set(kVert++, vsg::vec3(trinorm.x(), trinorm.y(), trinorm.z()));
+        vertices->set(kVert, vsg::vec3(tri.p3.x(), tri.p3.y(), tri.p3.z()));
+        texcoords->set(kVert, vsg::vec2(1, 1));
+        normals->set(kVert++, vsg::vec3(trinorm.x(), trinorm.y(), trinorm.z()));
+    }
 
-    // u,v may be unknown or not applicable, phantasy settings
-    texcoords = vsg::vec2Array::create({{0,0},{0,1},{1,0}});
-
-    indices = vsg::ushortArray::create({0,  1,  2});
+    indices = vsg::ushortArray::create(nVert);
+    for (int iIdx = 0; iIdx < nVert; iIdx ++) {
+        indices->set(iIdx, iIdx);
+    }
     double xmin, xmax, ymin, ymax, zmin, zmax;
     ChMatrix33<> rot;
-    tri.GetBoundingBox(xmin, xmax, ymin, ymax, zmin, zmax, &rot);
+    mesh->GetBoundingBox(xmin, xmax, ymin, ymax, zmin, zmax, &rot);
     // bounding sphere radius > sqrt(a^2+a^2+a^2)
-    boundingSphereRadius = 1.1f * sqrt(pow(xmax-xmin,2)+pow(ymax-ymin,2)+pow(zmax-zmin,2));
+    boundingSphereRadius = 1.1f * sqrt(pow(xmax - xmin, 2) + pow(ymax - ymin, 2) + pow(zmax - zmin, 2));
+    GetLog() << "Bounding Sphere = " << boundingSphereRadius << "\n";
 }
 }  // namespace vsg3d
 }  // namespace chrono
