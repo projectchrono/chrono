d9e814fccef3020a2669a84b00bde8011d208717
Radu Serban <serban@wisc.edu>
2022-07-04 14:16:28 +0200

    Modify HMMWV acceleration demo to provide more setup options

--- src/demos/vehicle/wheeled_models/demo_VEH_HMMWV_Accel.cpp
+++ src/demos/vehicle/wheeled_models/demo_VEH_HMMWV_Accel.cpp
@@ -30,6 +30,7 @@
 #include "chrono_vehicle/ChVehicleModelData.h"
 #include "chrono_vehicle/driver/ChPathFollowerDriver.h"
 #include "chrono_vehicle/terrain/RigidTerrain.h"
+#include "chrono_vehicle/terrain/FlatTerrain.h"
 #include "chrono_vehicle/utils/ChVehiclePath.h"
 #include "chrono_vehicle/wheeled_vehicle/utils/ChWheeledVehicleVisualSystemIrrlicht.h"
 
@@ -46,13 +47,6 @@ using namespace chrono::vehicle::hmmwv;
 
 // =============================================================================
 
-// Visualization type for vehicle parts (PRIMITIVES, MESH, or NONE)
-VisualizationType chassis_vis_type = VisualizationType::PRIMITIVES;
-VisualizationType suspension_vis_type = VisualizationType::PRIMITIVES;
-VisualizationType steering_vis_type = VisualizationType::PRIMITIVES;
-VisualizationType wheel_vis_type = VisualizationType::NONE;
-VisualizationType tire_vis_type = VisualizationType::PRIMITIVES;
-
 // Type of powertrain model (SHAFTS, SIMPLE, SIMPLE_CVT)
 PowertrainModelType powertrain_model = PowertrainModelType::SHAFTS;
 
@@ -60,13 +54,19 @@ PowertrainModelType powertrain_model = PowertrainModelType::SHAFTS;
 DrivelineTypeWV drive_type = DrivelineTypeWV::AWD;
 
 // Type of tire model (RIGID, RIGID_MESH, PACEJKA, LUGRE, FIALA, PAC89, PAC02, TMEASY)
-TireModelType tire_model = TireModelType::PAC02;
+TireModelType tire_model = TireModelType::TMEASY;
+
+enum class TerrainType {FLAT, RIGID};
+TerrainType terrain_type = TerrainType::RIGID;
 
 // Terrain length (X direction)
-double terrainLength = 300.0;
+double terrainLength = 400.0;
 
 // Lane direction
 double yaw_angle = 0 * CH_C_DEG_TO_RAD;
+
+// Include aerodynamic drag
+bool include_aero_drag = false;
 
 // Simulation step sizes
 double step_size = 1e-3;
@@ -77,6 +77,18 @@ double tire_step_size = 1e-3;
 int main(int argc, char* argv[]) {
     GetLog() << "Copyright (c) 2017 projectchrono.org\nChrono version: " << CHRONO_VERSION << "\n\n";
 
+    if (terrain_type == TerrainType::FLAT &&
+        (tire_model == TireModelType::RIGID || tire_model == TireModelType::RIGID_MESH)) {
+        std::cout << "Flat terrain incompatible with a rigid tire model!" << std::endl;
+        return 1;
+    }
+
+    ChQuaternion<> yaw_rot = Q_from_AngZ(yaw_angle);
+    ChCoordsys<> patch_sys(VNULL, yaw_rot);
+    ChVector<> init_loc = patch_sys.TransformPointLocalToParent(ChVector<>(-terrainLength / 2 + 5, 0, 0.7));
+    ChVector<> path_start = patch_sys.TransformPointLocalToParent(ChVector<>(-terrainLength / 2, 0, 0.5));
+    ChVector<> path_end = patch_sys.TransformPointLocalToParent(ChVector<>(+terrainLength / 2, 0, 0.5));
+
     // --------------
     // Create systems
     // --------------
@@ -86,38 +98,50 @@ int main(int argc, char* argv[]) {
     HMMWV_Full my_hmmwv;
     my_hmmwv.SetContactMethod(ChContactMethod::SMC);
     my_hmmwv.SetChassisFixed(false);
-    my_hmmwv.SetInitPosition(ChCoordsys<>(ChVector<>(-terrainLength / 2 + 5, 0, 0.7), Q_from_AngZ(yaw_angle)));
+    my_hmmwv.SetInitPosition(ChCoordsys<>(init_loc, yaw_rot));
     my_hmmwv.SetPowertrainType(powertrain_model);
     my_hmmwv.SetDriveType(drive_type);
     my_hmmwv.SetTireType(tire_model);
     my_hmmwv.SetTireStepSize(tire_step_size);
-    my_hmmwv.SetAerodynamicDrag(0.5, 5.0, 1.2);
+    if (include_aero_drag)
+        my_hmmwv.SetAerodynamicDrag(0.5, 5.0, 1.2);
     my_hmmwv.Initialize();
 
     // Set subsystem visualization mode
-    if (tire_model == TireModelType::RIGID_MESH)
-        tire_vis_type = VisualizationType::MESH;
-
-    my_hmmwv.SetChassisVisualizationType(chassis_vis_type);
-    my_hmmwv.SetSuspensionVisualizationType(suspension_vis_type);
-    my_hmmwv.SetSteeringVisualizationType(steering_vis_type);
-    my_hmmwv.SetWheelVisualizationType(wheel_vis_type);
-    my_hmmwv.SetTireVisualizationType(tire_vis_type);
+    my_hmmwv.SetChassisVisualizationType(VisualizationType::PRIMITIVES);
+    my_hmmwv.SetSuspensionVisualizationType(VisualizationType::PRIMITIVES);
+    my_hmmwv.SetSteeringVisualizationType(VisualizationType::PRIMITIVES);
+    my_hmmwv.SetWheelVisualizationType(VisualizationType::NONE);
+    my_hmmwv.SetTireVisualizationType(tire_model == TireModelType::RIGID_MESH ? VisualizationType::MESH
+                                                                              : VisualizationType::PRIMITIVES);
 
     // Create the terrain
-    RigidTerrain terrain(my_hmmwv.GetSystem());
-    auto patch_mat = chrono_types::make_shared<ChMaterialSurfaceSMC>();
-    patch_mat->SetFriction(0.9f);
-    patch_mat->SetRestitution(0.01f);
-    patch_mat->SetYoungModulus(2e7f);
-    patch_mat->SetPoissonRatio(0.3f);
-    auto patch = terrain.AddPatch(patch_mat, CSYSNORM, terrainLength, 5);
-    patch->SetColor(ChColor(0.8f, 0.8f, 0.5f));
-    patch->SetTexture(vehicle::GetDataFile("terrain/textures/tile4.jpg"), 200, 5);
-    terrain.Initialize();
+    std::shared_ptr<ChTerrain> terrain;
+    switch (terrain_type) {
+        case TerrainType::RIGID:
+        default: {
+            auto rigid_terrain = chrono_types::make_shared<RigidTerrain>(my_hmmwv.GetSystem());
+            auto patch_mat = chrono_types::make_shared<ChMaterialSurfaceSMC>();
+            patch_mat->SetFriction(0.9f);
+            patch_mat->SetRestitution(0.01f);
+            patch_mat->SetYoungModulus(2e7f);
+            patch_mat->SetPoissonRatio(0.3f);
+            auto patch = rigid_terrain->AddPatch(patch_mat, ChCoordsys<>(ChVector<>(0), yaw_rot), terrainLength, 5);
+            patch->SetColor(ChColor(0.8f, 0.8f, 0.5f));
+            patch->SetTexture(vehicle::GetDataFile("terrain/textures/tile4.jpg"), 200, 5);
+            rigid_terrain->Initialize();
+            terrain = rigid_terrain;
+            break;
+        }
+        case TerrainType::FLAT: {
+            auto flat_terrain = chrono_types::make_shared<FlatTerrain>(0, 0.9f);
+            terrain = flat_terrain;
+            break;
+        }
+    }
 
     // Create the straight path and the driver system
-    auto path = StraightLinePath(ChVector<>(-terrainLength / 2, 0, 0.5), ChVector<>(terrainLength / 2, 0, 0.5), 1);
+    auto path = StraightLinePath(path_start, path_end, 1);
     ChPathFollowerDriver driver(my_hmmwv.GetVehicle(), path, "my_path", 1000.0);
     driver.GetSteeringController().SetLookAheadDistance(5.0);
     driver.GetSteeringController().SetGains(0.5, 0, 0);
@@ -161,8 +185,9 @@ int main(int argc, char* argv[]) {
             if (time > 6 && std::abs((speed - last_speed) / step_size) < 2e-4) {
                 done = true;
                 timer.stop();
-                std::cout << "Simulation time: " << timer() << std::endl;
-                std::cout << "Maximum speed: " << speed << std::endl;
+                std::cout << "Maximum speed (m/s): " << speed << std::endl;
+                std::cout << "Time (s):            " << time << std::endl;
+                std::cout << "Simulation time (s): " << timer() << std::endl;
 #ifdef CHRONO_POSTPROCESS
                 postprocess::ChGnuPlot gplot;
                 gplot.SetGrid();
@@ -191,18 +216,21 @@ int main(int argc, char* argv[]) {
 
         // Update modules (process inputs from other modules)
         driver.Synchronize(time);
-        terrain.Synchronize(time);
-        my_hmmwv.Synchronize(time, driver_inputs, terrain);
+        terrain->Synchronize(time);
+        my_hmmwv.Synchronize(time, driver_inputs, *terrain);
         vis->Synchronize("Acceleration test", driver_inputs);
 
         // Advance simulation for one timestep for all modules
         driver.Advance(step_size);
-        terrain.Advance(step_size);
+        terrain->Advance(step_size);
         my_hmmwv.Advance(step_size);
         vis->Advance(step_size);
 
         // Increment frame number
         step_number++;
+
+        // Draw a coordinate system aligned with the world frame
+        tools::drawCoordsys(vis.get(), ChCoordsys<>(my_hmmwv.GetVehicle().GetPos(), QUNIT), 2);
 
         vis->EndScene();
     }
