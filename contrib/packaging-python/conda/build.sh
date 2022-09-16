8756e31833718a2b10ba7ace5ef14c42d4dd2bdb
Asher Elmquist <amelmquist@wisc.edu>
2022-09-16 10:59:29 -0400

    update sensor ci to optix 7.5

--- contrib/packaging-python/conda/build.sh
+++ contrib/packaging-python/conda/build.sh
@@ -55,7 +55,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DMKL_INCLUDE_DIR=$BUILD_PREFIX/include \
  -DMKL_RT_LIBRARY=$BUILD_PREFIX/lib/libmkl_rt.so \
  -DEIGEN3_INCLUDE_DIR=/usr/include/eigen3 \
- -DOptiX_INSTALL_DIR=/opt/optix/7.2.0 \
+ -DOptiX_INSTALL_DIR=/opt/optix/7.5.0 \
  -DNUMPY_INCLUDE_DIR=$NP_INCL \
  ./..
 
