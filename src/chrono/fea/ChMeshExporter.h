6177c02aadd8b5d8d8e2eb3ef213b0ccf3571b40
Radu Serban <serban@wisc.edu>
2022-11-15 20:37:14 +0100

    Clean up FEA mesh exporter class

--- src/chrono/fea/ChMeshExporter.h
+++ src/chrono/fea/ChMeshExporter.h
@@ -11,8 +11,8 @@
 // =============================================================================
 // Authors: Milad Rakhsha
 // =============================================================================
-#ifndef CHMESHEXPORTER_H_
-#define CHMESHEXPORTER_H_
+#ifndef CH_MESH_EXPORTER_H
+#define CH_MESH_EXPORTER_H
 
 #include "chrono/fea/ChElementHexaANCF_3813.h"
 #include "chrono/fea/ChElementCableANCF.h"
@@ -30,25 +30,23 @@ namespace fea {
 
 class ChApi ChMeshExporter {
   public:
-    /// This function is used to write the connectivity and information of a ChMesh
-    /// Currently it only supports ChElementCableANCF, ChElementShellANCF_3423, ChElementHexaANCF_3813
-    /// This connectivity is usually constant throughout the simulation. Hence, it may be
-    /// called once at the beginning of the simulation (or as many times as it is needed),
-    /// and should be used in the later stage when the nodal/elemental informations are updated.
-    static void writeMesh(std::shared_ptr<ChMesh> my_mesh,  ///< destination mesh
-                          std::string SaveAs                ///< name of the mesh file
+    /// Write FEA mesh connectivity to specified output file.
+    /// This connectivity is usually constant throughout the simulation. Hence, it may be called once at the beginning
+    /// of the simulation (or as many times as it is needed), and should be used in the later stage when the
+    /// nodal/elemental informations are updated.
+    static void WriteMesh(std::shared_ptr<ChMesh> mesh,     ///< destination mesh
+                          const std::string& mesh_filename  ///< output file name
     );
 
-    /// The task of this function is to write the information of a ChMesh into a file with vtk format.
+    /// Write current FEA mesh information in VTK format.
     /// The vtk files may be opened with external visualization software such as Paraview, Visit, etc.
-    /// This function requires the mesh connectivity file, which may be written by ChMeshExporter::writeMesh function
+    /// This function requires the mesh connectivity file, which may be written by ChMeshExporter::writeMesh function.
     /// Note that this function writes the current state of the mesh when is called. It writes some nodal
-    /// informations (see the implementation) and elemental informations at the "element center". The elemental
-    /// information may be converted to nodal information later on in the post-processing stage with a visualization
-    /// software like Paraview.
-    static void writeFrame(std::shared_ptr<ChMesh> my_mesh,  ///< destination mesh
-                           char SaveAsBuffer[256],           ///< name of the vtk file to export data to
-                           std::string MeshFileBuffer  ///< name of the mesh file written by ChMeshExporter::writeMesh
+    /// and elemental information at the "element center". The elemental information may be converted to nodal
+    /// information later on in the post-processing stage with a visualization software like Paraview.
+    static void WriteFrame(std::shared_ptr<ChMesh> mesh,      ///< destination mesh
+                           const std::string& mesh_filename,  ///< name of the mesh file with connectivity information
+                           const std::string& vtk_filename    ///< output file name
     );
 };
 
