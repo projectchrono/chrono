30cd3f2702cb58182e46d5b2724d2d2850a50e21
Radu Serban <serban@wisc.edu>
2023-01-06 12:19:13 +0100

    Fix bugs in ANCF nodes where the actual number of DOFs was not correct (when some are fixed)
    (cherry picked from commit 83c3d248d1050fac908fa159515c5f48aaa8436e)

--- src/chrono/fea/ChNodeFEAxyzDD.h
+++ src/chrono/fea/ChNodeFEAxyzDD.h
@@ -76,8 +76,14 @@ class ChApi ChNodeFEAxyzDD : public ChNodeFEAxyzD {
     /// Get the number of degrees of freedom.
     virtual int GetNdofX() const override { return 9; }
 
+    /// Get the number of degrees of freedom, derivative.
+    virtual int GetNdofW() const override { return 9; }
+
     /// Get the actual number of active degrees of freedom.
     virtual int GetNdofX_active() const override { return m_dof_actual; }
+
+    /// Get the actual number of active degrees of freedom, derivative.
+    virtual int GetNdofW_active() const override { return m_dof_actual; }
 
     // Functions for interfacing to the state bookkeeping
 
