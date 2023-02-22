0ea5903486eea8f05be5b88a7ff934ac1a5a6429
tasora <tasora@ied.unipr.it>
2023-02-22 22:01:07 +0100

    new Glyph mode: tensors

--- src/chrono/assets/ChGlyphs.cpp
+++ src/chrono/assets/ChGlyphs.cpp
@@ -30,6 +30,9 @@ ChGlyphs::ChGlyphs() {
     glyph_basis_type = eCh_GlyphBasis::CONSTANT;
     glyph_basis_prop = "";
     glyph_basis_constant = QUNIT;
+    glyph_eigenvalues_type = eCh_GlyphEigenvalues::CONSTANT;
+    glyph_eigenvalues_prop = "";
+    glyph_eigenvalue_constant = ChVector<>(1,1,1);
     glyph_color_type = eCh_GlyphColor::CONSTANT;
     glyph_color_prop = "";
     glyph_color_constant = ChColor(1,0,0);
@@ -40,6 +43,7 @@ ChGlyphs::ChGlyphs() {
     this->vectors = 0; 
     this->colors = 0;
     this->rotations = 0;
+    this->eigenvalues = 0;
 }
 
 ChGlyphs::~ChGlyphs() {
@@ -138,10 +142,48 @@ void ChGlyphs::SetGlyphVector(unsigned int id, ChVector<> mpoint, ChVector<> mve
     (*this->colors)[id] = mcolor;
 }
 
+// Fast method to set a glyph for GLYPH_VECTOR draw mode, local basis
+void ChGlyphs::SetGlyphVectorLocal(unsigned int id, ChVector<> mpoint, ChVector<> mvector, ChQuaternion<> mrot, ChColor mcolor) {
+    if (this->draw_mode != GLYPH_VECTOR)
+        SetDrawMode(GLYPH_VECTOR);
+
+    if (points.size() <= id)
+        points.resize(id + 1);
+    points[id] = mpoint;
+
+    if (!this->vectors) {
+        geometry::ChPropertyVector my_vectors;
+        my_vectors.name = "V";
+        this->AddProperty(my_vectors);
+        this->vectors = &((geometry::ChPropertyVector*)(m_properties.back()))->data;
+    }
+    if (!this->rotations) {
+        geometry::ChPropertyQuaternion my_quats;
+        my_quats.name = "rot";
+        this->AddProperty(my_quats);
+        this->rotations = &((geometry::ChPropertyQuaternion*)(m_properties.back()))->data;
+    }
+    if (!this->colors) {
+        geometry::ChPropertyColor my_colors;
+        my_colors.name = "color";
+        this->AddProperty(my_colors);
+        this->colors = &((geometry::ChPropertyColor*)(m_properties.back()))->data;
+    }
+
+    for (geometry::ChProperty* prop : this->m_properties) {
+        if (prop->GetSize() <= id)
+            prop->SetSize(id + 1);
+    }
+
+    (*this->vectors)[id] = mvector;
+    (*this->rotations)[id] = mrot;
+    (*this->colors)[id] = mcolor;
+}
+
 // Fast method to set a glyph for GLYPH_COORDSYS draw mode.
 // If the id is more than the reserved amount of glyphs (see Reserve() ) the csys are inflated.
 void ChGlyphs::SetGlyphCoordsys(unsigned int id, ChCoordsys<> mcoord) {
-    if (this->draw_mode != GLYPH_COORDSYS)
+    if (this->draw_mode != GLYPH_COORDSYS) 
         SetDrawMode(GLYPH_COORDSYS);
 
     if (points.size() <= id)
@@ -160,6 +202,40 @@ void ChGlyphs::SetGlyphCoordsys(unsigned int id, ChCoordsys<> mcoord) {
             prop->SetSize(id + 1);
     }
     (*this->rotations)[id] = mcoord.rot;
+}
+
+// Fast method to set a glyph for GLYPH_TENSOR draw mode.
+// If the id is more than the reserved amount of glyphs (see Reserve() ) the csys are inflated.
+void ChGlyphs::SetGlyphTensor(unsigned int id, ChVector<> mpoint, ChQuaternion<> mbasis, ChVector<> meigenvalues) {
+    if (this->draw_mode != GLYPH_TENSOR) {
+        SetDrawMode(GLYPH_TENSOR);
+        glyph_eigenvalues_type = ChGlyphs::eCh_GlyphEigenvalues::PROPERTY;
+        glyph_eigenvalues_prop = "eigenvalues";
+    }
+
+    if (points.size() <= id)
+        points.resize(id + 1);
+    points[id] = mpoint;
+
+    if (!this->rotations) {
+        geometry::ChPropertyQuaternion my_quats;
+        my_quats.name = "rot";
+        this->AddProperty(my_quats);
+        this->rotations = &((geometry::ChPropertyQuaternion*)(m_properties.back()))->data;
+    }
+    if (!this->eigenvalues) {
+        geometry::ChPropertyVector my_eigenvalues;
+        my_eigenvalues.name = "eigenvalues";
+        this->AddProperty(my_eigenvalues);
+        this->eigenvalues = &((geometry::ChPropertyVector*)(m_properties.back()))->data;
+    }
+
+    for (geometry::ChProperty* prop : this->m_properties) {
+        if (prop->GetSize() <= id)
+            prop->SetSize(id + 1);
+    }
+    (*this->rotations)[id] = mbasis;
+    (*this->eigenvalues)[id] = meigenvalues;
 }
 
 void ChGlyphs::ArchiveOUT(ChArchiveOut& marchive) {
--- src/chrono/assets/ChGlyphs.h
+++ src/chrono/assets/ChGlyphs.h
@@ -29,27 +29,34 @@ class ChApi ChGlyphs : public ChVisualShape {
     enum eCh_GlyphType { 
         GLYPH_POINT = 0, 
         GLYPH_VECTOR,
-        GLYPH_COORDSYS };
+        GLYPH_COORDSYS,
+        GLYPH_TENSOR };
 
-    /// Modes for type of glyph vector length 
+    /// Modes vector length, if GLYPH_VECTOR
     enum class eCh_GlyphLength {
         CONSTANT = 0,
         PROPERTY,
     };
 
-    /// Modes for type of glyph width or size
+    /// Modes for glyph width or size
     enum class eCh_GlyphWidth {
         CONSTANT = 0,
         PROPERTY,
     };
 
-    /// Modes for type of glyph rotation (for coordsys mode)
+    /// Modes for type of glyph rotation (for GLYPH_COORDSYS mode)
     enum class eCh_GlyphBasis {
         CONSTANT = 0,
         PROPERTY,
     };
 
-    /// Modes for colorizing vector or dot 
+    /// Modes for type of glyph eigenvalues (for GLYPH_TENSOR mode)
+    enum class eCh_GlyphEigenvalues {
+        CONSTANT = 0,
+        PROPERTY,
+    };
+
+    /// Modes for colorizing glyph 
     enum class eCh_GlyphColor {
         CONSTANT = 0,
         PROPERTY,
@@ -98,9 +105,17 @@ class ChApi ChGlyphs : public ChVisualShape {
     /// If the id is more than the reserved amount of glyphs (see Reserve() ) the vectors are inflated.
     void SetGlyphVector(unsigned int id, ChVector<> mpoint, ChVector<> mvector, ChColor mcolor = ChColor(1, 0, 0));
 
+    /// Fast method to set a glyph for GLYPH_VECTOR draw mode, with each vectors set in its own local (rotated) basis
+    /// If the id is more than the reserved amount of glyphs (see Reserve() ) the vectors are inflated.
+    void SetGlyphVectorLocal(unsigned int id, ChVector<> mpoint, ChVector<> mvector, ChQuaternion<> mrot, ChColor mcolor = ChColor(1, 0, 0));
+
     /// Fast method to set a glyph for GLYPH_COORDSYS draw mode.
     /// If the id is more than the reserved amount of glyphs (see Reserve() ) the csys are inflated.
     void SetGlyphCoordsys(unsigned int id, ChCoordsys<> mcoord);
+
+    /// Fast method to set a glyph for GLYPH_TENSOR draw mode.
+    /// If the id is more than the reserved amount of glyphs (see Reserve() ) the tensors are inflated.
+    void SetGlyphTensor(unsigned int id, ChVector<> mpoint, ChQuaternion<> mbasis, ChVector<> eigenvalues);
 
     /// Gets the current array of per-point properties, if any.
     std::vector<geometry::ChProperty*> getProperties() { return m_properties; }
@@ -120,6 +135,7 @@ class ChApi ChGlyphs : public ChVisualShape {
     CH_ENUM_VAL(GLYPH_POINT);
     CH_ENUM_VAL(GLYPH_VECTOR);
     CH_ENUM_VAL(GLYPH_COORDSYS);
+    CH_ENUM_VAL(GLYPH_TENSOR);
     CH_ENUM_MAPPER_END(eCh_GlyphType);
     /// @endcond
 
@@ -134,6 +150,9 @@ class ChApi ChGlyphs : public ChVisualShape {
     eCh_GlyphBasis  glyph_basis_type;
     std::string glyph_basis_prop; // selected property for basis
     ChQuaternion<> glyph_basis_constant;
+    eCh_GlyphEigenvalues  glyph_eigenvalues_type;
+    std::string glyph_eigenvalues_prop; // selected property for eigs
+    ChVector<> glyph_eigenvalue_constant;
     eCh_GlyphColor  glyph_color_type;
     std::string glyph_color_prop; // selected property for color
     ChColor glyph_color_constant;
@@ -148,6 +167,7 @@ class ChApi ChGlyphs : public ChVisualShape {
     std::vector<ChColor>* colors;
     std::vector<ChVector<double> >* vectors;
     std::vector<ChQuaternion<double> >* rotations;
+    std::vector<ChVector<double> >* eigenvalues;
 
   private:
     eCh_GlyphType draw_mode;
