ab5890b67c91bd0cafd223269dd8dc50da5c6acc
tasora <tasora@ied.unipr.it>
2023-02-22 22:02:33 +0100

    ChPathShape has a setting "thickness" that can be used by 3d renderers, if hey support 3d line rendering

--- src/chrono/assets/ChPathShape.cpp
+++ src/chrono/assets/ChPathShape.cpp
@@ -17,11 +17,11 @@ namespace chrono {
 // Register into the object factory, to enable run-time dynamic creation and persistence
 CH_FACTORY_REGISTER(ChPathShape)
 
-ChPathShape::ChPathShape() : npoints(200) {
+ChPathShape::ChPathShape() : npoints(200), thickness(10.0) {
     gpath = chrono_types::make_shared<geometry::ChLinePath>();
 }
 
-ChPathShape::ChPathShape(std::shared_ptr<geometry::ChLinePath>& mpath) : npoints(200), gpath(mpath) {}
+ChPathShape::ChPathShape(std::shared_ptr<geometry::ChLinePath>& mpath) : npoints(200), thickness(10.0), gpath(mpath) {}
 
 void ChPathShape::ArchiveOUT(ChArchiveOut& marchive) {
     // version number
@@ -30,6 +30,8 @@ void ChPathShape::ArchiveOUT(ChArchiveOut& marchive) {
     ChVisualShape::ArchiveOUT(marchive);
     // serialize all member data:
     marchive << CHNVP(gpath);
+    marchive << CHNVP(npoints);
+    marchive << CHNVP(thickness);
 }
 
 void ChPathShape::ArchiveIN(ChArchiveIn& marchive) {
@@ -39,6 +41,8 @@ void ChPathShape::ArchiveIN(ChArchiveIn& marchive) {
     ChVisualShape::ArchiveIN(marchive);
     // stream in all member data:
     marchive >> CHNVP(gpath);
+    marchive >> CHNVP(npoints);
+    marchive >> CHNVP(thickness);
 }
 
 }  // end namespace chrono
