dd95c4a173bfbdc67e879d3a4db7e780efc5fac1
pchaoWT <chao.peng@unipr.it>
2023-04-24 16:57:28 +0200

    Implement a static solver ChStaticNonlinearRigidMotion() to deal with the static problem for systems with rigid motion DOFs, such as, a pendulum or a mooring line

--- src/chrono/timestepper/ChStaticAnalysis.h
+++ src/chrono/timestepper/ChStaticAnalysis.h
@@ -287,6 +287,58 @@ class ChApi ChStaticNonLinearIncremental : public ChStaticAnalysis {
     friend class ChSystem;
 };
 
+
+
+/// Nonlinear static analysis for the system including rigid motion DOFs,
+/// for instance, a single pendulum.
+class ChApi ChStaticNonLinearRigidMotion : public ChStaticAnalysis {
+  public:
+    ChStaticNonLinearRigidMotion();
+    ~ChStaticNonLinearRigidMotion() {}
+
+    /// Enable/disable verbose output (default: false)
+    void SetVerbose(bool verbose) { m_verbose = verbose; }
+
+    /// Set the max number of iterations for the Newton Raphson procedure (default: 10).
+    void SetMaxIterations(int max_iters);
+
+    /// Set the number of steps that, for the first iterations, make the residual grow linearly (default: 6).
+    /// If =0, no incremental application of residual, so it is a classic Newton Raphson iteration, otherwise acts as a
+    /// continuation strategy. For values > 0 , it might help convergence. Must be less than maxiters.
+    void SetIncrementalSteps(int incr_steps);
+
+    /// Set stopping criteria based on WRMS norm of correction and the specified relative and absolute tolerances.
+    /// This is the default, with reltol = 1e-4, abstol = 1e-8.
+    /// The Newton Raphson procedure is stopped if the WRMS norm of the correction vector (based on the current state)
+    /// is less than 1.
+    void SetCorrectionTolerance(double reltol, double abstol);
+
+    /// Set stopping criteria based on norm of residual and the specified tolerance.
+    /// The Newton Raphson is stopped when the infinity norm of the residual is below the tolerance.
+    void SetResidualTolerance(double tol);
+
+    /// Get the max number of iterations for the Newton Raphson procedure.
+    int GetMaxIterations() const { return m_maxiters; }
+
+    /// Set the number of steps that, for the first iterations, make the residual grow linearly.
+    int GetIncrementalSteps() const { return m_incremental_steps; }
+
+  private:
+    /// Performs the static analysis, doing a non-linear solve.
+    virtual void StaticAnalysis() override;
+
+    bool m_verbose;
+    int m_maxiters;
+    int m_incremental_steps;
+    bool m_use_correction_test;
+    double m_reltol;
+    double m_abstol;
+
+    friend class ChSystem;
+};
+
+
+
 }  // end namespace chrono
 
 #endif
