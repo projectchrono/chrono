d502fbc8767df8c917dff9a63d6830b995fa1b9f
Rainer Gericke <rainer.gericke@mac.com>
2023-04-26 19:22:55 +0200

    Duro fragments updated to new powertrain model

--- src/chrono_models/vehicle/duro/Duro_AutomaticTransmissionShafts.h
+++ src/chrono_models/vehicle/duro/Duro_AutomaticTransmissionShafts.h
@@ -9,18 +9,18 @@
 // http://projectchrono.org/license-chrono.txt.
 //
 // =============================================================================
-// Authors: Rainer Gericke
+// Authors: Radu Serban
 // =============================================================================
 //
-// Duro powertrain model based on ChShaft objects.
+// Duro automatic transmission model based on ChShaft objects.
 //
 // =============================================================================
 
-#ifndef DURO_POWERTRAIN_H
-#define DURO_POWERTRAIN_H
+#ifndef Duro_AUTOMATIC_TRANSMISSION_SHAFTS_H
+#define Duro_AUTOMATIC_TRANSMISSION_SHAFTS_H
 
 #include "chrono_vehicle/ChVehicle.h"
-#include "chrono_vehicle/powertrain/ChShaftsPowertrain.h"
+#include "chrono_vehicle/powertrain/ChAutomaticTransmissionShafts.h"
 
 #include "chrono_models/ChApiModels.h"
 
@@ -32,33 +32,30 @@ namespace duro {
 /// @{
 
 /// Shafts-based powertrain model for the Duro vehicle.
-class CH_MODELS_API Duro_Powertrain : public ChShaftsPowertrain {
+class CH_MODELS_API Duro_AutomaticTransmissionShafts : public ChAutomaticTransmissionShafts {
   public:
-    Duro_Powertrain(const std::string& name);
-
-    ~Duro_Powertrain() {}
+    Duro_AutomaticTransmissionShafts(const std::string& name);
+    ~Duro_AutomaticTransmissionShafts() {}
 
     virtual void SetGearRatios(std::vector<double>& fwd, double& rev) override;
 
-    virtual double GetMotorBlockInertia() const override { return m_motorblock_inertia; }
-    virtual double GetCrankshaftInertia() const override { return m_crankshaft_inertia; }
+    virtual double GetTransmissionBlockInertia() const override { return m_transmissionblock_inertia; }
     virtual double GetIngearShaftInertia() const override { return m_ingear_shaft_inertia; }
-    virtual double GetPowershaftInertia() const override { return m_power_shaft_inertia; }
+    virtual double GetMotorshaftInertia() const override { return m_motorshaft_inertia; }
+    virtual double GetDriveshaftInertia() const override { return m_driveshaft_inertia; }
 
     virtual double GetUpshiftRPM() const override { return m_upshift_RPM; }
     virtual double GetDownshiftRPM() const override { return m_downshift_RPM; }
 
-    virtual void SetEngineTorqueMap(std::shared_ptr<ChFunction_Recorder>& map) override;
-    virtual void SetEngineLossesMap(std::shared_ptr<ChFunction_Recorder>& map) override;
     virtual void SetTorqueConverterCapacityFactorMap(std::shared_ptr<ChFunction_Recorder>& map) override;
     virtual void SetTorqeConverterTorqueRatioMap(std::shared_ptr<ChFunction_Recorder>& map) override;
 
   private:
     // Shaft inertias.
-    static const double m_motorblock_inertia;
-    static const double m_crankshaft_inertia;
+    static const double m_transmissionblock_inertia;
+    static const double m_motorshaft_inertia;
+    static const double m_driveshaft_inertia;
     static const double m_ingear_shaft_inertia;
-    static const double m_power_shaft_inertia;
 
     // Gear shifting characteristics
     static const double m_upshift_RPM;
@@ -67,8 +64,9 @@ class CH_MODELS_API Duro_Powertrain : public ChShaftsPowertrain {
 
 /// @} vehicle_models_Duro
 
-}  // namespace duro
+}  // end namespace Duro
 }  // end namespace vehicle
 }  // end namespace chrono
 
 #endif
+
