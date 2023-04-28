7411ae82a82faffa74cfa05f976b099a8af03ff6
Rainer Gericke <rainer.gericke@mac.com>
2023-04-28 11:17:16 +0200

    Marder with new Engine/Transmission

---
+++ src/chrono_models/vehicle/marder/powertrain/Marder_EngineShafts.h
@@ -0,0 +1,60 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Rainer Gericke
+// =============================================================================
+//
+// multi role engine model based on ChShaft objects.
+//
+// =============================================================================
+
+#ifndef MARDER_ENGINE_SHAFTS_H
+#define MARDER_ENGINE_SHAFTS_H
+
+#include "chrono_vehicle/ChVehicle.h"
+#include "chrono_vehicle/powertrain/ChEngineShafts.h"
+
+#include "chrono_models/ChApiModels.h"
+
+namespace chrono {
+namespace vehicle {
+namespace marder {
+
+/// @addtogroup vehicle_models_Marder
+/// @{
+
+/// Shafts-based engine model for the Marder vehicle.
+class CH_MODELS_API Marder_EngineShafts : public ChEngineShafts {
+  public:
+    Marder_EngineShafts(const std::string& name);
+
+    ~Marder_EngineShafts() {}
+
+    virtual double GetMotorBlockInertia() const override { return m_motorblock_inertia; }
+    virtual double GetMotorshaftInertia() const override { return m_motorshaft_inertia; }
+
+    virtual void SetEngineTorqueMap(std::shared_ptr<ChFunction_Recorder>& map) override;
+    virtual void SetEngineLossesMap(std::shared_ptr<ChFunction_Recorder>& map) override;
+
+  private:
+    // Shaft inertias.
+    static const double m_motorblock_inertia;
+    static const double m_motorshaft_inertia;
+};
+
+/// @} vehicle_models_Marder
+
+}  // namespace Marder
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
+
---
+++ src/chrono_models/vehicle/marder/powertrain/Marder_EngineSimple.h
@@ -0,0 +1,58 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban, Rainer Gericke
+// =============================================================================
+//
+// Marder simple engine model based on hyperbolical speed-torque curve (CVT)
+//
+// =============================================================================
+
+#ifndef MARDER_ENGINE_SIMPLE_H
+#define MARDER_ENGINE_SIMPLE_H
+
+#include "chrono_vehicle/powertrain/EngineSimple.h"
+
+#include "chrono_models/ChApiModels.h"
+
+namespace chrono {
+namespace vehicle {
+namespace marder {
+
+/// @addtogroup vehicle_models_Marder
+/// @{
+
+/// Marder simple engine model based on hyperbolical speed-torque curve (CVT).
+class CH_MODELS_API Marder_EngineSimple : public ChEngineSimple {
+  public:
+    Marder_EngineSimple(const std::string& name);
+
+    ~Marder_EngineSimple() {}
+
+    virtual double GetMaxTorque() const override { return m_max_torque; }
+    virtual double GetMaxPower() const override { return m_max_power; }
+    virtual double GetMaxSpeed() const override { return m_max_speed; }
+
+  private:
+    static const double m_max_torque;  ///< maximum motor torque
+    static const double m_max_power;   ///< maximum motor power
+    static const double m_max_speed;   ///< maximum engine speed
+};
+
+/// @} vehicle_models_Marder
+
+}  // end namespace Marder
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
+
+
