4902d7ceeaee6f473db5aa60ca9e01d9fbca4244
Radu Serban <serban@wisc.edu>
2023-05-14 13:07:54 +0200

    Add utility conversion functions RGB <-> HSV

--- src/chrono/assets/ChColor.cpp
+++ src/chrono/assets/ChColor.cpp
@@ -12,6 +12,9 @@
 // Authors: Alessandro Tasora
 // =============================================================================
 
+#include <algorithm>
+#include <cmath>
+
 #include "chrono/assets/ChColor.h"
 #include "chrono/core/ChClassFactory.h"
 
@@ -19,6 +22,91 @@ namespace chrono {
 
 // Register into the object factory, to enable run-time dynamic creation and persistence
 CH_FACTORY_REGISTER(ChColor)
+
+ChVector<float> ChColor::RGB2HSV(const ChColor& rgb) {
+    const float& R = rgb.R;
+    const float& G = rgb.G;
+    const float& B = rgb.B;
+
+    auto M = std::max(R, std::max(G, B));
+    auto m = std::min(R, std::min(G, B));
+    auto C = M - m;  // chroma
+
+    float S;
+    float H;
+    float V = M;
+
+    if (V > 0)
+        S = C / V;
+    else
+        return ChVector<float>(-1, 0, 0); // undefined H
+
+    if (C > 0) {
+        if (R == V)
+            H = (G - B) / C;
+        else if (G == V)
+            H = 2 + (B - R) / C;
+        else
+            H = 4 + (R - G) / C;
+    } else {
+        H = 0;
+    }
+
+    H = H * 60.0;  // degrees
+    if (H < 0)
+        H += 360.0;
+
+    return ChVector<float>(H, S, V);
+}
+
+ChColor ChColor::HSV2RGB(const ChVector<float>& hsv) {
+    const float& H = hsv[0];
+    const float& S = hsv[1];
+    const float& V = hsv[2];
+
+    float C = V * S;  // chroma
+    float HPrime = std::fmod(H / 60.0f, 6.0f);
+    float X = C * (1 - std::fabs(std::fmod(HPrime, 2.0f) - 1));
+    float M = V - C;
+
+    ChColor c;
+
+    if (0 <= HPrime && HPrime < 1) {
+        c.R = C;
+        c.G = X;
+        c.B = 0;
+    } else if (1 <= HPrime && HPrime < 2) {
+        c.R = X;
+        c.G = C;
+        c.B = 0;
+    } else if (2 <= HPrime && HPrime < 3) {
+        c.R = 0;
+        c.G = C;
+        c.B = X;
+    } else if (3 <= HPrime && HPrime < 4) {
+        c.R = 0;
+        c.G = X;
+        c.B = C;
+    } else if (4 <= HPrime && HPrime < 5) {
+        c.R = X;
+        c.G = 0;
+        c.B = C;
+    } else if (5 <= HPrime && HPrime < 6) {
+        c.R = C;
+        c.G = 0;
+        c.B = X;
+    } else {
+        c.R = 0;
+        c.G = 0;
+        c.B = 0;
+    }
+
+    c.R += M;
+    c.G += M;
+    c.B += M;
+
+    return c;
+}
 
 ChColor ChColor::ComputeFalseColor(double v, double vmin, double vmax, bool out_of_range_as_bw) {
     ChColor c = {1.0, 1.0, 1.0};  // default white
@@ -51,7 +139,7 @@ ChColor ChColor::ComputeFalseColor(double v, double vmin, double vmax, bool out_
         c.B = 0;
     }
 
-    return (c);
+    return c;
 }
 
 void ChColor::ArchiveOUT(ChArchiveOut& marchive) {
