7cd05275119ce54c2182a099973d432fd8a0a993
Dario Fusai <dario.fusai@unipr.it>
2023-09-06 13:57:48 +0200

    Show  visual material of ChModelFileShape in Irrlicht

--- src/chrono_irrlicht/ChVisualSystemIrrlicht.cpp
+++ src/chrono_irrlicht/ChVisualSystemIrrlicht.cpp
@@ -812,6 +812,7 @@ void ChVisualSystemIrrlicht::PopulateIrrNode(ISceneNode* node,
                 mchildnode->setPosition(shape_m4.getTranslation());
                 mchildnode->setRotation(shape_m4.getRotationDegrees());
 
+                SetVisualMaterial(mchildnode, shape);
                 mchildnode->setMaterialFlag(video::EMF_BACK_FACE_CULLING, true);
             }
         } else if (auto trimesh = std::dynamic_pointer_cast<ChTriangleMeshShape>(shape)) {
