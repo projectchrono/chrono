a9be1496b066f64d87313b8475d446637277bbad
AaronYoung5 <aryoung@mit.edu>
2023-09-18 23:52:43 -0400

    Added docs

--- src/demos/ros/demo_ROS_vehicle.cpp
+++ src/demos/ros/demo_ROS_vehicle.cpp
@@ -1,3 +1,21 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2021 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Aaron Young
+// =============================================================================
+//
+// Demo to show the use of Chrono::Vehicle with ROS
+//
+// =============================================================================
+
 #include "chrono/core/ChTypes.h"
 
 #include "chrono/physics/ChSystemNSC.h"
