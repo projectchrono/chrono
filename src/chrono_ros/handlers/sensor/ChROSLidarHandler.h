f84d95a3d939d51b3b05168ec0ca4b5bbfcf4b2e
AaronYoung5 <aryoung@mit.edu>
2023-10-09 20:51:03 -0400

    Added LaserScan support for ChROSLidarHandler

--- src/chrono_ros/handlers/sensor/ChROSLidarHandler.h
+++ src/chrono_ros/handlers/sensor/ChROSLidarHandler.h
@@ -23,25 +23,29 @@
 
 #include "chrono_sensor/sensors/ChLidarSensor.h"
 
-#include "rclcpp/publisher.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-
 namespace chrono {
 namespace ros {
 
 /// @addtogroup ros_sensor_handlers
 /// @{
 
+class ChROSLidarHandlerImpl;
+
 /// This handler interfaces a ChLidarSensor to ROS. Will publish sensor_msgs::msg::PointCloud2.
 class ChROSLidarHandler : public ChROSHandler {
   public:
     /// Constructor. The update rate is set to lidar->GetUpdateRate().
-    ChROSLidarHandler(std::shared_ptr<chrono::sensor::ChLidarSensor> lidar, const std::string& topic_name);
+    ChROSLidarHandler(std::shared_ptr<chrono::sensor::ChLidarSensor> lidar,
+                      const std::string& topic_name,
+                      bool use_pc2 = true);
 
     /// Full constructor. Takes a ChLidarSensor, update rate, and topic name.
     ChROSLidarHandler(double update_rate,
                       std::shared_ptr<chrono::sensor::ChLidarSensor> lidar,
-                      const std::string& topic_name);
+                      const std::string& topic_name,
+                      bool use_pc2 = true);
+
+    ~ChROSLidarHandler() = default;
 
     /// Initializes the handler.
     virtual bool Initialize(std::shared_ptr<ChROSInterface> interface) override;
@@ -50,11 +54,7 @@ class ChROSLidarHandler : public ChROSHandler {
     virtual void Tick(double time) override;
 
   private:
-    std::shared_ptr<chrono::sensor::ChLidarSensor> m_lidar;  ///< handle to the lidar sensor
-
-    const std::string m_topic_name;                                           ///< name of the topic to publish to
-    sensor_msgs::msg::PointCloud2 m_lidar_msg;                                ///< message to publish
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_publisher;  ///< the publisher for the lidar message
+    std::shared_ptr<ChROSLidarHandlerImpl> m_impl;
 };
 
 /// @} ros_sensor_handlers
