3a5d77bd868b3dacc56e4d94f6a787167fdc53e6
Radu Serban <serban@wisc.edu>
2023-10-23 19:53:54 +0200

    Add model and data files for the Mercedes G-500 vehicle

---
+++ src/chrono_models/vehicle/gclass/G500_BrakeShafts.h
@@ -0,0 +1,67 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2014 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// GCLASS shafts-based brake model.
+//
+// =============================================================================
+
+#ifndef G500_BRAKE_SHAFTS_H
+#define G500_BRAKE_SHAFTS_H
+
+#include "chrono_vehicle/wheeled_vehicle/brake/ChBrakeShafts.h"
+
+#include "chrono_models/ChApiModels.h"
+
+namespace chrono {
+namespace vehicle {
+namespace gclass {
+
+/// @addtogroup vehicle_models_uaz
+/// @{
+
+/// Shafts-based UAZ front brake subsystem (uses a clutch between two shafts).
+class CH_MODELS_API G500_BrakeShaftsFront : public ChBrakeShafts {
+  public:
+    G500_BrakeShaftsFront(const std::string& name);
+    ~G500_BrakeShaftsFront() {}
+
+    virtual double GetMaxBrakingTorque() override { return m_maxtorque; }
+    virtual double GetShaftInertia() override { return m_shaft_inertia; }
+
+  private:
+    static const double m_maxtorque;
+    static const double m_shaft_inertia;
+};
+
+/// Shafts-based UAZ rear brake subsystem (uses a clutch between two shafts).
+class CH_MODELS_API G500_BrakeShaftsRear : public ChBrakeShafts {
+  public:
+    G500_BrakeShaftsRear(const std::string& name);
+    ~G500_BrakeShaftsRear() {}
+
+    virtual double GetMaxBrakingTorque() override { return m_maxtorque; }
+    virtual double GetShaftInertia() override { return m_shaft_inertia; }
+
+  private:
+    static const double m_maxtorque;
+    static const double m_shaft_inertia;
+};
+
+/// @} vehicle_models_uaz
+
+}  // end namespace uaz
+}  // end namespace vehicle
+}  // end namespace chrono
+
+#endif
