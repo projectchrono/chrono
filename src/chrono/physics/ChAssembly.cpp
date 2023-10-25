a7b5f45a644471aa06d772b58b595ecb7047db27
Chao Peng <chao.peng@unipr.it>
2023-10-25 16:56:14 +0800

    Add more asserts for safe

--- src/chrono/physics/ChAssembly.cpp
+++ src/chrono/physics/ChAssembly.cpp
@@ -230,10 +230,11 @@ void ChAssembly::RemoveMesh(std::shared_ptr<fea::ChMesh> mesh) {
 
 void ChAssembly::AddOtherPhysicsItem(std::shared_ptr<ChPhysicsItem> item) {
     assert(!std::dynamic_pointer_cast<ChBody>(item));
+    assert(!std::dynamic_pointer_cast<ChShaft>(item));
     assert(!std::dynamic_pointer_cast<ChLinkBase>(item));
     assert(!std::dynamic_pointer_cast<ChMesh>(item));
     assert(std::find(std::begin(otherphysicslist), std::end(otherphysicslist), item) == otherphysicslist.end());
-    // assert(item->GetSystem()==nullptr); // should remove from other system before adding here
+     assert(item->GetSystem()==nullptr); // should remove from other system before adding here
 
     // set system and also add collision models to system
     item->SetSystem(system);
