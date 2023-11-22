e200b6900c3f6655979ea821ee21128a01e4cf26
Radu Serban <serban@wisc.edu>
2023-11-22 15:41:23 +0100

    Update template projects to reflect new collision API

--- template_project_vehicle_cosim/vehicle_cosim.cpp
+++ template_project_vehicle_cosim/vehicle_cosim.cpp
@@ -105,7 +105,7 @@ MyTerrain::~MyTerrain() {
 
 void MyTerrain::OnInitialize(unsigned int num_tires) {
     // Create the rigid terrain box with its top surface at init height = 0
-    auto ground = std::shared_ptr<ChBody>(m_system->NewBody());
+    auto ground = chrono_types::make_shared<ChBody>();
     m_system->AddBody(ground);
     ground->SetMass(1);
     ground->SetBodyFixed(true);
@@ -121,10 +121,8 @@ void MyTerrain::OnInitialize(unsigned int num_tires) {
     mat_terrain->SetKt(4e5f);
     mat_terrain->SetGt(4e1f);
 
-    ground->GetCollisionModel()->Clear();
     utils::AddBoxGeometry(ground.get(), mat_terrain, ChVector<>(m_dimX, m_dimY, 0.2), ChVector<>(0, 0, -0.1),
                           ChQuaternion<>(1, 0, 0, 0), true);
-    ground->GetCollisionModel()->Build();
 
     // Shared proxy contact material
     auto mat_proxy = chrono_types::make_shared<ChMaterialSurfaceSMC>();
@@ -149,9 +147,7 @@ void MyTerrain::OnInitialize(unsigned int num_tires) {
         m_bodies[i]->SetInertiaXX(ChVector<>(0.1, 0.1, 0.1));
         m_bodies[i]->SetCollide(true);
 
-        m_bodies[i]->GetCollisionModel()->Clear();
         utils::AddCylinderGeometry(m_bodies[i].get(), mat_proxy, tire_radius, tire_width / 2);
-        m_bodies[i]->GetCollisionModel()->Build();
 
         m_bodies[i]->GetVisualShape(0)->SetTexture(GetChronoDataFile("textures/greenwhite.png"));
 
