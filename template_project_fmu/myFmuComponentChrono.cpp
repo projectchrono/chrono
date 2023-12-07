8c19de1b58bec5a070e80809bb8c324618f51f81
Radu Serban <serban@wisc.edu>
2023-12-07 14:42:55 +0100

    Update Chrono::FMI template projects to reflect fmu_tools API changes

--- template_project_fmu/myFmuComponentChrono.cpp
+++ template_project_fmu/myFmuComponentChrono.cpp
@@ -1,33 +1,49 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// External project template for building a Chrono co-simulation FMU.
+// =============================================================================
 
-//#define FMI2_FUNCTION_PREFIX MyModel_
+// #define FMI2_FUNCTION_PREFIX MyModel_
 #include <cassert>
 #include <vector>
 #include <array>
 #include <map>
 #include <algorithm>
-#include <chrono/core/ChVector.h>
-#include <chrono/serialization/ChArchive.h>
-#include <chrono_fmu/FmuToolsChrono.h>
-#include "FmuComponentChrono.h"
 
+#include "chrono/core/ChVector.h"
+#include "chrono/serialization/ChArchive.h"
+#include "chrono_fmi/FmuToolsChrono.h"
 
-FmuComponent::FmuComponent(fmi2String _instanceName, fmi2Type _fmuType, fmi2String _fmuGUID):
-        FmuComponentBase(_instanceName, _fmuType, _fmuGUID)
-{
+#include "myFmuComponentChrono.h"
+
+FmuComponent::FmuComponent(fmi2String _instanceName, fmi2Type _fmuType, fmi2String _fmuGUID)
+    : FmuComponentBase(_instanceName, _fmuType, _fmuGUID) {
     initializeType(_fmuType);
 
     SetChronoDataPath(CHRONO_DATA_DIR);
 
     /// FMU_ACTION: declare relevant variables
-    AddFmuVariable(&x_tt,            "x_tt",            FmuVariable::Type::Real, "m/s2",   "cart acceleration");
-    AddFmuVariable(&x_t,             "x_t",             FmuVariable::Type::Real, "m/s",    "cart velocity");
-    AddFmuVariable(&x,               "x",               FmuVariable::Type::Real, "m",      "cart position");
-    AddFmuVariable(&theta_tt,        "theta_tt",        FmuVariable::Type::Real, "rad/s2", "pendulum ang acceleration");
-    AddFmuVariable(&theta_t,         "theta_t",         FmuVariable::Type::Real, "rad/s",  "pendulum ang velocity");
-    AddFmuVariable(&theta,           "theta",           FmuVariable::Type::Real, "rad",    "pendulum angle");
-    AddFmuVariable(&pendulum_length, "pendulum_length", FmuVariable::Type::Real, "m",      "pendulum length", FmuVariable::CausalityType::parameter, FmuVariable::VariabilityType::fixed);
-    AddFmuVariable(&cart_mass,       "cart_mass",       FmuVariable::Type::Real, "kg",     "pendulum mass", FmuVariable::CausalityType::parameter, FmuVariable::VariabilityType::fixed);
-    AddFmuVariable(&pendulum_mass,   "pendulum_mass",   FmuVariable::Type::Real, "kg",     "cart mass", FmuVariable::CausalityType::parameter, FmuVariable::VariabilityType::fixed);
+    AddFmuVariable(&x_tt, "x_tt", FmuVariable::Type::Real, "m/s2", "cart acceleration");
+    AddFmuVariable(&x_t, "x_t", FmuVariable::Type::Real, "m/s", "cart velocity");
+    AddFmuVariable(&x, "x", FmuVariable::Type::Real, "m", "cart position");
+    AddFmuVariable(&theta_tt, "theta_tt", FmuVariable::Type::Real, "rad/s2", "pendulum ang acceleration");
+    AddFmuVariable(&theta_t, "theta_t", FmuVariable::Type::Real, "rad/s", "pendulum ang velocity");
+    AddFmuVariable(&theta, "theta", FmuVariable::Type::Real, "rad", "pendulum angle");
+    AddFmuVariable(&pendulum_length, "pendulum_length", FmuVariable::Type::Real, "m", "pendulum length",
+                   FmuVariable::CausalityType::parameter, FmuVariable::VariabilityType::fixed);
+    AddFmuVariable(&cart_mass, "cart_mass", FmuVariable::Type::Real, "kg", "pendulum mass",
+                   FmuVariable::CausalityType::parameter, FmuVariable::VariabilityType::fixed);
+    AddFmuVariable(&pendulum_mass, "pendulum_mass", FmuVariable::Type::Real, "kg", "cart mass",
+                   FmuVariable::CausalityType::parameter, FmuVariable::VariabilityType::fixed);
 
     auto ground = chrono_types::make_shared<ChBody>();
     ground->SetBodyFixed(true);
@@ -51,12 +67,14 @@ FmuComponent::FmuComponent(fmi2String _instanceName, fmi2Type _fmuType, fmi2Stri
     sys.Add(cart_prism);
 
     auto pendulum_rev = chrono_types::make_shared<ChLinkRevolute>();
-    pendulum_rev->Initialize(pendulum, cart, true, ChFrame<>(VNULL, QUNIT), ChFrame<>(ChVector<>(0, +pendulum_length/2, 0), QUNIT));
+    pendulum_rev->Initialize(pendulum, cart, true, ChFrame<>(VNULL, QUNIT),
+                             ChFrame<>(ChVector<>(0, +pendulum_length / 2, 0), QUNIT));
     pendulum_rev->SetName("pendulum_rev");
     sys.Add(pendulum_rev);
 
     sys.DoFullAssembly();
 
+#ifdef CHRONO_IRRLICHT
     vis = chrono_types::make_shared<irrlicht::ChVisualSystemIrrlicht>();
     vis->AttachSystem(&sys);
     vis->SetWindowSize(800, 600);
@@ -66,10 +84,12 @@ FmuComponent::FmuComponent(fmi2String _instanceName, fmi2Type _fmuType, fmi2Stri
     vis->AddSkyBox();
     vis->AddCamera(ChVector<>(-0.5, -0.5, -1.0));
     vis->AddTypicalLights();
+#endif
 
-    updateVarsCallbacks.push_back([this](){ x_tt = this->sys.SearchBodyID(10)->GetPos_dtdt().x(); });
-    updateVarsCallbacks.push_back([this](){ x_t = this->sys.SearchBodyID(10)->GetPos_dt().x(); });
-    updateVarsCallbacks.push_back([this](){ x = this->sys.SearchBodyID(10)->GetPos().x(); });
+    // Specify functions to calculate FMU outputs (at end of step)
+    postStepCallbacks.push_back([this]() { x_tt = this->sys.SearchBodyID(10)->GetPos_dtdt().x(); });
+    postStepCallbacks.push_back([this]() { x_t = this->sys.SearchBodyID(10)->GetPos_dt().x(); });
+    postStepCallbacks.push_back([this]() { x = this->sys.SearchBodyID(10)->GetPos().x(); });
 };
 
 void FmuComponent::_preModelDescriptionExport() {
@@ -78,48 +98,37 @@ void FmuComponent::_preModelDescriptionExport() {
     archive_fmu << CHNVP(sys);
 }
 
-void FmuComponent::_postModelDescriptionExport() {
-
-}
-
+void FmuComponent::_postModelDescriptionExport() {}
 
 void FmuComponent::_exitInitializationMode() {
-
     sys.DoFullAssembly();
 };
 
-fmi2Status FmuComponent::_doStep(fmi2Real currentCommunicationPoint, fmi2Real communicationStepSize, fmi2Boolean noSetFMUStatePriorToCurrentPoint) {
+fmi2Status FmuComponent::_doStep(fmi2Real currentCommunicationPoint,
+                                 fmi2Real communicationStepSize,
+                                 fmi2Boolean noSetFMUStatePriorToCurrentPoint) {
+    while (time < currentCommunicationPoint + communicationStepSize) {
+        fmi2Real _stepSize = std::min((currentCommunicationPoint + communicationStepSize - time),
+                                      std::min(communicationStepSize, stepSize));
 
-    while (time < currentCommunicationPoint + communicationStepSize){
-        fmi2Real _stepSize = std::min((currentCommunicationPoint + communicationStepSize - time), std::min(communicationStepSize, stepSize));
-
-        if (vis)
-        {        
+#ifdef CHRONO_IRRLICHT
+        if (vis) {
+            vis->Run();
             vis->BeginScene();
             vis->Render();
+            vis->EndScene();
         }
+#endif
 
         sys.DoStepDynamics(_stepSize);
-        sendToLog("Step at time: " + std::to_string(time) + " with timestep: " + std::to_string(_stepSize) + "ms succeeded.\n", fmi2Status::fmi2OK, "logAll");
-        updateVars();
+        sendToLog("Step at time: " + std::to_string(time) + " with timestep: " + std::to_string(_stepSize) +
+                      "ms succeeded.\n",
+                  fmi2Status::fmi2OK, "logAll");
 
         time = time + _stepSize;
 
         realtime_timer.Spin(_stepSize);
-
-        
-        if (vis)
-        {        
-            vis->EndScene();
-        }
-
-
-
-
     }
-    
+
     return fmi2Status::fmi2OK;
 }
-
-
-
--- template_project_fmu/myFmuComponentChrono.h
+++ template_project_fmu/myFmuComponentChrono.h
@@ -1,56 +1,61 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// External project template for building a Chrono co-simulation FMU.
+// =============================================================================
 
 #pragma once
-//#define FMI2_FUNCTION_PREFIX MyModel_
+
 #include <FmuToolsExport.h>
 #include <vector>
 #include <array>
 
-#include <chrono/physics/ChSystemNSC.h>
-#include <chrono/physics/ChBodyEasy.h>
+#include "chrono/physics/ChSystemNSC.h"
+#include "chrono/physics/ChBodyEasy.h"
 #include "chrono/core/ChRealtimeStep.h"
 
 #ifdef CHRONO_IRRLICHT
-#include "chrono_irrlicht/ChVisualSystemIrrlicht.h"
+    #include "chrono_irrlicht/ChVisualSystemIrrlicht.h"
 #endif
-
-
 
 using namespace chrono;
 
-
-class FmuComponent: public FmuComponentBase{
-public:
+class FmuComponent : public FmuComponentBase {
+  public:
     FmuComponent(fmi2String _instanceName, fmi2Type _fmuType, fmi2String _fmuGUID);
 
-    virtual ~FmuComponent(){}
+    virtual ~FmuComponent() {}
 
     /// FMU_ACTION: override _doStep of the base class with the problem-specific implementation
-    virtual fmi2Status _doStep(fmi2Real currentCommunicationPoint, fmi2Real communicationStepSize, fmi2Boolean noSetFMUStatePriorToCurrentPoint) override;
+    virtual fmi2Status _doStep(fmi2Real currentCommunicationPoint,
+                               fmi2Real communicationStepSize,
+                               fmi2Boolean noSetFMUStatePriorToCurrentPoint) override;
 
-
-
-protected:
-
+  protected:
     virtual void _exitInitializationMode() override;
 
     virtual void _preModelDescriptionExport() override;
     virtual void _postModelDescriptionExport() override;
 
-    virtual bool is_cosimulation_available() const override { return true;}
-    virtual bool is_modelexchange_available() const override { return false;}
+    virtual bool is_cosimulation_available() const override { return true; }
+    virtual bool is_modelexchange_available() const override { return false; }
 
-    // Problem-specific data members   
+    // Problem-specific data members
     ChSystemNSC sys;
-    
+
     ChRealtimeStepTimer realtime_timer;
 
 #ifdef CHRONO_IRRLICHT
     std::shared_ptr<chrono::irrlicht::ChVisualSystemIrrlicht> vis;
 #endif
-
-
-
-
 
     double x_tt;
     double x_t;
@@ -61,14 +66,9 @@ protected:
     double pendulum_length = 0.5;
     double pendulum_mass = 1.0;
     double cart_mass = 1.0;
-
-
 };
 
-
 // FMU_ACTION:: implement the following functions
-FmuComponentBase* fmi2Instantiate_getPointer(fmi2String instanceName, fmi2Type fmuType, fmi2String fmuGUID){
+FmuComponentBase* fmi2Instantiate_getPointer(fmi2String instanceName, fmi2Type fmuType, fmi2String fmuGUID) {
     return new FmuComponent(instanceName, fmuType, fmuGUID);
 }
-
-
