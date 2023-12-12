40418b9486706aadbb60a6e516a200c4e874b249
Radu Serban <serban@wisc.edu>
2023-12-12 09:08:11 +0100

    Add missing diagonal "mass" term to the KRM block of ChExternalDynamics

--- src/chrono/physics/ChExternalDynamics.cpp
+++ src/chrono/physics/ChExternalDynamics.cpp
@@ -204,8 +204,8 @@ void ChExternalDynamics::IntFromDescriptor(const unsigned int off_v,  // offset 
 
 void ChExternalDynamics::KRMmatricesLoad(double Kfactor, double Rfactor, double Mfactor) {
     if (IsStiff()) {
-        // Recall to flip sign to load K = -dQ/dx and R = -dQ/dv
-        m_KRM.Get_K() = -Rfactor * m_jac;
+        // Recall to flip sign to load R = -dQ/dv (K is zero here)
+        m_KRM.Get_K() = Mfactor * ChMatrixDynamic<>::Identity(m_nstates, m_nstates) - Rfactor * m_jac;
     }
 }
 
