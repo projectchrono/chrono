38a40acffbcd4463e15c70b9c0c1877be784b307
Radu Serban <serban@wisc.edu>
2024-01-21 11:15:29 +0100

    Revert to using force accumulators to apply tire-terrain forces on wheeled vehicle spindles

--- src/chrono_vehicle/wheeled_vehicle/ChWheel.cpp
+++ src/chrono_vehicle/wheeled_vehicle/ChWheel.cpp
@@ -100,9 +100,12 @@ void ChWheel::Synchronize() {
 }
 
 void ChWheel::Synchronize(const TerrainForce& tire_force) {
-    m_spindle_terrain_force->SetForce(tire_force.force, false);
-    m_spindle_terrain_force->SetApplicationPoint(tire_force.point, false);
-    m_spindle_terrain_torque->SetTorque(tire_force.moment, false);
+    m_spindle->Accumulate_force(tire_force.force, tire_force.point, false);
+    m_spindle->Accumulate_torque(tire_force.moment, false);
+
+    ////m_spindle_terrain_force->SetForce(tire_force.force, false);
+    ////m_spindle_terrain_force->SetApplicationPoint(tire_force.point, false);
+    ////m_spindle_terrain_torque->SetTorque(tire_force.moment, false);
 }
 
 ChVector<> ChWheel::GetPos() const {
