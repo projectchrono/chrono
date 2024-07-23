ce61dfe1d9e1259a61ddc39c2a46ae884b9f66a1
Radu Serban <serban@wisc.edu>
2024-07-23 19:53:15 +0200

    Update FMI 2.0 template project

--- template_project_fmi2/FmuComponentChrono.h
+++ template_project_fmi2/FmuComponentChrono.h
@@ -9,7 +9,7 @@
 // http://projectchrono.org/license-chrono.txt.
 //
 // =============================================================================
-// External project template for building a Chrono co-simulation FMU.
+// External project template for building a Chrono co-simulation FMU for FMI 2.0.
 // =============================================================================
 
 #pragma once
@@ -23,7 +23,7 @@
     #include "chrono_irrlicht/ChVisualSystemIrrlicht.h"
 #endif
 
-class FmuComponent : public chrono::FmuChronoComponentBase {
+class FmuComponent : public chrono::fmi2::FmuChronoComponentBase {
   public:
     FmuComponent(fmi2String instanceName,
                  fmi2Type fmuType,
@@ -35,16 +35,15 @@ class FmuComponent : public chrono::FmuChronoComponentBase {
 
     virtual ~FmuComponent() {}
 
-    /// FMU_ACTION: override _doStep of the base class with the problem-specific implementation
-    virtual fmi2Status _doStep(fmi2Real currentCommunicationPoint,
-                               fmi2Real communicationStepSize,
-                               fmi2Boolean noSetFMUStatePriorToCurrentPoint) override;
+    virtual fmi2Status doStepIMPL(fmi2Real currentCommunicationPoint,
+                                  fmi2Real communicationStepSize,
+                                  fmi2Boolean noSetFMUStatePriorToCurrentPoint) override;
 
   protected:
-    virtual void _exitInitializationMode() override;
+    virtual fmi2Status exitInitializationModeIMPL() override;
 
-    virtual void _preModelDescriptionExport() override;
-    virtual void _postModelDescriptionExport() override;
+    virtual void preModelDescriptionExport() override;
+    virtual void postModelDescriptionExport() override;
 
     virtual bool is_cosimulation_available() const override { return true; }
     virtual bool is_modelexchange_available() const override { return false; }
@@ -68,14 +67,3 @@ class FmuComponent : public chrono::FmuChronoComponentBase {
     double cart_mass = 1.0;
     std::string experiment_name;
 };
-
-// FMU_ACTION:: implement the following functions
-FmuComponentBase* fmi2Instantiate_getPointer(fmi2String instanceName,
-                                             fmi2Type fmuType,
-                                             fmi2String fmuGUID,
-                                             fmi2String fmuResourceLocation,
-                                             const fmi2CallbackFunctions* functions,
-                                             fmi2Boolean visible,
-                                             fmi2Boolean loggingOn) {
-    return new FmuComponent(instanceName, fmuType, fmuGUID, fmuResourceLocation, functions, visible, loggingOn);
-}
