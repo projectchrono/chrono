22cf82cb6c26ee2829780b8f43ea7c6c531b564f
Dario Fusai <dario.fusai93@gmail.com>
2024-08-28 17:01:51 +0200

    Add util wrap angle function

--- src/chrono/utils/ChUtils.h
+++ src/chrono/utils/ChUtils.h
@@ -45,6 +45,23 @@ int ChSignum(T x) {
     return (x > T(0)) - (x < T(0));
 }
 
+/// Wrap angle in range.
+/// If symmetric, wrap in [-PI; PI).
+/// If not symmetric, wrap in [0; 2PI).
+template <typename T>
+T ChWrapAngle(T angle, bool symmetric = true) {
+    T wangle = angle;
+    if (symmetric) {  // [-PI; +PI)
+        wangle = std::fmod(wangle + CH_PI, CH_2PI);
+        wangle < 0 ? wangle += CH_PI : wangle -= CH_PI;
+    } else {  // [0; 2PI)
+        wangle = std::fmod(wangle, CH_2PI);
+        if (wangle < 0)
+            wangle += CH_2PI;
+    }
+    return wangle;
+}
+
 }  // end namespace chrono
 
 #endif
