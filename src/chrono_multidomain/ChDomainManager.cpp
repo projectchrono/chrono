ae1265bb09aaf609f047b928bdcfb6b35819c4ae
tasora <tasora@ied.unipr.it>
2024-09-22 01:47:09 +0200

    Helper class for creating a map of all pointers inside a complex object (ex a ChSystem) by traversing it via the same ArchiveIn ArchiveOut functions already used for serialization. If the object(s) implement a GetTag() function, it is used to fill a map of tags-pointers, that can be used for the Rebind feature of the ChArchiveIn.

--- src/chrono_multidomain/ChDomainManager.cpp
+++ src/chrono_multidomain/ChDomainManager.cpp
@@ -29,6 +29,60 @@ namespace multidomain {
 using namespace fea;
 
 
+//
+///////////////////////
+
+/// Helper class that simply builds a map of pointers-tags for all pointers
+/// and custom c++ objects embedded in an object (mostly the ChSystem), that is traversed like in a serialization. 
+/// The map can be used for debugging or to be used as a ExternalPointersMap()
+/// of an ChArchiveIn, for transplanting data to be re-bind incrementally.
+
+class ChArchiveOutPointerMap : public ChArchiveOut {
+public:
+	ChArchiveOutPointerMap() {}
+
+	virtual ~ChArchiveOutPointerMap() {}
+
+	virtual void out(ChNameValue<bool> bVal) {};
+	virtual void out(ChNameValue<int> bVal) {};
+	virtual void out(ChNameValue<double> bVal) {};
+	virtual void out(ChNameValue<float> bVal) {};
+	virtual void out(ChNameValue<char> bVal) {};
+	virtual void out(ChNameValue<unsigned int> bVal) {};
+	virtual void out(ChNameValue<unsigned long> bVal) {};
+	virtual void out(ChNameValue<unsigned long long> bVal) {};
+	virtual void out(ChNameValue<ChEnumMapperBase> bVal) {};
+	virtual void out(ChNameValue<const char*> bVal) {};
+	virtual void out(ChNameValue<std::string> bVal) {};
+	virtual void out_array_pre(ChValue& bVal, size_t size) {};
+	virtual void out_array_between(ChValue& bVal, size_t size) {};
+	virtual void out_array_end(ChValue& bVal, size_t size) {};
+
+	virtual void out(ChValue& bVal, bool tracked, size_t obj_ID) {
+		if (process_objects_byval && bVal.HasGetTag()) {
+			this->pointer_map_id_ptr[bVal.CallGetTag()] = bVal.GetRawPtr();
+			this->pointer_map_ptr_id[bVal.GetRawPtr()] = bVal.CallGetTag();
+		}
+		bVal.CallArchiveOut(*this);
+	}
+	virtual void out_ref(ChValue& bVal, bool already_inserted, size_t obj_ID, size_t ext_ID) {
+		if (!already_inserted) {
+			if (process_objects_byref && bVal.HasGetTag()) {
+				this->pointer_map_id_ptr[bVal.CallGetTag()] = bVal.GetRawPtr();
+				this->pointer_map_ptr_id[bVal.GetRawPtr()] = bVal.CallGetTag();
+			}
+			bVal.CallArchiveOut(*this);
+		}
+	}
+
+	bool process_objects_byval = false;
+	bool process_objects_byref = true;
+
+	/// Result goes here:
+	std::unordered_map<size_t, void*> pointer_map_id_ptr;
+	std::unordered_map<void*, size_t> pointer_map_ptr_id;
+};
+
 
 
 void ChDomainManager::PrintDebugDomainInfo(std::shared_ptr<ChDomain> domain) {
