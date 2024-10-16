d9046f9e7109ab2306ac719bf6a558061c2940b9
DarioMangoni <dariomangoni@gmail.com>
2024-10-16 14:26:47 +0200

    Split Eigen3 and Irrlicht dependencies

---
+++ cmake/FindIrrlicht.cmake
@@ -0,0 +1,120 @@
+
+#-------------------------------------------------------------------------------
+# Find IRRLICHT
+# This find script requires the following input variables:
+# IRRLICHT_DIR: location of irrlicht root directory
+# This find script provides the following output variables:
+# IRRLICHT_FOUND: if Irrlicht has been found
+# IRRLICHT_INCLUDE_DIRS: full path to include directories for Irrlicht
+# IRRLICHT_LIBRARIES: full path to Irrlicht libraries and its dependencies
+# IRRLICHT_DLL: full path to Irrlicht DLLs (Win only)
+# IRRLICHT_C_FLAGS | IRRLICHT_CXX_FLAGS: compiler flags for Irrlicht
+#
+# other variable are intended only for internal use
+#-------------------------------------------------------------------------------
+
+mark_as_advanced(IRRLICHT_LIBRARY_PATHS IRRLICHT_INCLUDE_PATHS)
+
+# Setting search paths for include and libraries
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+  if (NOT DEFINED "IRRLICHT_DIR")
+    message(STATUS "Looking for IRRLICHT in default find_package paths.")
+  else()
+    message(STATUS "Looking for IRRLICHT in IRRLICHT_DIR: ${IRRLICHT_DIR}")
+  endif()
+
+  set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include")
+
+  if(MSVC)
+    if (CMAKE_SIZEOF_VOID_P MATCHES 4)
+      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win32-visualstudio")
+      set(IRRLICHT_DLL "${IRRLICHT_DIR}/bin/Win32-VisualStudio/Irrlicht.dll")
+    else()
+      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win64-visualStudio")
+      set(IRRLICHT_DLL "${IRRLICHT_DIR}/bin/Win64-VisualStudio/Irrlicht.dll")
+    endif()
+  elseif(MINGW OR CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG OR CMAKE_COMPILER_IS_CLANGXX)
+    if (CMAKE_SIZEOF_VOID_P MATCHES 4)
+      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win32-gcc")
+    else()
+      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win64-gcc")
+    endif()
+  else()
+    if(DEFINED ENV{CONDA_BUILD})
+      set(IRRLICHT_DLL "$ENV{PREFIX}/Library/bin/Irrlicht.dll")
+    endif()
+  endif()
+
+  message(STATUS "IRRLICHT_DLL: ${IRRLICHT_DLL}")
+
+  set(IRRLICHT_DEPENDENCY_LIBS "")
+  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+
+  set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include" "/usr/include/irrlicht" "/usr/local/include/irrlicht")
+  set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Linux")
+  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
+  set(IRRLICHT_DEPENDENCY_LIBS "Xxf86vm;glut;X11;GL")
+
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+
+  set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include" "/usr/include/irrlicht" "/usr/local/include/irrlicht")
+  set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Linux" "/usr/local/lib")
+  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "/System/Library/Frameworks")
+
+  find_library(COCOA_LIBRARY Cocoa NO_CACHE)
+  find_library(OPENGL_LIBRARY OpenGL NO_CACHE)
+  find_library(IOKIT_LIBRARY IOKit NO_CACHE)
+  if (DEFINED COCOA_LIBRARY-NOTFOUND)
+    message(FATAL_ERROR "Could not find IRRLICHT dependency library: Cocoa.")
+    return()
+  endif()
+  if (DEFINED OPENGL_LIBRARY-NOTFOUND)
+    message(FATAL_ERROR "Could not find IRRLICHT dependency library: OpenGL.")
+    return()
+  endif()
+  if (DEFINED IOKIT_LIBRARY-NOTFOUND)
+    message(FATAL_ERROR "Could not find IRRLICHT dependency library: IOKit.")
+    return()
+  endif()
+  set(IRRLICHT_DEPENDENCY_LIBS "${COCOA_LIBRARY};${OPENGL_LIBRARY};${IOKIT_LIBRARY}")
+
+else()
+
+  set(IRRLICHT_INCLUDE_PATHS "/usr/include/irrlicht" "/usr/local/include/irrlicht")
+  set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/../../lib")
+  set(IRRLICHT_DEPENDENCY_LIBS "")
+  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
+
+endif()
+
+# Find IRRLICHT headers
+find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h PATHS ${IRRLICHT_INCLUDE_PATHS} NO_CACHE)
+message(STATUS "IRRLICHT_INCLUDE_DIR: ${IRRLICHT_INCLUDE_DIR}")
+if (IRRLICHT_INCLUDE_DIR MATCHES IRRLICHT_INCLUDE_DIR-NOTFOUND)
+  message(FATAL_ERROR "Could not find 'irrlicht.h' in any of the following directories: ${IRRLICHT_INCLUDE_PATHS}")
+  return()
+endif()
+set(IRRLICHT_INCLUDE_DIRS ${IRRLICHT_INCLUDE_DIR} ${IRRLICHT_DEPENDENCY_INCLUDE_DIRS})
+
+# Find IRRLICHT library
+find_library(IRRLICHT_LIBRARY NAMES IRRLICHT PATHS ${IRRLICHT_LIBRARY_PATHS} NO_CACHE)
+message(STATUS "IRRLICHT_LIBRARY: ${IRRLICHT_LIBRARY}")
+if (IRRLICHT_LIBRARY MATCHES IRRLICHT_LIBRARY-NOTFOUND)
+  message(FATAL_ERROR "Could not find 'IRRLICHT.${CMAKE_IMPORT_LIBRARY_SUFFIX}' in any of the following directories: ${IRRLICHT_LIBRARY_PATHS}")
+  return()
+endif()
+set(IRRLICHT_LIBRARIES ${IRRLICHT_LIBRARY} ${IRRLICHT_DEPENDENCY_LIBS})
+
+set(IRRLICHT_FOUND TRUE)
+
+
+# IRRLICHT-specific compiler flags
+set(IRRLICHT_CXX_FLAGS "")
+set(IRRLICHT_C_FLAGS "")
+if(MSVC)
+  # If using MSVC, disable warning 4275 (non-DLL-interface class used as base for DLL-interface class)
+  ###add_compile_options(/wd4275)
+  set(IRRLICHT_CXX_FLAGS "${IRRLICHT_CXX_FLAGS} /wd4275")
+  set(IRRLICHT_C_FLAGS "${IRRLICHT_C_FLAGS} /wd4275")
+endif()
