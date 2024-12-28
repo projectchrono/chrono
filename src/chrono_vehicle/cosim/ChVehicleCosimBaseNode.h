3495dc61c6fe898a407375a903227b6d87ad0bda
Josh Diyn <joshuadiyn@gmail.com>
2024-12-28 13:45:07 +1000

    Include ChSystem header in ChVehicleCosimBaseNode.h
    Added an include for ChSystem.h, since there was no context for the ChSystem on line 238 when building.

--- src/chrono_vehicle/cosim/ChVehicleCosimBaseNode.h
+++ src/chrono_vehicle/cosim/ChVehicleCosimBaseNode.h
@@ -30,6 +30,8 @@
 #include "chrono/core/ChVector3.h"
 #include "chrono/core/ChQuaternion.h"
 
+#include "chrono/physics/ChSystem.h"
+
 #include "chrono/utils/ChBodyGeometry.h"
 
 #include "chrono_vehicle/ChApiVehicle.h"
