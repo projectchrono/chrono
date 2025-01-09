b334333d9d8edbf442c17ee40edbb5c7610f0d72
DarioMangoni <dariomangoni@gmail.com>
2025-01-09 19:06:12 +0100

    Updated CSharp to use Modern CMake

--- cmake/FindIrrlicht.cmake
+++ cmake/FindIrrlicht.cmake
@@ -17,15 +17,14 @@ mark_as_advanced(IRRLICHT_LIBRARY_PATHS IRRLICHT_INCLUDE_PATHS)
 
 # Setting search paths for include and libraries
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  if (NOT DEFINED "IRRLICHT_DIR")
-    message(STATUS "Looking for IRRLICHT in default find_package paths.")
-  else()
-    message(STATUS "Looking for IRRLICHT in IRRLICHT_DIR: ${IRRLICHT_DIR}")
-  endif()
-
   set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include")
 
-  if(MSVC)
+  get_property(CURRENT_ENABLED_LANGUAGES GLOBAL PROPERTY ENABLED_LANGUAGES)
+  list(FIND CURRENT_ENABLED_LANGUAGES CSharp LANG_INDEX)
+  mark_as_advanced(FORCE LANG_INDEX)
+  mark_as_advanced(FORCE CURRENT_ENABLED_LANGUAGES)
+
+  if(MSVC OR LANG_INDEX GREATER_EQUAL 0)
     if (CMAKE_SIZEOF_VOID_P MATCHES 4)
       set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win32-visualstudio")
       set(IRRLICHT_DLL "${IRRLICHT_DIR}/bin/Win32-VisualStudio/Irrlicht.dll")
@@ -44,9 +43,6 @@ if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
       set(IRRLICHT_DLL "$ENV{PREFIX}/Library/bin/Irrlicht.dll")
     endif()
   endif()
-
-  message(STATUS "IRRLICHT_DLL: ${IRRLICHT_DLL}")
-
   set(IRRLICHT_DEPENDENCY_LIBS "")
   set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
@@ -90,20 +86,16 @@ endif()
 
 # Find IRRLICHT headers
 find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h PATHS ${IRRLICHT_INCLUDE_PATHS} NO_CACHE)
-message(STATUS "IRRLICHT_INCLUDE_DIR: ${IRRLICHT_INCLUDE_DIR}")
 if (IRRLICHT_INCLUDE_DIR MATCHES IRRLICHT_INCLUDE_DIR-NOTFOUND)
-  message(FATAL_ERROR "Could not find 'irrlicht.h' in any of the following directories: ${IRRLICHT_INCLUDE_PATHS}. Set IRRLICHT_DIR to the Irrlicht SDK installation directory.")
-  set(IRRLICHT_DIR "C:/irrlicht-1.8.5" CACHE PATH "Path to Irrlicht SDK installation.")
+  message(FATAL_ERROR "Could not find 'irrlicht.h' in any of the following directories: ${IRRLICHT_INCLUDE_PATHS}.\nSet IRRLICHT_DIR to the Irrlicht SDK installation directory.")
   return()
 endif()
 set(IRRLICHT_INCLUDE_DIRS ${IRRLICHT_INCLUDE_DIR} ${IRRLICHT_DEPENDENCY_INCLUDE_DIRS})
 
 # Find IRRLICHT library
-find_library(IRRLICHT_LIBRARY NAMES IRRLICHT PATHS ${IRRLICHT_LIBRARY_PATHS} NO_CACHE)
-message(STATUS "IRRLICHT_LIBRARY: ${IRRLICHT_LIBRARY}")
+find_library(IRRLICHT_LIBRARY NAMES Irrlicht PATHS ${IRRLICHT_LIBRARY_PATHS} NO_CACHE)
 if (IRRLICHT_LIBRARY MATCHES IRRLICHT_LIBRARY-NOTFOUND)
-  message(FATAL_ERROR "Could not find 'IRRLICHT.${CMAKE_IMPORT_LIBRARY_SUFFIX}' in any of the following directories: ${IRRLICHT_LIBRARY_PATHS}. Set IRRLICHT_DIR to the Irrlicht SDK installation directory.")
-  set(IRRLICHT_DIR "C:/irrlicht-1.8.5" CACHE PATH "Path to Irrlicht SDK installation.")
+  message(FATAL_ERROR "Could not find 'irrlicht${CMAKE_IMPORT_LIBRARY_SUFFIX}' in any of the following directories: ${IRRLICHT_LIBRARY_PATHS}.\nSet IRRLICHT_DIR to the Irrlicht SDK installation directory.")
   return()
 endif()
 set(IRRLICHT_LIBRARIES ${IRRLICHT_LIBRARY} ${IRRLICHT_DEPENDENCY_LIBS})
