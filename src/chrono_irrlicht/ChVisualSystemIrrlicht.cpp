45948caba978f05cfeac8aab3d63f3643690e0fd
Dario Fusai <dario.fusai93@gmail.com>
2025-01-27 12:01:47 +0100

    Revert modification on Irrlicht nodes assert to fix execution in release

--- src/chrono_irrlicht/ChVisualSystemIrrlicht.cpp
+++ src/chrono_irrlicht/ChVisualSystemIrrlicht.cpp
@@ -704,7 +704,8 @@ void ChVisualSystemIrrlicht::CreateIrrNode(std::shared_ptr<ChPhysicsItem> item) 
 
     // Create a new ChIrrNodeModel and populate it
     auto node = chrono_types::make_shared<ChIrrNodeModel>(item, m_container, GetSceneManager(), 0);
-    assert(m_nodes.insert({item.get(), node}).second);
+    bool ok = m_nodes.insert({item.get(), node}).second;
+    assert(ok);
 
     // Remove all Irrlicht scene nodes from the ChIrrNodeModel
     node->removeAll();
