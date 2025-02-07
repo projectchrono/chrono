6895fc0a318506b7835398a4120eadb83cc36fb5
Radu Serban <serban@wisc.edu>
2025-02-07 13:23:32 +0100

    Various fixes to the Chrono build system and project configuration script:
    - refactor FindIrrlicht to work on all supported platforms, both during Chrono configuration and used from chrono-config
    - fix issues with Chrono data location when using a build tree Chrono configuration vs. an install tree configuration
    - refactor chrono-config script

--- cmake/FindIrrlicht.cmake
+++ cmake/FindIrrlicht.cmake
@@ -1,63 +1,168 @@
-
 #-------------------------------------------------------------------------------
 # Find Irrlicht
-# This find script requires the following input variables:
-# IRRLICHT_DIR: location of irrlicht root directory
-# This find script provides the following output variables:
-# IRRLICHT_FOUND: if Irrlicht has been found
-# IRRLICHT_INCLUDE_DIRS: full path to include directories for Irrlicht
-# IRRLICHT_LIBRARIES: full path to Irrlicht libraries and its dependencies
-# IRRLICHT_DLL: full path to Irrlicht DLLs (Win only)
-# IRRLICHT_C_FLAGS | IRRLICHT_CXX_FLAGS: compiler flags for Irrlicht
 #
-# other variable are intended only for internal use
+# Input variables
+#    Irrlicht_ROOT: location of irrlicht root directory
+#    Irrlicht_INCLUDE_DIR: full path to the Irrlicht include directory
+#    Irrlicht_LIBRARY: full path to the Irrlicht library
+#    Irrlicht_DLL: full path to the Irrlicht DLL (windows only)
+# Use first Irrlicht_INCLUDE_DIR, Irrlicht_LIBRARY, and Irrlicht_DLL if defined.
+# Otherwise, if Irrlicht_ROOT is provided, look for the Irrlicht header and library
+# in IRRLICHT_ROOT and standard installatio ndirectories.
+# If Irrlicht still not found, ask the caller to provide IRRLICHT_ROOT and, if
+# still needed, Irrlicht_INCLUDE_DIR, Irrlicht_LIBRARY, and Irrlicht_DLL.
+#
+# Output variables
+#    Irrlicht_FOUND: true, if Irrlicht has been found
+#    Irrlicht::Irrlicht target
+# Additional output variables
+#    IRRLICHT_INCLUDE_DIR
+#    IRRLICHT_LIBRARY
+#    IRRLICHT_DLL
 #-------------------------------------------------------------------------------
 
 mark_as_advanced(IRRLICHT_LIBRARY_PATHS IRRLICHT_INCLUDE_PATHS)
 
-# Setting search paths for include and libraries
-if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include")
+set(Irrlicht_FOUND TRUE)
 
-  get_property(CURRENT_ENABLED_LANGUAGES GLOBAL PROPERTY ENABLED_LANGUAGES)
-  list(FIND CURRENT_ENABLED_LANGUAGES CSharp LANG_INDEX)
-  mark_as_advanced(FORCE LANG_INDEX)
-  mark_as_advanced(FORCE CURRENT_ENABLED_LANGUAGES)
+# ----------------------------------------------------------------------------------------------------
+# 1. Look for Irrlicht using Irrlicht_INCLUDE_DIR and Irrlicht_LIBRARY if both provided
+# ----------------------------------------------------------------------------------------------------
 
-  if(MSVC OR LANG_INDEX GREATER_EQUAL 0)
-    if (CMAKE_SIZEOF_VOID_P MATCHES 4)
-      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win32-visualstudio")
-      set(IRRLICHT_DLL "${IRRLICHT_DIR}/bin/Win32-VisualStudio/Irrlicht.dll")
-    else()
-      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win64-visualStudio")
-      set(IRRLICHT_DLL "${IRRLICHT_DIR}/bin/Win64-VisualStudio/Irrlicht.dll")
-    endif()
-  elseif(MINGW OR CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG OR CMAKE_COMPILER_IS_CLANGXX)
-    if (CMAKE_SIZEOF_VOID_P MATCHES 4)
-      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win32-gcc")
-    else()
-      set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Win64-gcc")
-    endif()
-  else()
-    if(DEFINED ENV{CONDA_BUILD})
+if(NOT "${Irrlicht_INCLUDE_DIR}" STREQUAL "" AND NOT "${Irrlicht_LIBRARY}" STREQUAL "")
+  find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h PATHS ${Irrlicht_INCLUDE_DIR} NO_CACHE)
+  if(IRRLICHT_INCLUDE_DIR MATCHES IRRLICHT_INCLUDE_DIR-NOTFOUND)
+    set(Irrlicht_FOUND FALSE)
+  endif()
+  set(IRRLICHT_LIBRARY ${Irrlicht_LIBRARY})
+else()
+  set(Irrlicht_FOUND FALSE)
+endif()
+
+if(NOT "${Irrlicht_DLL}" STREQUAL "")
+  set(IRRLICHT_DLL ${Irrlicht_DLL})
+endif()
+
+if(Irrlicht_FOUND)
+  if(NOT Irrlicht_FIND_QUIETLY)
+    message(STATUS "Found Irrlicht using Irrlicht_INCLUDE_DIR and Irrlicht_LIBRARY")
+  endif()
+endif()
+
+# ----------------------------------------------------------------------------------------------------
+# 2. Look for Irrlicht using Irrlicht_ROOT and/or in known locations
+# ----------------------------------------------------------------------------------------------------
+
+if(NOT Irrlicht_FOUND)
+
+  set(Irrlicht_FOUND TRUE)
+  set(IRRLICHT_DLL "")
+
+  # Windows
+  if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    set(IRRLICHT_INCLUDE_PATHS "${Irrlicht_ROOT}/include")
+  
+    get_property(CURRENT_ENABLED_LANGUAGES GLOBAL PROPERTY ENABLED_LANGUAGES)
+    list(FIND CURRENT_ENABLED_LANGUAGES CSharp LANG_INDEX)
+    mark_as_advanced(FORCE LANG_INDEX)
+    mark_as_advanced(FORCE CURRENT_ENABLED_LANGUAGES)
+  
+    if(MSVC OR LANG_INDEX GREATER_EQUAL 0)
+      if (CMAKE_SIZEOF_VOID_P MATCHES 4)
+        set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/lib/Win32-visualstudio")
+        set(IRRLICHT_DLL "${Irrlicht_ROOT}/bin/Win32-VisualStudio/Irrlicht.dll")
+      else()
+        set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/lib/Win64-visualStudio")
+        set(IRRLICHT_DLL "${Irrlicht_ROOT}/bin/Win64-VisualStudio/Irrlicht.dll")
+      endif()
+    elseif(MINGW OR CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG OR CMAKE_COMPILER_IS_CLANGXX)
+      if (CMAKE_SIZEOF_VOID_P MATCHES 4)
+        set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/lib/Win32-gcc")
+      else()
+        set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/lib/Win64-gcc")
+      endif()
+    elseif(DEFINED ENV{CONDA_BUILD})
       set(IRRLICHT_DLL "$ENV{PREFIX}/Library/bin/Irrlicht.dll")
     endif()
+  
+  # Linux
+  elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+  
+    set(IRRLICHT_INCLUDE_PATHS "${Irrlicht_ROOT}" "${Irrlicht_ROOT}/include" "/usr/include/irrlicht" "/usr/local/include/irrlicht")
+    set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/lib/Linux")
+  
+  # MacOS
+  elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+  
+    set(IRRLICHT_INCLUDE_PATHS "${Irrlicht_ROOT}" "${Irrlicht_ROOT}/include" "/usr/include/irrlicht" "/usr/local/include/irrlicht")
+    set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/lib/Linux" "/usr/local/lib")
+  
+  # Other
+  else()
+  
+    set(IRRLICHT_INCLUDE_PATHS "/usr/include/irrlicht" "/usr/local/include/irrlicht")
+    set(IRRLICHT_LIBRARY_PATHS "${Irrlicht_ROOT}/../../lib")
+  
   endif()
+  
+  # Find IRRLICHT headers
+  find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h PATHS ${IRRLICHT_INCLUDE_PATHS} NO_CACHE)
+  if (IRRLICHT_INCLUDE_DIR MATCHES IRRLICHT_INCLUDE_DIR-NOTFOUND)
+    set(Irrlicht_FOUND FALSE)
+  endif()
+  
+  # Find IRRLICHT library
+  find_library(IRRLICHT_LIBRARY NAMES Irrlicht PATHS ${IRRLICHT_LIBRARY_PATHS} NO_CACHE)
+  if (IRRLICHT_LIBRARY MATCHES IRRLICHT_LIBRARY-NOTFOUND)
+    set(Irrlicht_FOUND FALSE)
+  endif()
+  
+  if(Irrlicht_FOUND)
+    if(NOT Irrlicht_FIND_QUIETLY)
+      if("${Irrlicht_ROOT}" STREQUAL "")
+        message(STATUS "Found Irrlicht in standard locations")
+      else()
+        message(STATUS "Found Irrlicht using Irrlicht_ROOT")
+      endif()
+    endif()
+  endif()
+
+endif()
+
+# ----------------------------------------------------------------------------------------------------
+# 3. If Irrlicht not found and Irrlicht_ROOT not provided, ask for Irrlicht_ROOT
+# ----------------------------------------------------------------------------------------------------
+
+if(NOT Irrlicht_FOUND AND "${Irrlicht_ROOT}" STREQUAL "")
+  message("Could not find Irrlicht; set Irrlicht_ROOT")
+  set(Irrlicht_ROOT "" CACHE PATH "Path to root of Irrlicht SDK installation.")
+  return()
+endif()
+
+# ----------------------------------------------------------------------------------------------------
+# 4. If Irrlicht still not found, ask for Irrlicht_INCLUDE_DIR, Irrlicht_LIBRARY, and Irrlicht_DLL
+# ----------------------------------------------------------------------------------------------------
+
+if(NOT Irrlicht_FOUND)
+  message("Could not find Irrlicht; set Irrlicht_INCLUDE_DIR and Irrlicht_LIBRARY")
+  set(Irrlicht_INCLUDE_DIR "" CACHE PATH "Irrlicht include directory")
+  set(Irrlicht_LIBRARY "" CACHE PATH "Irrlicht library")
+  if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    set(Irrlicht_DLL "" CACHE PATH "Irrlicht DLL")
+  endif()
+  return()
+endif()
+
+# ----------------------------------------------------------------------------------------------------
+# Prepare target properties
+# ----------------------------------------------------------------------------------------------------
+
+# Set known dependencies
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
   set(IRRLICHT_DEPENDENCY_LIBS "")
-  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-
-  set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include" "/usr/include/irrlicht" "/usr/local/include/irrlicht")
-  set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Linux")
-  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
   set(IRRLICHT_DEPENDENCY_LIBS "Xxf86vm;glut;X11;GL")
-
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-
-  set(IRRLICHT_INCLUDE_PATHS "${IRRLICHT_DIR}/include" "/usr/include/irrlicht" "/usr/local/include/irrlicht")
-  set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/lib/Linux" "/usr/local/lib")
-  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "/System/Library/Frameworks")
-
   find_library(COCOA_LIBRARY Cocoa NO_CACHE)
   find_library(OPENGL_LIBRARY OpenGL NO_CACHE)
   find_library(IOKIT_LIBRARY IOKit NO_CACHE)
@@ -74,55 +179,35 @@ elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     return()
   endif()
   set(IRRLICHT_DEPENDENCY_LIBS "${COCOA_LIBRARY};${OPENGL_LIBRARY};${IOKIT_LIBRARY}")
-
 else()
-
-  set(IRRLICHT_INCLUDE_PATHS "/usr/include/irrlicht" "/usr/local/include/irrlicht")
-  set(IRRLICHT_LIBRARY_PATHS "${IRRLICHT_DIR}/../../lib")
   set(IRRLICHT_DEPENDENCY_LIBS "")
-  set(IRRLICHT_DEPENDENCY_INCLUDE_DIRS "")
-
 endif()
 
-# Find IRRLICHT headers
-find_path(IRRLICHT_INCLUDE_DIR NAMES irrlicht.h PATHS ${IRRLICHT_INCLUDE_PATHS} NO_CACHE)
-if (IRRLICHT_INCLUDE_DIR MATCHES IRRLICHT_INCLUDE_DIR-NOTFOUND)
-  #message(FATAL_ERROR "Could not find 'irrlicht.h' in any of the following directories: ${IRRLICHT_INCLUDE_PATHS}.\nSet IRRLICHT_DIR to the Irrlicht SDK installation directory.")
-  return()
+if(NOT Irrlicht_FIND_QUIETLY)
+  message(STATUS "  include dir:  ${IRRLICHT_INCLUDE_DIR}")
+  message(STATUS "  library:      ${IRRLICHT_LIBRARY}")
+  message(STATUS "  dependencies: ${IRRLICHT_DEPENDENCY_LIBS}")
+  message(STATUS "  DLL:          ${IRRLICHT_DLL}")
 endif()
-set(IRRLICHT_INCLUDE_DIRS ${IRRLICHT_INCLUDE_DIR} ${IRRLICHT_DEPENDENCY_INCLUDE_DIRS})
 
-# Find IRRLICHT library
-find_library(IRRLICHT_LIBRARY NAMES Irrlicht PATHS ${IRRLICHT_LIBRARY_PATHS} NO_CACHE)
-if (IRRLICHT_LIBRARY MATCHES IRRLICHT_LIBRARY-NOTFOUND)
-  #message(FATAL_ERROR "Could not find 'irrlicht${CMAKE_IMPORT_LIBRARY_SUFFIX}' in any of the following directories: ${IRRLICHT_LIBRARY_PATHS}.\nSet IRRLICHT_DIR to the Irrlicht SDK installation directory.")
-  return()
-endif()
-set(IRRLICHT_LIBRARIES ${IRRLICHT_LIBRARY} ${IRRLICHT_DEPENDENCY_LIBS})
+# ----------------------------------------------------------------------------------------------------
+# 5. Create Irrlicht::Irrlicht target
+# ----------------------------------------------------------------------------------------------------
 
-set(IRRLICHT_FOUND TRUE)
+if(Irrlicht_FOUND AND NOT TARGET Irrlicht::Irrlicht)
 
-
-if(IRRLICHT_FOUND AND NOT TARGET Irrlicht::Irrlicht)
   add_library(Irrlicht::Irrlicht SHARED IMPORTED)
-  set_target_properties(Irrlicht::Irrlicht PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${IRRLICHT_INCLUDE_DIRS}")
-  
+  set_target_properties(Irrlicht::Irrlicht PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${IRRLICHT_INCLUDE_DIR}")
+
   if(MSVC)
-    set_property(TARGET Irrlicht::Irrlicht PROPERTY
-                IMPORTED_LOCATION ${IRRLICHT_DLL})
-    set_property(TARGET Irrlicht::Irrlicht PROPERTY
-               INTERFACE_COMPILE_OPTIONS "/wd4275")
+    set_property(TARGET Irrlicht::Irrlicht PROPERTY IMPORTED_LOCATION ${IRRLICHT_DLL})
+    set_property(TARGET Irrlicht::Irrlicht PROPERTY INTERFACE_COMPILE_OPTIONS "/wd4275")
   else()
-    set_property(TARGET Irrlicht::Irrlicht PROPERTY
-                IMPORTED_LOCATION ${IRRLICHT_LIBRARY})
+    set_property(TARGET Irrlicht::Irrlicht PROPERTY IMPORTED_LOCATION ${IRRLICHT_LIBRARY})
   endif()
 
-  set_property(TARGET Irrlicht::Irrlicht PROPERTY
-              IMPORTED_IMPLIB ${IRRLICHT_LIBRARY})
-  set_property(TARGET Irrlicht::Irrlicht PROPERTY
-              IMPORTED_LINK_DEPENDENT_LIBRARIES ${IRRLICHT_DEPENDENCY_LIBS})
-
+  set_property(TARGET Irrlicht::Irrlicht PROPERTY IMPORTED_IMPLIB ${IRRLICHT_LIBRARY})
+  set_property(TARGET Irrlicht::Irrlicht PROPERTY IMPORTED_LINK_DEPENDENT_LIBRARIES ${IRRLICHT_DEPENDENCY_LIBS})
 
   if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set_property(TARGET Irrlicht::Irrlicht PROPERTY
