e5e9eaf2f7a6e9333b81796fe13b3a2392be749f
Radu Serban <serban@wisc.edu>
2025-02-08 11:24:58 +0100

    Fix FindOpenCRG when using default build type with single-configuration generators

--- cmake/FindOpenCRG.cmake
+++ cmake/FindOpenCRG.cmake
@@ -11,21 +11,49 @@
 # OpenCRG::OpenCRG: imported target
 #-------------------------------------------------------------------------------
 
-
-mark_as_advanced(FORCE OpenCRG_INCLUDE_DIR_INTERNAL OpenCRG_DLL_INTERNAL)
-
 # Try to find OpenCRG library in default paths + user specified paths
 find_path(OpenCRG_INCLUDE_DIR_INTERNAL NAMES crgBaseLib.h PATHS "${OpenCRG_INCLUDE_DIR}")
-if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-  find_file(OpenCRG_DLL_INTERNAL NAMES OpenCRG.dll PATHS "${OpenCRG_DLL}")
-endif()
 
-# check if library is found
-if(NOT OpenCRG_INCLUDE_DIR_INTERNAL OR NOT EXISTS "${OpenCRG_LIBRARY}")
-  message(ERROR "OpenCRG library cannot be found. Please set OpenCRG_INCLUDE_DIR, OpenCRG_LIBRARY variables to proper locations.")
+if(NOT OpenCRG_INCLUDE_DIR_INTERNAL)
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "The provided OpenCRG include directory does not contain crgBaseLib.h")
+  endif()
   set(OpenCRG_FOUND FALSE)
   return()
+else()
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Found crgBaseLib.h in provided include directory")
+  endif()
 endif()
+
+if(NOT EXISTS "${OpenCRG_LIBRARY}")
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "The provided OpenCRG library does not exist")
+  endif()
+  set(OpenCRG_FOUND FALSE)
+  return()
+else()
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Found OpenCRG library file")
+  endif()
+endif()
+
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+  find_file(OpenCRG_DLL_INTERNAL NAMES OpenCRG.dll PATHS "${OpenCRG_DLL}")
+  if(NOT OpenCRG_DLL_INTERNAL)
+    if(NOT OpenCRG_FIND_QUIETLY)
+      message(STATUS "OpenCRG DLL not found; assuming static OpenCRG library")
+    endif()
+    set(OpenCRG_SHARED FALSE)
+  else()
+    if(NOT OpenCRG_FIND_QUIETLY)
+      message(STATUS "OpenCRG DLL found; assuming shared OpenCRG library")
+    endif()
+    set(OpenCRG_SHARED TRUE)
+  endif()
+endif()
+
+mark_as_advanced(FORCE OpenCRG_INCLUDE_DIR_INTERNAL OpenCRG_DLL_INTERNAL)
 
 # Try to locate libraries for multiple configurations
 get_filename_component(OpenCRG_LIBRARY_DIR ${OpenCRG_LIBRARY} DIRECTORY)
@@ -35,36 +63,60 @@ find_library(OpenCRG_LIBRARY_DEBUG NAMES OpenCRG_d PATHS "${OpenCRG_LIBRARY_DIR}
 find_library(OpenCRG_LIBRARY_MINSIZEREL NAMES OpenCRG_s PATHS "${OpenCRG_LIBRARY_DIR}" PATHS ${OpenCRG_LIBRARY_DIR})
 find_library(OpenCRG_LIBRARY_RELWITHDEBINFO NAMES OpenCRG_rd PATHS "${OpenCRG_LIBRARY_DIR}" PATHS ${OpenCRG_LIBRARY_DIR})
 
-if (NOT OpenCRG_LIBRARY_DEBUG)
+if(OpenCRG_LIBRARY_DEBUG)
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Found Debug OpenCRG library")
+  endif()
+else()
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Debug OpenCRG library not found; using Release library")
+  endif()
   set(OpenCRG_LIBRARY_DEBUG ${OpenCRG_LIBRARY_RELEASE})
 endif()
-if (NOT OpenCRG_LIBRARY_MINSIZEREL)
+
+if(OpenCRG_LIBRARY_MINSIZEREL)
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Found MinSizeRel OpenCRG library")
+  endif()
+else()
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "MinSizeRel OpenCRG library not found; using Release library")
+  endif()
   set(OpenCRG_LIBRARY_MINSIZEREL ${OpenCRG_LIBRARY_RELEASE})
 endif()
-if (NOT OpenCRG_LIBRARY_RELWITHDEBINFO)
+
+if(OpenCRG_LIBRARY_RELWITHDEBINFO)
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Found RelWithDebInfo OpenCRG library")
+  endif()
+else()
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "RelWithDebInfo OpenCRG library not found; using Release library")
+  endif()
   set(OpenCRG_LIBRARY_RELWITHDEBINFO ${OpenCRG_LIBRARY_RELEASE})
 endif()
+
 mark_as_advanced(FORCE OpenCRG_LIBRARY_RELEASE OpenCRG_LIBRARY_DEBUG OpenCRG_LIBRARY_MINSIZEREL OpenCRG_LIBRARY_RELWITHDEBINFO)
 
-
-if(${CMAKE_SYSTEM_NAME} MATCHES "Windows" AND NOT OpenCRG_DLL_INTERNAL)
-  message(STATUS "Found OpenCRG as static library.")
-  set(OpenCRG_SHARED FALSE)
-else()
-  set(OpenCRG_SHARED TRUE)
-endif()
-
+# Set OpenCRG as found
 set(OpenCRG_FOUND TRUE)
 
-# provide alias target
+# Create target
+
 if(OpenCRG_FOUND AND NOT TARGET OpenCRG::OpenCRG)
+  if(NOT OpenCRG_FIND_QUIETLY)
+    message(STATUS "Create target OpenCRG::OpenCRG")
+  endif()
+
   if (OpenCRG_SHARED)
     add_library(OpenCRG::OpenCRG SHARED IMPORTED)
   else()
     add_library(OpenCRG::OpenCRG STATIC IMPORTED)
   endif()
-  set_target_properties(OpenCRG::OpenCRG PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${OpenCRG_INCLUDE_DIR}")
+
+  set_target_properties(OpenCRG::OpenCRG PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${OpenCRG_INCLUDE_DIR}")
+
+  set_target_properties(OpenCRG::OpenCRG PROPERTIES IMPORTED_LOCATION "${OpenCRG_LIBRARY}")
   set_target_properties(OpenCRG::OpenCRG PROPERTIES IMPORTED_LOCATION_RELEASE "${OpenCRG_LIBRARY_RELEASE}")
   set_target_properties(OpenCRG::OpenCRG PROPERTIES IMPORTED_LOCATION_DEBUG "${OpenCRG_LIBRARY_DEBUG}")
   set_target_properties(OpenCRG::OpenCRG PROPERTIES IMPORTED_LOCATION_RELWITHDEBINFO "${OpenCRG_LIBRARY_RELWITHDEBINFO}")
