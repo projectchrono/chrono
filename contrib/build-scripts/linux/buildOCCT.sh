2fd6cb9170d472f063a7f99fc09aa8b6d841b15e
Bocheng Zou <bocheng.zou@outlook.com>
2025-03-30 06:35:05 +0000

    Build opencascade

---
+++ contrib/build-scripts/linux/buildOCCT.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------------------------------------
+# Shell script for building OpenCASCADE based on the last official releases.
+# - Requires cmake, wget, and unzip
+# - Place in an arbitrary temporary directory.
+# - Specify the locations for the OpenCASCADE sources OR indicate that these should be downloaded.
+# - Specify the install directory.
+# - Decide whether to build shared or static libraries and whether to also build debug libraries.
+# - Run the script (sh ./buildOCCT.sh).
+# - The install directory will contain (under subdirectories of CASCADE_INSTALL_DIR/lib/shared) all VSG CMake
+#   project configuration scripts required to configure Chrono with the Chorno::VSG module enabled.
+#
+# -------------------------------------------------------------------------------------------------------
+
+DOWNLOAD=ON
+
+CASCADE_INSTALL_DIR="$HOME/Packages/opencascade-7.4.0"
+
+BUILDSHARED=ON
+BUILDDEBUG=OFF
+BUILDSYSTEM="Ninja Multi-Config"
+
+if [ ${DOWNLOAD} = OFF ]
+then    
+    OCCT_SOURCE_DIR="$HOME/Sources/opencascade"
+fi
+
+# ------------------------------------------------------------------------
+# Allow overriding installation directory through command line argument
+
+if [ $# -eq 1 ]
+then
+    VSG_INSTALL_DIR=$1
+fi
+
+# ------------------------------------------------------------------------
+
+if [ ${DOWNLOAD} = ON ]
+then
+    echo "Download sources from GitHub"
+
+    rm -rf OCCT-7_4_0
+    mkdir OCCT-7_4_0
+
+    wget https://codeload.github.com/Open-Cascade-SAS/OCCT/tar.gz/refs/tags/V7_4_0 -O OCCT-7.4.0.tar.gz
+    tar xzvf OCCT-7.4.0.tar.gz
+    OCCT_SOURCE_DIR=OCCT-7_4_0
+
+else
+    echo "Using provided source directories"
+fi
+
+echo -e "\nSources in:"
+echo "  "  ${OCCT_SOURCE_DIR}
+
+# ------------------------------------------------------------------------
+
+rm -rf ${CASCADE_INSTALL_DIR}
+mkdir ${CASCADE_INSTALL_DIR}
+
+# --- OCCT ----------------------------------------------------------------
+
+echo -e "\n------------------------ Configure OCCT\n"
+rm -rf build
+cmake  -G "${BUILDSYSTEM}" -B build -S ${OCCT_SOURCE_DIR}
+
+echo -e "\n------------------------ Build and install OCCT\n"
+cmake --build build --config Release
+cmake --install build --config Release --prefix ${CASCADE_INSTALL_DIR}
+if [ ${BUILDDEBUG} = ON ]
+then
+    cmake --build build --config Debug
+    cmake --install build --config Debug --prefix ${CASCADE_INSTALL_DIR}
+else
+    echo "No Debug build of vsg"
+fi
