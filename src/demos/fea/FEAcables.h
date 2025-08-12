8c1b30ea0622b85c06fdb4817e02a9b284a33331
Radu Serban <serban@wisc.edu>
2025-08-12 08:41:15 +0200

    Clean up ANCF cable demo

--- src/demos/fea/FEAcables.h
+++ src/demos/fea/FEAcables.h
@@ -40,44 +40,39 @@ class Model1 {
 
         // Create a section, i.e. thickness and material properties
         // for beams. This will be shared among some beams.
-        auto msection_cable = chrono_types::make_shared<ChBeamSectionCable>();
-        msection_cable->SetDiameter(beam_diameter);
-        msection_cable->SetYoungModulus(0.01e9);
-        msection_cable->SetRayleighDamping(0.000);
+        auto section = chrono_types::make_shared<ChBeamSectionCable>();
+        section->SetDiameter(beam_diameter);
+        section->SetYoungModulus(0.01e9);
+        section->SetRayleighDamping(0.000);
 
         // Create the nodes
-        auto hnodeancf1 = chrono_types::make_shared<ChNodeFEAxyzD>(ChVector3d(0, 0, -0.2), ChVector3d(1, 0, 0));
-        auto hnodeancf2 = chrono_types::make_shared<ChNodeFEAxyzD>(ChVector3d(beam_L, 0, -0.2), ChVector3d(1, 0, 0));
-
-        mesh->AddNode(hnodeancf1);
-        mesh->AddNode(hnodeancf2);
+        auto node1 = chrono_types::make_shared<ChNodeFEAxyzD>(ChVector3d(0, 0, -0.2), ChVector3d(1, 0, 0));
+        auto node2 = chrono_types::make_shared<ChNodeFEAxyzD>(ChVector3d(beam_L, 0, -0.2), ChVector3d(1, 0, 0));
+        mesh->AddNode(node1);
+        mesh->AddNode(node2);
 
         // Create the element
+        auto element1 = chrono_types::make_shared<ChElementCableANCF>();
+        element1->SetNodes(node1, node2);
+        element1->SetSection(section);
+        mesh->AddElement(element1);
 
-        auto belementancf1 = chrono_types::make_shared<ChElementCableANCF>();
+        // Fix node1 and apply a force to node2
+        node1->SetFixed(true);
+        node2->SetForce(ChVector3d(0, 3, 0));
 
-        belementancf1->SetNodes(hnodeancf1, hnodeancf2);
-        belementancf1->SetSection(msection_cable);
-
-        mesh->AddElement(belementancf1);
-
-        // Apply a force or a torque to a node:
-        hnodeancf2->SetForce(ChVector3d(0, 3, 0));
-
-        hnodeancf1->SetFixed(true);
-
-        // Add a rigid body connected to the end of the beam:
-
+        // Add a rigid body connected to the end of the beam
         body = chrono_types::make_shared<ChBodyEasyBox>(0.1, 0.02, 0.02, 1000);
-        body->SetPos(hnodeancf2->GetPos() + ChVector3d(0.05, 0, 0));
+        body->SetPos(node2->GetPos() + ChVector3d(0.05, 0, 0));
         system.Add(body);
 
+        // Fix position and direction of node2 to the body
         auto constraint_pos = chrono_types::make_shared<ChLinkNodeFrame>();
-        constraint_pos->Initialize(hnodeancf2, body);
+        constraint_pos->Initialize(node2, body);
         system.Add(constraint_pos);
 
         auto constraint_dir = chrono_types::make_shared<ChLinkNodeSlopeFrame>();
-        constraint_dir->Initialize(hnodeancf2, body);
+        constraint_dir->Initialize(node2, body);
         constraint_dir->SetDirectionInAbsoluteCoords(ChVector3d(1, 0, 0));
         system.Add(constraint_dir);
     }
@@ -100,40 +95,36 @@ class Model1 {
 class Model2 {
   public:
     Model2(ChSystem& system, std::shared_ptr<ChMesh> mesh) {
-        // Create a section, i.e. thickness and material properties
-        // for beams. This will be shared among some beams.
+        // Create ground body
+        auto ground = chrono_types::make_shared<ChBody>();
+        ground->SetFixed(true);
 
-        auto msection_cable2 = chrono_types::make_shared<ChBeamSectionCable>();
-        msection_cable2->SetDiameter(0.015);
-        msection_cable2->SetYoungModulus(0.01e9);
-        msection_cable2->SetRayleighDamping(0.000);
+        // Create a section, i.e. thickness and material properties for beams.
+        auto section = chrono_types::make_shared<ChBeamSectionCable>();
+        section->SetDiameter(0.015);
+        section->SetYoungModulus(0.01e9);
+        section->SetRayleighDamping(0.000);
 
-        // This ChBuilderCableANCF helper object is very useful because it will
-        // subdivide 'beams' into sequences of finite elements of beam type, ex.
-        // one 'beam' could be made of 5 FEM elements of ChElementBeamANCF_3333 class.
-        // If new nodes are needed, it will create them for you.
+        // Use BuildBeam to create a beam from a point to another
         ChBuilderCableANCF builder;
+        builder.BuildBeam(mesh,                       // container FEA mesh
+                          section,                    // cable section
+                          10,                         // number of cable elements
+                          ChVector3d(0, 0, -0.1),     // point A (beginning of beam)
+                          ChVector3d(0.5, 0, -0.1));  // point B (end of beam)
 
-        // Now, simply use BuildBeam to create a beam from a point to another:
-        builder.BuildBeam(mesh,             // the mesh where to put the created nodes and elements
-                          msection_cable2,  // the ChBeamSectionCable to use for the ChElementBeamANCF_3333 elements
-                          10,               // the number of ChElementBeamANCF_3333 to create
-                          ChVector3d(0, 0, -0.1),     // the 'A' point in space (beginning of beam)
-                          ChVector3d(0.5, 0, -0.1));  // the 'B' point in space (end of beam)
+        // Apply a force on the first node of the beam
+        builder.GetLastBeamNodes().front()->SetForce(ChVector3d(1.0, 0, 0));
 
-        // After having used BuildBeam(), you can retrieve the nodes used for the beam,
-        // For example say you want to fix both pos and dir of A end and apply a force to the B end:
-        // builder.GetLastBeamNodes().back()->SetFixed(true);
-        builder.GetLastBeamNodes().front()->SetForce(ChVector3d(0, -0.2, 0));
-
-        // For instance, now retrieve the A end and add a constraint to
-        // block the position only of that node:
-        auto mtruss = chrono_types::make_shared<ChBody>();
-        mtruss->SetFixed(true);
-
-        auto constraint_hinge = chrono_types::make_shared<ChLinkNodeFrame>();
-        constraint_hinge->Initialize(builder.GetLastBeamNodes().back(), mtruss);
-        system.Add(constraint_hinge);
+        // Spherical or cantilever constraint at last node of the beam
+        ////{
+        ////    auto constraint_hinge = chrono_types::make_shared<ChLinkNodeFrame>();
+        ////    constraint_hinge->Initialize(builder.GetLastBeamNodes().back(), ground);
+        ////    system.Add(constraint_hinge);
+        ////}
+        {
+            builder.GetLastBeamNodes().back()->SetFixed(true);
+        }
     }
 };
 
@@ -145,21 +136,21 @@ class Model2 {
 class Model3 {
   public:
     Model3(ChSystem& system, std::shared_ptr<ChMesh> mesh, int n_chains = 6) : bodies(n_chains) {
-        auto msection_cable2 = chrono_types::make_shared<ChBeamSectionCable>();
-        msection_cable2->SetDiameter(0.015);
-        msection_cable2->SetYoungModulus(0.01e9);
-        msection_cable2->SetRayleighDamping(0.000);
+        auto section = chrono_types::make_shared<ChBeamSectionCable>();
+        section->SetDiameter(0.015);
+        section->SetYoungModulus(0.01e9);
+        section->SetRayleighDamping(0.000);
 
-        auto mtruss = chrono_types::make_shared<ChBody>();
-        mtruss->SetFixed(true);
+        auto ground = chrono_types::make_shared<ChBody>();
+        ground->SetFixed(true);
 
         for (int j = 0; j < n_chains; ++j) {
             ChBuilderCableANCF builder;
 
-            // Now, simply use BuildBeam to create a beam from a point to another:
-            builder.BuildBeam(mesh,             // the mesh where to put the created nodes and elements
-                              msection_cable2,  // ChBeamSectionCable to use for the ChElementBeamANCF_3333 elements
-                              1 + j,            // number of ChElementBeamANCF_3333 to create
+            // Use BuildBeam to create a beam from a point to another
+            builder.BuildBeam(mesh,                                   // container FEA mesh
+                              section,                                // cable section
+                              1 + j,                                  // number of cable elements
                               ChVector3d(0, 0, -0.1 * j),             // point A (beginning of beam)
                               ChVector3d(0.1 + 0.1 * j, 0, -0.1 * j)  // point B (end of beam)
             );
@@ -167,45 +158,45 @@ class Model3 {
             builder.GetLastBeamNodes().back()->SetForce(ChVector3d(0, -0.2, 0));
 
             auto constraint_hinge = chrono_types::make_shared<ChLinkNodeFrame>();
-            constraint_hinge->Initialize(builder.GetLastBeamNodes().front(), mtruss);
+            constraint_hinge->Initialize(builder.GetLastBeamNodes().front(), ground);
             system.Add(constraint_hinge);
 
             auto msphere = chrono_types::make_shared<ChVisualShapeSphere>(0.02);
             constraint_hinge->AddVisualShape(msphere);
 
-            // make a box and connect it
-            auto mbox = chrono_types::make_shared<ChBodyEasyBox>(0.2, 0.04, 0.04, 1000);
-            mbox->SetPos(builder.GetLastBeamNodes().back()->GetPos() + ChVector3d(0.1, 0, 0));
-            system.Add(mbox);
+            // Create and connect the intermediate body
+            auto mid_body = chrono_types::make_shared<ChBodyEasyBox>(0.2, 0.04, 0.04, 1000);
+            mid_body->SetPos(builder.GetLastBeamNodes().back()->GetPos() + ChVector3d(0.1, 0, 0));
+            system.Add(mid_body);
 
             auto constraint_pos = chrono_types::make_shared<ChLinkNodeFrame>();
-            constraint_pos->Initialize(builder.GetLastBeamNodes().back(), mbox);
+            constraint_pos->Initialize(builder.GetLastBeamNodes().back(), mid_body);
             system.Add(constraint_pos);
 
             auto constraint_dir = chrono_types::make_shared<ChLinkNodeSlopeFrame>();
-            constraint_dir->Initialize(builder.GetLastBeamNodes().back(), mbox);
+            constraint_dir->Initialize(builder.GetLastBeamNodes().back(), mid_body);
             constraint_dir->SetDirectionInAbsoluteCoords(ChVector3d(1, 0, 0));
             system.Add(constraint_dir);
 
-            // make another beam
+            // Create a second beam
             builder.BuildBeam(
-                mesh,                // mesh where to put the created nodes and elements
-                msection_cable2,     // ChBeamSectionCable to use for the ChElementBeamANCF_3333 elements
-                1 + (n_chains - j),  // number of ChElementBeamANCF_3333 to create
-                ChVector3d(mbox->GetPos().x() + 0.1, 0, -0.1 * j),                        // point A (beginning of beam)
-                ChVector3d(mbox->GetPos().x() + 0.1 + 0.1 * (n_chains - j), 0, -0.1 * j)  // point B (end of beam)
+                mesh,                                                   // container FEA mesh
+                section,                                                // cable section
+                1 + (n_chains - j),                                     // number of cable elements
+                ChVector3d(mid_body->GetPos().x() + 0.1, 0, -0.1 * j),  // point A (beginning of beam)
+                ChVector3d(mid_body->GetPos().x() + 0.1 + 0.1 * (n_chains - j), 0, -0.1 * j)  // point B (end of beam)
             );
 
             auto constraint_pos2 = chrono_types::make_shared<ChLinkNodeFrame>();
-            constraint_pos2->Initialize(builder.GetLastBeamNodes().front(), mbox);
+            constraint_pos2->Initialize(builder.GetLastBeamNodes().front(), mid_body);
             system.Add(constraint_pos2);
 
             auto constraint_dir2 = chrono_types::make_shared<ChLinkNodeSlopeFrame>();
-            constraint_dir2->Initialize(builder.GetLastBeamNodes().front(), mbox);
+            constraint_dir2->Initialize(builder.GetLastBeamNodes().front(), mid_body);
             constraint_dir2->SetDirectionInAbsoluteCoords(ChVector3d(1, 0, 0));
             system.Add(constraint_dir2);
 
-            // make a box and connect it
+            // Create and connect the end body
             bodies[j] = chrono_types::make_shared<ChBodyEasyBox>(0.2, 0.04, 0.04, 1000);
             bodies[j]->SetPos(builder.GetLastBeamNodes().back()->GetPos() + ChVector3d(0.1, 0, 0));
             system.Add(bodies[j]);
