87470a5722ce11c7f60eb714f08ce52acc209e89
tasora <tasora@ied.unipr.it>
2025-09-26 00:58:58 +0200

    Add tetrahedron FE (8 nodes)

--- src/chrono/fea/ChFeaMaterial.h
+++ src/chrono/fea/ChFeaMaterial.h
@@ -320,6 +320,160 @@ private:
 
 
 
+// -----------------------------------------------------------------------------
+
+/// Linear hexahedron for generic constitutive laws. Can be used for multiphysics.
+/// It uses 8 nodes.
+
+class ChApi ChFeaElementTetrahedron_8 : public ChFeaElement {
+public:
+    ChFeaElementTetrahedron_8() {}
+    virtual ~ChFeaElementTetrahedron_8() {}
+
+    //
+    // Tetrahedron-specific
+    // 
+
+    /// Return the specified tetrahedron node (0 <= n <= 8).
+    virtual std::shared_ptr<ChFeaNodeXYZ> GetTetrahedronNode(unsigned int n) { return nodes[n]; }
+
+    /// Set the nodes used by this tetrahedron.
+    virtual void SetNodes(std::array<std::shared_ptr<ChFeaNodeXYZ>, 8> mynodes) {
+        nodes = mynodes;
+    }
+
+    //
+    // Interface
+    // 
+
+    /// Get the number of nodes used by this element.
+    virtual unsigned int GetNumNodes() override { return 8; };
+
+    /// Access the nth node.
+    virtual std::shared_ptr<ChNodeFEAbase> GetNode(unsigned int n) override {
+        return nodes[n];
+    }
+
+    // Return dimension of embedding space X
+    virtual int GetSpatialDimensions() const override { return 3; };
+
+    // Return dimension of represented manifold in space
+    virtual int GetManifoldDimensions() const override { return 3; };
+
+    // Compute the 8 shape functions N at eta parametric coordinates. 
+    virtual void ComputeN(const ChVector3d eta, ChRowVectorDynamic<>& N) override {
+        N.resize(GetNumNodes());
+        N[0] = 0.125 * (1 - eta[0])*(1 - eta[1])*(1 - eta[2]);
+        N[1] = 0.125 * (1 + eta[0])*(1 - eta[1])*(1 - eta[2]);
+        N[2] = 0.125 * (1 + eta[0])*(1 + eta[1])*(1 - eta[2]);
+        N[3] = 0.125 * (1 - eta[0])*(1 + eta[1])*(1 - eta[2]);
+        N[4] = 0.125 * (1 - eta[0])*(1 - eta[1])*(1 + eta[2]);
+        N[5] = 0.125 * (1 + eta[0])*(1 - eta[1])*(1 + eta[2]);
+        N[6] = 0.125 * (1 + eta[0])*(1 + eta[1])*(1 + eta[2]);
+        N[7] = 0.125 * (1 - eta[0])*(1 + eta[1])*(1 + eta[2]);
+    };
+
+    // Compute shape function material derivatives dN/d\eta at eta parametric coordinates.
+    // Write shape functions dN_j(\eta)/d\eta_i in dNde, a matrix with 4 columns, and 3 rows. 
+    virtual void ComputedNde(const ChVector3d eta, ChMatrixDynamic<>& dNde) override {
+        dNde.setZero(GetManifoldDimensions(), GetNumNodes());
+        dNde(0, 0) = -  (1 - eta[1]) * (1 - eta[2]);
+        dNde(0, 1) = +  (1 - eta[1]) * (1 - eta[2]);
+        dNde(0, 2) = +  (1 + eta[1]) * (1 - eta[2]);
+        dNde(0, 3) = -  (1 + eta[1]) * (1 - eta[2]);
+        dNde(0, 4) = -  (1 - eta[1]) * (1 + eta[2]);
+        dNde(0, 5) = +  (1 - eta[1]) * (1 + eta[2]);
+        dNde(0, 6) = +  (1 + eta[1]) * (1 + eta[2]);
+        dNde(0, 7) = -  (1 + eta[1]) * (1 + eta[2]);
+
+        dNde(1, 0) = - (1 - eta[0]) * (1 - eta[2]);
+        dNde(1, 1) = - (1 + eta[0]) * (1 - eta[2]);
+        dNde(1, 2) = + (1 + eta[0]) * (1 - eta[2]);
+        dNde(1, 3) = + (1 - eta[0]) * (1 - eta[2]);
+        dNde(1, 4) = - (1 - eta[0]) * (1 + eta[2]);
+        dNde(1, 5) = - (1 + eta[0]) * (1 + eta[2]);
+        dNde(1, 6) = + (1 + eta[0]) * (1 + eta[2]);
+        dNde(1, 7) = + (1 - eta[0]) * (1 + eta[2]);
+
+        dNde(2, 0) = - (1 - eta[0]) * (1 - eta[1]);
+        dNde(2, 1) = - (1 + eta[0]) * (1 - eta[1]);
+        dNde(2, 2) = - (1 + eta[0]) * (1 + eta[1]);
+        dNde(2, 3) = - (1 - eta[0]) * (1 + eta[1]);
+        dNde(2, 4) = + (1 - eta[0]) * (1 - eta[1]);
+        dNde(2, 5) = + (1 + eta[0]) * (1 - eta[1]);
+        dNde(2, 6) = + (1 + eta[0]) * (1 + eta[1]);
+        dNde(2, 7) = + (1 - eta[0]) * (1 + eta[1]);
+
+        dNde *= 0.125; // the 1/8 factor
+    }
+
+    // Compute Jacobian J, and returns its determinant. J is square 3x3
+    virtual double ComputeJ(const ChVector3d eta, ChMatrix33d& J) override {
+        ChMatrixNM<double, 3, 8> Xhat;
+        for (int i = 0; i < 8; ++i)
+            Xhat.block<3, 1>(0, i) = std::static_pointer_cast<ChFeaNodeXYZ>(this->GetNode(i))->eigen();
+        // J = Xhat * dNde^T
+        ChMatrixDynamic<double> dNde(3,8);
+        ComputedNde(eta, dNde);
+        J = Xhat * dNde.transpose();
+        return J.determinant();
+    }
+
+    // Compute Jacobian Jinv, and returns its determinant. Jinv is square 3x3
+    virtual double ComputeJinv(const ChVector3d eta, ChMatrix33d& Jinv) override {
+        ChMatrix33<double> J;
+        this->ComputeJ(eta, J);
+        double mdet;
+        bool isinvertible;
+        J.computeInverseAndDetWithCheck(Jinv, mdet, isinvertible, 1e-9);
+        return mdet;
+    }
+
+    // Tell the minimum required quadrature order when integrating over the element
+    virtual int GetMinQuadratureOrder() const override {
+        return 1;
+    }
+
+    // Tell how many Gauss points are needed for integration
+    virtual int GetNumQuadraturePoints(const int order) const override {
+        if (order == 1)
+            return 1; // shortcut
+        ChQuadratureTables* mtables = ChQuadrature::GetStaticTables();
+        int points_on_abscissa = mtables->Weight[order - 1].size();
+        return (points_on_abscissa * points_on_abscissa * points_on_abscissa);
+    }
+
+    // Get i-th Gauss point weight and parametric coordinates
+    virtual void GetQuadraturePointWeight(const int order, const int i, double& weight, ChVector3d& coords)  const override {
+        ChQuadratureTables* mtables = ChQuadrature::GetStaticTables();
+        int points_on_abscissa = mtables->Weight[order - 1].size();
+        int j = i / (points_on_abscissa * points_on_abscissa);
+        int k = (i / points_on_abscissa) % points_on_abscissa;
+        int l = i % points_on_abscissa;
+        coords.x() = mtables->Lroots[order - 1][j];
+        coords.y() = mtables->Lroots[order - 1][k];
+        coords.z() = mtables->Lroots[order - 1][l];
+        weight = mtables->Weight[order - 1][j] * mtables->Weight[order - 1][k] * mtables->Weight[order - 1][l]; 
+    }
+
+    // *************************************
+
+
+    /// Update, called at least at each time step.
+    /// If the element has to keep updated some auxiliary data, such as the rotation matrices for corotational approach,
+    /// this should be implemented in this function.
+    virtual void Update() {}
+
+
+private:
+    /// Initial setup (called once before start of simulation).
+    /// This is used mostly to precompute matrices that do not change during the simulation, i.e. the local stiffness of
+    /// each element, if any, the mass, etc.
+    virtual void SetupInitial(ChSystem* system) {}
+
+    std::array<std::shared_ptr<ChFeaNodeXYZ>, 8> nodes;
+};
+
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
