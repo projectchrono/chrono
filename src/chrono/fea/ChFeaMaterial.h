ba6992ce7a87069336f6cbb8cfcf22b499669ef3
tasora <tasora@ied.unipr.it>
2025-09-25 20:24:57 +0200

    add material property classes  for 3D elasticity - now simply the base class for all 3d elasticity plus one simple child: the StVenant-Kirchhoff linear elasticity. Other in future (Ogden, etc)

--- src/chrono/fea/ChFeaMaterial.h
+++ src/chrono/fea/ChFeaMaterial.h
@@ -1036,6 +1036,151 @@ private:
 };
 
 
+/// Class for the basic properties of materials in an elastic continuum.
+
+class ChFea3DMaterialStress : public ChFea3DDensity {
+public:
+    ChFea3DMaterialStress() {}
+
+    //ChFea3DMaterialStress(const ChContinuumElastic& other);
+
+    virtual ~ChFea3DMaterialStress() {}
+
+    /// Compute elastic stress from elastic strain.  Assuming the 2* factor in the last 3 values of strain Voigt notation.
+    /// Assuming strain is Green-Lagrange tensor E, in Voigt notation. For small strains it coincides with espilon tensor.
+    /// Assuming stress if Piola-Kirchhoff tensor S, in Voigt notation. 
+    virtual void ComputeElasticStress(ChStressTensor<>& P_stress, const ChStrainTensor<>& E_strain) = 0;
+
+    /// Computes the tangent modulus for a given strain. For linear elasticity it is the constant E matrix.
+    /// Assuming strain is Green-Lagrange tensor, in Voigt notation. For small strains it coincides with espilon tensor.
+    virtual void ComputeTangentModulus(ChMatrixNM<double, 6, 6>& StressStrainMatrix, const ChStrainTensor<>& strain) = 0;
+
+    //virtual void ArchiveOut(ChArchiveOut& archive_out) override;  TODO
+    //virtual void ArchiveIn(ChArchiveIn& archive_in) override; 
+};
+
+
+
+class ChFea3DMaterialStressStVenant : public ChFea3DMaterialStress {
+private:
+    double m_E;                            ///< Young Modulus
+    double m_poisson;                      ///< Poisson ratio
+    double m_lamefirst;                    ///< Lame's first parameter
+    ChMatrixDynamic<> StressStrainMatrix;  ///< Elasticity (stiffness) matrix :   = [E] ε
+
+    double m_rayl_damping_alpha;  ///< Rayleigh damping coeff, M proportional
+    double m_rayl_damping_beta;   ///< Rayleigh damping coeff, K proportional
+
+public:
+    ChFea3DMaterialStressStVenant(double young = 10000000, double poisson = 0.4, double density = 1000) {
+        m_E = young;
+        SetPoissonRatio(poisson);     // sets also Lamé
+        this->m_rayl_damping_alpha = 0;
+        this->m_rayl_damping_beta = 0;
+    }
+
+    //ChFea3DMaterialStressStVenant(const ChContinuumElastic& other);
+
+    virtual ~ChFea3DMaterialStressStVenant() {}
+
+    /// Set the Young elastic modulus, in Pa (N/m^2), as the ratio of the uniaxial
+    /// stress over the uniaxial strain, for hookean materials.
+    void SetYoungModulus(double E) {
+        m_E = E;
+        m_lamefirst = (m_poisson * m_E) / ((1 + m_poisson) * (1 - 2 * m_poisson));  // Lame's constant l
+        //ComputeStressStrainMatrix();                                                // updates Elasticity matrix
+    }
+
+    /// Get the Young elastic modulus, in Pa (N/m^2).
+    double GetYoungModulus() const { return m_E; }
+
+    /// Set the Poisson ratio, as v=-transverse_strain/axial_strain, so
+    /// takes into account the 'squeezing' effect of materials that are pulled.
+    /// Note: v=0.5 means perfectly incompressible material, that could give problems with some type of solvers.
+    /// Setting v also changes G.
+    void SetPoissonRatio(double v) {
+        m_poisson = v;
+        m_lamefirst = (m_poisson * m_E) / ((1 + m_poisson) * (1 - 2 * m_poisson));  // Lame's constant l
+        //ComputeStressStrainMatrix();                                                // updates Elasticity matrix
+    }
+
+    /// Get the Poisson ratio, as v=-transverse_strain/axial_strain.
+    double GetPoissonRatio() const { return m_poisson; }
+
+    /// Set the shear modulus G, in Pa (N/m^2).
+    /// Setting G also changes Poisson ratio v.
+    void SetShearModulus(double G) {
+        m_poisson = (m_E / (2 * G)) - 1;                                            // fixed G, E, get v
+        m_lamefirst = (m_poisson * m_E) / ((1 + m_poisson) * (1 - 2 * m_poisson));  // Lame's constant l
+        //ComputeStressStrainMatrix();                                                // updates Elasticity matrix
+    }
+
+    /// Get the shear modulus G, in Pa (N/m^2)
+    double GetShearModulus() const { return m_E / (2 * (1 + m_poisson)); }
+
+    /// Get Lamé first parameter (the second is shear modulus, so GetShearModulus() )
+    double GetLameFirstParam() const { return m_lamefirst; }
+
+    /// Get bulk modulus (increase of pressure for decrease of volume), in Pa.
+    double GetBulkModulus() const { return m_E / (3 * (1 - 2 * m_poisson)); }
+
+    /// Get P-wave modulus (if V=speed of propagation of a P-wave, then (M/density)=V^2 )
+    double GetPWaveModulus() const { return m_E * ((1 - m_poisson) / (1 + m_poisson) * (1 - 2 * m_poisson)); }
+
+    /// Compute elastic stress from elastic strain.  Assuming the 2* factor in the last 3 values of strain Voigt notation.
+    /// Assuming strain is Green-Lagrange E tensor, in Voigt notation. For small strains it coincides with espilon tensor.
+    /// Assuming stress if Piola-Kirchhoff S tensor, in Voigt notation. 
+    virtual void ComputeElasticStress(ChStressTensor<>& stress, const ChStrainTensor<>& strain) override {
+            double G = GetShearModulus();
+            stress.XX() = strain.XX() * (m_lamefirst + 2 * G) + strain.YY() * m_lamefirst + strain.ZZ() * m_lamefirst;
+            stress.YY() = strain.XX() * m_lamefirst + strain.YY() * (m_lamefirst + 2 * G) + strain.ZZ() * m_lamefirst;
+            stress.ZZ() = strain.XX() * m_lamefirst + strain.YY() * m_lamefirst + strain.ZZ() * (m_lamefirst + 2 * G);
+            stress.XY() = strain.XY() * G;
+            stress.XZ() = strain.XZ() * G;
+            stress.YZ() = strain.YZ() * G;
+     }
+
+    /// Computes the tangent modulus for a given strain.   Assuming the 2* factor in the last 3 values of strain Voigt notation.
+    /// Since here we are using the StVenant model, this is linear elasticity and E is constant. 
+    /// Assuming strain is Green-Lagrange E tensor, in Voigt notation. For small strains it coincides with espilon tensor.
+    virtual void ComputeTangentModulus(ChMatrixNM<double, 6, 6>& C, const ChStrainTensor<>& strain) override {
+        C.setZero(6, 6);
+        double G = GetShearModulus();
+        // Fill the upper-left 3x3 block
+        for (int i = 0; i < 3; ++i) {
+            C(i,i) = m_lamefirst + 2.0 * G;
+            for (int j = 0; j < 3; ++j) {
+                if (i != j) {
+                    C(i,j) = m_lamefirst;
+                }
+            }
+        }
+        // Fill the shear components
+        C(3, 3) = G;
+        C(4, 4) = G;
+        C(5, 5) = G;
+    }
+
+
+    /// Set the Rayleigh mass-proportional damping factor alpha, to
+    /// build damping R as R=alpha*M + beta*K
+    void SetRayleighDampingAlpha(double alpha) { m_rayl_damping_alpha = alpha; }
+
+    /// Set the Rayleigh mass-proportional damping factor alpha, in R=alpha*M + beta*K
+    double GetRayleighDampingAlpha() const { return m_rayl_damping_alpha; }
+
+    /// Set the Rayleigh stiffness-proportional damping factor beta, to
+    /// build damping R as R=alpha*M + beta*K
+    void SetRayleighDampingBeta(double beta) { m_rayl_damping_beta = beta; }
+
+    /// Set the Rayleigh stiffness-proportional damping factor beta, in R=alpha*M + beta*K
+    double GetRayleighDampingBeta() const { return m_rayl_damping_beta; }
+
+    //virtual void ArchiveOut(ChArchiveOut& archive_out) override;  TODO
+    //virtual void ArchiveIn(ChArchiveIn& archive_in) override; 
+};
+
+
 
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
