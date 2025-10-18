d89b0e065a7eebac29bf3d48f05d29ae6fa5beda
tasora <tasora@ied.unipr.it>
2025-10-18 11:53:41 +0200

    Correct visualization of dot glyphs in Irrlicht (since csys were swapped to lhs to rhs), otherwise inverted triangle culling

--- src/chrono_irrlicht/ChIrrNodeShape.cpp
+++ src/chrono_irrlicht/ChIrrNodeShape.cpp
@@ -484,8 +484,14 @@ void ChIrrNodeShape::UpdateGlyphs(std::shared_ptr<ChGlyphs> glyphs) {
     // set buffers
 
     if (glyphs->GetDrawMode() == ChGlyphs::GLYPH_POINT) {
-        const u32 u[36] = {0,  1,  2,  0,  2,  3,  4,  6,  5,  4,  7,  6,  8,  9,  10, 8,  10, 11,
-                           12, 14, 13, 12, 15, 14, 16, 18, 17, 16, 19, 18, 20, 21, 22, 20, 22, 23};
+
+        // left handed coords, node ordering 
+        // const u32 u[36] = {0,  1,  2,  0,  2,  3,  4,  6,  5,  4,  7,  6,  8,  9,  10, 8,  10, 11,
+        //                    12, 14, 13, 12, 15, 14, 16, 18, 17, 16, 19, 18, 20, 21, 22, 20, 22, 23};
+        
+        // right handed coords, node ordering 
+        const u32 u[36] = {0,  2,  1,  0,  3,  2,  4,  5,  6,  4,  6,  7,  8,  10,  9, 8,  11, 10,
+                           12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 22, 21, 20, 23, 22};
 
         int itri = 0;
 
