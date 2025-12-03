db0947bfd77552ab7ab014791abd0f64b091eef4
Radu Serban <serban@wisc.edu>
2025-12-03 11:31:24 +0100

    Remove HydroChrono files from Chrono::TDPF project

--- src/chrono_fsi/ChApiFsi.h
+++ src/chrono_fsi/ChApiFsi.h
@@ -32,12 +32,11 @@
     @brief Fluid-Solid Interation modeling and simulation
 
     This module provides support for modeling multi-phase systems for fluid-solid interaction problems.
-    Currently, this module consists of the following libraries:
-    - Chrono_fsi implements a generic interface between a Chrono mutibody system and an arbitraru FSI-capable fluid solver.
-    - Chrono_fsisph implements a concrete, SPH-based FSI fluid solver.
-    and granular-solid interaction problems.
-    - Chrono_fsisph_vsg implements a customized Chrono::VSG run-time visualization system for SPH FSI problems.
-    - Chrono_fsitdpf implements a concrete fluid solver, based on the time-dependent potential flow solver in HydroChrono.
+    The Chrono::FSI module provides the following libraries:
+    - Chrono_fsi:        implements a generic interface between a Chrono mutibody system and an arbitrary FSI-capable fluid solver.
+    - Chrono_fsisph:     concrete, SPH-based FSI solver for incompressible Navier-Stokes and continuous granular dyynamics.
+    - Chrono_fsitdpf:    concrete fluid solver, based on the time-dependent potential flow solver in HydroChrono.
+    - Chrono_fsisph_vsg: customized VSG-based run-time visualization system for SPH FSI problems.
 
     For additional information, see:
     - the [Installation guide](@ref module_fsi_installation)
