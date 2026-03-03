dadb813d1a50b2ba3652947e4ba90fa3245826a9
Dario Fusai <dario.fusai93@gmail.com>
2026-03-03 15:23:31 +0100

    fix doc comments in collision shape

--- src/chrono/collision/ChCollisionShape.h
+++ src/chrono/collision/ChCollisionShape.h
@@ -65,13 +65,13 @@ class ChApi ChCollisionShape {
     /// The default implementation returns an inverted AABB.
     virtual ChAABB GetBoundingBox() const { return ChAABB(); }
 
-    /// Set this visualization shape as modifiable (default: false).
+    /// Set this collision shape as modifiable (default: false).
     /// Set to true to indicate that the asset may change and therefore requires updates (e.g. for a deformable
     /// triangular mesh). Note that this also includes changes in materials.
-    /// A visualization system may take advantage of this setting to accelerate rendering.
+    /// A collision system may take advantage of this setting to accelerate rendering.
     void SetMutable(bool val) { is_mutable = val; }
 
-    /// Return true if the visualization shape is marked as modifiable.
+    /// Return true if the collision shape is marked as modifiable.
     bool IsMutable() const { return is_mutable; }
 
     /// Method to allow serialization of transient data to archives.
