cd4d8f5ff35e493295e007a4c5017ad7f7eb411e
tasora <tasora@ied.unipr.it>
2026-03-06 09:38:39 +0100

    Add new ChLinkField constraint, for constraining ONE node - also with rheonomic function

--- src/chrono/fea/ChLinkFieldNode.cpp
+++ src/chrono/fea/ChLinkFieldNode.cpp
@@ -20,6 +20,177 @@
 namespace chrono {
 namespace fea {
 
+
+// Register into the object factory, to enable run-time dynamic creation and persistence
+CH_FACTORY_REGISTER(ChLinkField)
+
+ChLinkField::ChLinkField() : react(0) {
+    auto mfun = chrono_types::make_shared<ChFunctionConst>(0);
+    this->offset_function = mfun;
+}
+
+ChLinkField::ChLinkField(const ChLinkField& other) : ChLinkBase(other) {
+    react = other.react;
+    offset_function = other.offset_function;
+}
+
+int ChLinkField::Initialize(
+    std::shared_ptr<ChNodeFEAbase> node,  ///< node 1, associated to some scalar field state
+    std::shared_ptr<ChFieldBase> field
+) {
+    assert(node);
+    assert(field);
+    assert(field->IsNodeAdded(node));
+
+    m_node = node;
+
+    m_field = field;
+
+    std::vector<ChVariables*> mvars;
+    mvars.push_back(&(field->GetNodeDataPointer(node)->GetVariable()));
+    m_constraint1.SetVariables(mvars);
+
+    return true;
+}
+
+void ChLinkField::SetOffset(double offset) {
+    auto mfun = chrono_types::make_shared<ChFunctionConst>(offset);
+    offset_function = mfun;
+}
+
+void ChLinkField::SetOffset(std::shared_ptr<ChFunction> offset_f) {
+    offset_function = offset_f;
+}
+
+void ChLinkField::Update(double time, UpdateFlags update_flags) {
+    // Inherit time changes of parent class
+    ChPhysicsItem::Update(time, update_flags);
+
+    // update class data
+    // ...
+}
+
+ChVectorDynamic<> ChLinkField::GetConstraintViolation() const {
+    double res = m_field->GetNodeDataPointer(m_node)->State()(0) - this->offset_function->GetVal(this->ChTime);
+    ChVectorN<double, 1> C;
+    C(0) = res;
+    return C;
+}
+
+//// STATE BOOKKEEPING FUNCTIONS
+
+void ChLinkField::IntStateGatherReactions(const unsigned int off_L, ChVectorDynamic<>& L) {
+    L(off_L + 0) = react;
+}
+
+void ChLinkField::IntStateScatterReactions(const unsigned int off_L, const ChVectorDynamic<>& L) {
+    react = L(off_L + 0);
+}
+
+void ChLinkField::IntLoadResidual_CqL(const unsigned int off_L,    // offset in L multipliers
+                                           ChVectorDynamic<>& R,        // result: the R residual, R += c*Cq'*L
+                                           const ChVectorDynamic<>& L,  // the L vector
+                                           const double c               // a scaling factor
+) {
+    if (!IsActive())
+        return;
+
+    m_constraint1.AddJacobianTransposedTimesScalarInto(R, L(off_L + 0) * c);
+}
+
+void ChLinkField::IntLoadConstraint_C(const unsigned int off_L,  // offset in Qc residual
+                                           ChVectorDynamic<>& Qc,     // result: the Qc residual, Qc += c*C
+                                           const double c,            // a scaling factor
+                                           bool do_clamp,             // apply clamping to c*C?
+                                           double recovery_clamp      // value for min/max clamping of c*C
+) {
+    if (!IsActive())
+        return;
+
+    double res = m_field->GetNodeDataPointer(m_node)->State()(0) - this->offset_function->GetVal(this->ChTime);
+    double cres = res * c;
+
+    if (do_clamp) {
+        cres = std::min(std::max(cres, -recovery_clamp), recovery_clamp);
+    }
+    Qc(off_L + 0) += cres;
+}
+
+void ChLinkField::IntToDescriptor(const unsigned int off_v,
+                                       const ChStateDelta& v,
+                                       const ChVectorDynamic<>& R,
+                                       const unsigned int off_L,
+                                       const ChVectorDynamic<>& L,
+                                       const ChVectorDynamic<>& Qc) {
+    if (!IsActive())
+        return;
+
+    m_constraint1.SetLagrangeMultiplier(L(off_L + 0));
+
+    m_constraint1.SetRightHandSide(Qc(off_L + 0));
+}
+
+void ChLinkField::IntFromDescriptor(const unsigned int off_v,
+                                         ChStateDelta& v,
+                                         const unsigned int off_L,
+                                         ChVectorDynamic<>& L) {
+    if (!IsActive())
+        return;
+
+    L(off_L + 0) = m_constraint1.GetLagrangeMultiplier();
+}
+
+// SOLVER INTERFACES
+
+void ChLinkField::InjectConstraints(ChSystemDescriptor& descriptor) {
+    if (!IsActive())
+        return;
+
+    descriptor.InsertConstraint(&m_constraint1);
+}
+
+void ChLinkField::ConstraintsBiReset() {
+    m_constraint1.SetRightHandSide(0.);
+}
+
+void ChLinkField::ConstraintsBiLoad_C(double factor, double recovery_clamp, bool do_clamp) {
+    double res = m_field->GetNodeDataPointer(m_node)->State()(0) - this->offset_function->GetVal(this->ChTime);
+
+    m_constraint1.SetRightHandSide(m_constraint1.GetRightHandSide() + factor * res);
+}
+
+void ChLinkField::ConstraintsBiLoad_Ct(double factor) {
+    // if (!IsActive())
+    //	return;
+
+    // nothing
+}
+
+void ChLinkField::LoadConstraintJacobians() {
+    // compute jacobians
+
+    m_constraint1.Get_Cq_N(0)(0) = 1.0;
+}
+
+void ChLinkField::ConstraintsFetch_react(double factor) {
+    // From constraints to react vector:
+    react = m_constraint1.GetLagrangeMultiplier() * factor;
+}
+
+// FILE I/O
+
+void ChLinkField::ArchiveOut(ChArchiveOut& archive_out) {
+    //// TODO
+}
+
+void ChLinkField::ArchiveIn(ChArchiveIn& archive_in) {
+    //// TODO
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 // Register into the object factory, to enable run-time dynamic creation and persistence
 CH_FACTORY_REGISTER(ChLinkFieldField)
 
@@ -33,22 +204,23 @@ ChLinkFieldField::ChLinkFieldField(const ChLinkFieldField& other) : ChLinkBase(o
     offset_function = other.offset_function;
 }
 
-int ChLinkFieldField::Initialize        (std::shared_ptr<ChNodeFEAxyz> node1,   ///< node 1, associated to some scalar field state
-                                         std::shared_ptr<ChFieldScalar> field1, ///< scalar field with the 1st field state to constrain (must contain node 1).
-                                         std::shared_ptr<ChNodeFEAxyz> node2,   ///< node 2, associated to some scalar field state
-                                         std::shared_ptr<ChFieldScalar> field2 ///< scalar field with the 2nd field state to constrain (must contain node 2).
+int ChLinkFieldField::Initialize        (std::shared_ptr<ChNodeFEAbase> node1,   ///< node 1, associated to some scalar field state
+                                         std::shared_ptr<ChFieldBase> field1, ///< scalar field with the 1st field state to constrain (must contain node 1).
+                                         std::shared_ptr<ChNodeFEAbase> node2,  ///< node 2, associated to some scalar field state
+                                         std::shared_ptr<ChFieldBase> field2 ///< scalar field with the 2nd field state to constrain (must contain node 2).
 ) {
     assert(node1 && node2);
     assert(field1 && field2);
-    assert(field1->GetNodeDataMap().find(node1) != field1->GetNodeDataMap().end());
-    assert(field2->GetNodeDataMap().find(node2) != field2->GetNodeDataMap().end());
+    assert(field1->IsNodeAdded(node1));
+    assert(field2->IsNodeAdded(node2));
 
     m_node1 = node1;
     m_node2 = node2;
     m_field_1 = field1;
     m_field_2 = field2;
 
-    m_constraint1.SetVariables(&(field1->NodeData(node1).GetVariable()), &(field2->NodeData(node2).GetVariable()));
+    m_constraint1.SetVariables(&(field1->GetNodeDataPointer(node1)->GetVariable()),
+                               &(field2->GetNodeDataPointer(node2)->GetVariable()));
 
     return true;
 }
@@ -72,7 +244,8 @@ void ChLinkFieldField::Update(double time, UpdateFlags update_flags) {
 }
 
 ChVectorDynamic<> ChLinkFieldField::GetConstraintViolation() const {
-    double res = m_field_1->NodeData(m_node1).State()(0) - m_field_2->NodeData(m_node2).State()(0) - this->offset_function->GetVal(this->ChTime);
+    double res = m_field_1->GetNodeDataPointer(m_node1)->State()(0) - m_field_2->GetNodeDataPointer(m_node2)->State()(0) -
+                 this->offset_function->GetVal(this->ChTime);
     ChVectorN<double, 1> C;
     C(0) = res;
     return C;
@@ -108,7 +281,9 @@ void ChLinkFieldField::IntLoadConstraint_C(const unsigned int off_L,  // offset 
     if (!IsActive())
         return;
 
-    double res = m_field_1->NodeData(m_node1).State()(0) - m_field_2->NodeData(m_node2).State()(0) - this->offset_function->GetVal(this->ChTime);
+    double res = m_field_1->GetNodeDataPointer(m_node1)->State()(0) -
+                 m_field_2->GetNodeDataPointer(m_node2)->State()(0) -
+                 this->offset_function->GetVal(this->ChTime);
     double cres = res * c;
 
     if (do_clamp) {
@@ -155,7 +330,9 @@ void ChLinkFieldField::ConstraintsBiReset() {
 }
 
 void ChLinkFieldField::ConstraintsBiLoad_C(double factor, double recovery_clamp, bool do_clamp) {
-    double res = m_field_1->NodeData(m_node1).State()(0) - m_field_2->NodeData(m_node2).State()(0) - this->offset_function->GetVal(this->ChTime);
+    double res = m_field_1->GetNodeDataPointer(m_node1)->State()(0) -
+                 m_field_2->GetNodeDataPointer(m_node2)->State()(0) -
+                 this->offset_function->GetVal(this->ChTime);
 
     m_constraint1.SetRightHandSide(m_constraint1.GetRightHandSide() + factor * res);
 }
@@ -169,9 +346,6 @@ void ChLinkFieldField::ConstraintsBiLoad_Ct(double factor) {
 
 void ChLinkFieldField::LoadConstraintJacobians() {
     // compute jacobians
-    ChMatrix33<> Jxa(1.0);
-
-    ChMatrix33<> Jxb(-1.0);
 
     m_constraint1.Get_Cq_a()(0) = 1.0;
 
--- src/chrono/fea/ChLinkFieldNode.h
+++ src/chrono/fea/ChLinkFieldNode.h
@@ -16,8 +16,10 @@
 #define CHLINKFIELD_H
 
 #include "chrono/physics/ChLinkBase.h"
+#include "chrono/fea/ChField.h"
 #include "chrono/solver/ChConstraintTwoGeneric.h"
-#include "chrono/fea/ChNodeFEAxyz.h"
+#include "chrono/solver/ChConstraintNgeneric.h"
+
 
 namespace chrono {
 
@@ -29,6 +31,122 @@ class ChFieldScalar;  // forward ref
 
 /// @addtogroup fea_constraints
 /// @{
+
+
+
+/// Constraint between on a scalar field state, (ex. temperature, or voltage, etc.)
+/// to enformce that the state at node must be equal to some "offset" value.
+/// The offset can be either a constant value, or a function of time.
+
+class ChApi ChLinkField : public ChLinkBase {
+  public:
+    ChLinkField();
+    ChLinkField(const ChLinkField& other);
+    ~ChLinkField() {}
+
+    /// "Virtual" copy constructor (covariant return type).
+    virtual ChLinkField* Clone() const override { return new ChLinkField(*this); }
+
+    /// Get the number of scalar variables affected by constraints in this link.
+    virtual unsigned int GetNumAffectedCoords() override { return 1; }
+
+    /// Number of scalar constraints.
+    virtual unsigned int GetNumConstraintsBilateral() override { return 1; }
+
+    // Get constraint violations.
+    virtual ChVectorDynamic<> GetConstraintViolation() const override;
+
+    /// Initialize the constraint, given a node, and a scalar
+    /// field to whom the node belong
+    virtual int Initialize(std::shared_ptr<ChNodeFEAbase> node,  ///< node, associated to some scalar field state
+                           std::shared_ptr<ChFieldBase> field  ///< scalar field with the field state to constrain (must contain node).
+    );
+
+    /// Set the offset for the field state at node. The constraint is assumed satisfied when
+    ///  state1 = offset;
+    /// Default is 0 offset.
+    virtual void SetOffset(double offset);
+
+    /// Set the offset for the field state at node, where the offset is a function of time.
+    /// The constraint is assumed satisfied when
+    ///  state1 = offset(t);
+    virtual void SetOffset(std::shared_ptr<ChFunction> offset_f);
+
+    /// Get the connected node.
+    std::shared_ptr<fea::ChNodeFEAbase> GetNode() const { return m_node; }
+
+    /// Get the reaction force considered as applied to the 1st node.
+    ChVector3d GetReactionOnNode() const { return -react; }
+
+    /// Update all auxiliary data of the gear transmission at given time.
+    virtual void Update(double time, UpdateFlags update_flags) override;
+
+    /// Method to allow serialization of transient data to archives.
+    virtual void ArchiveOut(ChArchiveOut& archive_out) override;
+
+    /// Method to allow deserialization of transient data from archives.
+    virtual void ArchiveIn(ChArchiveIn& archive_in) override;
+
+    // Override/implement interfaces for global state vectors, see ChPhysicsItem for comments.
+
+    virtual void IntStateGatherReactions(const unsigned int off_L, ChVectorDynamic<>& L) override;
+    virtual void IntStateScatterReactions(const unsigned int off_L, const ChVectorDynamic<>& L) override;
+    virtual void IntLoadResidual_CqL(const unsigned int off_L,
+                                     ChVectorDynamic<>& R,
+                                     const ChVectorDynamic<>& L,
+                                     const double c) override;
+    virtual void IntLoadConstraint_C(const unsigned int off,
+                                     ChVectorDynamic<>& Qc,
+                                     const double c,
+                                     bool do_clamp,
+                                     double recovery_clamp) override;
+    virtual void IntToDescriptor(const unsigned int off_v,
+                                 const ChStateDelta& v,
+                                 const ChVectorDynamic<>& R,
+                                 const unsigned int off_L,
+                                 const ChVectorDynamic<>& L,
+                                 const ChVectorDynamic<>& Qc) override;
+    virtual void IntFromDescriptor(const unsigned int off_v,
+                                   ChStateDelta& v,
+                                   const unsigned int off_L,
+                                   ChVectorDynamic<>& L) override;
+
+    // Override/implement system functions of ChPhysicsItem
+    // (to assemble/manage data for system solver)
+
+    virtual void InjectConstraints(ChSystemDescriptor& descriptor) override;
+    virtual void ConstraintsBiReset() override;
+    virtual void ConstraintsBiLoad_C(double factor = 1, double recovery_clamp = 0.1, bool do_clamp = false) override;
+    virtual void ConstraintsBiLoad_Ct(double factor = 1) override;
+    virtual void LoadConstraintJacobians() override;
+    virtual void ConstraintsFetch_react(double factor = 1) override;
+
+  private:
+    double react;
+
+    std::shared_ptr<ChFunction> offset_function;
+
+    // used as an interface to the solver.
+    ChConstraintNgeneric m_constraint1;
+
+    std::shared_ptr<fea::ChNodeFEAbase> m_node;
+    std::shared_ptr<fea::ChFieldBase> m_field;
+
+    // Provide dummy implementation for the following 4 members tht are meaningless anyway for this kind of link:
+
+    /// Get the link frame 1, on the 1st node, expressed in the absolute frame.
+    virtual ChFramed GetFrame1Abs() const override { return ChFramed(); }
+    /// Get the link frame 2, on the 2nd node, expressed in the absolute frame.
+    virtual ChFramed GetFrame2Abs() const override { return ChFramed(); }
+    /// Get reaction force and torque on 1st node, expressed on link frame 1.
+    virtual ChWrenchd GetReaction1() const override { return {ChVector3d(react, 0, 0), VNULL}; }
+    /// Get reaction force and torque on 2nd node, expressed on link frame 2.
+    virtual ChWrenchd GetReaction2() const override { return {ChVector3d(-react, 0, 0), VNULL}; }
+};
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 /// Constraint between two field states, (ex. temperature-temperature, or voltage-voltage, etc.)
@@ -57,10 +175,10 @@ public:
 
     /// Initialize the constraint, given the two nodes, and scalar 
     /// fields to whom the two nodes belong
-    virtual int Initialize(std::shared_ptr<ChNodeFEAxyz> node1,  ///< node 1, associated to some scalar field state
-                           std::shared_ptr<ChFieldScalar> field1, ///< scalar field with the 1st field state to constrain (must contain node 1).
-                           std::shared_ptr<ChNodeFEAxyz> node2,   ///< node 2, associated to some scalar field state
-                           std::shared_ptr<ChFieldScalar> field2 ///< scalar field with the 2nd field state to constrain (must contain node 2).
+    virtual int Initialize(std::shared_ptr<ChNodeFEAbase> node1,  ///< node 1, associated to some scalar field state
+                           std::shared_ptr<ChFieldBase> field1, ///< scalar field with the 1st field state to constrain (must contain node 1).
+                           std::shared_ptr<ChNodeFEAbase> node2,  ///< node 2, associated to some scalar field state
+                           std::shared_ptr<ChFieldBase> field2 ///< scalar field with the 2nd field state to constrain (must contain node 2).
     );
 
     /// Set the offset between the two field states. The constraint is assumed satisfied when  
@@ -139,8 +257,8 @@ public:
 
     std::shared_ptr<fea::ChNodeFEAbase> m_node1;
     std::shared_ptr<fea::ChNodeFEAbase> m_node2;
-    std::shared_ptr<fea::ChFieldScalar> m_field_1;
-    std::shared_ptr<fea::ChFieldScalar> m_field_2;
+    std::shared_ptr<fea::ChFieldBase> m_field_1;
+    std::shared_ptr<fea::ChFieldBase> m_field_2;
 
 
     // Provide dummy implementation for the following 4 members tht are meaningless anyway for this kind of link:
