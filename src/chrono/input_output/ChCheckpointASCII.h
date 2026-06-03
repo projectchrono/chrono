f69eb8cc3e17ebe16c2e344dc1560406b5c6e813
Radu Serban <serban@wisc.edu>
2026-06-03 17:16:01 +0200

    Refactor ChCheckpoint and ChCheckpointASCI (WIP).
    - provide high-level functions for saving and loading a checkpoint and for reading/writing a checkpoint to file.
    - refactor ChCheckpointASCII to allow working with in-memory checkpoints, in addition to checkpoints from files.

--- src/chrono/input_output/ChCheckpointASCII.h
+++ src/chrono/input_output/ChCheckpointASCII.h
@@ -38,57 +38,55 @@ class ChApi ChCheckpointASCII : public ChCheckpoint {
 
     virtual void Initialize() override;
 
+    virtual void ReadFile(const std::string& filename) override;
+    virtual void WriteFile(const std::string& filename) override;
+
+  private:
     // Checkpoint export functions
 
-    virtual void WriteState(ChSystem* sys) override;
+    virtual void SaveState(ChSystem* sys) override;
 
-    virtual void WriteTime(double time) override;
-    virtual void WriteBodies(const std::vector<std::shared_ptr<ChBody>>& bodies) override;
-    virtual void WriteShafts(const std::vector<std::shared_ptr<ChShaft>>& shafts) override;
-    virtual void WriteJoints(const std::vector<std::shared_ptr<ChLink>>& joints) override;
-    virtual void WriteCouples(const std::vector<std::shared_ptr<ChShaftsCouple>>& couples) override;
-    virtual void WriteLinSprings(const std::vector<std::shared_ptr<ChLinkTSDA>>& springs) override;
-    virtual void WriteRotSprings(const std::vector<std::shared_ptr<ChLinkRSDA>>& springs) override;
-    virtual void WriteBodyBodyLoads(const std::vector<std::shared_ptr<ChLoadBodyBody>>& loads) override;
-    virtual void WriteLinMotors(const std::vector<std::shared_ptr<ChLinkMotorLinear>>& motors) override;
-    virtual void WriteRotMotors(const std::vector<std::shared_ptr<ChLinkMotorRotation>>& motors) override;
+    virtual void SaveBodies(const std::vector<std::shared_ptr<ChBody>>& bodies) override;
+    virtual void SaveShafts(const std::vector<std::shared_ptr<ChShaft>>& shafts) override;
+    virtual void SaveJoints(const std::vector<std::shared_ptr<ChLink>>& joints) override;
+    virtual void SaveCouples(const std::vector<std::shared_ptr<ChShaftsCouple>>& couples) override;
+    virtual void SaveLinSprings(const std::vector<std::shared_ptr<ChLinkTSDA>>& springs) override;
+    virtual void SaveRotSprings(const std::vector<std::shared_ptr<ChLinkRSDA>>& springs) override;
+    virtual void SaveBodyBodyLoads(const std::vector<std::shared_ptr<ChLoadBodyBody>>& loads) override;
+    virtual void SaveLinMotors(const std::vector<std::shared_ptr<ChLinkMotorLinear>>& motors) override;
+    virtual void SaveRotMotors(const std::vector<std::shared_ptr<ChLinkMotorRotation>>& motors) override;
 
-    virtual void WriteDouble(double value) override;
-    virtual void WriteInteger(int value) override;
-    virtual void WriteVector(const std::vector<double>& vector) override;
-    virtual void WriteChVector3(const ChVector3d& vector) override;
-    virtual void WriteChQuaternion(const ChQuaterniond& quat) override;
-
-    virtual void WriteFile(const std::string& filename) override;
+    virtual void SaveDouble(double value) override;
+    virtual void SaveInteger(int value) override;
+    virtual void SaveVector(const std::vector<double>& vector) override;
+    virtual void SaveChVector3(const ChVector3d& vector) override;
+    virtual void SaveChQuaternion(const ChQuaterniond& quat) override;
 
     // Checkpoint import functions
 
-    virtual void OpenFile(const std::string& filename) override;
+    virtual void LoadState(ChSystem* sys) override;
 
-    virtual void ReadState(ChSystem* sys) override;
+    virtual void LoadBodies(std::vector<std::shared_ptr<ChBody>>& bodies) override;
+    virtual void LoadShafts(std::vector<std::shared_ptr<ChShaft>>& shafts) override;
+    virtual void LoadJoints(std::vector<std::shared_ptr<ChLink>>& joints) override;
+    virtual void LoadCouples(std::vector<std::shared_ptr<ChShaftsCouple>>& couples) override;
+    virtual void LoadLinSprings(std::vector<std::shared_ptr<ChLinkTSDA>>& springs) override;
+    virtual void LoadRotSprings(std::vector<std::shared_ptr<ChLinkRSDA>>& springs) override;
+    virtual void LoadBodyBodyLoads(std::vector<std::shared_ptr<ChLoadBodyBody>>& loads) override;
+    virtual void LoadLinMotors(std::vector<std::shared_ptr<ChLinkMotorLinear>>& motors) override;
+    virtual void LoadRotMotors(std::vector<std::shared_ptr<ChLinkMotorRotation>>& motors) override;
 
-    virtual void ReadTime(double& time) override;
-    virtual void ReadBodies(std::vector<std::shared_ptr<ChBody>>& bodies) override;
-    virtual void ReadShafts(std::vector<std::shared_ptr<ChShaft>>& shafts) override;
-    virtual void ReadJoints(std::vector<std::shared_ptr<ChLink>>& joints) override;
-    virtual void ReadCouples(std::vector<std::shared_ptr<ChShaftsCouple>>& couples) override;
-    virtual void ReadLinSprings(std::vector<std::shared_ptr<ChLinkTSDA>>& springs) override;
-    virtual void ReadRotSprings(std::vector<std::shared_ptr<ChLinkRSDA>>& springs) override;
-    virtual void ReadBodyBodyLoads(std::vector<std::shared_ptr<ChLoadBodyBody>>& loads) override;
-    virtual void ReadLinMotors(std::vector<std::shared_ptr<ChLinkMotorLinear>>& motors) override;
-    virtual void ReadRotMotors(std::vector<std::shared_ptr<ChLinkMotorRotation>>& motors) override;
+    virtual void LoadDouble(double& value) override;
+    virtual void LoadInteger(int& value) override;
+    virtual void LoadVector(std::vector<double>& vector) override;
+    virtual void LoadChVector3(ChVector3d& vector) override;
+    virtual void LoadChQuaternion(ChQuaterniond& quat) override;
 
-    virtual void ReadDouble(double& value) override;
-    virtual void ReadInteger(int& value) override;
-    virtual void ReadVector(std::vector<double>& vector) override;
-    virtual void ReadChVector3(ChVector3d& vector) override;
-    virtual void ReadChQuaternion(ChQuaterniond& quat) override;
-
-  private:
     void CheckIfOpen() const;
 
-    ChWriterCSV m_csv;      ///< output checkpoint CSV
-    std::ifstream m_ifile;  ///< input checkpoint file
+    ChWriterCSV m_csv;             ///< output checkpoint CSV
+    bool m_file_read;              ///< true if the checkpoint was read from a file
+    std::istringstream m_istream;  ///< checkpoint data read from file
 
     size_t m_np;  ///< number of position-level states
     size_t m_nv;  ///< number of velocity-level states
