8fbe259e3b44d8039855bb5273c320b74c3c1d98
Radu Serban <serban@wisc.edu>
2026-06-15 16:26:53 +0200

    Refactor Chrono actuator models.
    - add ChActuator, a base class for actuators between two bodies with optional internal dynamics (defined as ODEs).
    - change the existing hydraulic actuators to derive from ChActuator.
    - add skeleton for a electromechanic actuator.

--- src/chrono/physics/ChHydraulicActuator.cpp
+++ src/chrono/physics/ChHydraulicActuator.cpp
@@ -17,32 +17,11 @@
 
 namespace chrono {
 
-ChHydraulicActuatorBase::ChHydraulicActuatorBase()
-    : pP(7.6e6), pT(0.1e6), is_attached(false), calculate_consistent_IC(false) {}
+ChHydraulicActuatorBase::ChHydraulicActuatorBase() : pP(7.6e6), pT(0.1e6) {}
 
 void ChHydraulicActuatorBase::SetPressures(double pump_pressure, double tank_pressure) {
     pP = pump_pressure;
     pT = tank_pressure;
-}
-
-void ChHydraulicActuatorBase::SetActuatorInitialLength(double len) {
-    s_0 = len;
-    s = len;
-    sd = 0;
-}
-
-void ChHydraulicActuatorBase::SetActuatorLength(double len, double vel) {
-    // Do nothing if not attached to bodies
-    if (is_attached)
-        return;
-
-    s = len;
-    sd = vel;
-}
-
-void ChHydraulicActuatorBase::SetInitialLoad(double initial_load) {
-    calculate_consistent_IC = true;
-    F0 = initial_load;
 }
 
 void ChHydraulicActuatorBase::Initialize() {
@@ -50,39 +29,6 @@ void ChHydraulicActuatorBase::Initialize() {
 
     // Initialize the external dynamics
     ChExternalDynamicsODE::Initialize();
-}
-
-void ChHydraulicActuatorBase::Initialize(std::shared_ptr<ChBody> body1,  // first connected body
-                                         std::shared_ptr<ChBody> body2,  // second connected body
-                                         bool local,                     // true if locations given in body local frames
-                                         ChVector3d loc1,                // location of connection point on body 1
-                                         ChVector3d loc2                 // location of connection point on body 2
-) {
-    is_attached = true;
-
-    // Call base initialization
-    Initialize();
-
-    // Cache connected bodies and body local connection points
-    m_body1 = body1.get();
-    m_body2 = body2.get();
-
-    if (local) {
-        m_loc1 = loc1;
-        m_loc2 = loc2;
-        m_aloc1 = body1->TransformPointLocalToParent(loc1);
-        m_aloc2 = body2->TransformPointLocalToParent(loc2);
-    } else {
-        m_loc1 = body1->TransformPointParentToLocal(loc1);
-        m_loc2 = body2->TransformPointParentToLocal(loc2);
-        m_aloc1 = loc1;
-        m_aloc2 = loc2;
-    }
-
-    s_0 = (m_aloc1 - m_aloc2).Length();
-
-    // Resize temporary vector of generalized body forces
-    m_Qforce.resize(12);
 }
 
 double ChHydraulicActuatorBase::GetValvePosition() {
@@ -97,68 +43,6 @@ std::array<double, 2> ChHydraulicActuatorBase::GetCylinderPressures() {
 double ChHydraulicActuatorBase::GetActuatorForce() {
     Vec2 p = ExtractCylinderPressures();
     return cyl.EvalForce(p, s - s_0, sd);
-}
-
-double ChHydraulicActuatorBase::GetInput(double t) const {
-    return ChClamp(ref_fun->GetVal(t), -1.0, +1.0);
-}
-
-void ChHydraulicActuatorBase::Update(double time, UpdateFlags update_flags) {
-    // Update the external dynamics
-    ChExternalDynamicsODE::Update(time, update_flags);
-
-    // If the actuator is attached to bodies, update its length and rate from the body states
-    // and calculate the generated force to the two bodies.
-    if (is_attached) {
-        m_aloc1 = m_body1->TransformPointLocalToParent(m_loc1);
-        m_aloc2 = m_body2->TransformPointLocalToParent(m_loc2);
-
-        auto avel1 = m_body1->PointSpeedLocalToParent(m_loc1);
-        auto avel2 = m_body2->PointSpeedLocalToParent(m_loc2);
-
-        ChVector3d dir = (m_aloc1 - m_aloc2).GetNormalized();
-
-        s = (m_aloc1 - m_aloc2).Length();
-        sd = Vdot(dir, avel1 - avel2);
-
-        ////std::cout << "time = " << time << "    s=" << s << "   sd=" << sd << std::endl;
-
-        // Actuator force
-        auto f = GetActuatorForce();
-        ChVector3d force = f * dir;
-
-        // Force and moment acting on body 1
-        auto atorque1 = Vcross(m_aloc1 - m_body1->GetPos(), force);          // applied torque (absolute frame)
-        auto ltorque1 = m_body1->TransformDirectionParentToLocal(atorque1);  // applied torque (local frame)
-        m_Qforce.segment(0, 3) = force.eigen();
-        m_Qforce.segment(3, 3) = ltorque1.eigen();
-
-        // Force and moment acting on body 2
-        auto atorque2 = Vcross(m_aloc2 - m_body2->GetPos(), -force);         // applied torque (absolute frame)
-        auto ltorque2 = m_body2->TransformDirectionParentToLocal(atorque2);  // applied torque (local frame)
-        m_Qforce.segment(6, 3) = -force.eigen();
-        m_Qforce.segment(9, 3) = ltorque2.eigen();
-    }
-}
-
-void ChHydraulicActuatorBase::IntLoadResidual_F(const unsigned int off, ChVectorDynamic<>& R, const double c) {
-    if (!IsActive())
-        return;
-
-    // Load external dynamics
-    ChExternalDynamicsODE::IntLoadResidual_F(off, R, c);
-
-    if (is_attached) {
-        // Add forces to connected bodies (calculated in Update)
-        if (m_body1->Variables().IsActive()) {
-            R.segment(m_body1->Variables().GetOffset() + 0, 3) += c * m_Qforce.segment(0, 3);
-            R.segment(m_body1->Variables().GetOffset() + 3, 3) += c * m_Qforce.segment(3, 3);
-        }
-        if (m_body2->Variables().IsActive()) {
-            R.segment(m_body2->Variables().GetOffset() + 0, 3) += c * m_Qforce.segment(6, 3);
-            R.segment(m_body2->Variables().GetOffset() + 3, 3) += c * m_Qforce.segment(9, 3);
-        }
-    }
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -287,8 +171,7 @@ Vec2 ChHydraulicActuator2::EvaluatePressureRates(double t, const Vec2& p, double
 
 // ---------------------------------------------------------------------------------------------------------------------
 
-ChHydraulicActuator3::ChHydraulicActuator3()
-    : hose1V(3.14e-5), hose2V(7.85e-5), hose3V(4.71e-5), Bo(1500e6), Bh(150e6), Bc(31500e6) {}
+ChHydraulicActuator3::ChHydraulicActuator3() : hose1V(3.14e-5), hose2V(7.85e-5), hose3V(4.71e-5), Bo(1500e6), Bh(150e6), Bc(31500e6) {}
 
 void ChHydraulicActuator3::SetBulkModuli(double oil_bulk_modulus, double hose_bulk_modulus, double cyl_bulk_modulus) {
     Bo = oil_bulk_modulus;
@@ -296,9 +179,7 @@ void ChHydraulicActuator3::SetBulkModuli(double oil_bulk_modulus, double hose_bu
     Bc = cyl_bulk_modulus;
 }
 
-void ChHydraulicActuator3::SetHoseVolumes(double hose_tvalve_piston,
-                                          double hose_dvalve_rod,
-                                          double hose_dvalve_tvalve) {
+void ChHydraulicActuator3::SetHoseVolumes(double hose_tvalve_piston, double hose_dvalve_rod, double hose_dvalve_tvalve) {
     hose1V = hose_tvalve_piston;
     hose2V = hose_dvalve_rod;
     hose3V = hose_dvalve_tvalve;
